627bbee54b4cd2e7284b7480946e15277c2fc5c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 03:19:39 +0000

    Add package ominfra/0.0.0.dev145/ominfra-0.0.0.dev145-py3-none-any.whl

---
+++ packages/ominfra/ominfra-0.0.0.dev145-py3-none-any.whl/ominfra/manage/remote/channel.py
@@ -0,0 +1,52 @@
+# ruff: noqa: UP006 UP007
+import json
+import struct
+import typing as ta
+
+from omlish.lite.json import json_dumps_compact
+from omlish.lite.marshal import OBJ_MARSHALER_MANAGER
+from omlish.lite.marshal import ObjMarshalerManager
+
+
+T = ta.TypeVar('T')
+
+
+class RemoteChannel:
+    def __init__(
+            self,
+            input: ta.IO,  # noqa
+            output: ta.IO,
+            *,
+            msh: ObjMarshalerManager = OBJ_MARSHALER_MANAGER,
+    ) -> None:
+        super().__init__()
+
+        self._input = input
+        self._output = output
+        self._msh = msh
+
+    def set_marshaler(self, msh: ObjMarshalerManager) -> None:
+        self._msh = msh
+
+    def send_obj(self, o: ta.Any, ty: ta.Any = None) -> None:
+        j = json_dumps_compact(self._msh.marshal_obj(o, ty))
+        d = j.encode('utf-8')
+
+        self._output.write(struct.pack('<I', len(d)))
+        self._output.write(d)
+        self._output.flush()
+
+    def recv_obj(self, ty: ta.Type[T]) -> ta.Optional[T]:
+        d = self._input.read(4)
+        if not d:
+            return None
+        if len(d) != 4:
+            raise EOFError
+
+        sz = struct.unpack('<I', d)[0]
+        d = self._input.read(sz)
+        if len(d) != sz:
+            raise EOFError
+
+        j = json.loads(d.decode('utf-8'))
+        return self._msh.unmarshal_obj(j, ty)
