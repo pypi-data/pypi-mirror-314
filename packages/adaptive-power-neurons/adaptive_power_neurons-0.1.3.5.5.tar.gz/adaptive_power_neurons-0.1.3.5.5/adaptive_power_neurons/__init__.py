60bf9d7f0e3c2506b05de637f5cc9978cfa45cdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 15:03:40 +0000

    Add package adaptive-power-neurons/0.1.3.5.5/adaptive_power_neurons-0.1.3.5.5.tar.gz

---
+++ packages/adaptive-power-neurons/adaptive_power_neurons-0.1.3.5.5.tar.gz/adaptive_power_neurons-0.1.3.5.5/adaptive_power_neurons/__init__.py
@@ -0,0 +1,7 @@
+# __init__.py
+
+from .activation import Activation
+from .optimizer import SGD, Optimizer
+from .adaptive_power_neuron import AdaptivePowerNeuron
+from .dense_layer import DenseLayer
+from .adaptive_power_model import AdaptivePowerModel
