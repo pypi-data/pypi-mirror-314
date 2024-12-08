31402b4968a745fe5ef51f47a18012afd7b086b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 14:03:27 +0000

    Add package erkc63/0.2.23/erkc63-0.2.23.tar.gz

---
+++ packages/erkc63/erkc63-0.2.23.tar.gz/erkc63-0.2.23/erkc63/errors.py
@@ -0,0 +1,26 @@
+class ErkcError(Exception):
+    pass
+
+
+class ParsingError(ErkcError):
+    pass
+
+
+class ApiError(ErkcError):
+    pass
+
+
+class AccountBindingError(ApiError):
+    pass
+
+
+class AuthorizationError(ApiError):
+    pass
+
+
+class AuthorizationRequired(ApiError):
+    pass
+
+
+class AccountNotFound(ApiError):
+    pass
