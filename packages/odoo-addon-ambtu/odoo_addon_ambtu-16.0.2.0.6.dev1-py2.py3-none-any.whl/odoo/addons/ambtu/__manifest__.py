08fb55ea0e8230a79506e26ec94b5aa3d1dd2555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 14:03:47 +0000

    Add package odoo-addon-ambtu/16.0.2.0.6.dev1/odoo_addon_ambtu-16.0.2.0.6.dev1-py2.py3-none-any.whl

---
+++ packages/odoo-addon-ambtu/odoo_addon_ambtu-16.0.2.0.6.dev1-py2.py3-none-any.whl/odoo/addons/ambtu/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "AmbTu module customizations",
+    "summary": """ """,
+    "author": "Coopdevs Treball SCCL",
+    "website": "https://git.coopdevs.org/coopdevs/odoo/odoo-addons/odoo-ambtu",  # noqa
+    "category": "Uncategorized",
+    "version": "16.0.2.0.6",
+    "license": "AGPL-3",
+    "depends": ["web", "account"],
+    "data": [
+        "reports/external_layout_standard.xml",
+        "reports/report_invoice_document.xml",
+    ],
+}
