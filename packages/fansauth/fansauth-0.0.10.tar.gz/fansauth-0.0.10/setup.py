fb3815a68e331100ae2d0d718857be310336d8a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 14:03:21 +0000

    Add package fansauth/0.0.10/fansauth-0.0.10.tar.gz

---
+++ packages/fansauth/fansauth-0.0.10.tar.gz/fansauth-0.0.10/setup.py
@@ -0,0 +1,21 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='fansauth',
+    version='0.0.10',
+    packages=find_packages(),
+    install_requires=[],
+    author='fans656',
+    author_email='fans656@yahoo.com',
+    description='',
+    long_description='',
+    long_description_content_type='text/markdown',
+    url='',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
