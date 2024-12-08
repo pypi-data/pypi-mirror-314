2d914e52229ea9c4a5ac6a451f222f1993251d03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 14:03:38 +0000

    Add package pdf-docx-pic/5.24.12.8/pdf_docx_pic-5.24.12.8.tar.gz

---
+++ packages/pdf-docx-pic/pdf_docx_pic-5.24.12.8.tar.gz/pdf_docx_pic-5.24.12.8/README.rst
@@ -0,0 +1,25 @@
+请注意
+
+最新版本3.1.2.2023.6.19.16.57.42支持2.6, 2.7, 3.3, 3.4, 3.5, 3.6, 3.12
+
+但我们不确定依赖库是否存在。
+
+请使用以上版本的谨慎安装
+
+
+
+
+Please note
+
+
+
+The latest version 3.1.2.2023.6.19.16.57.42 supports 2.6, 2.7, 3.3, 3.4, 3.5, 3.6, 3.12
+
+
+
+But we are not sure if the dependent library exists.
+
+
+
+Please use the above version for careful installation
+
---
+++ packages/pdf-docx-pic/pdf_docx_pic-5.24.12.8.tar.gz/pdf_docx_pic-5.24.12.8/setup.py
@@ -0,0 +1,36 @@
+from setuptools import *
+
+rst = open(".\\README.rst", mode='r', encoding = 'UTF-8').read()
+
+setup(
+    name = "pdf_docx_pic",
+    version = "5.24.12.8",
+    packages = find_packages(),
+    python_requires = ">=3.10, <=3.12",
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "License :: OSI Approved :: CEA CNRS Inria Logiciel Libre License, version 2.1 (CeCILL-2.1)",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows :: Windows 7",
+        "Operating System :: Microsoft :: Windows :: Windows 8.1",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Operating System :: Microsoft :: Windows :: Windows 11",
+        "Operating System :: POSIX :: Linux",
+        "Natural Language :: Chinese (Simplified)",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Typing :: Typed",
+    ],
+    install_requires = [
+        "ttkbootstrap>=1.10, <1.11",
+        "pdf2docx>=0.5.5, <0.6",
+        "docx2pdf>=0.1.7, <0.2",
+        "PyMuPDF>=1.24.1, <1.26",
+        "Pillow>=10, <12",
+    ],
+    long_description = rst
+)
