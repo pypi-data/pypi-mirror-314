fbbf98d9e8cfbd8b7b2e174b27b9685b5281bf91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 14:03:31 +0000

    Add package funmaterial/1.0.17/funmaterial-1.0.17-py3-none-any.whl

---
+++ packages/funmaterial/funmaterial-1.0.17-py3-none-any.whl/funmatrial/schema/__init__.py
@@ -0,0 +1,17 @@
+from .base import (
+    AudioInfo,
+    ImageInfo,
+    MaterialInfo,
+    MaterialType,
+    ProviderType,
+    VideoInfo,
+)
+
+__all__ = [
+    "MaterialInfo",
+    "MaterialType",
+    "AudioInfo",
+    "ImageInfo",
+    "VideoInfo",
+    "ProviderType",
+]
