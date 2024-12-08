56ea851273b024095599b5e4b6e347fb4ec56cb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 14:03:38 +0000

    Add package adaptive-power-neurons/0.1.2.5.5/adaptive_power_neurons-0.1.2.5.5-py3-none-any.whl

---
+++ packages/adaptive-power-neurons/adaptive_power_neurons-0.1.2.5.5-py3-none-any.whl/adaptive_power_neurons/__init__.py
@@ -0,0 +1,5 @@
+from .adaptive_power_neurons import AdaptivePowerNeuron
+from .optimizer import Optimizer
+from .adaptive_power_neurons_model import AdaptivePowerNeurons
+
+__all__ = ['AdaptivePowerNeuron', 'Optimizer', 'AdaptivePowerNeurons']
