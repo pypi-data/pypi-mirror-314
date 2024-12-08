d923df56f2388072e345c87b2511786402194704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 15:03:42 +0000

    Add package randomcontent/0.1.3/randomcontent-0.1.3-py3-none-any.whl

---
+++ packages/randomcontent/randomcontent-0.1.3-py3-none-any.whl/randomcontent/__init__.py
@@ -0,0 +1,7 @@
+# randomcontent/__init__.py
+
+from .generators.text_generator import generate_text
+from .generators.number_generator import generate_number
+from .generators.structured_generator import generate_mock_data
+
+__all__ = ["generate_text", "generate_number", "generate_mock_data"]
