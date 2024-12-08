51dc047bf29e75f6569b9f6f65f5bbfb23ffae45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 15:04:03 +0000

    Add package teo/0.3.6/teo-0.3.6-cp313-cp313-manylinux_2_28_aarch64.whl

---
+++ packages/teo/teo-0.3.6-cp313-cp313-manylinux_2_28_aarch64.whl/teo/annotations/__init__.py
@@ -0,0 +1,17 @@
+from typing import TypedDict
+
+
+class CapturesAnnotationMark(TypedDict):
+    pass
+
+
+class RequestBodyObjectAnnotationMark(TypedDict):
+    pass
+
+
+class TeoAnnotationMark:
+    pass
+
+
+class ModelObjectAnnotationMark:
+    pass
