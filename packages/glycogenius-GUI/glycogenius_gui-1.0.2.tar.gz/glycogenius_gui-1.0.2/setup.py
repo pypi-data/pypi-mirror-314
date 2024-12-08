64b2f8ad148512a90cf6cad2a9db918ccf8892f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 14:03:31 +0000

    Add package glycogenius-GUI/1.0.2/glycogenius_gui-1.0.2.tar.gz

---
+++ packages/glycogenius-GUI/glycogenius_gui-1.0.2.tar.gz/glycogenius_gui-1.0.2/setup.py
@@ -0,0 +1,64 @@
+# GlycoGenius: Glycomics Data Analysis Tool
+# Copyright (C) 2023 by Hector Franco Loponte
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or 
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. It is accessible within the program files
+# or by typing 'license' after running it stand-alone in the terminal
+# by typing 'glycogenius'. If not, see <https://www.gnu.org/licenses/>.
+
+from setuptools import setup, find_packages
+from distutils.command.install import INSTALL_SCHEMES
+
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+    
+long_description_from_file = ""
+with open("README.md", "r", encoding="utf-8") as f:
+    for lines in f:
+        if lines[0] != "!":
+            long_description_from_file+= lines
+    f.close()
+
+setup(
+    name='glycogenius_GUI',
+    version='1.0.2',
+    author='Hector Franco Loponte',
+    author_email='hectorfloponte@gmail.com',
+    description='Accessory GUI for Glycogenius',
+    long_description=long_description_from_file,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'glycogenius_GUI': ['Assets/*']
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+    install_requires=[
+        "pandas", "scipy", "pyteomics",
+        "pillow", "ttkwidgets", "matplotlib",
+        "dill", "numpy==1.26.4", "lxml",
+        "openpyxl", "setuptools",
+        "xlsxwriter", "psutil", "glycogenius==1.2.6",
+        "mpl_scatter_density", "reportlab"
+    ],
+    entry_points={
+        'gui_scripts': [
+            'glycogenius_GUI = glycogenius_GUI:glycogenius_GUI',
+        ]
+    }
+)
