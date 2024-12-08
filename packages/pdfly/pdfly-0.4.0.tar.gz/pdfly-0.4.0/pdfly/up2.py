8785ad4c94ffa8230b1a31f6b40f43eaea5ebab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 15:03:34 +0000

    Add package pdfly/0.4.0/pdfly-0.4.0.tar.gz

---
+++ packages/pdfly/pdfly-0.4.0.tar.gz/pdfly-0.4.0/pdfly/up2.py
@@ -0,0 +1,31 @@
+"""
+Create a booklet-style PDF from a single input.
+
+Pairs of two pages will be put on one page (left and right)
+
+usage: python 2-up.py input_file output_file
+"""
+
+import sys
+from pathlib import Path
+
+from pypdf import PdfReader, PdfWriter
+
+
+def main(pdf: Path, output: Path) -> None:
+    reader = PdfReader(str(pdf))
+    writer = PdfWriter()
+    for i in range(0, len(reader.pages) - 1, 2):
+        lhs = reader.pages[i]
+        rhs = reader.pages[i + 1]
+        lhs.merge_translated_page(
+            rhs, tx=float(lhs.mediabox.width), ty=0, expand=True
+        )
+        writer.add_page(lhs)
+        print(str(i) + " ")
+        sys.stdout.flush()
+
+    print(f"writing {output}")
+    with open(output, "wb") as fp:
+        writer.write(fp)
+    print("done.")
