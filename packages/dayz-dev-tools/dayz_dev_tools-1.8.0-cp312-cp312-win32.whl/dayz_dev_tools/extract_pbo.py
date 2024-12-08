51d7fe80d8fe96e78caef268c7ac615d9c9c5988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 14:03:47 +0000

    Add package dayz-dev-tools/1.8.0/dayz_dev_tools-1.8.0-cp312-cp312-win32.whl

---
+++ packages/dayz-dev-tools/dayz_dev_tools-1.8.0-cp312-cp312-win32.whl/dayz_dev_tools/extract_pbo.py
@@ -0,0 +1,139 @@
+import io
+import os
+import re
+import typing
+
+from dayz_dev_tools import config_cpp
+from dayz_dev_tools import pbo_file
+from dayz_dev_tools import pbo_reader
+
+
+INVALID_FILENAME_RE = re.compile(b"[\t?*\x80-\xff]")
+
+OBFUSCATE_RE = re.compile(
+    b'^(?:(?://[^\\r\\n]*|/\\*(?:\\*(?!\\/)|[^*])*\\*/)\\r\\n)?#include "([^"]+)"\\r\\n$')
+
+
+def _invalid_filename(filename: bytes) -> bool:
+    return INVALID_FILENAME_RE.search(filename) is not None
+
+
+_deobfs_count = 0
+
+
+def _extract_file(
+    reader: pbo_reader.PBOReader, pbofile: pbo_file.PBOFile, verbose: bool, deobfuscate: bool,
+    cfgconvert: typing.Optional[str], ignored: list[bytes]
+) -> None:
+    if deobfuscate and (
+            (pbofile.filename in ignored)
+            or (_invalid_filename(pbofile.filename) and not pbofile.filename.endswith(b".c"))):
+        if verbose:
+            print(f"Skipping obfuscation file: {pbofile.normalized_filename()}")
+        return
+
+    prefix = reader.prefix()
+
+    parts = pbofile.split_filename()
+
+    if len(parts) == 0 or len(parts[-1]) == 0:
+        print("Skipping empty obfuscation filename")
+        return
+
+    if len(parts) > 1:
+        os.makedirs(os.path.join(*parts[:-1]), exist_ok=True)
+
+    if parts[-1].lower() == b"config.bin" and cfgconvert is not None:
+        converted_filename = os.path.join(
+            os.path.dirname(pbofile.normalized_filename()), "config.cpp")
+
+        if verbose:
+            print(f"Converting {pbofile.normalized_filename()} -> {converted_filename}")
+
+        buffer = io.BytesIO()
+        pbofile.unpack(buffer)
+        try:
+            cpp_content = config_cpp.bin_to_cpp(buffer.getvalue(), cfgconvert)
+            with open(converted_filename, "w+b") as out_file:
+                out_file.write(cpp_content)
+                return
+        except Exception as error:
+            print(f"Failed to convert {pbofile.normalized_filename()}: {error}")
+
+    renamed_filename: typing.Optional[str] = None
+    normalized = pbofile.normalized_filename()
+
+    if deobfuscate and _invalid_filename(parts[-1]) and parts[-1].endswith(b".c"):
+        global _deobfs_count
+        parts[-1] = f"deobfs{_deobfs_count:05}.c".encode()
+        _deobfs_count += 1
+
+        normalized = renamed_filename = pbo_file.normalize_filename(parts)
+
+    with open(normalized, "w+b") as out_file:
+        if verbose:
+            if renamed_filename is None:
+                print(f"Extracting {pbofile.normalized_filename()}")
+            else:
+                print(f"Extracting {pbofile.normalized_filename()} -> {renamed_filename}")
+
+        if deobfuscate:
+            buffer = io.BytesIO()
+            pbofile.unpack(buffer)
+            content = buffer.getvalue()
+
+            if (match := OBFUSCATE_RE.match(content)) is not None:
+                target_filename = match.group(1)
+
+                if prefix is not None and not target_filename.startswith(prefix + b"\\"):
+                    target_filename = prefix + b"\\" + target_filename
+
+                unobfuscated = reader.file(target_filename)
+
+                if unobfuscated is None:
+                    if verbose:
+                        print(f"Unable to deobfuscate {pbofile.normalized_filename()}")
+                    out_file.write(content)
+                else:
+                    ignored.append(unobfuscated.filename)
+
+                    unobfuscated.unpack(out_file)
+            else:
+                out_file.write(content)
+
+        else:
+            pbofile.unpack(out_file)
+
+
+def extract_pbo(
+    reader: pbo_reader.PBOReader, files_to_extract: list[str], *, verbose: bool,
+    deobfuscate: bool, cfgconvert: typing.Optional[str]
+) -> None:
+    """Extract one or more files contained in a PBO archive.
+
+    :Parameters:
+      - `reader`: A :class:`~dayz_dev_tools.pbo_reader.PBOReader` instance representing the PBO
+        archive containing the file(s) to be extracted.
+      - `files_to_extract`: A list of fully-qualified paths of the files to be extracted.
+      - `verbose`: When `True`, print the paths of the files being extracted to stdout.
+      - `deobfuscate`: When `True`, **attempt** to deobfuscate obfuscated script files.
+
+    .. note:: Deobfuscation may not always work, as obfuscation techniques may evolve over time.
+    """
+    global _deobfs_count
+    _deobfs_count = 0
+
+    ignored: list[bytes] = []
+
+    if len(files_to_extract) == 0:
+        for file in reader.files():
+            _extract_file(reader, file, verbose, deobfuscate, cfgconvert, ignored)
+
+    else:
+        for file_to_extract in files_to_extract:
+            pbofile = reader.file(file_to_extract)
+
+            if pbofile is None:
+                raise Exception(f"File not found: {file_to_extract}")
+
+            _extract_file(reader, pbofile, verbose, deobfuscate, cfgconvert, [])
---
+++ packages/dayz-dev-tools/dayz_dev_tools-1.8.0-cp312-cp312-win32.whl/dayz_dev_tools/unpbo.py
@@ -0,0 +1,57 @@
+import argparse
+import logging
+import os
+import sys
+
+import dayz_dev_tools
+from dayz_dev_tools import extract_pbo
+from dayz_dev_tools import list_pbo
+from dayz_dev_tools import logging_configuration
+from dayz_dev_tools import pbo_reader
+from dayz_dev_tools import tools_directory
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        description="View or extract a PBO archive",
+        epilog="See also: https://community.bistudio.com/wiki/PBO_File_Format")
+    parser.add_argument(
+        "-l", "--list", action="store_true", help="List contents of the PBO archive")
+    parser.add_argument(
+        "-b", "--no-convert", action="store_true",
+        help="Do not convert config.bin files to config.cpp files")
+    parser.add_argument(
+        "-d", "--deobfuscate", action="store_true", help="Attempt to deobfuscate extracted files")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose output")
+    parser.add_argument("-D", "--debug", action="store_true", help="Enable debug logs")
+    parser.add_argument("-V", "--version", action="version", version=dayz_dev_tools.version)
+    parser.add_argument("pbofile", help="The PBO archive to read")
+    parser.add_argument("files", nargs="*", help="Files to extract from the PBO archive")
+    args = parser.parse_args()
+
+    logging_configuration.configure_logging(debug=args.debug)
+
+    try:
+        with open(args.pbofile, "rb") as pbo_file:
+            reader = pbo_reader.PBOReader(pbo_file)
+
+            if args.list:
+                list_pbo.list_pbo(reader, verbose=args.verbose)
+            else:
+                cfgconvert = None
+                if args.no_convert is False:
+                    tools_dir = tools_directory.tools_directory()
+                    if tools_dir is not None:
+                        cfgconvert = os.path.join(tools_dir, "bin", "CfgConvert", "CfgConvert.exe")
+
+                extract_pbo.extract_pbo(
+                    reader, args.files,
+                    verbose=args.verbose, deobfuscate=args.deobfuscate, cfgconvert=cfgconvert)
+    except Exception as error:
+        logging.debug("Uncaught exception in main", exc_info=True)
+        logging.error("%s: %s", type(error).__name__, error)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
