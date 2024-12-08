f9e19d13506bcbf0eaf32c2a4fbb486c42a853f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 14:03:46 +0000

    Add package gang-python-calculator/0.1.0/gang_python_calculator-0.1.0.tar.gz

---
+++ packages/gang-python-calculator/gang_python_calculator-0.1.0.tar.gz/gang_python_calculator-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gang_python_calculator',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='gang',
+    author_email='gangadriyarraballi@example.com',
+    description='A simple calculator package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
