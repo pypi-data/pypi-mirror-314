927dd9dbc4964f4044d9f948ca8b74eb0af4a895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 03:18:20 +0000

    Add package dojo/0.3.0/dojo-0.3.0.tar.gz

---
+++ packages/dojo/dojo-0.3.0.tar.gz/dojo-0.3.0/README.md
@@ -0,0 +1,15 @@
+[![PyPI](https://img.shields.io/pypi/v/dojo.svg)](https://pypi.python.org/pypi/dojo/#history)
+
+# ⛩️ Dojo
+
+World model research.
+
+## Structure
+
+```
+dojo/
+  core/  # Streams, replay, utils
+  envs/  # Interactive simulators
+  jax/   # JAX infra
+  run/   # Training loops
+```
---
+++ packages/dojo/dojo-0.3.0.tar.gz/dojo-0.3.0/setup.py
@@ -0,0 +1,37 @@
+import pathlib
+import re
+import setuptools
+
+
+def parse_reqs(filename):
+  requirements = pathlib.Path(filename)
+  requirements = requirements.read_text().split('\n')
+  requirements = [x for x in requirements if x.strip()]
+  return requirements
+
+
+def parse_version(filename):
+  text = (pathlib.Path(__file__).parent / filename).read_text()
+  version = re.search(r"__version__ = '(.*)'", text).group(1)
+  return version
+
+
+setuptools.setup(
+    name='dojo',
+    version=parse_version('dojo/__init__.py'),
+    author='Danijar Hafner',
+    author_email='mail@danijar.com',
+    description='World model research',
+    url='http://github.com/danijar/dojo',
+    long_description=pathlib.Path('README.md').read_text(),
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=parse_reqs('requirements.txt'),
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+)
