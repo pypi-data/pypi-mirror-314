144619abc4c89acc219e88c6706867b97b43195a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 14:03:22 +0000

    Add package stepslist/0.3.9/stepslist-0.3.9.tar.gz

---
+++ packages/stepslist/stepslist-0.3.9.tar.gz/stepslist-0.3.9/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='stepslist',
+    version='0.3.9',
+    packages=find_packages(),
+    entry_points={
+        'markdown.extensions': [
+            'stepslist = stepslist.stepslist:makeExtension',
+        ],
+    },
+    author='Chris van Liere',
+    author_email='c.vnliere@gmail.com',
+    description='A MkDocs extension to convert <steps> tags into ordered lists.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/a3bagged/stepslist-extension',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
