40b09948165f6a88982ea0d234801693601d7777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 14:03:34 +0000

    Add package funmaterial/1.0.20/funmaterial-1.0.20-py3-none-any.whl

---
+++ packages/funmaterial/funmaterial-1.0.20-py3-none-any.whl/funmaterial/video/schema.py
@@ -0,0 +1,34 @@
+from enum import Enum
+
+import pydantic
+
+
+class _Config:
+    arbitrary_types_allowed = True
+
+
+@pydantic.dataclasses.dataclass(config=_Config)
+class MaterialInfo:
+    provider: str = "pexels"
+    url: str = ""
+    duration: int = 0
+
+
+class VideoAspect(str, Enum):
+    landscape = "16:9"
+    portrait = "9:16"
+    square = "1:1"
+
+    def to_resolution(self):
+        if self == VideoAspect.landscape.value:
+            return 1920, 1080
+        elif self == VideoAspect.portrait.value:
+            return 1080, 1920
+        elif self == VideoAspect.square.value:
+            return 1080, 1080
+        return 1080, 1920
+
+
+class VideoConcatMode(str, Enum):
+    random = "random"
+    sequential = "sequential"
