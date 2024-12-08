499d22d75262d40f3482f5931a026a05da75f77a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 03:18:17 +0000

    Add package average-facer/0.7.4/average_facer-0.7.4-py3-none-any.whl

---
+++ packages/average-facer/average_facer-0.7.4-py3-none-any.whl/facer/__init__.py
@@ -0,0 +1,16 @@
+# Facer
+# John W. Miller
+# 2024
+
+import sys
+
+assert sys.version_info[0] == 3, "facer requires Python 3."
+
+"""Simple Python face averaging"""
+
+__author__ = "John W. Miller"
+__url__ = "https://github.com/johnwmillr/facer"
+__description__ = "Face averaging in Python made simple"
+__license__ = "MIT"
+
+print("Loading face detector and landmark prediction models...")
