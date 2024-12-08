578442f463ee301e4a83e706e1500d6465080825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 03:19:50 +0000

    Add package omlish/0.0.0.dev145/omlish-0.0.0.dev145.tar.gz

---
+++ packages/omlish/omlish-0.0.0.dev145.tar.gz/omlish-0.0.0.dev145/omlish/lite/cached.py
@@ -0,0 +1,34 @@
+import functools
+import typing as ta
+
+
+T = ta.TypeVar('T')
+
+CallableT = ta.TypeVar('CallableT', bound=ta.Callable)
+
+
+class _cached_nullary:  # noqa
+    def __init__(self, fn):
+        super().__init__()
+        self._fn = fn
+        self._value = self._missing = object()
+        functools.update_wrapper(self, fn)
+
+    def __call__(self, *args, **kwargs):  # noqa
+        if self._value is self._missing:
+            self._value = self._fn()
+        return self._value
+
+    def __get__(self, instance, owner):  # noqa
+        bound = instance.__dict__[self._fn.__name__] = self.__class__(self._fn.__get__(instance, owner))
+        return bound
+
+
+def cached_nullary(fn):  # ta.Callable[..., T]) -> ta.Callable[..., T]:
+    return _cached_nullary(fn)
+
+
+def static_init(fn: CallableT) -> CallableT:
+    fn = cached_nullary(fn)
+    fn()
+    return fn
