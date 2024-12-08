62cc449ba8b581f5b186dc7397c396dd18a1429f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 15:03:42 +0000

    Add package LughaatNLP/1.2.0/LughaatNLP-1.2.0-py3-none-any.whl

---
+++ packages/LughaatNLP/LughaatNLP-1.2.0-py3-none-any.whl/LughaatNLP/__init__.py
@@ -0,0 +1,7 @@
+from .LughaatNLP import LughaatNLP
+from .POS_urdu import POS_urdu
+from .NER_Urdu import NER_Urdu
+from .NER_Urdu import NER_Urdu
+from .TextSummarization import TextSummarization
+from .UrduSpeech import UrduSpeech
+__all__ = ['LughaatNLP', 'POS_urdu', 'NER_Urdu' , 'TextSummarization' , 'UrduSpeech']
