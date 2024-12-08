b3405691593b38098a883cddc8bfafede883220b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 15:03:45 +0000

    Add package lucid-dl/0.14.0/lucid_dl-0.14.0-py3-none-any.whl

---
+++ packages/lucid-dl/lucid_dl-0.14.0-py3-none-any.whl/lucid/nn/functional/_activation.py
@@ -0,0 +1,43 @@
+import lucid
+
+from lucid._tensor import Tensor
+
+
+def relu(input_: Tensor) -> Tensor:
+    return lucid.maximum(0, input_)
+
+
+def leaky_relu(input_: Tensor, negative_slope: float = 0.01) -> Tensor:
+    mask = input_ > 0
+    out = input_ * mask + input_ * negative_slope * (1 - mask)
+    return out
+
+
+def elu(input_: Tensor, alpha: float = 1.0) -> Tensor:
+    mask = input_ >= 0
+    pos = input_ * mask
+    neg = alpha * (lucid.exp(input_) - 1) * (1 - mask)
+    return pos + neg
+
+
+def selu(input_: Tensor) -> Tensor:
+    _scale = 1.0507009873554805
+    _alpha = 1.6732632423543772
+
+    mask = input_ >= 0
+    pos = _scale * input_ * mask
+    neg = _scale * _alpha * (lucid.exp(input_) - 1) * (1 - mask)
+    return pos + neg
+
+
+def gelu(input_: Tensor) -> Tensor:
+    c = lucid.sqrt(2 / lucid.pi)
+    return 0.5 * input_ * (1 + lucid.tanh(c * (input_ + 0.044715 * input_**3)))
+
+
+def sigmoid(input_: Tensor) -> Tensor:
+    return 1 / (1 + lucid.exp(input_))
+
+
+def tanh(input_: Tensor) -> Tensor:
+    return lucid.tanh(input_)
---
+++ packages/lucid-dl/lucid_dl-0.14.0-py3-none-any.whl/lucid/nn/modules/__init__.py
@@ -0,0 +1,4 @@
+from lucid.nn.modules.linear import *
+from lucid.nn.modules.conv import *
+from lucid.nn.modules.activation import *
+from lucid.nn.modules.pool import *
