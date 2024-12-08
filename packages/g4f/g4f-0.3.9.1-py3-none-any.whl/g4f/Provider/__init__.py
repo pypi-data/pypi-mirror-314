66e443c39ecf654ce5a463c3e0b68e88fbc24f06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 14:03:22 +0000

    Add package g4f/0.3.9.1/g4f-0.3.9.1-py3-none-any.whl

---
+++ packages/g4f/g4f-0.3.9.1-py3-none-any.whl/g4f/Provider/__init__.py
@@ -0,0 +1,64 @@
+from __future__ import annotations
+
+from ..providers.types          import BaseProvider, ProviderType
+from ..providers.retry_provider import RetryProvider, IterListProvider
+from ..providers.base_provider  import AsyncProvider, AsyncGeneratorProvider
+from ..providers.create_images  import CreateImagesProvider
+
+from .deprecated       import *
+from .selenium         import *
+from .needs_auth       import *
+from .not_working      import *
+from .local            import *
+
+from .Airforce         import Airforce
+from .AmigoChat        import AmigoChat
+from .Blackbox         import Blackbox
+from .Blackbox2        import Blackbox2
+from .ChatGpt          import ChatGpt
+from .ChatGptEs        import ChatGptEs
+from .Cloudflare       import Cloudflare
+from .Copilot          import Copilot
+from .DarkAI           import DarkAI
+from .DDG              import DDG
+from .DeepInfraChat    import DeepInfraChat
+from .Free2GPT         import Free2GPT
+from .FreeGpt          import FreeGpt
+from .GizAI            import GizAI
+from .Liaobots         import Liaobots
+from .MagickPen        import MagickPen
+from .PerplexityLabs   import PerplexityLabs
+from .Pi               import Pi
+from .Pizzagpt         import Pizzagpt
+from .PollinationsAI   import PollinationsAI
+from .Prodia           import Prodia
+from .Reka             import Reka
+from .ReplicateHome    import ReplicateHome
+from .RobocodersAPI    import RobocodersAPI
+from .RubiksAI         import RubiksAI
+from .TeachAnything    import TeachAnything
+from .Upstage          import Upstage
+from .You              import You
+from .Mhystical        import Mhystical
+from .Flux             import Flux
+
+import sys
+
+__modules__: list = [
+    getattr(sys.modules[__name__], provider) for provider in dir()
+    if not provider.startswith("__")
+]
+__providers__: list[ProviderType] = [
+    provider for provider in __modules__
+    if isinstance(provider, type)
+    and issubclass(provider, BaseProvider)
+]
+__all__: list[str] = [
+    provider.__name__ for provider in __providers__
+]
+__map__: dict[str, ProviderType] = dict([
+    (provider.__name__, provider) for provider in __providers__
+])
+
+class ProviderUtils:
+    convert: dict[str, ProviderType] = __map__
