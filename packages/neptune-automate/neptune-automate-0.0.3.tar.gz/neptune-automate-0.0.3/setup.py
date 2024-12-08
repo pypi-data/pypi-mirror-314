a4affa1c81592dc7fdbe13e5b59592db4bdf495a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 15:04:02 +0000

    Add package neptune-automate/0.0.3/neptune-automate-0.0.3.tar.gz

---
+++ packages/neptune-automate/neptune-automate-0.0.3.tar.gz/neptune-automate-0.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="neptune-automate",
+    version="0.0.3",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "statsmodels", 
+        "scikit-learn", 
+        "xgboost", 
+        "lightgbm", 
+        "catboost", 
+        "numpy", 
+        "pandas", 
+        "twine", 
+        "setuptools"
+    ],
+)
