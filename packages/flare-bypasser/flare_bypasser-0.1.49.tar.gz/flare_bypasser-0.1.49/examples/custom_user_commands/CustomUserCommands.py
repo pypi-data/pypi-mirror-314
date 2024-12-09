4657367d5cfa823141d177888ce892fea0346490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 14:03:30 +0000

    Add package flare-bypasser/0.1.49/flare_bypasser-0.1.49.tar.gz

---
+++ packages/flare-bypasser/flare_bypasser-0.1.49.tar.gz/flare_bypasser-0.1.49/examples/custom_user_commands/CustomUserCommands.py
@@ -0,0 +1,25 @@
+import zendriver_flare_bypasser as zendriver
+from flare_bypasser import BaseCommandProcessor, Request, Response, BrowserWrapper
+
+
+class MyClickCommandProcessor(BaseCommandProcessor):
+  async def preprocess_command(self, req: Request, driver: BrowserWrapper) -> Request:
+    # Here we can check some required parameters in req.params and raise error.
+    return req
+
+  async def process_command(self, res: Response, req: Request, driver: BrowserWrapper) -> Response:
+    nodriver_tab: zendriver.Tab = driver.get_driver()
+    dom = await nodriver_tab.get_content()  # get source code of page (actual DOM)
+    els = await nodriver_tab.select_all('input[type=submit]')  # find submit button
+    if not els:
+      raise Exception("MyClickCommandProcessor: no input for click: " + str(dom))
+    await els[0].click()  # click submit
+    res.response = await nodriver_tab.get_content()  # get actual DOM after click and return it in response
+    # Expect here "Bledny kod" text in DOM (appears only after click)
+    return res
+
+
+def get_user_commands():
+  return {
+    'my-click': MyClickCommandProcessor()
+  }
---
+++ packages/flare-bypasser/flare_bypasser-0.1.49.tar.gz/flare_bypasser-0.1.49/tests/server_tests/sites_test.sh
@@ -0,0 +1,63 @@
+SERVER_URL=${SERVER_URL:-http://localhost:20080}
+
+set -o pipefail
+TMP_DIR=/tmp/flare_bypasser_tests/
+mkdir -p "$TMP_DIR"
+
+# Standard API tests.
+function site_test {
+  URL="$1"
+  PROXY="$2"
+  PROXY_PART=''
+  RED='\033[0;31m'
+  GREEN='\033[0;32m'
+  NO_COLOR='\033[0m'
+  if [ "$PROXY" != "" ] ; then
+    PROXY_PART=',"proxy": "'"$PROXY"'"'
+  fi
+  curl -s -XPOST "$SERVER_URL"'/get_cookies' -H 'Content-Type: application/json' \
+    --data-raw '{"maxTimeout": 60000, "url": "'"$URL"'"'"$PROXY_PART"'}' \
+    >"$TMP_DIR/get_cookies.result" 2>"$TMP_DIR/get_cookies.err" && \
+    cat "$TMP_DIR/get_cookies.result" | python3 -c '
+import sys, json
+res = json.load(sys.stdin)
+solved = ("solution" in res and "cookies" in res["solution"])
+blocked = ("status" in res and res["status"] == "error" and "message" in res and "Cloudflare has blocked" in res["message"])
+cf_clearance_present = False
+cookies_count = 0
+if solved:
+  cookies = res["solution"]["cookies"]
+  cookies_count = len(cookies)
+  for c in cookies:
+    if c["name"] == "cf_clearance":
+      cf_clearance_present = True
+      break
+
+if not solved and not blocked:
+  sys.exit(1)
+elif solved:
+  print("solved: cf_clearance " + ("present" if cf_clearance_present else ("not present, " + str(cookies_count) + " cookies")))
+else:
+  print("blocked")
+' >"$TMP_DIR/get_cookies.check_result.out" && \
+    echo -e "$URL: ${GREEN}success${NO_COLOR} ($(cat "$TMP_DIR/get_cookies.check_result.out"))" || \
+    ( echo -e "$URL: ${RED}fail${NO_COLOR}, response:" 1>&2 ; cat "$TMP_DIR/get_cookies.result" | sed -r 's/^/  /' >&2 ; echo >&2 ; exit 1 ; )
+  return $?
+}
+
+RES=0
+site_test 'https://torrentleech.pl/login.php?returnto=%2F' || export RES=1
+site_test 'https://xcv.ashoo.org/' || export RES=1
+site_test 'https://myg.ashoo.live/' || export RES=1
+site_test 'https://hdf3im.lordfilm1.pics/' || export RES=1
+site_test 'https://www.ygg.re' 'socks5://91.142.74.232:40001' || export RES=1
+site_test 'https://ext.to/latest/' 'socks5://91.142.74.232:40001' || export RES=1
+site_test 'https://prowlarr.servarr.com/v1/ping' 'socks5://91.142.74.232:40001' || export RES=1 # Expected blocked here.
+site_test 'https://extratorrent.st' 'socks5://91.142.74.232:40001' || export RES=1
+site_test 'https://1337x.unblockninja.com/cat/Movies/time/desc/1/' 'socks5://91.142.74.232:40001' || export RES=1
+site_test 'https://solscan.io/' || export RES=1
+site_test 'https://www.ygg.re/engine/search?do=search&order=desc&sort=publish_date&category=al' || export RES=1
+site_test 'https://1337x.torrentbay.st/cat/Movies/time/desc/1/' || export RES=1
+site_test 'http://www.google.nl/' || export RES=1
+
+exit $RES
