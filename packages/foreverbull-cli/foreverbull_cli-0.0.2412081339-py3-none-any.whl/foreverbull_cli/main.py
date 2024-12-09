c5a3f00d0b618b1fb3e8dd402260c3c491ab4dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 03:20:10 +0000

    Add package foreverbull-cli/0.0.2412081339/foreverbull_cli-0.0.2412081339-py3-none-any.whl

---
+++ packages/foreverbull-cli/foreverbull_cli-0.0.2412081339-py3-none-any.whl/foreverbull_cli/main.py
@@ -0,0 +1,47 @@
+import logging
+import os
+
+import typer
+
+from rich.logging import RichHandler
+
+from foreverbull_cli.backtest import backtest
+from foreverbull_cli.env import env
+from foreverbull_cli.output import console
+
+
+cli = typer.Typer()
+
+cli.add_typer(backtest, name="backtest")
+cli.add_typer(env, name="env")
+
+
+@cli.callback()
+def setup_logging(
+    ctx: typer.Context,
+    verbose: bool = typer.Option(False, "--verbose", "-v"),
+    very_verbose: bool = typer.Option(False, "--very-verbose", "-vv"),
+    debug: bool = typer.Option(False, "-vvv"),
+):
+    if verbose:
+        level = "WARNING"
+        os.environ["LOGGING_LEVEL"] = "WARNING"
+    elif very_verbose:
+        level = "INFO"
+        os.environ["LOGGING_LEVEL"] = "INFO"
+    elif debug:
+        level = "DEBUG"
+        os.environ["LOGGING_LEVEL"] = "DEBUG"
+    else:
+        level = "ERROR"
+        os.environ["LOGGING_LEVEL"] = "ERROR"
+    logging.basicConfig(
+        level=level,
+        format="%(message)s",
+        datefmt="[%X]",
+        handlers=[RichHandler(markup=True, console=console)],
+    )
+
+
+def main():
+    cli()
