1f9d20202ca436c95a58d5bf14dfe6c6551415c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 15:03:44 +0000

    Add package dnd-auction-game/0.3.0/dnd_auction_game-0.3.0-py3-none-any.whl

---
+++ packages/dnd-auction-game/dnd_auction_game-0.3.0-py3-none-any.whl/dnd_auction_game/connection_manager.py
@@ -0,0 +1,48 @@
+
+
+
+from typing import List
+from fastapi import (
+    WebSocket,
+)
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def add_connection(self, websocket: WebSocket):
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+        
+    async def disconnect_all(self):
+        print("disconnect all")
+        for ws in self.active_connections:
+            try:
+                await ws.close()            
+            except:
+                print("error closing connection")
+                pass
+
+        self.active_connections = []
+
+    async def send_message(self, message: dict, websocket: WebSocket):
+        await websocket.send_json(message)
+
+    async def broadcast(self, message: dict):
+        for connection in self.active_connections:
+            to_disconnect = []
+            try:
+                await connection.send_json(message)                
+            except:
+                to_disconnect.append(connection)
+
+        for connection in to_disconnect:
+            await connection.close()
+            self.disconnect(connection)
+                
+            
+
+
