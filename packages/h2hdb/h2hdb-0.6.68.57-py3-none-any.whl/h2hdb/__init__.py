3ad9eaba0b818f6b0e7c40df0577c47b97f27942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 14:04:03 +0000

    Add package h2hdb/0.6.68.57/h2hdb-0.6.68.57-py3-none-any.whl

---
+++ packages/h2hdb/h2hdb-0.6.68.57-py3-none-any.whl/h2hdb/__init__.py
@@ -0,0 +1,21 @@
+__all__ = [
+    "__version__",
+    "__version_info__",
+    "H2HDB",
+    "DatabaseConfig",
+    "LoggerConfig",
+    "H2HConfig",
+    "Config",
+    "load_config",
+    "HentaiDBLogger",
+    "setup_logger",
+]
+__author__ = "Kuan-Lun Wang"
+
+__version__ = "0.6.68.56"
+__version_info__ = tuple(map(int, __version__.split(".")))
+
+
+from .h2h_db import H2HDB
+from .config_loader import DatabaseConfig, LoggerConfig, H2HConfig, Config, load_config
+from .logger import HentaiDBLogger, setup_logger
