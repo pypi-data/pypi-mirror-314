31d12a386a65f30ba2223e5483f83dd0c2263e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 14:03:40 +0000

    Add package pytestomatio/2.8.2.dev39/pytestomatio-2.8.2.dev39-py3-none-any.whl

---
+++ packages/pytestomatio/pytestomatio-2.8.2.dev39-py3-none-any.whl/pytestomatio/connect/connector.py
@@ -0,0 +1,190 @@
+import requests
+from requests.exceptions import HTTPError, ConnectionError
+import logging
+from os.path import join, normpath
+from os import getenv
+from pytestomatio.utils.helper import safe_string_list
+from pytestomatio.testing.testItem import TestItem
+
+log = logging.getLogger('pytestomatio')
+
+
+class Connector:
+    def __init__(self, base_url: str = '', api_key: str = None):
+        self.base_url = base_url
+        self.session = requests.Session()
+        self.session.verify = True
+        self.jwt: str = ''
+        self.api_key = api_key
+
+    def load_tests(
+            self,
+            tests: list[TestItem],
+            no_empty: bool = False,
+            no_detach: bool = False,
+            structure: bool = False,
+            create: bool = False,
+            directory: str = None
+    ):
+        request = {
+            "framework": "pytest",
+            "language": "python",
+            "noempty": no_empty,
+            "no-detach": no_detach,
+            "structure": structure if not no_empty else False,
+            "create": create,
+            "sync": True,
+            "tests": []
+        }
+        for test in tests:
+            request['tests'].append({
+                "name": test.sync_title,
+                "suites": [
+                    test.class_name
+                ],
+                "code": test.source_code,
+                "file": test.file_path if structure else (
+                    test.file_name if directory is None else normpath(join(directory, test.file_name))),
+                "labels": safe_string_list(getenv('TESTOMATIO_SYNC_LABELS')),
+            })
+
+        try:
+            response = self.session.post(f'{self.base_url}/api/load?api_key={self.api_key}', json=request)
+        except ConnectionError:
+            log.error(f'Failed to connect to {self.base_url}')
+            return
+        except HTTPError:
+            log.error(f'Failed to connect to {self.base_url}')
+            return
+        except Exception as e:
+            log.error(f'Generic exception happened. Please report an issue. {e}')
+            return
+
+        if response.status_code < 400:
+            log.info(f'Tests loaded to {self.base_url}')
+        else:
+            log.error(f'Failed to load tests to {self.base_url}. Status code: {response.status_code}')
+
+    def get_tests(self, test_metadata: list[TestItem]) -> dict:
+        # with safe_request('Failed to get test ids from testomat.io'):
+        response = self.session.get(f'{self.base_url}/api/test_data?api_key={self.api_key}')
+        return response.json()
+
+    def create_test_run(self, title: str, group_title, env: str, label: str, shared_run: bool, parallel, ci_build_url: str) -> dict | None:
+        request = {
+            "api_key": self.api_key,
+            "title": title,
+            "group_title": group_title,
+            "env": env,
+            "label": label,
+            "parallel": parallel,
+            "ci_build_url": ci_build_url,
+        }
+        filtered_request = {k: v for k, v in request.items() if v is not None}
+        try:
+            response = self.session.post(f'{self.base_url}/api/reporter', json=filtered_request)
+        except ConnectionError:
+            log.error(f'Failed to connect to {self.base_url}')
+            return
+        except HTTPError:
+            log.error(f'Failed to connect to {self.base_url}')
+            return
+        except Exception as e:
+            log.error(f'Generic exception happened. Please report an issue. {e}')
+            return
+
+        if response.status_code == 200:
+            log.info(f'Test run created {response.json()["uid"]}')
+            return response.json()
+
+    def update_test_run(self, id: str, title: str, group_title,
+                        env: str, label: str, shared_run: bool, parallel, ci_build_url: str) -> dict | None:
+        request = {
+            "api_key": self.api_key,
+            "title": title,
+            "group_title": group_title,
+            # "env": env, TODO: enabled when bug with 500 response fixed
+            # "label": label, TODO: enabled when bug with 500 response fixed
+            "parallel": parallel,
+            "ci_build_url": ci_build_url,
+        }
+        filtered_request = {k: v for k, v in request.items() if v is not None}
+
+        try:
+            response = self.session.put(f'{self.base_url}/api/reporter/{id}', json=filtered_request)
+        except ConnectionError:
+            log.error(f'Failed to connect to {self.base_url}')
+            return
+        except HTTPError:
+            log.error(f'Failed to connect to {self.base_url}')
+            return
+        except Exception as e:
+            log.error(f'Generic exception happened. Please report an issue. {e}')
+            return
+
+        if response.status_code == 200:
+            log.info(f'Test run updated {response.json()["uid"]}')
+            return response.json()
+
+    def update_test_status(self, run_id: str,
+                           status: str,
+                           title: str,
+                           suite_title: str,
+                           suite_id: str,
+                           test_id: str,
+                           message: str,
+                           stack: str,
+                           run_time: float,
+                           artifacts: list[str],
+                           steps: str,
+                           code: str,
+                           example: dict) -> None:
+
+        request = {
+            "status": status,  # Enum: "passed" "failed" "skipped"
+            "title": title,
+            "suite_title": suite_title,
+            "suite_id": suite_id,
+            "test_id": test_id,
+            "message": message,
+            "stack": stack,
+            "run_time": run_time,
+            "example": example,
+            "artifacts": artifacts,
+            "steps": steps,
+            "code": code
+        }
+        filtered_request = {k: v for k, v in request.items() if v is not None}
+        try:
+            response = self.session.post(f'{self.base_url}/api/reporter/{run_id}/testrun?api_key={self.api_key}',
+                                         json=filtered_request)
+        except ConnectionError:
+            log.error(f'Failed to connect to {self.base_url}')
+            return
+        except HTTPError:
+            log.error(f'Failed to connect to {self.base_url}')
+            return
+        except Exception as e:
+            log.error(f'Generic exception happened. Please report an issue. {e}')
+            return
+        if response.status_code == 200:
+            log.info('Test status updated')
+
+    # TODO: I guess this class should be just an API client and used within testRun (testRunConfig)
+    def finish_test_run(self, run_id: str, is_final=False) -> None:
+        status_event = 'finish_parallel' if is_final else 'finish'
+        try:
+            self.session.put(f'{self.base_url}/api/reporter/{run_id}?api_key={self.api_key}',
+                             json={"status_event": status_event})
+        except ConnectionError:
+            log.error(f'Failed to connect to {self.base_url}')
+            return
+        except HTTPError:
+            log.error(f'Failed to connect to {self.base_url}')
+            return
+        except Exception as e:
+            log.error(f'Generic exception happened. Please report an issue. {e}')
+            return
+
+    def disconnect(self):
+        self.session.close()
---
+++ packages/pytestomatio/pytestomatio-2.8.2.dev39-py3-none-any.whl/pytestomatio/testomatio/testomatio.py
@@ -0,0 +1,29 @@
+from _pytest.python import Function
+from .testRunConfig import TestRunConfig
+from pytestomatio.connect.s3_connector import S3Connector
+from pytestomatio.connect.connector import Connector
+
+
+class Testomatio:
+    def __init__(self, test_run_config: TestRunConfig = None,
+                 s3_connector: S3Connector = None) -> None:
+        self.s3_connector: S3Connector = s3_connector
+        self.test_run_config: TestRunConfig = test_run_config
+        self.connector: Connector = None
+
+    def upload_file(self, file_path: str, key: str = None, bucket_name: str = None) -> str:
+        if self.test_run_config.test_run_id is None:
+            print("Skipping file upload when testomatio test run is not created")
+            return ""
+        return self.s3_connector.upload_file(file_path, key, bucket_name)
+
+    def upload_file_object(self, file_bytes: bytes, key: str, bucket_name: str = None) -> str:
+        if self.test_run_config.test_run_id is None:
+            print("Skipping file upload when testomatio test run is not created")
+            return ""
+        return self.s3_connector.upload_file_object(file_bytes, key, bucket_name)
+
+    def add_artifacts(self, node: Function, urls: list[str]) -> None:
+        artifact_urls = node.stash.get("artifact_urls", [])
+        artifact_urls.extend(urls)
+        node.stash["artifact_urls"] = artifact_urls
