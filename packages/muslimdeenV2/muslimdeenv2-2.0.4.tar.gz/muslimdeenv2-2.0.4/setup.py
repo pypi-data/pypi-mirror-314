e57f32a062906182a5147b7200b49b9f0b03c4ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 04:04:24 +0000

    Add package muslimdeenV2/2.0.4/muslimdeenv2-2.0.4.tar.gz

---
+++ packages/muslimdeenV2/muslimdeenv2-2.0.4.tar.gz/muslimdeenv2-2.0.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+# Lire les dépendances depuis requirements.txt
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+# Lire le README.md pour la description longue
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='muslimdeenV2',
+    version='2.0.4',
+    packages=find_packages(),
+    install_requires=required,
+    description='MuslimDeen: Package pour gérer les sourates, noms d\'Allah, ablutions et salat',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="personne monsieur",
+    author_email="monsieurnobody01@gmail.com",
+    url='https://gitlab.com/misternobody01/muslimdeen#',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
