0fbcad236791dc4466d0665058a1db915c6126cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 05:04:04 +0000

    Add package pysndlib/0.7.0/pysndlib-0.7.0-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pysndlib/pysndlib-0.7.0-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pysndlib/analog_filter.pyx
@@ -0,0 +1,20 @@
+import math
+from pysndlib import *
+import numpy as np
+
+#==================================================================================
+# The code is an attempt at translation of Bill Schottstedaet's 'analog-filter.scm' 
+# file available at https://ccrma.stanford.edu/software/snd/sndlib/
+#==================================================================================
+
+
+# def analog2digital(n, num, den, fz):
+#     g = 1.0
+#     Q = 1.0
+#     wc = math.tan(math.pi * fz)
+#     c = np.zeros(2 * n, dtype=np.double)
+    
+    
+
+
+
