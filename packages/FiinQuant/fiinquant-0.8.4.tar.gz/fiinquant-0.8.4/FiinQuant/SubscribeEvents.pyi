f529dfba60ba4ce6e86f6fc43b379759f722eecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 16:04:25 +0000

    Add package FiinQuant/0.8.4/fiinquant-0.8.4.tar.gz

---
+++ packages/FiinQuant/fiinquant-0.8.4.tar.gz/fiinquant-0.8.4/FiinQuant/SubscribeEvents.pyi
@@ -0,0 +1,9 @@
+class SubscribeEvents:
+    def __init__(self,tickers: list, callback: callable) -> None:
+        self.tickers: list
+        self._stop: bool
+        
+    def start(self) -> None: ...
+        
+    def stop(self) -> None: ...
+
---
+++ packages/FiinQuant/fiinquant-0.8.4.tar.gz/fiinquant-0.8.4/FiinQuant/TickerData.pyi
@@ -0,0 +1,41 @@
+import pandas as pd
+
+class TickerData:
+    def __init__(self, data: pd.DataFrame) -> None:
+        self.__private_attribute: pd.DataFrame 
+        self.TotalMatchVolume: int
+        self.MarketStatus: str
+        self.TradingDate: str
+        self.MatchType: str
+        self.ComGroupCode: str
+        self.OrganCode: str
+        self.Ticker: str
+        self.ReferencePrice: float
+        self.OpenPrice: float
+        self.ClosePrice: float
+        self.CeilingPrice: float
+        self.FloorPrice: float
+        self.HighestPrice: float
+        self.LowestPrice: float
+        self.MatchPrice: float
+        self.PriceChange: float
+        self.PercentPriceChange: float
+        self.MatchVolume: int
+        self.MatchValue: float
+        self.TotalMatchValue: float
+        self.TotalBuyTradeVolume: int
+        self.TotalSellTradeVolume: int
+        self.DealPrice: float
+        self.TotalDealVolume: int
+        self.TotalDealValue: float
+        self.ForeignBuyVolumeTotal: int
+        self.ForeignBuyValueTotal: float
+        self.ForeignSellVolumeTotal: int
+        self.ForeignSellValueTotal: float
+        self.ForeignTotalRoom: int
+        self.ForeignCurrentRoom: int
+
+    def to_dataFrame(self) -> pd.DataFrame: ...
+
+
+    
