db7defd6ece739cdf6d2fdda4ef2eb6a84a3e499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 03:20:23 +0000

    Add package xyl-client.py/0.1.0/xyl_client_py-0.1.0.tar.gz

---
+++ packages/xyl-client.py/xyl_client_py-0.1.0.tar.gz/xyl_client_py-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xyl-client.py',  # Name of the package
+    version='0.1.0',    # Initial version
+    description='Client library to interact with the XYL Network',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='debxylen',
+    author_email='priyanshudeb3@gmail.com',
+    url='https://github.com/debxylen/xyl-client.py',  # GitHub or project URL
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Minimum required Python version
+)
