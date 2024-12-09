f4ae1cdced36c00ef6baed5a790e7bff8cbf7d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 05:03:54 +0000

    Add package Burningkeys/2024.12.4/burningkeys-2024.12.4-py3-none-any.whl

---
+++ packages/Burningkeys/burningkeys-2024.12.4-py3-none-any.whl/burningkeys/__init__.py
@@ -0,0 +1,21 @@
+import argparse
+
+from burningkeys import example_config, read_config, execute_config
+
+
+def command_entry_point():
+    parser = argparse.ArgumentParser(prog="bkeys", description="Burningkeys is a lightweight python script to invoke "
+                                                               "certain commands using hotkeys. This cli is meant mainly for coding."
+                                                               "For example you might be editing in a basic text editor and would"
+                                                               "like to invoke a linter on your project. Well now you can set that up")
+
+    parser.add_argument("--create-example", action="store_true")
+
+    args = parser.parse_args()
+
+    if args.create_example:
+        example_config.create()
+        return
+
+    config = read_config.config()
+    execute_config.execute(config)
