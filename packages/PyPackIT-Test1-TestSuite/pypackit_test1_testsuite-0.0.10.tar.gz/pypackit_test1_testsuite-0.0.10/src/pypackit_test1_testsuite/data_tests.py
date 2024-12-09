926e3116263db114dc252490af5899c6191d56b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 03:20:27 +0000

    Add package PyPackIT-Test1-TestSuite/0.0.10/pypackit_test1_testsuite-0.0.10.tar.gz

---
+++ packages/PyPackIT-Test1-TestSuite/pypackit_test1_testsuite-0.0.10.tar.gz/pypackit_test1_testsuite-0.0.10/src/pypackit_test1_testsuite/data_tests.py
@@ -0,0 +1,28 @@
+# PyPackIT-Test1-TestSuite © 2024 Armin Ariamajd
+# SPDX-License-Identifier: MIT
+
+"""Unit tests for the data sub-package."""
+
+import pytest
+
+import pypackit_test1 as pkg
+from pypackit_test1.exception.data import DataFileNotFoundError
+
+
+def test_file() -> None:
+    """Test the integrity of the `__test_file__` marker file.
+
+    The marker file is used to indicate that the package data files are present.
+    """
+    marker = (
+        "This is a test file used by our test suite "
+        "to verify that package data is being included correctly."
+    )
+    filepath = pkg.data.filepath("__test_file__")
+    assert filepath.read_text().rstrip() == marker
+
+
+def test_no_file() -> None:
+    """Test that an error is raised when the input data file is missing."""
+    with pytest.raises(DataFileNotFoundError):
+        pkg.data.filepath("path/to/nonexistent/file")
