8283e388adb152d3761cc26eb20688a29349f0a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 14:03:55 +0000

    Add package LibDOM/0.0.5/LibDOM-0.0.5-py3-none-any.whl

---
+++ packages/LibDOM/LibDOM-0.0.5-py3-none-any.whl/LibDOM/Attributes.py
@@ -0,0 +1,573 @@
+Accept = "accept"
+"""List of types the server accepts, typically a file type."""
+
+AcceptCharset = "accept-charset"
+"""The character set, which if provided must be "UTF-8"."""
+
+Accesskey = "accesskey"
+"""Keyboard shortcut to activate or add focus to the element."""
+
+Action = "action"
+"""
+        The URI of a program that processes the information submitted via the
+        form.
+      """
+
+Align = "align"
+"""Specifies the horizontal alignment of the element."""
+
+Allow = "allow"
+"""Specifies a feature-policy for the iframe."""
+
+Alt = "alt"
+"""Alternative text in case an image can't be displayed."""
+
+As = "as"
+"""Specifies the type of content being loaded by the link."""
+
+Async = "async"
+"""Executes the script asynchronously."""
+
+Autocapitalize = "autocapitalize"
+"""Sets whether input is automatically capitalized when entered by user"""
+
+Autocomplete = "autocomplete"
+"""
+        Indicates whether controls in this form can by default have their values
+        automatically completed by the browser.
+      """
+
+Autoplay = "autoplay"
+"""The audio or video should play as soon as possible."""
+
+Background = "background"
+"""Specifies the URL of an image file.
+        
+          
+            Note: Although browsers and email clients may still
+            support this attribute, it is obsolete. Use CSS
+            background-image instead.
+          
+        
+      """
+
+Bgcolor = "bgcolor"
+"""
+        Background color of the element.
+        
+          
+            Note: This is a legacy attribute. Please use the
+            CSS background-color property instead.
+          
+        
+      """
+
+Border = "border"
+"""
+        The border width.
+        
+          
+            Note: This is a legacy attribute. Please use the
+            CSS border property instead.
+          
+        
+      """
+
+Capture = "capture"
+"""
+        From the Media Capture specification,
+        specifies a new file can be captured.
+      """
+
+Charset = "charset"
+"""Declares the character encoding of the page or script."""
+
+Checked = "checked"
+"""Indicates whether the element should be checked on page load."""
+
+Cite = "cite"
+"""Contains a URI which points to the source of the quote or change."""
+
+Class = "class"
+"""Often used with CSS to style elements with common properties."""
+
+Color = "color"
+"""
+        
+          This attribute sets the text color using either a named color or a
+          color specified in the hexadecimal #RRGGBB format.
+        
+        
+          
+            Note: This is a legacy attribute. Please use the
+            CSS color property instead.
+          
+        
+      """
+
+Cols = "cols"
+"""Defines the number of columns in a textarea."""
+
+Colspan = "colspan"
+"""The colspan attribute defines the number of columns a cell should span."""
+
+Content = "content"
+"""
+        A value associated with http-equiv or
+        name depending on the context.
+      """
+
+Contenteditable = "contenteditable"
+"""Indicates whether the element's content is editable."""
+
+Controls = "controls"
+"""Indicates whether the browser should show playback controls to the user."""
+
+Coords = "coords"
+"""A set of values specifying the coordinates of the hot-spot region."""
+
+Crossorigin = "crossorigin"
+"""How the element handles cross-origin requests"""
+
+Csp = "csp"
+"""
+        Specifies the Content Security Policy that an embedded document must
+        agree to enforce upon itself.
+      """
+
+Data = "data"
+"""Specifies the URL of the resource."""
+
+Datetime = "datetime"
+"""Indicates the date and time associated with the element."""
+
+Decoding = "decoding"
+"""Indicates the preferred method to decode the image."""
+
+Default = "default"
+"""
+        Indicates that the track should be enabled unless the user's preferences
+        indicate something different.
+      """
+
+Defer = "defer"
+"""
+        Indicates that the script should be executed after the page has been
+        parsed.
+      """
+
+Dir = "dir"
+"""
+        Defines the text direction. Allowed values are ltr (Left-To-Right) or
+        rtl (Right-To-Left)
+      """
+
+Dirname = "dirname"
+""""""
+
+Disabled = "disabled"
+"""Indicates whether the user can interact with the element."""
+
+Download = "download"
+"""Indicates that the hyperlink is to be used for downloading a resource."""
+
+Draggable = "draggable"
+"""Defines whether the element can be dragged."""
+
+Enctype = "enctype"
+"""
+        Defines the content type of the form data when the
+        method is POST.
+      """
+
+Enterkeyhint = "enterkeyhint"
+"""
+        The enterkeyhint
+        specifies what action label (or icon) to present for the enter key on
+        virtual keyboards. The attribute can be used with form controls (such as
+        the value of textarea elements), or in elements in an
+        editing host (e.g., using contenteditable attribute).
+      """
+
+For = "for"
+"""Describes elements which belongs to this one."""
+
+Form = "form"
+"""Indicates the form that is the owner of the element."""
+
+Formaction = "formaction"
+"""
+        Indicates the action of the element, overriding the action defined in
+        the <form>.
+      """
+
+Formenctype = "formenctype"
+"""
+        If the button/input is a submit button (e.g. type="submit"),
+        this attribute sets the encoding type to use during form submission. If
+        this attribute is specified, it overrides the
+        enctype attribute of the button's
+        form owner.
+      """
+
+Formmethod = "formmethod"
+"""
+        If the button/input is a submit button (e.g. type="submit"),
+        this attribute sets the submission method to use during form submission
+        (GET, POST, etc.). If this attribute is
+        specified, it overrides the method attribute of the
+        button's form owner.
+      """
+
+Formnovalidate = "formnovalidate"
+"""
+        If the button/input is a submit button (e.g. type="submit"),
+        this boolean attribute specifies that the form is not to be validated
+        when it is submitted. If this attribute is specified, it overrides the
+        novalidate attribute of the button's
+        form owner.
+      """
+
+Formtarget = "formtarget"
+"""
+        If the button/input is a submit button (e.g. type="submit"),
+        this attribute specifies the browsing context (for example, tab, window,
+        or inline frame) in which to display the response that is received after
+        submitting the form. If this attribute is specified, it overrides the
+        target attribute of the button's
+        form owner.
+      """
+
+Headers = "headers"
+"""
+        IDs of the <th> elements which applies to this
+        element.
+      """
+
+Height = "height"
+"""
+        
+          Specifies the height of elements listed here. For all other elements,
+          use the CSS height property.
+        
+        
+          
+            Note: In some instances, such as
+            <div>, this is a legacy attribute, in
+            which case the CSS height property should
+            be used instead.
+          
+        
+      """
+
+Hidden = "hidden"
+"""
+        Prevents rendering of given element, while keeping child elements, e.g.
+        script elements, active.
+      """
+
+High = "high"
+"""Indicates the lower bound of the upper range."""
+
+Href = "href"
+"""The URL of a linked resource."""
+
+Hreflang = "hreflang"
+"""Specifies the language of the linked resource."""
+
+HttpEquiv = "http-equiv"
+"""Defines a pragma directive."""
+
+Id = "id"
+"""
+        Often used with CSS to style a specific element. The value of this
+        attribute must be unique.
+      """
+
+Integrity = "integrity"
+"""
+        
+          Specifies a
+          Subresource Integrity
+          value that allows browsers to verify what they fetch.
+        
+      """
+
+Intrinsicsize = "intrinsicsize"
+"""
+        This attribute tells the browser to ignore the actual intrinsic size of
+        the image and pretend it's the size specified in the attribute.
+      """
+
+Inputmode = "inputmode"
+"""
+        Provides a hint as to the type of data that might be entered by the user
+        while editing the element or its contents. The attribute can be used
+        with form controls (such as the value of
+        textarea elements), or in elements in an editing host
+        (e.g., using contenteditable attribute).
+      """
+
+Ismap = "ismap"
+"""Indicates that the image is part of a server-side image map."""
+
+Itemprop = "itemprop"
+""""""
+
+Kind = "kind"
+"""Specifies the kind of text track."""
+
+Label = "label"
+"""Specifies a user-readable title of the element."""
+
+Lang = "lang"
+"""Defines the language used in the element."""
+
+Language = "language"
+"""Defines the script language used in the element."""
+
+Loading = "loading"
+"""
+        Indicates if the element should be loaded lazily
+        (loading="lazy") or loaded immediately
+        (loading="eager").
+      """
+
+List = "list"
+"""Identifies a list of pre-defined options to suggest to the user."""
+
+Loop = "loop"
+"""
+        Indicates whether the media should start playing from the start when
+        it's finished.
+      """
+
+Low = "low"
+"""Indicates the upper bound of the lower range."""
+
+Max = "max"
+"""Indicates the maximum value allowed."""
+
+Maxlength = "maxlength"
+"""Defines the maximum number of characters allowed in the element."""
+
+Minlength = "minlength"
+"""Defines the minimum number of characters allowed in the element."""
+
+Media = "media"
+"""
+        Specifies a hint of the media for which the linked resource was
+        designed.
+      """
+
+Method = "method"
+"""
+        Defines which HTTP method to use when
+        submitting the form. Can be GET (default) or
+        POST.
+      """
+
+Min = "min"
+"""Indicates the minimum value allowed."""
+
+Multiple = "multiple"
+"""
+        Indicates whether multiple values can be entered in an input of the type
+        email or file.
+      """
+
+Muted = "muted"
+"""Indicates whether the audio will be initially silenced on page load."""
+
+Name = "name"
+"""
+        Name of the element. For example used by the server to identify the
+        fields in form submits.
+      """
+
+Novalidate = "novalidate"
+"""
+        This attribute indicates that the form shouldn't be validated when
+        submitted.
+      """
+
+Open = "open"
+"""
+        Indicates whether the contents are currently visible (in the case of
+        a <details> element) or whether the dialog is active
+        and can be interacted with (in the case of a
+        <dialog> element).
+      """
+
+Optimum = "optimum"
+"""Indicates the optimal numeric value."""
+
+Pattern = "pattern"
+"""
+        Defines a regular expression which the element's value will be validated
+        against.
+      """
+
+Ping = "ping"
+"""
+        The ping attribute specifies a space-separated list of URLs
+        to be notified if a user follows the hyperlink.
+      """
+
+Placeholder = "placeholder"
+"""Provides a hint to the user of what can be entered in the field."""
+
+Playsinline = "playsinline"
+"""A Boolean attribute indicating that the video is to be played "inline"; that is, within the element's playback area. Note that the absence of this attribute does not imply that the video will always be played in fullscreen."""
+
+Poster = "poster"
+"""A URL indicating a poster frame to show until the user plays or seeks."""
+
+Preload = "preload"
+"""
+        Indicates whether the whole resource, parts of it or nothing should be
+        preloaded.
+      """
+
+Readonly = "readonly"
+"""Indicates whether the element can be edited."""
+
+Referrerpolicy = "referrerpolicy"
+"""Specifies which referrer is sent when fetching the resource."""
+
+Rel = "rel"
+"""Specifies the relationship of the target object to the link object."""
+
+Required = "required"
+"""Indicates whether this element is required to fill out or not."""
+
+Reversed = "reversed"
+"""
+        Indicates whether the list should be displayed in a descending order
+        instead of an ascending order.
+      """
+
+Role = "role"
+"""Defines an explicit role for an element for use by assistive technologies."""
+
+Rows = "rows"
+"""Defines the number of rows in a text area."""
+
+Rowspan = "rowspan"
+"""Defines the number of rows a table cell should span over."""
+
+Sandbox = "sandbox"
+"""
+        Stops a document loaded in an iframe from using certain features (such
+        as submitting forms or opening new windows).
+      """
+
+Scope = "scope"
+"""
+        Defines the cells that the header test (defined in the
+        th element) relates to.
+      """
+
+Scoped = "scoped"
+""""""
+
+Selected = "selected"
+"""Defines a value which will be selected on page load."""
+
+Shape = "shape"
+""""""
+
+Size = "size"
+"""
+        Defines the width of the element (in pixels). If the element's
+        type attribute is text or
+        password then it's the number of characters.
+      """
+
+Sizes = "sizes"
+""""""
+
+Slot = "slot"
+"""Assigns a slot in a shadow DOM shadow tree to an element."""
+
+Span = "span"
+""""""
+
+Spellcheck = "spellcheck"
+"""Indicates whether spell checking is allowed for the element."""
+
+Src = "src"
+"""The URL of the embeddable content."""
+
+Srcdoc = "srcdoc"
+""""""
+
+Srclang = "srclang"
+""""""
+
+Srcset = "srcset"
+"""One or more responsive image candidates."""
+
+Start = "start"
+"""Defines the first number if other than 1."""
+
+Step = "step"
+""""""
+
+Style = "style"
+"""Defines CSS styles which will override styles previously set."""
+
+Summary = "summary"
+""""""
+
+Tabindex = "tabindex"
+"""
+        Overrides the browser's default tab order and follows the one specified
+        instead.
+      """
+
+Target = "target"
+"""
+        Specifies where to open the linked document (in the case of an
+        <a> element) or where to display the response received
+        (in the case of a <form> element)
+      """
+
+Title = "title"
+"""Text to be displayed in a tooltip when hovering over the element."""
+
+Translate = "translate"
+"""
+        Specify whether an element's attribute values and the values of its
+        Text node
+        children are to be translated when the page is localized, or whether to
+        leave them unchanged.
+      """
+
+Type = "type"
+"""Defines the type of the element."""
+
+Usemap = "usemap"
+""""""
+
+Value = "value"
+"""
+        Defines a default value which will be displayed in the element on page
+        load.
+      """
+
+Width = "width"
+"""
+        For the elements listed here, this establishes the element's width.
+        
+          
+            Note: For all other instances, such as
+            <div>, this is a legacy attribute, in
+            which case the CSS width property should be
+            used instead.
+          
+        
+      """
+
+Wrap = "wrap"
+"""Indicates whether the text should be wrapped."""
