70ccb0ae4a483321bfe47cab82ac8cff265a778f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 16:03:56 +0000

    Add package datachain/0.7.10/datachain-0.7.10.tar.gz

---
+++ packages/datachain/datachain-0.7.10.tar.gz/datachain-0.7.10/examples/get_started/torch-loader.py
@@ -0,0 +1,91 @@
+"""
+To install the required dependencies:
+
+  pip install datachain[torch]
+
+"""
+
+import os
+from posixpath import basename
+
+import torch
+from torch import nn, optim
+from torch.utils.data import DataLoader
+from torchvision.transforms import v2
+
+from datachain import C, DataChain
+from datachain.torch import label_to_int
+
+STORAGE = "gs://datachain-demo/dogs-and-cats/"
+NUM_EPOCHS = os.getenv("NUM_EPOCHS", "3")
+
+# Define transformation for data preprocessing
+transform = v2.Compose(
+    [
+        v2.ToTensor(),
+        v2.Resize((64, 64)),
+        v2.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ]
+)
+
+
+CLASSES = ["cat", "dog"]
+
+
+# Define torch model
+class CNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1)
+        self.fc1 = nn.Linear(64 * 8 * 8, 512)
+        self.fc2 = nn.Linear(512, len(CLASSES))
+
+    def forward(self, x):
+        x = torch.relu(self.conv1(x))
+        x = torch.relu(self.conv2(x))
+        x = torch.relu(self.conv3(x))
+        x = x.view(-1, 64 * 8 * 8)
+        x = torch.relu(self.fc1(x))
+        return self.fc2(x)
+
+
+if __name__ == "__main__":
+    ds = (
+        DataChain.from_storage(STORAGE, type="image")
+        .filter(C("file.path").glob("*.jpg"))
+        .map(
+            label=lambda path: label_to_int(basename(path)[:3], CLASSES),
+            params=["file.path"],
+            output=int,
+        )
+    )
+
+    train_loader = DataLoader(
+        ds.to_pytorch(transform=transform),
+        batch_size=16,
+        num_workers=2,
+    )
+
+    model = CNN()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    # Train the model
+    for epoch in range(int(NUM_EPOCHS)):
+        for i, data in enumerate(train_loader):
+            inputs, labels = data
+            optimizer.zero_grad()
+
+            # Forward pass
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+
+            # Backward pass and optimize
+            loss.backward()
+            optimizer.step()
+
+            print(f"[{epoch + 1}, {i + 1:5d}] loss: {loss.item():.3f}")
+
+    print("Finished Training")
---
+++ packages/datachain/datachain-0.7.10.tar.gz/datachain-0.7.10/tests/examples/test_examples.py
@@ -0,0 +1,108 @@
+import glob
+import os
+import subprocess
+import sys
+from typing import Optional
+
+import pytest
+
+get_started_examples = sorted(
+    [
+        filename
+        for filename in glob.glob("examples/get_started/**/*.py", recursive=True)
+        # torch-loader will not finish within an hour on Linux runner
+        if "torch" not in filename or os.environ.get("RUNNER_OS") != "Linux"
+    ]
+)
+
+llm_and_nlp_examples = sorted(
+    [
+        filename
+        for filename in glob.glob("examples/llm_and_nlp/**/*.py", recursive=True)
+        # no anthropic token, HF runs against actual API - thus run it only once
+        if "claude" not in filename
+        and (
+            "hf-" not in filename
+            or (sys.platform == "darwin" and sys.version_info >= (3, 12))
+        )
+    ]
+)
+
+multimodal_examples = sorted(
+    [
+        filename
+        for filename in glob.glob("examples/multimodal/**/*.py", recursive=True)
+        # no OpenAI token
+        if "openai" not in filename
+    ]
+)
+
+computer_vision_examples = sorted(
+    [
+        filename
+        for filename in glob.glob("examples/computer_vision/**/*.py", recursive=True)
+        # fashion product images tutorial out of scope
+        # and hf download painfully slow
+        if "image_desc" not in filename and "fashion_product_images" not in filename
+    ]
+)
+
+
+def smoke_test(example: str, env: Optional[dict] = None):
+    try:
+        completed_process = subprocess.run(  # noqa: S603
+            [sys.executable, example],
+            env={**os.environ, **(env or {})},
+            capture_output=True,
+            cwd=os.path.abspath(os.path.join(__file__, "..", "..", "..")),
+            check=True,
+        )
+    except subprocess.CalledProcessError as e:
+        print(f"Example failed: {example}")
+        print()
+        print()
+        print("stdout:")
+        print(e.stdout.decode("utf-8"))
+        print()
+        print()
+        print("stderr:")
+        print(e.stderr.decode("utf-8"))
+        pytest.fail("subprocess returned a non-zero exit code")
+
+    example_has_some_output = bool(completed_process.stdout or completed_process.stderr)
+    assert example_has_some_output
+
+
+@pytest.mark.examples
+@pytest.mark.get_started
+@pytest.mark.parametrize("example", get_started_examples)
+def test_get_started_examples(example):
+    smoke_test(example, {"NUM_EPOCHS": "1"})
+
+
+@pytest.mark.examples
+@pytest.mark.llm_and_nlp
+@pytest.mark.parametrize("example", llm_and_nlp_examples)
+def test_llm_and_nlp_examples(example):
+    smoke_test(example)
+
+
+@pytest.mark.examples
+@pytest.mark.multimodal
+@pytest.mark.parametrize("example", multimodal_examples)
+def test_multimodal(example):
+    smoke_test(
+        example,
+        {
+            "IMAGE_TARS": "gs://datachain-demo/datacomp-small/shards/00001286.tar",
+            "PARQUET_METADATA": "gs://datachain-demo/datacomp-small/metadata/036d6b9ae87a00e738f8fc554130b65b.parquet",
+            "NPZ_METADATA": "gs://datachain-demo/datacomp-small/metadata/036d6b9ae87a00e738f8fc554130b65b.npz",
+        },
+    )
+
+
+@pytest.mark.examples
+@pytest.mark.computer_vision
+@pytest.mark.parametrize("example", computer_vision_examples)
+def test_computer_vision_examples(example):
+    smoke_test(example)
