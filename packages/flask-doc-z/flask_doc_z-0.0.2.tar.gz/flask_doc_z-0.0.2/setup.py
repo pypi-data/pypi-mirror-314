ec7e51953c30b6634d162833cae92427995e033f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 14:03:30 +0000

    Add package flask-doc-z/0.0.2/flask_doc_z-0.0.2.tar.gz

---
+++ packages/flask-doc-z/flask_doc_z-0.0.2.tar.gz/flask_doc_z-0.0.2/setup.py
@@ -0,0 +1,23 @@
+# coding=utf8
+__author__ = 'Zhong.PengQun'
+
+from setuptools import setup
+
+setup(name='flask_doc_z',
+      version='0.0.2',
+      description='Simple Flask API doc generator，forked from https://github.com/ipconfiger/flask-apidoc',
+      url='',
+      author='Zhong.PengQun',
+      author_email='',
+      license='',
+      packages=['flask_doc_z'],
+      package_data = {
+            "flask_doc": ["*.html", "*.gif"],
+      },
+      install_requires=[
+          'flask','markdown'
+      ],
+      entry_points = {
+        'console_scripts': ['fkapidoc=flask_doc.generator:main'],
+      },
+      zip_safe=False)
