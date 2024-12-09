d40ae9b8c367a2919ddc9798cdfb5cddb3b9b3ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 04:04:29 +0000

    Add package bioversions/0.6.0/bioversions-0.6.0.tar.gz

---
+++ packages/bioversions/bioversions-0.6.0.tar.gz/bioversions-0.6.0/src/bioversions/sources/mesh.py
@@ -0,0 +1,32 @@
+"""A getter for MeSH."""
+
+import ftplib
+
+from bioversions.utils import Getter, VersionType
+
+__all__ = [
+    "MeshGetter",
+]
+
+
+class MeshGetter(Getter):
+    """A getter for MeSH."""
+
+    bioregistry_id = "mesh"
+    name = "MeSH"
+    homepage_fmt = "ftp://nlmpubs.nlm.nih.gov/online/mesh/{version}"
+    version_type = VersionType.year
+
+    def get(self):
+        """Get the latest MeSH version number."""
+        with ftplib.FTP("nlmpubs.nlm.nih.gov") as ftp:
+            ftp.login()
+            ftp.cwd("/online/mesh/MESH_FILES/xmlmesh/")
+            for name, _ in ftp.mlsd():
+                if name.startswith("desc") and name.endswith(".gz"):
+                    return name[len("desc") : -len(".gz")]
+        raise ValueError
+
+
+if __name__ == "__main__":
+    MeshGetter.print()
