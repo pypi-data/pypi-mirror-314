c4aa1e9a443b2b0a760a121a693e84c787c550ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 03:20:27 +0000

    Add package PyPackIT-Test1-TestSuite/0.0.10/PyPackIT_Test1_TestSuite-0.0.10-py3-none-any.whl

---
+++ packages/PyPackIT-Test1-TestSuite/PyPackIT_Test1_TestSuite-0.0.10-py3-none-any.whl/pypackit_test1_testsuite/data/config/__init__.py
@@ -0,0 +1,5 @@
+# This is added due to this mypy issue: https://github.com/python/mypy/issues/18128
+# ruff: noqa: D104
+
+# PyPackIT-Test1-TestSuite © 2024 Armin Ariamajd
+# SPDX-License-Identifier: MIT
