98658a2e74256b10b5ff657fd6c629c79890c16f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 03:20:10 +0000

    Add package stayforge/0.0.0/stayforge-0.0.0-py3-none-any.whl

---
+++ packages/stayforge/stayforge-0.0.0-py3-none-any.whl/stayforge/apis/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+
+# import apis into api package
+from apis.branches_api import BranchesApi
+from apis.key_manager_api import KeyManagerApi
+from apis.orders_api import OrdersApi
+from apis.room_types_api import RoomTypesApi
+from apis.rooms_api import RoomsApi
+from apis.webhooks_manager_api import WebhooksManagerApi
+
