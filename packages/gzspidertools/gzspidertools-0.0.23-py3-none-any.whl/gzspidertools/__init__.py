997539be6e47562ee2c7dc7a9f2e752538c1048f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 15:04:59 +0000

    Add package gzspidertools/0.0.23/gzspidertools-0.0.23-py3-none-any.whl

---
+++ packages/gzspidertools/gzspidertools-0.0.23-py3-none-any.whl/gzspidertools/__init__.py
@@ -0,0 +1,16 @@
+from gzspidertools.config import logger
+from gzspidertools.items import AyuItem
+from gzspidertools.scraper.http.request.aiohttp import AiohttpRequest
+from gzspidertools.scraper.spiders import AyuSpider
+from gzspidertools.scraper.spiders.crawl import AyuCrawlSpider
+
+__all__ = [
+    "AiohttpRequest",
+    "AyuItem",
+    "AyuSpider",
+    "AyuCrawlSpider",
+    "logger",
+    "__version__",
+]
+
+__version__ = "0.0.23"
