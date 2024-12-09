e9896d1d631024eab5ea6fb9bd624cbe215e7bca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 15:04:53 +0000

    Add package Tloop/0.0.1/tloop-0.0.1.tar.gz

---
+++ packages/Tloop/tloop-0.0.1.tar.gz/tloop-0.0.1/Tloop/__init__.py
@@ -0,0 +1,11 @@
+from multiprocessing import Process
+
+def Tloop(target_func, thr, *args):
+    processes = []
+    for _ in range(thr):
+        process = Process(target=target_func, args=args)
+        processes.append(process)
+        process.start()
+
+    for process in processes:
+        process.join()
