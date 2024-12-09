3b2ed1dff510d9c4d6eb7675b9f27b76a1c4984e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 14:04:54 +0000

    Add package rcquant-sdk/0.0.241209.11/rcquant_sdk-0.0.241209.11-py3-none-any.whl

---
+++ packages/rcquant-sdk/rcquant_sdk-0.0.241209.11-py3-none-any.whl/rcquant_sdk/packer/market/fin_persist_read_param_data_packer.py
@@ -0,0 +1,31 @@
+from ...interface import IPacker
+from ...data.market.fin_persist_filed_data import FinPersistFiledData
+
+
+class FinPersistReadParamDataPacker(IPacker):
+    def __init__(self, obj) -> None:
+        super().__init__(obj)
+
+    def obj_to_tuple(self):
+        tempfileds = []
+        for filed in self._obj.DataFileds:
+            tempfileds.append(filed.obj_to_tuple())
+
+        return [str(self._obj.InstrumentID), str(self._obj.Period), int(self._obj.StartDate),
+                int(self._obj.EndDate), tempfileds, str(self._obj.BasePath), bool(self._obj.IsReadByte)]
+
+    def tuple_to_obj(self, t):
+        if len(t) >= 7:
+            self._obj.InstrumentID = t[0]
+            self._obj.Period = t[1]
+            self._obj.StartDate = t[2]
+            self._obj.EndDate = t[3]
+            for temp in t[4]:
+                fd = FinPersistFiledData()
+                fd.tuple_to_obj(temp)
+                self._obj.DataFileds.append(fd)
+            self._obj.BathPath = t[5]
+            self._obj.IsReadByte = t[6]
+
+            return True
+        return False
