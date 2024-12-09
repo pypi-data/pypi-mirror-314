1666a83f730a4db4d349cde577b7b910bb5e47cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 03:20:51 +0000

    Add package functional-parsing-library/0.0.21/functional_parsing_library-0.0.21.tar.gz

---
+++ packages/functional-parsing-library/functional_parsing_library-0.0.21.tar.gz/functional_parsing_library-0.0.21/functional_parsing_library/combinators/sequence/many_till.py
@@ -0,0 +1,16 @@
+from functional_parsing_library.combinators.sequence.many import many
+from functional_parsing_library.parser import Parser, U, S
+
+
+def many_till(parser: Parser[U], until: Parser[S]) -> Parser[list[U]]:
+    """
+    Parses one or more matches for parser, followed by a match for until. Consumes the match for until.
+    For example,
+    >>> from functional_parsing_library.strings import char
+    >>> parser = many_till(char('a'), char('b'))
+    >>> parser('aab').result
+    ['a', 'a']
+    >>> parser('aab').remainder
+    ''
+    """
+    return many(parser) < until
