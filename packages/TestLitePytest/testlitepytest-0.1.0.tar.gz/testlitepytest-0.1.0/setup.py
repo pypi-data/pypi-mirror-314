0daf23b51d06277ebb0b8e098af67fb20cb2c281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 03:19:45 +0000

    Add package TestLitePytest/0.1.0/testlitepytest-0.1.0.tar.gz

---
+++ packages/TestLitePytest/testlitepytest-0.1.0.tar.gz/testlitepytest-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='TestLitePytest',  # Replace with your package’s name
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.32.3',
+        'pytest>=8.3.2'
+    ],
+    author='Dmitry Skryabin',  
+    author_email='skryabind98@gmail.com',
+    description='Pytest adaptor for TestLite TMS system',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # License type
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+
+)
