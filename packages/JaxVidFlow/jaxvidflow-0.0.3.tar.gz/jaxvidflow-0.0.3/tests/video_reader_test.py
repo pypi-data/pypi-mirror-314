42ffec0973da9f4a42fd86fb0ee495b17a994c67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 15:04:53 +0000

    Add package JaxVidFlow/0.0.3/jaxvidflow-0.0.3.tar.gz

---
+++ packages/JaxVidFlow/jaxvidflow-0.0.3.tar.gz/jaxvidflow-0.0.3/tests/video_reader_test.py
@@ -0,0 +1,30 @@
+import fractions
+import os
+import platform
+import sys
+
+import jax
+from jax import numpy as jnp
+import numpy as np
+import pytest
+
+sys.path.append('.')
+
+from JaxVidFlow import video_reader
+
+def test_video_reader():
+  reader = video_reader.VideoReader('test_files/lionfish.mp4')
+  assert reader.width() == 5120
+  assert reader.height() == 3840
+  assert reader.frame_rate() == fractions.Fraction(25, 1)
+  assert reader.num_frames() == 203
+  assert reader.duration() == pytest.approx(8.12, 0.1)
+  reader.seek(4.3)
+  frame = next(reader)
+  assert frame.frame_time == pytest.approx(4.3, 0.1)
+  reader.seek(6.1)
+  frame = next(reader)
+  assert frame.frame_time == pytest.approx(6.1, 0.1)
+  reader.seek(0.5)
+  frame = next(reader)
+  assert frame.frame_time == pytest.approx(0.5, 0.1)
