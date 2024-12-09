2e79341b03a2cd197587d5dcdb48cfe3255c9449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 16:04:36 +0000

    Add package PPIFold/0.0.3/ppifold-0.0.3.tar.gz

---
+++ packages/PPIFold/ppifold-0.0.3.tar.gz/ppifold-0.0.3/setup.py
@@ -0,0 +1,20 @@
+"""Install script for setuptools."""
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='PPIFold',
+    version='0.0.3',
+    description=(
+        'Pipeline that automates AlphaPulldown'
+    ),
+    author='Quentin Rouger',
+    author_email='quentin.rouger@univ-rennes.fr',
+    license='GPL-3.0 license',
+    url='https://github.com/Qrouger/PPIFold',
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=[
+        'alphapulldown'
+    ]
+)
