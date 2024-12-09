0210ebcc21b8bb8ebef8751628c5fb36b8855de2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 14:04:06 +0000

    Add package auto-chart-patterns/0.1.2/auto_chart_patterns-0.1.2.tar.gz

---
+++ packages/auto-chart-patterns/auto_chart_patterns-0.1.2.tar.gz/auto_chart_patterns-0.1.2/tests/integration/conftest.py
@@ -0,0 +1,31 @@
+import pytest
+from .helpers import scan_properties_fp, zigzag_fp, scan_properties_tl, zigzag_tl, \
+    scan_properties_rp, zigzag_rp
+
+@pytest.fixture
+def tickers():
+    return ["BTCUSDT", "SOLUSDT", "ETHUSDT", "AVAXUSDT", "DOGEUSDT", "SHIBUSDT"]
+
+@pytest.fixture
+def default_properties_flags_pennants():
+    return scan_properties_fp
+
+@pytest.fixture
+def zigzag_flags_pennants():
+    return zigzag_fp
+
+@pytest.fixture
+def default_properties_trend_lines():
+    return scan_properties_tl
+
+@pytest.fixture
+def zigzag_trend_lines():
+    return zigzag_tl
+
+@pytest.fixture
+def default_properties_reversals():
+    return scan_properties_rp
+
+@pytest.fixture
+def zigzag_reversals():
+    return zigzag_rp
