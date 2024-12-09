c0479f3ff93c68f735ff050ffa7306c5f7339c49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 16:04:23 +0000

    Add package discord-ext-ipcx/0.4.0/discord_ext_ipcx-0.4.0-py3-none-any.whl

---
+++ packages/discord-ext-ipcx/discord_ext_ipcx-0.4.0-py3-none-any.whl/discord/ext/ipcx/__init__.py
@@ -0,0 +1,37 @@
+"""
+Discord.py IPC Extension
+~~~~~~~~~~~~~~~~~~~~~~~~
+
+A upgraded version of discord-ext-ipc that now works with discord.py v2.
+
+:copyright: (c) 2020-present Ext-Creators, No767
+:license: Apache-2.0, see LICENSE for more details
+"""
+
+__title__ = "discord-ext-ipcx"
+__author__ = "Ext-Creators, No767"
+__license__ = "Apache-2.0"
+__copyright__ = "(c) 2020-present Ext-Creators, No767"
+__version__ = "0.4.0"
+
+from typing import Literal, NamedTuple
+
+from .client import Client as Client
+from .server import Server as Server, route as route
+
+
+class VersionInfo(NamedTuple):
+    major: int
+    minor: int
+    micro: int
+    releaselevel: Literal["alpha", "beta", "final"]
+
+    def __str__(self) -> str:
+        return f"{self.major}.{self.minor}.{self.micro}-{self.releaselevel}"
+
+
+version_info = VersionInfo(major=0, minor=4, micro=0, releaselevel="final")
+
+# Since this class is always exported, we just want to delete it at the end
+# So others can't use it
+del VersionInfo, Literal, NamedTuple
---
+++ packages/discord-ext-ipcx/discord_ext_ipcx-0.4.0-py3-none-any.whl/discord/ext/ipcx/client.py
@@ -0,0 +1,168 @@
+from __future__ import annotations
+
+import asyncio
+import logging
+from typing import TYPE_CHECKING, Any, Optional
+
+import aiohttp
+
+from .errors import NotConnectedError
+
+if TYPE_CHECKING:
+    from types import TracebackType
+
+    from typing_extensions import Self
+
+log = logging.getLogger(__name__)
+
+
+class Client:
+    """
+    Handles webserver side requests to the bot process.
+
+    Operations with ``async with`` will automatically initialize the client and automatically cleans up.
+
+    Parameters
+    ----------
+    host: str
+        The IP or host of the IPC server, defaults to localhost
+    Optional[port]: int
+        The port of the IPC server. If not supplied the port will be found automatically, defaults to None
+    multicast_port: Optional[int]
+        The mutlicast post of the IPC server. If not supplied, the port used will be 20000.
+    secret_key: Union[str, bytes]
+        The secret key for your IPC server. Must match the server secret_key or requests will not go ahead, defaults to None
+    """
+
+    def __init__(
+        self,
+        host: str = "localhost",
+        port: Optional[int] = None,
+        multicast_port: int = 20000,
+        secret_key: Optional[str] = None,
+    ):
+        self.secret_key = secret_key
+
+        self.host = host
+        self.port = port
+        self.multicast_port = multicast_port
+
+        self.session: Optional[aiohttp.ClientSession] = None
+
+    @property
+    def url(self):
+        return "ws://{0.host}:{1}".format(
+            self, self.port if self.port else self.multicast_port
+        )
+
+    async def __aenter__(self) -> Self:
+        await self._get_session()
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: Optional[type[BaseException]],
+        exc_value: Optional[BaseException],
+        traceback: Optional[TracebackType],
+    ) -> None:
+        await self.close()
+
+    async def _get_session(self) -> aiohttp.ClientSession:
+        if not self.session:
+            self.session = aiohttp.ClientSession()
+        return self.session
+
+    async def _get_port(self) -> int:
+        if not self.port:
+            log.debug(
+                "No port was provided - initiating multicast connection at %s.",
+                self.url,
+            )
+            session = await self._get_session()
+            async with session.ws_connect(self.url, autoping=False) as multicast:
+                payload = {
+                    "connect": True,
+                    "headers": {"Authorization": self.secret_key},
+                }
+
+                await multicast.send_json(payload)
+                recv = await multicast.receive()
+
+                log.debug("Multicast Server > %r", recv)
+
+                if recv.type in (
+                    aiohttp.WSMsgType.CLOSE,
+                    aiohttp.WSMsgType.CLOSED,
+                ):
+                    log.error(
+                        "WebSocket connection unexpectedly closed. Multicast Server is unreachable."
+                    )
+                    raise NotConnectedError("Multicast server connection failed.")
+
+                port_data = recv.json()
+                self.port = port_data["port"]
+
+        return self.port
+
+    async def close(self) -> None:
+        """Properly closes the :class:`aiohttp.ClientSession` session used for connections
+
+        .. warning::
+
+            This is required in order to clean up any remaining connections held in :class:`aiohttp.ClientSession`.
+            Without doing so, your webserver will complain about having an unclosed client session, which is the result
+            of not closing it manually.
+        """
+        if self.session:
+            await self.session.close()
+
+    async def request(self, endpoint: str, **kwargs) -> Any:
+        """Make a request to the IPC server process.
+
+        Parameters
+        ----------
+        endpoint: str
+            The endpoint to request on the server
+        **kwargs
+            The data to send to the endpoint
+        """
+        log.info("Requesting IPC Server for %r with %r", endpoint, kwargs)
+        if not self.port:
+            self.port = await self._get_port()
+
+        session = await self._get_session()
+        async with session.ws_connect(
+            self.url, autoping=False, autoclose=False
+        ) as websocket:
+            payload = {
+                "endpoint": endpoint,
+                "data": kwargs,
+                "headers": {"Authorization": self.secret_key},
+            }
+
+            await websocket.send_json(payload)
+
+            recv = await websocket.receive()
+
+            log.debug("Client < %r", recv)
+
+            if recv.type == aiohttp.WSMsgType.PING:
+                log.info("Received request to PING")
+                await websocket.ping()
+
+                return await self.request(endpoint, **kwargs)
+
+            if recv.type == aiohttp.WSMsgType.PONG:
+                log.info("Received PONG")
+                return await self.request(endpoint, **kwargs)
+
+            if recv.type == aiohttp.WSMsgType.CLOSED:
+                log.error(
+                    "WebSocket connection unexpectedly closed. IPC Server is unreachable. Attempting reconnection in 5 seconds."
+                )
+
+                await asyncio.sleep(5)
+
+                return await self.request(endpoint, **kwargs)
+
+            return recv.json()
