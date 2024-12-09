aacafbee6af9fe8fc4a647a53113971252426a7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 03:19:13 +0000

    Add package hg-cpp/0.0.4/hg-cpp-0.0.4.tar.gz

---
+++ packages/hg-cpp/hg-cpp-0.0.4.tar.gz/hg-cpp-0.0.4/include/hgraph/python/pyb.h
@@ -0,0 +1,8 @@
+#ifndef HGRAPH_PYB_H
+#define HGRAPH_PYB_H
+
+#include <nanobind/nanobind.h>
+
+namespace nb = nanobind;
+
+#endif // HGRAPH_PYB_H
---
+++ packages/hg-cpp/hg-cpp-0.0.4.tar.gz/hg-cpp-0.0.4/src/cpp/python/_hgraph_types.cpp
@@ -0,0 +1,9 @@
+
+#include <hgraph/python/pyb_wiring.h>
+
+void export_types(nb::module_ &m) {
+    // using namespace hgraph;
+
+    // node_type_enum_py_register(m);
+    // injectable_type_enum(m);
+}
