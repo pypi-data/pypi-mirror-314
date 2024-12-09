ae1935acee3559a424a165afced51fb3e763e9f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 16:04:27 +0000

    Add package mindstudio-probe/1.1.1/mindstudio_probe-1.1.1-py3-none-any.whl

---
+++ packages/mindstudio-probe/mindstudio_probe-1.1.1-py3-none-any.whl/msprobe/pytorch/api_accuracy_checker/compare/compare_column.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (c) 2024-2024, Huawei Technologies Co., Ltd.
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0  (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from msprobe.core.common.const import CompareConst
+
+
+class CompareColumn:
+    def __init__(self):
+        self.bench_type = CompareConst.SPACE
+        self.npu_type = CompareConst.SPACE
+        self.shape = CompareConst.SPACE
+        self.cosine_sim = CompareConst.SPACE
+        self.max_abs_err = CompareConst.SPACE
+        self.rel_err_hundredth = CompareConst.SPACE
+        self.rel_err_thousandth = CompareConst.SPACE
+        self.rel_err_ten_thousandth = CompareConst.SPACE
+        self.error_rate = CompareConst.SPACE
+        self.eb = CompareConst.SPACE
+        self.rmse = CompareConst.SPACE
+        self.small_value_err_ratio = CompareConst.SPACE
+        self.max_rel_error = CompareConst.SPACE
+        self.mean_rel_error = CompareConst.SPACE
+        self.inf_nan_error_ratio = CompareConst.SPACE
+        self.rel_err_ratio = CompareConst.SPACE
+        self.abs_err_ratio = CompareConst.SPACE
+        self.max_ulp_error = CompareConst.SPACE
+        self.mean_ulp_error = CompareConst.SPACE
+        self.ulp_error_proportion = CompareConst.SPACE
+
+    def to_column_value(self, is_pass, message):
+        return [self.bench_type, self.npu_type, self.shape, self.cosine_sim, self.max_abs_err, self.rel_err_hundredth,
+                self.rel_err_thousandth, self.rel_err_ten_thousandth, self.error_rate, self.eb, self.rmse, 
+                self.small_value_err_ratio, self.max_rel_error, self.mean_rel_error, self.inf_nan_error_ratio, 
+                self.rel_err_ratio, self.abs_err_ratio, self.max_ulp_error, self.mean_ulp_error, 
+                self.ulp_error_proportion, is_pass, message]
+
+
+class ApiPrecisionOutputColumn:
+    def __init__(self):
+        self.api_name = CompareConst.SPACE
+        self.small_value_err_ratio = CompareConst.SPACE
+        self.small_value_err_status = CompareConst.SPACE
+        self.rmse_ratio = CompareConst.SPACE
+        self.rmse_status = CompareConst.SPACE
+        self.max_rel_err_ratio = CompareConst.SPACE
+        self.max_rel_err_status = CompareConst.SPACE
+        self.mean_rel_err_ratio = CompareConst.SPACE
+        self.mean_rel_err_status = CompareConst.SPACE
+        self.eb_ratio = CompareConst.SPACE
+        self.eb_status = CompareConst.SPACE
+        self.inf_nan_error_ratio = CompareConst.SPACE
+        self.inf_nan_error_ratio_status = CompareConst.SPACE
+        self.rel_err_ratio = CompareConst.SPACE
+        self.rel_err_ratio_status = CompareConst.SPACE
+        self.abs_err_ratio = CompareConst.SPACE
+        self.abs_err_ratio_status = CompareConst.SPACE
+        self.error_rate = CompareConst.SPACE
+        self.error_rate_status = CompareConst.SPACE
+        self.mean_ulp_err = CompareConst.SPACE
+        self.ulp_err_proportion = CompareConst.SPACE
+        self.ulp_err_proportion_ratio = CompareConst.SPACE
+        self.ulp_err_status = CompareConst.SPACE
+        self.rel_err_thousandth = CompareConst.SPACE
+        self.rel_err_thousandth_status = CompareConst.SPACE
+        self.compare_result = CompareConst.SPACE
+        self.compare_algorithm = CompareConst.SPACE
+        self.compare_message = CompareConst.SPACE
+
+    def to_column_value(self):
+        return [self.api_name, self.small_value_err_ratio, self.small_value_err_status, self.rmse_ratio, 
+                self.rmse_status, self.max_rel_err_ratio, self.max_rel_err_status, self.mean_rel_err_ratio, 
+                self.mean_rel_err_status, self.eb_ratio, self.eb_status, self.inf_nan_error_ratio, 
+                self.inf_nan_error_ratio_status, self.rel_err_ratio, self.rel_err_ratio_status, self.abs_err_ratio, 
+                self.abs_err_ratio_status, self.error_rate, self.error_rate_status, self.mean_ulp_err, 
+                self.ulp_err_proportion, self.ulp_err_proportion_ratio, self.ulp_err_status, self.rel_err_thousandth, 
+                self.rel_err_thousandth_status, self.compare_result, self.compare_algorithm, self.compare_message]
+        
