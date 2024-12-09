ea8e29706052d8526e04fa1c83900a8722810ae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 04:04:29 +0000

    Add package lightning-pose/1.6.1/lightning_pose-1.6.1.tar.gz

---
+++ packages/lightning-pose/lightning_pose-1.6.1.tar.gz/lightning_pose-1.6.1/lightning_pose/train.py
@@ -0,0 +1,343 @@
+"""Example model training function."""
+import copy
+import os
+import random
+import shutil
+import sys
+import warnings
+from pathlib import Path
+
+import lightning.pytorch as pl
+import numpy as np
+import torch
+from omegaconf import DictConfig, ListConfig, OmegaConf, open_dict
+from typeguard import typechecked
+
+from lightning_pose.utils import pretty_print_cfg, pretty_print_str
+from lightning_pose.utils.cropzoom import generate_cropped_labeled_frames, generate_cropped_video
+from lightning_pose.utils.io import (
+    check_video_paths,
+    ckpt_path_from_base_path,
+    return_absolute_data_paths,
+    return_absolute_path,
+)
+from lightning_pose.utils.predictions import (
+    export_predictions_and_labeled_video,
+    load_model_from_checkpoint,
+    predict_dataset,
+)
+from lightning_pose.utils.scripts import (
+    calculate_train_batches,
+    compute_metrics,
+    get_callbacks,
+    get_data_module,
+    get_dataset,
+    get_imgaug_transform,
+    get_loss_factories,
+    get_model,
+)
+
+# to ignore imports for sphix-autoapidoc
+__all__ = ["train"]
+
+
+@typechecked
+def train(cfg: DictConfig) -> None:
+
+    # reset all seeds
+    seed = 0
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+    # record lightning-pose version
+    from lightning_pose import __version__ as lightning_pose_version
+    with open_dict(cfg):
+        cfg.model.lightning_pose_version = lightning_pose_version
+
+    print("Our Hydra config file:")
+    pretty_print_cfg(cfg)
+
+    # path handling for toy data
+    data_dir, video_dir = return_absolute_data_paths(data_cfg=cfg.data)
+
+    # ----------------------------------------------------------------------------------
+    # Set up data/model objects
+    # ----------------------------------------------------------------------------------
+
+    # imgaug transform
+    imgaug_transform = get_imgaug_transform(cfg=cfg)
+
+    # dataset
+    dataset = get_dataset(cfg=cfg, data_dir=data_dir, imgaug_transform=imgaug_transform)
+
+    # datamodule; breaks up dataset into train/val/test
+    data_module = get_data_module(cfg=cfg, dataset=dataset, video_dir=video_dir)
+
+    # build loss factory which orchestrates different losses
+    loss_factories = get_loss_factories(cfg=cfg, data_module=data_module)
+
+    # model
+    model = get_model(cfg=cfg, data_module=data_module, loss_factories=loss_factories)
+
+    # ----------------------------------------------------------------------------------
+    # Save configuration in output directory
+    # ----------------------------------------------------------------------------------
+    # Done before training; files will exist even if script dies prematurely.
+    hydra_output_directory = os.getcwd()
+    print(f"Hydra output directory: {hydra_output_directory}")
+
+    # save config file
+    dest_config_file = Path(hydra_output_directory) / "config.yaml"
+    OmegaConf.save(config=cfg, f=dest_config_file, resolve=True)
+
+    # save labeled data file(s)
+    if isinstance(cfg.data.csv_file, str):
+        # single view
+        csv_files = [cfg.data.csv_file]
+    else:
+        # multi view
+        assert isinstance(cfg.data.csv_file, ListConfig)
+        csv_files = cfg.data.csv_file
+    for csv_file in csv_files:
+        src_csv_file = Path(csv_file)
+        if not src_csv_file.is_absolute():
+            src_csv_file = Path(data_dir) / src_csv_file
+
+        dest_csv_file = Path(hydra_output_directory) / src_csv_file.name
+        shutil.copyfile(src_csv_file, dest_csv_file)
+
+    # ----------------------------------------------------------------------------------
+    # Set up and run training
+    # ----------------------------------------------------------------------------------
+
+    # logger
+    logger = pl.loggers.TensorBoardLogger("tb_logs", name=cfg.model.model_name)
+    # Log hydra config to tensorboard as helpful metadata.
+    for key, value in cfg.items():
+        logger.experiment.add_text(
+            "hydra_config_%s" % key, "```\n%s```" % OmegaConf.to_yaml(value)
+        )
+
+    # early stopping, learning rate monitoring, model checkpointing, backbone unfreezing
+    callbacks = get_callbacks(
+        cfg,
+        early_stopping=cfg.training.get("early_stopping", False),
+        lr_monitor=True,
+        ckpt_every_n_epochs=cfg.training.get("ckpt_every_n_epochs", None)
+    )
+
+    # calculate number of batches for both labeled and unlabeled data per epoch
+    limit_train_batches = calculate_train_batches(cfg, dataset)
+
+    # set up trainer
+
+    # Old configs may have num_gpus: 0. We will remove support in a future release.
+    if cfg.training.num_gpus == 0:
+        warnings.warn(
+            "Config contains unsupported value num_gpus: 0. "
+            "Update num_gpus to 1 in your config."
+        )
+    cfg.training.num_gpus = max(cfg.training.num_gpus, 1)
+
+    trainer = pl.Trainer(
+        accelerator="gpu",
+        devices=cfg.training.num_gpus,
+        max_epochs=cfg.training.max_epochs,
+        min_epochs=cfg.training.min_epochs,
+        check_val_every_n_epoch=min(
+            cfg.training.check_val_every_n_epoch,
+            cfg.training.max_epochs,  # for debugging or otherwise training for a short time
+        ),
+        log_every_n_steps=cfg.training.log_every_n_steps,
+        callbacks=callbacks,
+        logger=logger,
+        limit_train_batches=limit_train_batches,
+        accumulate_grad_batches=cfg.training.get("accumulate_grad_batches", 1),
+        profiler=cfg.training.get("profiler", None),
+        sync_batchnorm=True,
+    )
+
+    # train model!
+    trainer.fit(model=model, datamodule=data_module)
+
+    # When devices > 0, lightning creates a process per device.
+    # Kill processes other than the main process, otherwise they all go forward.
+    if not trainer.is_global_zero:
+        sys.exit(0)
+
+    # ----------------------------------------------------------------------------------
+    # Post-training analysis
+    # ----------------------------------------------------------------------------------
+    # get best ckpt
+    best_ckpt = ckpt_path_from_base_path(
+            base_path=hydra_output_directory, model_name=cfg.model.model_name
+        )
+    print(f"Best checkpoint: {os.path.basename(best_ckpt)}")
+    # check if best_ckpt is a file
+    if not os.path.isfile(best_ckpt):
+        raise FileNotFoundError("Cannot find checkpoint. Have you trained for too few epochs?")
+
+    # make unaugmented data_loader if necessary
+    if cfg.training.imgaug != "default":
+        cfg_pred = copy.deepcopy(cfg)
+        cfg_pred.training.imgaug = "default"
+        imgaug_transform_pred = get_imgaug_transform(cfg=cfg_pred)
+        dataset_pred = get_dataset(
+            cfg=cfg_pred, data_dir=data_dir, imgaug_transform=imgaug_transform_pred
+        )
+        data_module_pred = get_data_module(cfg=cfg_pred, dataset=dataset_pred, video_dir=video_dir)
+        data_module_pred.setup()
+    else:
+        data_module_pred = data_module
+
+    model = load_model_from_checkpoint(
+        cfg=cfg,
+        ckpt_file=best_ckpt,
+        eval=True,
+        data_module=data_module_pred,
+    )
+
+    # ----------------------------------------------------------------------------------
+    # predict on all labeled frames (train/val/test)
+    # ----------------------------------------------------------------------------------
+    # Rebuild trainer with devices=1 for prediction. Training flags not needed.
+    trainer = pl.Trainer(accelerator="gpu", devices=1)
+    pretty_print_str("Predicting train/val/test images...")
+    # compute and save frame-wise predictions
+    preds_file = os.path.join(hydra_output_directory, "predictions.csv")
+    predict_dataset(
+        cfg=cfg,
+        trainer=trainer,
+        model=model,
+        data_module=data_module_pred,
+        preds_file=preds_file,
+    )
+    # compute and save various metrics
+    # for multiview, predict_dataset outputs one pred file per view.
+    multiview_pred_files = [
+        str(Path(hydra_output_directory) / p)
+        for p in Path(hydra_output_directory).glob("predictions_*.csv")
+    ]
+    if len(multiview_pred_files) > 0:
+        preds_file = multiview_pred_files
+    compute_metrics(cfg=cfg, preds_file=preds_file, data_module=data_module_pred)
+
+    is_detector = (
+        cfg.get("detector") is not None and cfg.detector.get("crop_ratio") is not None
+    )
+    if is_detector:
+        generate_cropped_labeled_frames(
+            root_directory=Path(data_dir),
+            output_directory=Path(hydra_output_directory),
+            detector_cfg=cfg.detector,
+        )
+
+    # ----------------------------------------------------------------------------------
+    # predict folder of videos
+    # ----------------------------------------------------------------------------------
+    if cfg.eval.predict_vids_after_training or is_detector:
+        pretty_print_str("Predicting videos...")
+        if cfg.eval.test_videos_directory is None:
+            filenames = []
+        else:
+            filenames = check_video_paths(return_absolute_path(cfg.eval.test_videos_directory))
+            vidstr = "video" if (len(filenames) == 1) else "videos"
+            pretty_print_str(
+                f"Found {len(filenames)} {vidstr} to predict (in cfg.eval.test_videos_directory)"
+            )
+
+        for video_file in filenames:
+            assert os.path.isfile(video_file)
+
+            pretty_print_str(f"Predicting video: {video_file}...")
+            # get save name for prediction csv file
+            video_pred_dir = os.path.join(hydra_output_directory, "video_preds")
+            video_pred_name = os.path.splitext(os.path.basename(video_file))[0]
+            prediction_csv_file = os.path.join(video_pred_dir, video_pred_name + ".csv")
+            # get save name labeled video csv
+            if cfg.eval.save_vids_after_training:
+                labeled_vid_dir = os.path.join(video_pred_dir, "labeled_videos")
+                labeled_mp4_file = os.path.join(labeled_vid_dir, video_pred_name + "_labeled.mp4")
+            else:
+                labeled_mp4_file = None
+            # predict on video
+            export_predictions_and_labeled_video(
+                video_file=video_file,
+                cfg=cfg,
+                prediction_csv_file=prediction_csv_file,
+                labeled_mp4_file=labeled_mp4_file,
+                trainer=trainer,
+                model=model,
+                data_module=data_module_pred,
+                save_heatmaps=cfg.eval.get(
+                    "predict_vids_after_training_save_heatmaps", False
+                ),
+            )
+
+            compute_metrics(
+                cfg=cfg,
+                preds_file=prediction_csv_file,
+                data_module=data_module_pred,
+            )
+
+            if is_detector:
+                generate_cropped_video(
+                    video_path=Path(video_file),
+                    detector_model_dir=Path(hydra_output_directory),
+                    detector_cfg=cfg.detector,
+                )
+
+    # ----------------------------------------------------------------------------------
+    # predict on OOD frames
+    # ----------------------------------------------------------------------------------
+    # update config file to point to OOD data
+    if isinstance(cfg.data.csv_file, list) or isinstance(cfg.data.csv_file, ListConfig):
+        csv_file_ood = []
+        for csv_file in cfg.data.csv_file:
+            csv_file_ood.append(
+                os.path.join(cfg.data.data_dir, csv_file).replace(".csv", "_new.csv"))
+    else:
+        csv_file_ood = os.path.join(
+            cfg.data.data_dir, cfg.data.csv_file).replace(".csv", "_new.csv")
+
+    if (isinstance(csv_file_ood, str) and os.path.exists(csv_file_ood)) \
+            or (isinstance(csv_file_ood, list) and os.path.exists(csv_file_ood[0])):
+        cfg_ood = cfg.copy()
+        cfg_ood.data.csv_file = csv_file_ood
+        cfg_ood.training.imgaug = "default"
+        cfg_ood.training.train_prob = 1
+        cfg_ood.training.val_prob = 0
+        cfg_ood.training.train_frames = 1
+        # build dataset/datamodule
+        imgaug_transform_ood = get_imgaug_transform(cfg=cfg_ood)
+        dataset_ood = get_dataset(
+            cfg=cfg_ood, data_dir=data_dir, imgaug_transform=imgaug_transform_ood
+        )
+        data_module_ood = get_data_module(cfg=cfg_ood, dataset=dataset_ood, video_dir=video_dir)
+        data_module_ood.setup()
+        pretty_print_str("Predicting OOD images...")
+        # compute and save frame-wise predictions
+        preds_file_ood = os.path.join(hydra_output_directory, "predictions_new.csv")
+        predict_dataset(
+            cfg=cfg_ood,
+            trainer=trainer,
+            model=model,
+            data_module=data_module_ood,
+            preds_file=preds_file_ood,
+        )
+        # compute and save various metrics
+        try:
+            # take care of multiview case, where multiple csv files have been saved
+            preds_files = [
+                os.path.join(hydra_output_directory, path) for path in
+                os.listdir(hydra_output_directory) if path.startswith("predictions_new")
+            ]
+            if len(preds_files) > 1:
+                preds_file_ood = preds_files
+            compute_metrics(cfg=cfg_ood, preds_file=preds_file_ood, data_module=data_module_ood)
+        except Exception as e:
+            print(f"Error computing metrics\n{e}")
---
+++ packages/lightning-pose/lightning_pose-1.6.1.tar.gz/lightning_pose-1.6.1/tests/utils/test_scripts.py
@@ -0,0 +1,208 @@
+"""Test the scripts module.
+
+Note that many of the functions in the scripts module are explicitly used (and therefore implicitly
+tested) in conftest.py
+
+"""
+
+import copy
+import os
+from unittest.mock import Mock
+
+import lightning.pytorch as pl
+import numpy as np
+import pytest
+from omegaconf import OmegaConf
+from omegaconf.errors import ValidationError
+
+from lightning_pose.data.datasets import BaseTrackingDataset
+from lightning_pose.utils.scripts import (
+    calculate_train_batches,
+    get_data_module,
+)
+
+
+def test_calculate_train_batches(cfg, base_dataset):
+    """Test the computation of train batches, which is a function of labeled and unlabeled info."""
+    cfg_tmp = copy.deepcopy(cfg)
+
+    # return value if set in config
+    for n in [2, 12, 22]:
+        cfg_tmp.training.limit_train_batches = n
+        n_batches = calculate_train_batches(cfg_tmp, base_dataset)
+        assert n_batches == n
+
+    # None with small number of train frames - return minimum of 10
+    cfg_tmp.training.limit_train_batches = None
+    cfg_tmp.training.train_frames = 2
+    n_batches = calculate_train_batches(cfg_tmp, base_dataset)
+    assert n_batches == 10
+
+    # None with large number of frames
+    n = 50
+    cfg_tmp.training.limit_train_batches = None
+    cfg_tmp.training.train_frames = n
+    cfg_tmp.training.train_batch_size = 1
+    n_batches = calculate_train_batches(cfg_tmp, base_dataset)
+    assert n_batches == n
+
+
+def _supervised_multi_gpu_cfg(cfg):
+    return OmegaConf.merge(
+        cfg,
+        OmegaConf.create(
+            {
+                "model": {
+                    "losses_to_use": [],
+                },
+                "training": {
+                    "num_gpus": 2,
+                    "train_batch_size": 4,
+                    "val_batch_size": 16,
+                    "test_batch_size": 16,
+                },
+            }
+        ),
+    )
+
+
+def _unsupervised_multi_gpu_cfg(cfg):
+    cfg = _supervised_multi_gpu_cfg(cfg)
+    cfg.model.losses_to_use = ["temporal"]  # trigger unsupervised datamodule
+    return cfg
+
+
+def test_get_data_module_num_gpus_0(cfg):
+    cfg = _supervised_multi_gpu_cfg(cfg)
+    # when num_gpus is set to 0, i.e. from a deprecated config
+    cfg.training.num_gpus = 0
+    data_module = get_data_module(cfg, Mock(spec=BaseTrackingDataset))
+
+    # assert num_gpus gets modified to 1
+    assert cfg.training.num_gpus == 1
+    # the rest of the behavior follows correctly
+    assert data_module.train_batch_size == cfg.training.train_batch_size
+    assert data_module.val_batch_size == cfg.training.val_batch_size
+    assert data_module.test_batch_size == cfg.training.test_batch_size
+
+
+def test_get_data_module_multi_gpu_batch_size_adjustment_supervised(cfg):
+    cfg = _supervised_multi_gpu_cfg(cfg)
+    data_module = get_data_module(cfg, Mock(spec=BaseTrackingDataset))
+    # train, val batch sizes should be divided by num_gpus
+    assert (
+        data_module.train_batch_size
+        == cfg.training.train_batch_size / cfg.training.num_gpus
+    )
+    assert (
+        data_module.val_batch_size
+        == cfg.training.val_batch_size / cfg.training.num_gpus
+    )
+    assert data_module.test_batch_size == cfg.training.test_batch_size
+
+
+def test_get_data_module_multi_gpu_batch_size_adjustment_unsupervised(
+    cfg, heatmap_dataset, toy_data_dir
+):
+    cfg = _unsupervised_multi_gpu_cfg(cfg)
+    data_module = get_data_module(
+        cfg, heatmap_dataset, os.path.join(toy_data_dir, "videos")
+    )
+    # train, val batch sizes should be divided by num_gpus
+    assert (
+        data_module.train_batch_size
+        == cfg.training.train_batch_size / cfg.training.num_gpus
+    )
+    assert (
+        data_module.val_batch_size
+        == cfg.training.val_batch_size / cfg.training.num_gpus
+    )
+    assert data_module.test_batch_size == cfg.training.test_batch_size
+
+    # sequence length should be divided by num_gups
+    assert (
+        data_module.dali_config.base.train.sequence_length
+        == cfg.dali.base.train.sequence_length / cfg.training.num_gpus
+    )
+
+    # context batch size is more nuance, tested separately
+
+
+def test_get_data_module_multi_gpu_batch_size_adjustment_ceiling(
+    cfg, heatmap_dataset, toy_data_dir
+):
+    cfg = _unsupervised_multi_gpu_cfg(cfg)
+    data_module = get_data_module(
+        cfg, heatmap_dataset, os.path.join(toy_data_dir, "videos")
+    )
+
+    # When batch_size is indivisible by 2
+    cfg.training.train_batch_size += 1
+    cfg.training.val_batch_size += 1
+    cfg.dali.base.train.sequence_length += 1
+    cfg.dali.context.train.batch_size += 1
+
+    data_module = get_data_module(
+        cfg, heatmap_dataset, os.path.join(toy_data_dir, "videos")
+    )
+
+    # batch size should be the ceiling of batch_size divided by num_gups
+    assert data_module.train_batch_size == int(
+        np.ceil(cfg.training.train_batch_size / cfg.training.num_gpus)
+    )
+    assert data_module.val_batch_size == int(
+        np.ceil(cfg.training.val_batch_size / cfg.training.num_gpus)
+    )
+    assert data_module.test_batch_size == cfg.training.test_batch_size
+
+    # sequence length should be divided by num_gups
+    assert data_module.dali_config.base.train.sequence_length == int(
+        np.ceil(cfg.dali.base.train.sequence_length / cfg.training.num_gpus)
+    )
+
+    # context batch size is more nuance, tested separately
+
+
+def test_get_data_module_multi_gpu_context_batch_size_adjustment(
+    cfg, heatmap_dataset, toy_data_dir
+):
+    cfg = _unsupervised_multi_gpu_cfg(cfg)
+    cfg.model.model_type = "heatmap_mhcrnn"
+    data_module = get_data_module(
+        cfg, heatmap_dataset, os.path.join(toy_data_dir, "videos")
+    )
+
+    # batch size of 6 -> effective 2 -> per-gpu effective 1 -> per-gpu 5
+    cfg.dali.context.train.batch_size = 6
+    data_module = get_data_module(
+        cfg, heatmap_dataset, os.path.join(toy_data_dir, "videos")
+    )
+    assert data_module.dali_config.context.train.batch_size == 5
+
+    # batch size of 5 -> effective 1 -> per-gpu effective 1 -> per-gpu 5
+    cfg.dali.context.train.batch_size = 5
+    data_module = get_data_module(
+        cfg, heatmap_dataset, os.path.join(toy_data_dir, "videos")
+    )
+    assert data_module.dali_config.context.train.batch_size == 5
+
+    # batch size of 28 -> effective 24 -> per-gpu effective 12 -> per-gpu 16
+    cfg.dali.context.train.batch_size = 28
+    data_module = get_data_module(
+        cfg, heatmap_dataset, os.path.join(toy_data_dir, "videos")
+    )
+    assert data_module.dali_config.context.train.batch_size == 16
+
+    # batch size of 27 -> effective 23 -> per-gpu effective 12 -> per-gpu 16
+    cfg.dali.context.train.batch_size = 27
+    data_module = get_data_module(
+        cfg, heatmap_dataset, os.path.join(toy_data_dir, "videos")
+    )
+    assert data_module.dali_config.context.train.batch_size == 16
+
+    # batch size of 4 -> effective 0 -> should throw an error.
+    cfg.dali.context.train.batch_size = 4
+    with pytest.raises(ValidationError):
+        data_module = get_data_module(
+            cfg, heatmap_dataset, os.path.join(toy_data_dir, "videos")
+        )
