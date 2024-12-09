c5d7efca931211181222fa91bd3d7446a3e222c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 04:04:48 +0000

    Add package crypto-connector/0.4.3/crypto_connector-0.4.3.tar.gz

---
+++ packages/crypto-connector/crypto_connector-0.4.3.tar.gz/crypto_connector-0.4.3/tests/account/test_get_transfer_history.py
@@ -0,0 +1,21 @@
+import pytest
+
+from crypto_connector.base.exchange import Exchange
+from crypto_connector.base.schemas import Transfer
+
+
+@pytest.mark.parametrize(
+    "exchange",
+    argvalues=[
+        ("Binance"),
+        ("HTX"),
+    ],
+    indirect=["exchange"],
+)
+def test_get_transfer_history(exchange: Exchange) -> None:
+    transfers = exchange.get_transfer_history()
+    assert [Transfer.model_validate(tr) for tr in transfers]
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
