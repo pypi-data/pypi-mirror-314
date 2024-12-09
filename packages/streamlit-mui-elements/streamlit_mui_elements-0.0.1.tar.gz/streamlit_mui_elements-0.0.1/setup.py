0bd537d1840c94cd478a29c07fd61ec432660dd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 15:05:32 +0000

    Add package streamlit-mui-elements/0.0.1/streamlit_mui_elements-0.0.1.tar.gz

---
+++ packages/streamlit-mui-elements/streamlit_mui_elements-0.0.1.tar.gz/streamlit_mui_elements-0.0.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+setuptools.setup(
+    name="streamlit-mui-elements",
+    version="0.0.1",
+    author="Tom Jose",
+    author_email="tomj0311@gmail.com",
+    description="",
+    long_description="",
+    long_description_content_type="text/plain",
+    url="",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.6",
+    install_requires=[
+        # By definition, a Custom Component depends on Streamlit.
+        # If your component has other Python dependencies, list
+        # them here.
+        "streamlit >= 0.63",
+    ],
+    extras_require={
+        "devel": [
+            "wheel",
+        ]
+    }
+)
