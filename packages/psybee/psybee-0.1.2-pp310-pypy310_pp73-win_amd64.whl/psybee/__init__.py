502cdbcd449dba84db33e1fbb48015442c81b275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 03:20:20 +0000

    Add package psybee/0.1.2/psybee-0.1.2-pp310-pypy310_pp73-win_amd64.whl

---
+++ packages/psybee/psybee-0.1.2-pp310-pypy310_pp73-win_amd64.whl/psybee/__init__.py
@@ -0,0 +1,16 @@
+"""""" # start delvewheel patch
+def _delvewheel_patch_1_9_0():
+    import os
+    if os.path.isdir(libs_dir := os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'psybee.libs'))):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_9_0()
+del _delvewheel_patch_1_9_0
+# end delvewheel patch
+
+from .psybee import *
+
+__doc__ = psybee.__doc__
+if hasattr(psybee, "__all__"):
+    __all__ = psybee.__all__
