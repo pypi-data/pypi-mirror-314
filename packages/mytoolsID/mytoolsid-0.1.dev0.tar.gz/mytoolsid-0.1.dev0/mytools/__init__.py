0725227aefb017ebfbf07add373780dc2407f0d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 15:03:45 +0000

    Add package mytoolsID/0.1.dev0/mytoolsid-0.1.dev0.tar.gz

---
+++ packages/mytoolsID/mytoolsid-0.1.dev0.tar.gz/mytoolsid-0.1.dev0/mytools/__init__.py
@@ -0,0 +1,22 @@
+from .button import Button
+from .chatbot import Api, ImageGen
+from .database import LocalDataBase, MongoDataBase
+from .encrypt import BinaryEncryptor, CryptoEncryptor, ShiftChipher
+from .getuser import Extract
+from .logger import LoggerHandler
+from .misc import Handler
+from .trans import Translate
+
+__version__ = "0.1.dev00"
+
+mytoolsID = f"""
+{"="*60}
+  __  ____   __  _____ ___   ___  _     ____    ___ ____  
+ |  \/  \ \ / / |_   _/ _ \ / _ \| |   / ___|  |_ _|  _ \ 
+ | |\/| |\ V /    | || | | | | | | |   \___ \   | || | | |
+ | |  | | | |     | || |_| | |_| | |___ ___) |  | || |_| |
+ |_|  |_| |_|     |_| \___/ \___/|_____|____/  |___|____/ 
+{"="*60}
+"""
+
+print(f"\033[1;37m{mytoolsID}\033[0m")
