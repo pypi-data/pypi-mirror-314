e936bdfc25cb6c897fe7cc6e296fc0281715e02a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 14:04:09 +0000

    Add package baykeshop/1.3.7/baykeshop-1.3.7-py3-none-any.whl

---
+++ packages/baykeshop/baykeshop-1.3.7-py3-none-any.whl/baykeshop/__init__.py
@@ -0,0 +1,8 @@
+INSTALLED_APPS = [
+    'baykeshop.sites.AdminConfig',
+    'baykeshop.contrib.shop',
+    'baykeshop.contrib.member',
+    'baykeshop.contrib.system',
+    'baykeshop.contrib.article'
+]
+
