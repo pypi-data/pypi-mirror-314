8644df50f0fe95c1aee7a22ad0723c6699e0faf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 04:04:47 +0000

    Add package dotenvhub/0.2.3/dotenvhub-0.2.3-py3-none-any.whl

---
+++ packages/dotenvhub/dotenvhub-0.2.3-py3-none-any.whl/dotenvhub/widgets/interactionpanel.py
@@ -0,0 +1,119 @@
+from pathlib import Path
+
+from textual import on
+from textual.containers import Container, Vertical, Horizontal
+from textual.widgets import Button, Input, Label, ListView, TextArea
+
+from dotenvhub.utils import (
+    copy_path_to_clipboard,
+    create_copy_in_cwd,
+    create_shell_export_str,
+    write_to_file,
+)
+from dotenvhub.widgets.modals import ModalSaveScreen, ModalShellSelector
+
+
+class InteractionPanel(Container):
+    def compose(self):
+        yield Button(
+            "Create Shell\nString",
+            id="btn-shell-export",
+            disabled=True,
+            variant="primary",
+        )
+        yield Button(
+            "Export File to current dir",
+            id="btn-file-export",
+            disabled=True,
+            variant="primary",
+        )
+        yield Button(
+            "Copy Path to Clipboard",
+            id="btn-copy-path",
+            disabled=True,
+            variant="primary",
+        )
+        with Vertical(id="interaction-shell-select"):
+            yield Label("Select Shell")
+            yield Button(
+                label=self.app.current_shell, id="btn-shell-select", variant="primary"
+            )
+        with Vertical(id="interaction-export-name"):
+            yield Label("Export filename")
+            yield Input(
+                value=".env",
+                placeholder="env file name for export",
+                id="export-env-name",
+            )
+        with Horizontal(id="horizontal-save-new"):
+            yield Button(
+                "New Env File", id="btn-new-file", disabled=False, variant="success"
+            )
+            yield Button(
+                "Save Env File", id="btn-save-file", disabled=True, variant="success"
+            )
+
+    # Export Interactions
+    @on(Button.Pressed, "#btn-copy-path")
+    def copy_env_path(self):
+        copy_str = copy_path_to_clipboard(path=self.app.file_to_show_path)
+        self.notify(title="Copied to Clipboard", message=f"Path: [green]{copy_str}[/]")
+
+    @on(Button.Pressed, "#btn-file-export")
+    def export_env_file(self):
+        export_filename = self.query_one(Input).value
+        create_copy_in_cwd(
+            filename=export_filename, filepath=self.app.file_to_show_path
+        )
+        self.notify(title="Env File Created", message=f"Created: {export_filename}")
+
+    @on(Button.Pressed, "#btn-shell-export")
+    def export_env_str_shell(self):
+        shell_str = create_shell_export_str(
+            shell=self.app.current_shell, env_content=self.app.current_content
+        )
+        self.notify(
+            title="Copied to Clipboard",
+            message=f"Command: [green]{shell_str}[/]",
+        )
+
+    # Shell Select Interactions
+    @on(Button.Pressed, "#btn-shell-select")
+    def pop_modal_shell(self):
+        self.app.push_screen(ModalShellSelector())
+
+    # Env File Interactions
+    @on(Button.Pressed, "#btn-new-file")
+    def new_file(self, event: Button.Pressed):
+        self.app.file_to_show = ""
+        self.app.file_to_show_path = ""
+        self.app.current_content = ""
+
+        text_widget = self.app.query_one(TextArea)
+        text_widget.text = ""
+        text_widget.disabled = False
+        text_widget.focus()
+
+        event.button.disabled = True
+        self.query_one("#btn-save-file").disabled = False
+
+        self.app.query_one("#file-preview").border_title = "Creating New .Env File ..."
+
+        for views in self.app.query(ListView):
+            views.query(Button).remove_class("active")
+            views.index = None
+
+    @on(Button.Pressed, "#btn-save-file")
+    def save_file(self, event: Button.Pressed):
+        text_widget = self.app.query_one(TextArea)
+        self.app.current_content = text_widget.text
+        text_widget.disabled = True
+        self.query_one("#btn-new-file").disabled = False
+        event.button.disabled = True
+
+        if self.app.file_to_show:
+            write_to_file(
+                path=Path(self.app.file_to_show_path), content=self.app.current_content
+            )
+        else:
+            self.app.push_screen(ModalSaveScreen())
---
+++ packages/dotenvhub/dotenvhub-0.2.3-py3-none-any.whl/dotenvhub/widgets/modals.py
@@ -0,0 +1,120 @@
+from pathlib import Path
+
+from textual import on
+from textual.app import ComposeResult
+from textual.binding import Binding
+from textual.containers import Vertical
+from textual.reactive import reactive
+from textual.screen import ModalScreen
+from textual.validation import Regex
+from textual.widgets import Button, Input, Label, TextArea
+
+from dotenvhub.config import cfg
+from dotenvhub.constants import ENV_FILE_DIR_PATH, SHELLS
+from dotenvhub.utils import update_file_tree, write_to_file
+from dotenvhub.widgets.filepanel import EnvFileSelector
+
+
+class ModalShellSelector(ModalScreen):
+    CSS_PATH = Path("../assets/modal_shell.css")
+
+    def compose(self) -> ComposeResult:
+        shell_buttons = [
+            Button(label=shell, id=shell, variant="primary", classes="shell")
+            for shell in SHELLS
+        ]
+        for btn in shell_buttons:
+            btn.can_focus = False
+        yield Vertical(
+            Label("Which Shell are you using?"),
+            *shell_buttons,
+            Button("Cancel", id="btn-modal-cancel"),
+            id="modal-shell-vert",
+        )
+
+    @on(Button.Pressed, "#btn-modal-cancel")
+    def close_window(self) -> None:
+        self.dismiss()
+
+    @on(Button.Pressed, ".shell")
+    def pop_up_shell_select(self, event: Button.Pressed) -> None:
+        self.dismiss()
+        selected_shell = event.button.id
+        self.app.current_shell = selected_shell
+        cfg.shell = self.app.current_shell
+
+        self.app.query_one("#btn-shell-select").label = self.app.current_shell
+        self.notify(
+            title="Shell Selected",
+            message=f"Current active Shell: [green]{self.app.current_shell}[/]",
+        )
+
+
+class ModalSaveScreen(ModalScreen):
+    CSS_PATH = Path("../assets/modal_save.css")
+    BINDINGS = [Binding(key="escape", action="close_window", show=False, priority=True)]
+    preview = reactive(":page_facing_up: Enter File Name")
+
+    def compose(self) -> ComposeResult:
+        yield Vertical(
+            Label("How to save file: e.g. FOLDER/FILE"),
+            Input(
+                placeholder="New File Name",
+                id="inp-new-file-name",
+                valid_empty=False,
+                validators=[Regex("^[a-zA-Z0-9_.]*(/[a-zA-Z0-9_.]*)?$")],
+                validate_on=["changed", "submitted"],
+            ),
+            Label(self.preview, id="lbl-new-file-name"),
+            Button("Save", id="btn-modal-save", disabled=True),
+            Button("Cancel", id="btn-modal-cancel"),
+            id="modal-save-vert",
+        )
+
+    @on(Input.Submitted)
+    def press_button(self):
+        self.query_one("#btn-modal-save", Button).press()
+
+    @on(Button.Pressed, "#btn-modal-cancel")
+    def action_close_window(self) -> None:
+        self.dismiss()
+        self.app.query_one(TextArea).disabled = False
+        self.app.query_one(TextArea).focus()
+
+    @on(Button.Pressed, "#btn-modal-save")
+    async def save_new_file(self) -> None:
+        self.dismiss()
+
+        new_path = ENV_FILE_DIR_PATH / self.query_one(Input).value
+        write_to_file(path=new_path, content=self.app.current_content)
+
+        self.app.file_tree = update_file_tree()
+
+        await self.app.query_one(EnvFileSelector).remove()
+        self.app.query_one("#app-grid").mount(
+            EnvFileSelector(id="file-selector"), before="#file-preview"
+        )
+
+    @on(Input.Changed, "#inp-new-file-name")
+    def format_name(self, event: Input.Changed):
+        text = event.input.value or event.input.placeholder
+        if "/" not in text:
+            preview_name = f":page_facing_up: {text}"
+        elif len(text.split("/")) == 2:
+            folder, file = [el.strip() for el in text.split("/")]
+            preview_name = f":file_folder: {folder} / :page_facing_up: {file}"
+        else:
+            preview_name = ":cross_mark: Enter a valid Folder/File Name"
+
+        self.preview = preview_name
+
+        if self.query_one(Input).is_valid:
+            self.query_one("#btn-modal-save", Button).disabled = False
+        else:
+            self.query_one("#btn-modal-save", Button).disabled = True
+
+        self.query_one("#lbl-new-file-name", Label).update(self.preview)
+        # self.query_one("#lbl-new-file-name").remove()
+        # self.mount(
+        #     Label(self.preview, id="lbl-new-file-name"), after="#inp-new-file-name"
+        # )
