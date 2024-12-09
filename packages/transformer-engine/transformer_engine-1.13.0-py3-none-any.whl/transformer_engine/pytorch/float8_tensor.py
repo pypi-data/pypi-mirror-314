d527064f6070bea987629adff8594ef8297e2808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 04:04:38 +0000

    Add package transformer-engine/1.13.0/transformer_engine-1.13.0-py3-none-any.whl

---
+++ packages/transformer-engine/transformer_engine-1.13.0-py3-none-any.whl/transformer_engine/pytorch/float8_tensor.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022-2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# See LICENSE for license information.
+
+"""Tensor class with FP8 data"""
+
+from .tensor import Float8Tensor
+
+__all__ = ["Float8Tensor"]
