e1e4f4d8614b1916d0df87c98d9c53dd5af8fa2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 16:04:25 +0000

    Add package REMI-z/0.5.7/remi_z-0.5.7.tar.gz

---
+++ packages/REMI-z/remi_z-0.5.7.tar.gz/remi_z-0.5.7/tests/test_old_tokenizer.py
@@ -0,0 +1,19 @@
+from multitrack.legacy_tokenizer import RemiTokenizer
+
+def test_old_tokenizer():
+    pass
+
+import unittest
+
+class TestCoreFunctions(unittest.TestCase):
+    
+    def test_add(self):
+        tk = RemiTokenizer()
+
+        # Testing add function
+        self.assertEqual(add(2, 3), 5)  # Expected result: 2 + 3 = 5
+        self.assertEqual(add(-1, 1), 0) # Expected result: -1 + 1 = 0
+        self.assertEqual(add(0, 0), 0)  # Expected result: 0 + 0 = 0
+
+if __name__ == '__main__':
+    unittest.main()
