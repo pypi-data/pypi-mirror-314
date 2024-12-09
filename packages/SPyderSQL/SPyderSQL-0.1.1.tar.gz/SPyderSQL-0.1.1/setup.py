f298b14531ef5796a31011b0c6cc0ef08ee183eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 14:03:22 +0000

    Add package SPyderSQL/0.1.1/SPyderSQL-0.1.1.tar.gz

---
+++ packages/SPyderSQL/SPyderSQL-0.1.1.tar.gz/SPyderSQL-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SPyderSQL",
+    version="0.1.1",
+    author="Emil Artemev",
+    author_email="jordanman1300@gmail.com",
+    description="modern ORM and framework development SQL database",
+    long_description=open("README.rst", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/RealWakeArmilus/MasterSQL",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "requests>=2.25.1",
+        "numpy>=1.19.0",
+        "pytest>=6.2.5",
+        "flake8>=3.9.2"
+    ],
+    python_requires=">=3.6",
+)
