22b96d74fcabff5b1ad827e87ac9d20831b29973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 05:03:54 +0000

    Add package Burningkeys/2024.12.3/burningkeys-2024.12.3-py3-none-any.whl

---
+++ packages/Burningkeys/burningkeys-2024.12.3-py3-none-any.whl/burningkeys/execute_config.py
@@ -0,0 +1,25 @@
+import shlex
+from subprocess import run
+
+import keyboard
+
+
+def parse_command_sequence(key, value):
+    if isinstance(value, str):
+        command_sequence = [value]
+    else:
+        command_sequence = list(value)
+
+    return key, command_sequence
+
+
+def execute_command_sequence(command_sequence):
+    for command in command_sequence:
+        run(shlex.split(command))
+
+
+def execute(config):
+    for key, value in config:
+        hotkey, command_sequence = parse_command_sequence(key, value)
+        keyboard.add_hotkey(hotkey, execute_command_sequence, args=command_sequence)
+    pass
