7886dffc52fe1cbb8a18897ff3c04ba4120b1882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 03:20:09 +0000

    Add package tradeflow/0.0.20/tradeflow-0.0.20-cp39-cp39-win32.whl

---
+++ packages/tradeflow/tradeflow-0.0.20-cp39-cp39-win32.whl/tradeflow/constants.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class OrderSelectionMethodAR(Enum):
+    PACF = "pacf"
+
+    def __str__(self) -> str:
+        return self._value_
+
+
+class FitMethodAR(Enum):
+    YULE_WALKER = "yule_walker"
+    OLS_WITH_CST = "ols_with_cst"
+
+    def __str__(self) -> str:
+        return self._value_
