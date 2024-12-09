6dd8f6c639b7c3be9a6ab69404595d4964c9d732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 14:04:45 +0000

    Add package gvsbuild/2024.12.0/gvsbuild-2024.12.0.tar.gz

---
+++ packages/gvsbuild/gvsbuild-2024.12.0.tar.gz/gvsbuild-2024.12.0/gvsbuild/projects/gstreamer.py
@@ -0,0 +1,250 @@
+#  Copyright (C) 2016 The Gvsbuild Authors
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+import os
+
+from gvsbuild.utils.base_builders import Meson
+from gvsbuild.utils.base_expanders import Tarball
+from gvsbuild.utils.base_project import Project, project_add
+
+
+@project_add
+class GStreamer(Tarball, Meson):
+    def __init__(self):
+        Project.__init__(
+            self,
+            "gstreamer",
+            repository="https://gitlab.freedesktop.org/gstreamer/gstreamer",
+            version="1.24.9",
+            lastversion_even=True,
+            archive_url="https://gstreamer.freedesktop.org/src/gstreamer/gstreamer-{version}.tar.xz",
+            hash="ebf47b6beef508a00c8557d4c1f1713e5c7ef9ba70dac45deed80e182bcf260f",
+            dependencies=["meson", "ninja", "glib", "orc"],
+        )
+
+        if self.opts.enable_gi:
+            self.add_dependency("gobject-introspection")
+            enable_gi = "enabled"
+        else:
+            enable_gi = "disabled"
+
+        self.add_param(f"-Dintrospection={enable_gi}")
+
+    def build(self):
+        add_path = os.path.join(self.builder.opts.msys_dir, "usr", "bin")
+
+        Meson.build(
+            self, add_path=add_path, meson_params="-Dtests=disabled -Dexamples=disabled"
+        )
+        self.install(r".\COPYING share\doc\gstreamer")
+
+
+@project_add
+class Orc(Tarball, Meson):
+    def __init__(self):
+        Project.__init__(
+            self,
+            "orc",
+            version="0.4.40",
+            lastversion_even=True,
+            repository="https://gitlab.freedesktop.org/gstreamer/orc",
+            archive_url="https://gstreamer.freedesktop.org/src/orc/orc-{version}.tar.xz",
+            hash="3fc2bee78dfb7c41fd9605061fc69138db7df007eae2f669a1f56e8bacef74ab",
+            dependencies=["meson", "ninja"],
+        )
+
+    def build(self):
+        Meson.build(self, meson_params="-Dbenchmarks=disabled -Dtools=enabled")
+        self.install(r"COPYING share\doc\orc")
+
+
+@project_add
+class GstPluginsBase(Tarball, Meson):
+    def __init__(self):
+        Project.__init__(
+            self,
+            "gst-plugins-base",
+            repository="https://gitlab.freedesktop.org/gstreamer/gstreamer",
+            version="1.24.9",
+            lastversion_even=True,
+            archive_url="https://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-base-{version}.tar.xz",
+            hash="5bb3b946907d3ce04dd842b610c8111c2b0611351b25a1fa22af5efa897857cb",
+            dependencies=[
+                "meson",
+                "ninja",
+                "gstreamer",
+                "opus",
+                "ogg",
+            ],
+        )
+        # Examples depend on GTK3
+        self.add_param("-Dexamples=disabled")
+
+        if self.opts.enable_gi:
+            self.add_dependency("gobject-introspection")
+            enable_gi = "enabled"
+        else:
+            enable_gi = "disabled"
+
+        self.add_param(f"-Dintrospection={enable_gi}")
+
+    def build(self):
+        Meson.build(
+            self, meson_params=f'-Dc_link_args="{self.builder.gtk_dir}\\lib\\ogg.lib"'
+        )
+        self.install(r".\COPYING share\doc\gst-plugins-base")
+
+
+@project_add
+class GstPluginsGood(Tarball, Meson):
+    def __init__(self):
+        Project.__init__(
+            self,
+            "gst-plugins-good",
+            repository="https://gitlab.freedesktop.org/gstreamer/gstreamer",
+            version="1.24.9",
+            lastversion_even=True,
+            archive_url="https://gstreamer.freedesktop.org/src/gst-plugins-good/gst-plugins-good-{version}.tar.xz",
+            hash="897de50bff337e3ca2f86f1eaa28e0828d83024156162a50c4ea0af86e29799f",
+            dependencies=[
+                "meson",
+                "ninja",
+                "gst-plugins-base",
+                "libvpx",
+            ],
+        )
+
+    def build(self):
+        Meson.build(self)
+        self.install(r".\COPYING share\doc\gst-plugins-good")
+
+
+@project_add
+class GstPluginsBad(Tarball, Meson):
+    def __init__(self):
+        Project.__init__(
+            self,
+            "gst-plugins-bad",
+            repository="https://gitlab.freedesktop.org/gstreamer/gstreamer",
+            version="1.24.9",
+            lastversion_even=True,
+            archive_url="https://gstreamer.freedesktop.org/src/gst-plugins-bad/gst-plugins-bad-{version}.tar.xz",
+            hash="36fcf7a9af0a753b43bb03b9835246f74d72f7124369e66a1e2dc7b04f5a5cab",
+            dependencies=[
+                "meson",
+                "ninja",
+                "gst-plugins-base",
+                "webrtc-audio-processing",
+            ],
+            patches=[
+                "wasapisink-reduce-buffer-latency.patch",
+            ],
+        )
+        self.add_param("-Dcurl=disabled")
+        self.add_param("-Dcurl-ssh2=disabled")
+
+        # Adding webrtc-audio-processing adds the isac plugin. It fails to compile.
+        self.add_param("-Disac=disabled")
+
+        if self.opts.enable_gi:
+            self.add_dependency("gobject-introspection")
+            enable_gi = "enabled"
+        else:
+            enable_gi = "disabled"
+
+        self.add_param(f"-Dintrospection={enable_gi}")
+
+    def build(self):
+        Meson.build(self)
+        self.install(r".\COPYING share\doc\gst-plugins-bad")
+
+
+@project_add
+class GstPluginsUgly(Tarball, Meson):
+    def __init__(self):
+        Project.__init__(
+            self,
+            "gst-plugins-ugly",
+            repository="https://gitlab.freedesktop.org/gstreamer/gstreamer",
+            version="1.24.9",
+            lastversion_even=True,
+            archive_url="https://gstreamer.freedesktop.org/src/gst-plugins-ugly/gst-plugins-ugly-{version}.tar.xz",
+            hash="4b6b30110f38cd05eb67422297142b75a55fe00003105f48b13603e6761cc3b6",
+            dependencies=["meson", "ninja", "gst-plugins-base"],
+        )
+
+    def build(self):
+        Meson.build(self)
+        self.install(r".\COPYING share\doc\gst-plugins-ugly")
+
+
+@project_add
+class GstDevTools(Tarball, Meson):
+    def __init__(self):
+        Project.__init__(
+            self,
+            "gst-devtools",
+            repository="https://gitlab.freedesktop.org/gstreamer/gstreamer",
+            version="1.24.9",
+            lastversion_even=True,
+            archive_url="https://gstreamer.freedesktop.org/src/gst-devtools/gst-devtools-{version}.tar.xz",
+            hash="cf1de1aafa6eb4fd0365932b251528e2340b88697541e021628c5f7259970fb8",
+            dependencies=["meson", "ninja", "json-glib"],
+        )
+
+        if self.opts.enable_gi:
+            self.add_dependency("gobject-introspection")
+            enable_gi = "enabled"
+        else:
+            enable_gi = "disabled"
+
+        self.add_param(f"-Dintrospection={enable_gi}")
+
+    def build(self):
+        Meson.build(self)
+        self.install(r".\COPYING share\doc\gst-devtools")
+
+
+@project_add
+class GstPython(Tarball, Meson):
+    def __init__(self):
+        Project.__init__(
+            self,
+            "gst-python",
+            repository="https://gitlab.freedesktop.org/gstreamer/gstreamer",
+            version="1.24.9",
+            lastversion_even=True,
+            archive_url="https://gstreamer.freedesktop.org/src/gst-python/gst-python-{version}.tar.xz",
+            hash="80e61d587c34cbac79e46c927b71cf32c4bf9bb0868ce5aa4b7444bdad83f42e",
+            dependencies=["meson", "ninja", "pygobject", "gst-plugins-base"],
+            patches=["0001-fix-python3-not-found.patch"],
+        )
+
+    def build(self):
+        Meson.build(self)
+        self.install(r".\COPYING share\doc\gst-python")
