e139320971cb3eff68da38a7a83aa795fa97cf11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 04:03:36 +0000

    Add package tinybird-python-sdk/0.1.3/tinybird_python_sdk-0.1.3.tar.gz

---
+++ packages/tinybird-python-sdk/tinybird_python_sdk-0.1.3.tar.gz/tinybird_python_sdk-0.1.3/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setup(
+    name="tinybird-python",
+    author="tinybird.co",
+    author_email="support@tinybird.co",
+    description="SDK around Tinybird APIs",
+    version="0.1.3",
+    url="https://github.com/tinybirdco/tinybird-python-sdk",
+    install_requires=required,
+    packages=["tb"],
+)
