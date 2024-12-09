e731fb1afb2f7e7ba347951f3e885d32f9e22831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 14:03:29 +0000

    Add package g2p-id-py/0.4.1/g2p_id_py-0.4.1.tar.gz

---
+++ packages/g2p-id-py/g2p_id_py-0.4.1.tar.gz/g2p_id_py-0.4.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+from pathlib import Path
+
+this_path = Path(__file__).parent
+
+readme_path = this_path / "README.md"
+requirements_path = this_path / "requirements.txt"
+
+long_description = readme_path.read_text(encoding="utf-8")
+
+with open(requirements_path, "r", encoding="utf-8") as requirements_file:
+    requirements = requirements_file.read().splitlines()
+
+if __name__ == "__main__":
+    setup(
+        name="g2p_id_py",
+        version="0.4.1",
+        description="Indonesian G2P.",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        author="w11wo",
+        author_email="wilson@bookbotkids.com",
+        url="https://github.com/bookbot-kids/g2p_id",
+        license="Apache License",
+        packages=find_packages(),
+        install_requires=requirements,
+        include_package_data=True,
+        platforms=["linux", "unix", "windows"],
+        python_requires=">=3.8",
+    )
