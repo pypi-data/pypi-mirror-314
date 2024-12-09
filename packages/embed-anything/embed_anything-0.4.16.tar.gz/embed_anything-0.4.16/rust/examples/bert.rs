0c6bdea6c1defa23ea7c907ab680e771db4f836f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 14:03:52 +0000

    Add package embed-anything/0.4.16/embed_anything-0.4.16.tar.gz

---
+++ packages/embed-anything/embed_anything-0.4.16.tar.gz/embed_anything-0.4.16/rust/examples/bert.rs
@@ -0,0 +1,48 @@
+use embed_anything::config::TextEmbedConfig;
+use embed_anything::embeddings::embed::{EmbedData, Embedder, TextEmbedder};
+use embed_anything::text_loader::SplittingStrategy;
+use embed_anything::{embed_directory_stream, embed_file};
+use std::sync::Arc;
+use std::{path::PathBuf, time::Instant};
+
+#[tokio::main]
+async fn main() {
+    let model = Arc::new(Embedder::Text(
+        TextEmbedder::from_pretrained_hf("jina", "jinaai/jina-embeddings-v2-small-en", None)
+            .unwrap(),
+    ));
+    let config = TextEmbedConfig::default()
+        .with_chunk_size(256, Some(0.3))
+        .with_batch_size(32)
+        .with_buffer_size(32)
+        .with_splitting_strategy(SplittingStrategy::Sentence)
+        .with_semantic_encoder(Arc::clone(&model));
+
+    let _out = embed_file(
+        "test_files/test.pdf",
+        &model,
+        Some(&config),
+        None::<fn(Vec<EmbedData>)>,
+    )
+    .await
+    .unwrap()
+    .unwrap();
+
+    let now = Instant::now();
+
+    let _out = embed_directory_stream(
+        PathBuf::from("bench"),
+        &model,
+        None,
+        // Some(vec!["txt".to_string()]),
+        Some(&config),
+        None::<fn(Vec<EmbedData>)>,
+    )
+    .await
+    .unwrap()
+    .unwrap();
+
+    println!("Number of chunks: {:?}", _out.len());
+    let elapsed_time = now.elapsed();
+    println!("Elapsed Time: {}", elapsed_time.as_secs_f32());
+}
---
+++ packages/embed-anything/embed_anything-0.4.16.tar.gz/embed_anything-0.4.16/rust/src/file_processor/mod.rs
@@ -0,0 +1,16 @@
+/// This module contains the file processor for different file types.
+pub mod pdf_processor;
+
+/// This module contains the file processor for markdown files.
+pub mod markdown_processor;
+
+/// This module contains the file processor for text files.
+pub mod txt_processor;
+
+/// This module contains the processor to process web links.
+pub mod website_processor;
+
+/// This module contains the file processor for HTML files.
+pub mod html_processor;
+
+pub mod audio;
