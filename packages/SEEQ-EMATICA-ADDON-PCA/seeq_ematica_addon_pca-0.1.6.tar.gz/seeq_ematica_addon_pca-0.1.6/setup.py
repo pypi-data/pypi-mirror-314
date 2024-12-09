98251990a27c51e592d5db00c08d4243ba21c2f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 04:04:05 +0000

    Add package SEEQ-EMATICA-ADDON-PCA/0.1.6/seeq_ematica_addon_pca-0.1.6.tar.gz

---
+++ packages/SEEQ-EMATICA-ADDON-PCA/seeq_ematica_addon_pca-0.1.6.tar.gz/seeq_ematica_addon_pca-0.1.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SEEQ_EMATICA_ADDON_PCA",  # Replace with your project name
+    version="0.1.06",  # Initial version
+    author="Alessandro Robbiano",
+    author_email="alessandro.robbiano@e-matica.it",
+    description="PCA Package for Seeq Addons",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=open("requirements.txt").read().splitlines(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
