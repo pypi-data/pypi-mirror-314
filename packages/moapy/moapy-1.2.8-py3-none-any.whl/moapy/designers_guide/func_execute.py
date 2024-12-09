ab6e6cb7a59defdc4f79b77f7b5c8948e5ab7d5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 15:05:08 +0000

    Add package moapy/1.2.8/moapy-1.2.8-py3-none-any.whl

---
+++ packages/moapy/moapy-1.2.8-py3-none-any.whl/moapy/designers_guide/func_execute.py
@@ -0,0 +1,61 @@
+from moapy.designers_guide.func_general import execute_calc_content, DG_Result_Reports
+
+def calc_content_1(G4_COMP_1: str) -> DG_Result_Reports:
+    target_components = ['G4_COMP_4', 'G4_COMP_5', 'G4_COMP_6', 'G4_COMP_7', 'G4_COMP_8', 'G4_COMP_9']
+    inputs = {"G4_COMP_1": G4_COMP_1}
+    return execute_calc_content(target_components, inputs)
+
+def calc_content_10(G3_COMP_2: float, G3_COMP_6: float, G3_COMP_10: float, G3_COMP_15: str, G10_COMP_3: float, G10_COMP_4: float, G10_COMP_5: float, G10_COMP_6: float) -> DG_Result_Reports:
+    target_components = ['G10_COMP_1']
+    inputs = {"G3_COMP_2": G3_COMP_2, "G3_COMP_6": G3_COMP_6, "G3_COMP_10": G3_COMP_10, "G3_COMP_15": G3_COMP_15, "G10_COMP_3": G10_COMP_3, "G10_COMP_4": G10_COMP_4, "G10_COMP_5": G10_COMP_5, "G10_COMP_6": G10_COMP_6}
+    return execute_calc_content(target_components, inputs)
+
+def calc_content_11(G11_COMP_1: str, G11_COMP_4: str, G11_COMP_5: float, G11_COMP_11: str, G11_COMP_14: str, G11_COMP_18: float, G11_COMP_19: float, G11_COMP_25: float, G11_COMP_26: float, G11_COMP_30: str, G11_COMP_33: float, G11_COMP_34: float) -> DG_Result_Reports:
+    target_components = ['G11_COMP_2', 'G11_COMP_3']
+    inputs = {"G11_COMP_1": G11_COMP_1, "G11_COMP_4": G11_COMP_4, "G11_COMP_5": G11_COMP_5, "G11_COMP_11": G11_COMP_11, "G11_COMP_14": G11_COMP_14, "G11_COMP_18": G11_COMP_18, "G11_COMP_19": G11_COMP_19, "G11_COMP_25": G11_COMP_25, "G11_COMP_26": G11_COMP_26, "G11_COMP_30": G11_COMP_30, "G11_COMP_33": G11_COMP_33, "G11_COMP_34": G11_COMP_34}
+    return execute_calc_content(target_components, inputs)
+
+def calc_content_12(G12_COMP_1: str, G12_COMP_2: str, G12_COMP_4: str, G12_COMP_5: float, G12_COMP_6: float) -> DG_Result_Reports:
+    target_components = ['G12_COMP_15', 'G12_COMP_16', 'G12_COMP_17']
+    inputs = {"G12_COMP_1": G12_COMP_1, "G12_COMP_2": G12_COMP_2, "G12_COMP_4": G12_COMP_4, "G12_COMP_5": G12_COMP_5, "G12_COMP_6": G12_COMP_6}
+    return execute_calc_content(target_components, inputs)
+
+def calc_content_2(G3_COMP_2: float, G3_COMP_6: float, G3_COMP_10: float, G3_COMP_15: str) -> DG_Result_Reports:
+    target_components = ['G3_COMP_24']
+    inputs = {"G3_COMP_2": G3_COMP_2, "G3_COMP_6": G3_COMP_6, "G3_COMP_10": G3_COMP_10, "G3_COMP_15": G3_COMP_15}
+    return execute_calc_content(target_components, inputs)
+
+def calc_content_3(G1_COMP_1: str, G1_COMP_3: float, G1_COMP_4: float, G1_COMP_6: float, G1_COMP_7: float) -> DG_Result_Reports:
+    target_components = ['G1_COMP_16', 'G1_COMP_17']
+    inputs = {"G1_COMP_1": G1_COMP_1, "G1_COMP_3": G1_COMP_3, "G1_COMP_4": G1_COMP_4, "G1_COMP_6": G1_COMP_6, "G1_COMP_7": G1_COMP_7}
+    return execute_calc_content(target_components, inputs)
+
+def calc_content_4(G2_COMP_4: float, G2_COMP_7: float, G6_COMP_2: str) -> DG_Result_Reports:
+    target_components = ['G2_COMP_1', 'G2_COMP_5', 'G2_COMP_6']
+    inputs = {"G2_COMP_4": G2_COMP_4, "G2_COMP_7": G2_COMP_7, "G6_COMP_2": G6_COMP_2}
+    return execute_calc_content(target_components, inputs)
+
+def calc_content_5(G3_COMP_2: float, G3_COMP_6: float, G3_COMP_10: float, G3_COMP_15: str, G5_COMP_6: float, G5_COMP_7: float, G5_COMP_8: str, G5_COMP_9: str, G5_COMP_11: float, G5_COMP_12: str, G5_COMP_17: float) -> DG_Result_Reports:
+    target_components = ['G5_COMP_1', 'G5_COMP_13', 'G5_COMP_16']
+    inputs = {"G3_COMP_2": G3_COMP_2, "G3_COMP_6": G3_COMP_6, "G3_COMP_10": G3_COMP_10, "G3_COMP_15": G3_COMP_15, "G5_COMP_6": G5_COMP_6, "G5_COMP_7": G5_COMP_7, "G5_COMP_8": G5_COMP_8, "G5_COMP_9": G5_COMP_9, "G5_COMP_11": G5_COMP_11, "G5_COMP_12": G5_COMP_12, "G5_COMP_17": G5_COMP_17}
+    return execute_calc_content(target_components, inputs)
+
+def calc_content_6(G6_COMP_2: str, G6_COMP_4: float, G6_COMP_6: str, G6_COMP_9: float, G6_COMP_10: float, G6_COMP_11: float) -> DG_Result_Reports:
+    target_components = ['G6_COMP_1']
+    inputs = {"G6_COMP_2": G6_COMP_2, "G6_COMP_4": G6_COMP_4, "G6_COMP_6": G6_COMP_6, "G6_COMP_9": G6_COMP_9, "G6_COMP_10": G6_COMP_10, "G6_COMP_11": G6_COMP_11}
+    return execute_calc_content(target_components, inputs)
+
+def calc_content_7(G7_COMP_3: float, G7_COMP_4: float, G7_COMP_8: float) -> DG_Result_Reports:
+    target_components = ['G7_COMP_1', 'G7_COMP_2', 'G7_COMP_7', 'G7_COMP_9']
+    inputs = {"G7_COMP_3": G7_COMP_3, "G7_COMP_4": G7_COMP_4, "G7_COMP_8": G7_COMP_8}
+    return execute_calc_content(target_components, inputs)
+
+def calc_content_8(G8_COMP_3: float, G8_COMP_4: float, G8_COMP_8: float) -> DG_Result_Reports:
+    target_components = ['G8_COMP_1', 'G8_COMP_2', 'G8_COMP_10', 'G8_COMP_11', 'G8_COMP_12', 'G8_COMP_13']
+    inputs = {"G8_COMP_3": G8_COMP_3, "G8_COMP_4": G8_COMP_4, "G8_COMP_8": G8_COMP_8}
+    return execute_calc_content(target_components, inputs)
+
+def calc_content_9(G8_COMP_8: float, G9_COMP_4: float, G9_COMP_5: str, G9_COMP_8: float, G9_COMP_17: float) -> DG_Result_Reports:
+    target_components = ['G9_COMP_1', 'G9_COMP_2', 'G9_COMP_11', 'G9_COMP_13', 'G9_COMP_14']
+    inputs = {"G8_COMP_8": G8_COMP_8, "G9_COMP_4": G9_COMP_4, "G9_COMP_5": G9_COMP_5, "G9_COMP_8": G9_COMP_8, "G9_COMP_17": G9_COMP_17}
+    return execute_calc_content(target_components, inputs)
