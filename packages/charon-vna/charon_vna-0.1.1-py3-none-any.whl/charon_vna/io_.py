dab84e8cef946ce4ba505a9573f6afd3a9f673a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 04:04:29 +0000

    Add package charon-vna/0.1.1/charon_vna-0.1.1-py3-none-any.whl

---
+++ packages/charon-vna/charon_vna-0.1.1-py3-none-any.whl/charon_vna/io_.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+import skrf as rf
+import xarray as xr
+from util import net2s
+
+
+# scikit-rf has no way to save files aside from touchstone and pickle
+def cal2zarr(cal: rf.calibration.Calibration, outpath: Path):
+    ideals = [net2s(net) for net in cal.ideals]
+    measured = [net2s(net) for net in cal.measured]
+    # s.to_zarr(outpath)
