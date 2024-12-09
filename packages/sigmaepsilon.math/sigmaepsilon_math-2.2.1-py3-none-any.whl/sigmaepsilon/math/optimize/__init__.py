c0e0a73fc896e4efa218ed7797c0a272fa692edd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 03:20:19 +0000

    Add package sigmaepsilon.math/2.2.1/sigmaepsilon_math-2.2.1-py3-none-any.whl

---
+++ packages/sigmaepsilon.math/sigmaepsilon_math-2.2.1-py3-none-any.whl/sigmaepsilon/math/optimize/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from .bga import BinaryGeneticAlgorithm
+from .lp import LinearProgrammingProblem
+
+
+__all__ = [
+    "BinaryGeneticAlgorithm",
+    "LinearProgrammingProblem",
+]
