b757569eef9b668162770cff5db412125461f0f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 16:04:02 +0000

    Add package partest/0.1.31/partest-0.1.31.tar.gz

---
+++ packages/partest/partest-0.1.31.tar.gz/partest-0.1.31/partest/test_types.py
@@ -0,0 +1,8 @@
+
+class TypesTestCases:
+    type_default = 'default'
+    type_405 = '405'
+    type_params = 'params'
+    type_elem = 'elem'
+    type_gen = 'generation_data'
+    type_health = 'health'
