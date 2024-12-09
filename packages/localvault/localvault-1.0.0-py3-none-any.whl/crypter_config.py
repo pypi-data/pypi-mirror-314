000102246ada80e30c7318c67e3e49b8c749e1c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 03:18:41 +0000

    Add package localvault/1.0.0/localvault-1.0.0-py3-none-any.whl

---
+++ packages/localvault/localvault-1.0.0-py3-none-any.whl/crypter_config.py
@@ -0,0 +1,23 @@
+# BSD 3-Clause License
+# Copyright (c) 2024, mac
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+
+# 1. Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+
+# 3. Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+
+import os
+
+
+class CrypterConfig():
+    CONFIG_DIR = os.environ['HOME']
+    CRYPTER_DIR = ".crypter"
+    DB_NAME = "crypter"
