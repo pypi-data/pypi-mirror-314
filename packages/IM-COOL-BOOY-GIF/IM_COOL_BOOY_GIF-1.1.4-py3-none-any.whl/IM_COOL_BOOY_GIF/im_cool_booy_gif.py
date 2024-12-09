b52e0e891a8795618e0f044bfe47def5e9f8b55a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 04:04:14 +0000

    Add package IM-COOL-BOOY-GIF/1.1.4/IM_COOL_BOOY_GIF-1.1.4-py3-none-any.whl

---
+++ packages/IM-COOL-BOOY-GIF/IM_COOL_BOOY_GIF-1.1.4-py3-none-any.whl/IM_COOL_BOOY_GIF/im_cool_booy_gif.py
@@ -0,0 +1,7 @@
+# im_cool_booy_gif.py
+
+def main():
+    print("GIF creation started...")
+    print("1️⃣ Create a folder named 'Download' in your sdcard.")
+    print("2️⃣ Place the photos you want to include in the GIF into that folder.")
+    print("3️⃣ Run the command 'IM-COOL-BOOY-GIF'.")
