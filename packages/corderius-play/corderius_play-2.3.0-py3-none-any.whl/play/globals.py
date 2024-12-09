fa115dd056289a303b115dd369b4e2cd9d925f49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 04:04:24 +0000

    Add package corderius-play/2.3.0/corderius_play-2.3.0-py3-none-any.whl

---
+++ packages/corderius-play/corderius_play-2.3.0-py3-none-any.whl/play/globals.py
@@ -0,0 +1,14 @@
+"""Global variables for the game"""
+
+import pygame
+
+all_sprites = []
+sprites_group = pygame.sprite.Group()
+
+_walls = []
+
+backdrop = (255, 255, 255)
+
+FRAME_RATE = 60
+WIDTH = 800
+HEIGHT = 600
