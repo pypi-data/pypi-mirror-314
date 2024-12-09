06c3054ee4a5f5b60c8e56d2c6b69de5bce571f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 15:04:35 +0000

    Add package omlish/0.0.0.dev147/omlish-0.0.0.dev147.tar.gz

---
+++ packages/omlish/omlish-0.0.0.dev147.tar.gz/omlish-0.0.0.dev147/omlish/docker/helpers.py
@@ -0,0 +1,29 @@
+import os
+import shlex
+
+
+##
+
+
+_DEFAULT_TIMEBOMB_NAME = '-'.join([*__name__.split('.'), 'timebomb'])
+
+
+def timebomb_payload(delay_s: float, name: str = _DEFAULT_TIMEBOMB_NAME) -> str:
+    return (
+        '('
+        f'echo {shlex.quote(name)} && '
+        f'sleep {delay_s:g} && '
+        'sh -c \'killall5 -9 -o $PPID -o $$ ; kill 1\''
+        ') &'
+    )
+
+
+##
+
+
+# Set by pyproject, docker-dev script
+DOCKER_HOST_PLATFORM_KEY = 'DOCKER_HOST_PLATFORM'
+
+
+def get_docker_host_platform() -> str | None:
+    return os.environ.get(DOCKER_HOST_PLATFORM_KEY)
