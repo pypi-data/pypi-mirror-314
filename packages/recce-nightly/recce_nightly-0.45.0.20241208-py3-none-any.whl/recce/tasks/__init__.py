86a127e74769430a8134f7f463fb1cdfab9e27c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 04:04:19 +0000

    Add package recce-nightly/0.45.0.20241208/recce_nightly-0.45.0.20241208-py3-none-any.whl

---
+++ packages/recce-nightly/recce_nightly-0.45.0.20241208-py3-none-any.whl/recce/tasks/__init__.py
@@ -0,0 +1,6 @@
+from .core import Task
+from .profile import ProfileDiffTask
+from .query import QueryTask, QueryDiffTask
+from .rowcount import RowCountDiffTask
+from .valuediff import ValueDiffTask, ValueDiffDetailTask
+from .top_k import TopKDiffTask
