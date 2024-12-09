0b8745138050960180f0813189bde2960f4a8626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 04:03:39 +0000

    Add package h5visualizer/0.0.5/h5visualizer-0.0.5-py3-none-any.whl

---
+++ packages/h5visualizer/h5visualizer-0.0.5-py3-none-any.whl/h5visualizer/__init__.py
@@ -0,0 +1,6 @@
+from .__main__ import visualize_model_from_filepath, visualize_model_layers_as_cubes
+__version__ = '1.0.0'
+__all__ = [
+    'visualize_model_from_filepath',
+    'visualize_model_layers_as_cubes'
+]
