2cc09e685baee7bcc237f9d535d2aeef097d3e08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 03:19:45 +0000

    Add package KIB-LAP/0.3.19/kib_lap-0.3.19.tar.gz

---
+++ packages/KIB-LAP/kib_lap-0.3.19.tar.gz/kib_lap-0.3.19/KIB_LAP/Examples/ShearDesign.py
@@ -0,0 +1,11 @@
+# from KIB_LAP.Betonbau import BeamRectangular
+from KIB_LAP.Betonbau import beam_sub_section
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import pandas as pd
+import time
+import os
+
+
