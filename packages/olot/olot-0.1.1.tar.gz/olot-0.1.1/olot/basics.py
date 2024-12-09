e753b992a94f19369214c6f574f4a19d3d845828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 16:04:15 +0000

    Add package olot/0.1.1/olot-0.1.1.tar.gz

---
+++ packages/olot/olot-0.1.1.tar.gz/olot-0.1.1/olot/basics.py
@@ -0,0 +1,30 @@
+
+import hashlib
+
+
+class HashingWriter:
+    def __init__(self, base_writer, hash_func=None):
+        self.base_writer = base_writer
+        self.hash_func = hash_func or hashlib.sha256()
+
+    def write(self, data: bytes):
+        self.hash_func.update(data)
+        return self.base_writer.write(data)
+    
+    def tell(self):
+        return self.base_writer.tell()
+
+    def close(self):
+        self.base_writer.close()
+
+
+def get_file_hash(path) -> str:
+    h = hashlib.sha256()
+    with open(path, "rb") as f:
+        while chunk := f.read(4096):
+            h.update(chunk)
+    return h.hexdigest()
+
+
+if __name__ == "__main__":
+    print("?")
