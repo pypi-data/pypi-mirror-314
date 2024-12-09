2147c11a7d45b082ff269c858203eb042bdbba0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 14:04:43 +0000

    Add package LeOpardLink/0.1.3.1/LeOpardLink-0.1.3.1-py3-none-any.whl

---
+++ packages/LeOpardLink/LeOpardLink-0.1.3.1-py3-none-any.whl/LeOpardLink/__init__.py
@@ -0,0 +1,8 @@
+"""
+LeOpardLink
+
+A package to generate all possible graphs for wildlife individual identification, and visualize them.
+"""
+
+__version__ = "0.1.3.1"
+__author__ = 'Courtney Allen, Ziqi Guo, Guy Bennevat Haninovich, Jiangyue Wang'
