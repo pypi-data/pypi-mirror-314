82d44ba23ba157d468434b5420142f9fe6e32c9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 03:20:23 +0000

    Add package trollbot/1.0/trollbot-1.0.tar.gz

---
+++ packages/trollbot/trollbot-1.0.tar.gz/trollbot-1.0/trollbot/headers.py
@@ -0,0 +1,14 @@
+URL = "ws://www.windows93.net:8081/"
+HEADERS = {
+    "Accept": "*/*",
+    "Accept-Encoding": "identity",
+    "Accept-Language": "*",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive",
+    "Cookie": "",
+    "Host": "www.windows93.net",
+    "Origin": "http://www.windows93.net",
+    "Pragma": "no-cache",
+    "Referer": "http://www.windows93.net/trollbox/index.php",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36"
+}
