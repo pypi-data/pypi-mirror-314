f9f0242ba8bc04a3f234b485843c453c3bfbde33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 03:19:13 +0000

    Add package euclidean-algorithm/0.1.3/euclidean_algorithm-0.1.3.tar.gz

---
+++ packages/euclidean-algorithm/euclidean_algorithm-0.1.3.tar.gz/euclidean_algorithm-0.1.3/euclidean_algorithm/__init__.py
@@ -0,0 +1,15 @@
+"""
+:author: Hspu1
+:license: MIT
+:copyright: (c) 2024 Hspu1
+"""
+
+__all__ = (
+    "euclidean_algorithm"
+)
+
+from . import euclidean_algorithm
+
+__author__ = "Hspu1"
+__version__ = "0.1.3"
+__email__ = "rumyantsev1m0s1@gmail.com"
