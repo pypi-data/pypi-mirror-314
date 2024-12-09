a93f3a39d6b2aa2b76a806e58a3a2a3ab99c1bf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 04:04:20 +0000

    Add package morsedecode/1.0.1/morsedecode-1.0.1.tar.gz

---
+++ packages/morsedecode/morsedecode-1.0.1.tar.gz/morsedecode-1.0.1/src/morsedecode/cli.py
@@ -0,0 +1,25 @@
+import sys
+
+import morsedecode
+
+
+def encode() -> None:
+    """CLI utility to convert text to morse code."""
+    data = sys.argv[1:]
+    if not data:
+        print("Usage: morsecode Hello World!")
+        sys.exit(1)
+
+    text = " ".join(data)
+    print(morsedecode.encode(text))
+
+
+def decode() -> None:
+    """CLI utility to convert morse code to text."""
+    data = sys.argv[1:]
+    if not data:
+        print("Usage: morsedecode -- .... . . -.. . -.. ---", file=sys.stderr)
+        sys.exit(1)
+
+    text = " ".join(data)
+    print(morsedecode.decode(text))
