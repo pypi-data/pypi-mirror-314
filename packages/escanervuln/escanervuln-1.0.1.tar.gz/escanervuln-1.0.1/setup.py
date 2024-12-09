2d47ef89102b23d84fa394f1f67927c90556e0b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 05:03:54 +0000

    Add package escanervuln/1.0.1/escanervuln-1.0.1.tar.gz

---
+++ packages/escanervuln/escanervuln-1.0.1.tar.gz/escanervuln-1.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='escanervuln',
+    version='1.0.1',
+    description='Librería Modular de Detección de Vulnerabilidades en Python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Ignacio',
+    author_email='knjasu098@gmail.com',
+    url='https://github.com/tuusuario/vulnscanner',  # Reemplázalo con tu repo
+    packages=find_packages(),
+    install_requires=[
+        'flask',   # Dependencias necesarias
+        'pyyaml',
+        "AST"
+    ],
+    entry_points={
+        'console_scripts': [
+            'vulnscanner=vulnscanner.cli:main'  # Crear comando 'vulnscanner' en la CLI
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
