7a4129b37aee96a6ddf09f201c3354bcc43e0de7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 15:03:44 +0000

    Add package tenantv1/0.0.3/tenantv1-0.0.3-py3-none-any.whl

---
+++ packages/tenantv1/tenantv1-0.0.3-py3-none-any.whl/tenantv1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+ Copyright (c) 2024 Baidu, Inc.
+# All rights reserved.
+#
+# File    : setup
+# Author  : zhoubohan
+# Date    : 2024/12/05
+# Time    : 15:53
+# Description :
+"""
+import setuptools
+
+setuptools.setup()
