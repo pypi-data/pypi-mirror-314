cdd43d54d788aa8b9e674cab7080aade6337d3d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 03:18:32 +0000

    Add package cmdbox/0.2.0/cmdbox-0.2.0.tar.gz

---
+++ packages/cmdbox/cmdbox-0.2.0.tar.gz/cmdbox-0.2.0/cmdbox/app/features/web/cmdbox_web_do_signout.py
@@ -0,0 +1,25 @@
+from cmdbox import version
+from cmdbox.app import feature
+from cmdbox.app.web import Web
+from fastapi import FastAPI, Request, Response
+from fastapi.responses import HTMLResponse, RedirectResponse
+
+
+class DoSignout(feature.WebFeature):
+    def __init__(self, ver=version):
+        super().__init__(ver=ver)
+
+    def route(self, web:Web, app:FastAPI) -> None:
+        """
+        webモードのルーティングを設定します
+
+        Args:
+            web (Web): Webオブジェクト
+            app (FastAPI): FastAPIオブジェクト
+        """
+        @app.get('/dosignout/{next}', response_class=HTMLResponse)
+        @app.post('/dosignout/{next}', response_class=HTMLResponse)
+        async def do_signout(next, req:Request, res:Response):
+            if 'signin' in req.session:
+                del req.session['signin']
+            return RedirectResponse(url=f'/{next}')
