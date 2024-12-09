ada37f6cd458ad19626d42fc899de33649b0d91b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-09 03:20:24 +0000

    Add package pyrio/1.3.4/pyrio-1.3.4-py3-none-any.whl

---
+++ packages/pyrio/pyrio-1.3.4-py3-none-any.whl/pyrio/streams/base_stream.py
@@ -0,0 +1,336 @@
+from collections.abc import Mapping
+
+from pyrio.iterators.generator import Generator
+from pyrio.utils.decorator import pre_call, handle_consumed
+from pyrio.utils.dict_item import DictItem
+from pyrio.utils.exception import IllegalStateError, UnsupportedTypeError
+from pyrio.utils.optional import Optional
+
+
+@pre_call(handle_consumed)
+class BaseStream:
+    """Base class for Stream objects; describes core supported operations"""
+    def __init__(self, iterable):
+        self._iterable = iterable
+        self._is_consumed = False
+
+    def __iter__(self):
+        return iter(self.iterable)
+
+    @property
+    def iterable(self):
+        if isinstance(self._iterable, Mapping):
+            return (DictItem(k, v) for k, v in self._iterable.items())
+        return self._iterable
+
+    @iterable.setter
+    def iterable(self, value):
+        self._iterable = value
+
+    @classmethod
+    def empty(cls):
+        """Creates empty Stream"""
+        return cls([])
+
+    def concat(self, *streams):
+        """Concatenates several streams together or adds new streams/collections to the current one"""
+        self.iterable = Generator.concat(self.iterable, *streams)
+        return self
+
+    def prepend(self, iterable):
+        """Prepends iterable to current stream"""
+        self.iterable = Generator.concat(iterable, self.iterable)
+        return self
+
+    def filter(self, predicate):
+        """Filters values in stream based on given predicate function"""
+        self.iterable = Generator.filter(self.iterable, predicate)
+        return self
+
+    def map(self, mapper):
+        """Returns a stream consisting of the results of applying the given function to the elements of this stream"""
+        self.iterable = Generator.map(self.iterable, mapper)
+        return self
+
+    def filter_map(self, mapper, *, falsy=False):
+        """Filters out all None or falsy values and applies mapper function to the elements of the stream"""
+        self.iterable = Generator.filter_map(self.iterable, mapper, falsy)
+        return self
+
+    def flat_map(self, mapper):
+        """Maps each element of the stream and yields the elements of the produced iterators"""
+        self.iterable = Generator.flat_map(self.iterable, mapper)
+        return self
+
+    def flatten(self):
+        """Converts a Stream of multidimensional collection into a one-dimensional"""
+        self.iterable = Generator.flatten(self.iterable)
+        return self
+
+    def peek(self, operation):
+        """Performs the provided operation on each element of the stream without consuming it"""
+        self.iterable = Generator.peek(self.iterable, operation)
+        return self
+
+    def distinct(self):
+        """Returns a stream with the distinct elements of the current one"""
+        self.iterable = Generator.distinct(self.iterable)
+        return self
+
+    def count(self):
+        """Returns the count of elements in the stream"""
+        return len(tuple(self.iterable))
+
+    def sum(self):
+        """Sums the elements of the stream"""
+        if len(self.iterable) == 0:
+            return 0
+        if not any(isinstance(x, (int | float | None)) for x in self.iterable):
+            raise ValueError("Cannot apply sum on non-number elements")
+        return sum(self.iterable)
+
+    def skip(self, count):
+        """Discards the first n elements of the stream and returns a new stream with the remaining ones"""
+        if count < 0:
+            raise ValueError("Skip count cannot be negative")
+        self.iterable = Generator.skip(self.iterable, count)
+        return self
+
+    def limit(self, count):
+        """Returns a stream with the first n elements, or fewer if the underlying iterator ends sooner"""
+        if count < 0:
+            raise ValueError("Limit count cannot be negative")
+        self.iterable = Generator.limit(self.iterable, count)
+        return self
+
+    def head(self, count):
+        """Alias for 'limit'"""
+        if count < 0:
+            raise ValueError("Head count cannot be negative")
+        self.iterable = Generator.limit(self.iterable, count)
+        return self
+
+    def tail(self, count):
+        """Returns a stream with the last n elements, or fewer if the underlying iterator ends sooner"""
+        if count < 0:
+            raise ValueError("Tail count cannot be negative")
+        self.iterable = Generator.tail(self.iterable, count)
+        return self
+
+    def take_while(self, predicate):
+        """Returns a stream that yields elements based on a predicate"""
+        self.iterable = Generator.take_while(self.iterable, predicate)
+        return self
+
+    def drop_while(self, predicate):
+        """Returns a stream that skips elements based on a predicate and yields the remaining ones"""
+        self.iterable = Generator.drop_while(self.iterable, predicate)
+        return self
+
+    def sort(self, comparator=None, *, reverse=False):
+        """Sorts the elements of the current stream according to natural order or based on the given comparator.
+        If 'reverse' flag is True, the elements are sorted in descending order"""
+        self.iterable = Generator.sort(self.iterable, comparator, reverse)
+        return self
+
+    def reverse(self, comparator=None):
+        """Sorts the elements of the current stream in descending order.
+        Alias for 'sort(comparator, reverse=True)'"""
+        self.iterable = Generator.sort(self.iterable, comparator, reverse=True)
+        return self
+
+    def find_first(self, predicate=None):
+        """Searches for an element of the stream that satisfies a predicate.
+        Returns an Optional with the first found value, if any, or None"""
+        return Optional.of_nullable(next(filter(predicate, self.iterable), None))
+
+    def find_any(self, predicate=None):
+        """Searches for an element of the stream that satisfies a predicate.
+        Returns an Optional with some of the found values, if any, or None"""
+        import random
+
+        if predicate:
+            self.filter(predicate)
+        try:
+            return Optional.of(random.choice(list(self.iterable)))
+        except IndexError:
+            return Optional.of_nullable(None)
+
+    def any_match(self, predicate):
+        """Returns whether any elements of the stream match the given predicate"""
+        return any(predicate(i) for i in self.iterable)
+
+    def all_match(self, predicate):
+        """Returns whether all elements of the stream match the given predicate"""
+        return all(predicate(i) for i in self.iterable)
+
+    def none_match(self, predicate):
+        """Returns whether no elements of the stream match the given predicate"""
+        return any(not predicate(i) for i in self.iterable)
+
+    def min(self, comparator=None, default=None):
+        """Returns the minimum element of the stream according to the given comparator"""
+        return Optional.of_nullable(min(self.iterable, key=comparator, default=default))
+
+    def max(self, comparator=None, default=None):
+        """Returns the maximum element of the stream according to the given comparator"""
+        return Optional.of_nullable(max(self.iterable, key=comparator, default=default))
+
+    def for_each(self, operation):
+        """Performs an action for each element of this stream"""
+        for i in self.iterable:
+            operation(i)
+
+    def reduce(self, accumulator, identity=None):
+        """Reduces the elements to a single one, by repeatedly applying a reducing operation.
+        Returns Optional with the result, if any, or None"""
+        if len(self.iterable) == 0:
+            return Optional.of_nullable(identity)
+
+        curr_iter = iter(self.iterable)
+        if identity is None:
+            identity = next(curr_iter)
+
+        for i in curr_iter:
+            identity = accumulator(identity, i)
+        return Optional.of_nullable(identity)
+
+    def compare_with(self, other, comparator=None):
+        """Compares current stream with another one based on a given comparator"""
+        return not any((comparator and not comparator(i, j)) or i != j for i, j in zip(self.iterable, other))
+
+    # ### collectors ###
+    def collect(self, collection_type, dict_collector=None, dict_merger=None, str_delimiter=None):
+        """Returns a collection from the stream.
+
+        In case of dict:
+        The 'dict_collector' function receives an element from the stream and returns a (key, value) pair or a DictItem
+        specifying how the dict should be constructed.
+
+        The 'dict_merger' functions indicates in the case of a collision (duplicate keys), which entry should be kept.
+        E.g. lambda old, new: new
+
+        In case of str:
+        Concatenates the elements of the Stream, separated by the specified 'str_delimiter'.
+        If no delimiter is provided, ", " is used as the default one"""
+        import builtins
+
+        match collection_type:
+            case builtins.tuple:
+                return self.to_tuple()
+            case builtins.list:
+                return self.to_list()
+            case builtins.set:
+                return self.to_set()
+            case builtins.dict:
+                return self.to_dict(dict_collector, dict_merger)
+            case builtins.str:
+                return self.to_string(str_delimiter)
+            case _:
+                raise ValueError("Invalid collection type")
+
+    def to_list(self):
+        """Returns a list of the elements of the current stream"""
+        return list(self.iterable)
+
+    def to_tuple(self):
+        """Returns a tuple of the elements of the current stream"""
+        return tuple(self.iterable)
+
+    def to_set(self):
+        """Returns a set of the elements of the current stream"""
+        return set(self.iterable)
+
+    def to_dict(self, collector=None, merger=None):
+        """Returns a dict of the elements of the current stream.
+
+        The 'collector' function receives an element from the stream and returns a (key, value) pair or a DictItem
+        specifying how the dict should be constructed.
+
+        The 'merger' functions indicates in the case of a collision (duplicate keys), which entry should be kept.
+        E.g. lambda old, new: new"""
+        result = {}
+        source = (collector(i) for i in self.iterable) if collector else self.iterable
+        for item in source:
+            k, v = self._unpack_dict_item(item)
+            if k in result:
+                if merger is None:
+                    raise IllegalStateError(f"Key '{k}' already exists")
+                v = merger(result[k], v)
+            result[k] = v
+        return result
+
+    def _unpack_dict_item(self, item):  # noqa
+        match item:
+            case tuple():
+                return item[0], item[1]
+            case DictItem():
+                # let's not make unnecessary calls to property getters
+                return item._key, item._value  # noqa
+            case _:
+                raise UnsupportedTypeError(f"Cannot create dict items from '{item.__class__.__name__}' type")
+
+    def to_string(self, delimiter=None):
+        """Concatenates the elements of the Stream, separated by the specified delimiter.
+        If no delimiter is provided, ", " is used as the default one"""
+        return self._join(delimiter)
+
+    def _join(self, delimiter=None):
+        if delimiter is None:
+            delimiter = ", "
+        return f"{self.__class__.__name__}({delimiter.join(str(i) for i in self.iterable)})"
+
+    def group_by(self, classifier=None, collector=None):
+        """Performs a "group by" operation on the elements of the stream according to a classification function.
+        Returns the results in a dict built using collector function (optionally provided by the user or via a default one)"""
+        if collector is None:
+            return {key: list(group) for key, group in self._group_by(classifier)}
+
+        result = {}
+        for key, group in self._group_by(classifier):
+            key, group = collector(key, list(group))
+            if hasattr(group, "__iter__"):
+                if key not in result:
+                    result[key] = []
+                result[key] += group
+            else:
+                result[key] = group
+        return result
+
+    def _group_by(self, classifier=None):
+        # https://docs.python.org/3/library/itertools.html#itertools.groupby
+        classifier = (lambda x: x) if classifier is None else classifier
+        iterator = iter(self.iterable)
+        exhausted = False
+
+        def _grouper(target_key):  # noqa
+            nonlocal curr_value, curr_key, exhausted
+            yield curr_value
+            for curr_value in iterator:
+                curr_key = classifier(curr_value)
+                if curr_key != target_key:
+                    return
+                yield curr_value
+            exhausted = True
+
+        try:
+            curr_value = next(iterator)
+        except StopIteration:
+            return
+        curr_key = classifier(curr_value)
+
+        while not exhausted:
+            target_key = curr_key
+            curr_group = _grouper(target_key)
+            yield curr_key, curr_group
+            if curr_key == target_key:
+                for _ in curr_group:
+                    pass
+
+    def quantify(self, predicate=bool):
+        """Count how many of the elements are Truthy or evaluate to True based on a given predicate"""
+        return sum(self.map(predicate))
+
+    # ### let's look nice ###
+    def __repr__(self):
+        return self._join()
---
+++ packages/pyrio/pyrio-1.3.4-py3-none-any.whl/pyrio/streams/file_stream.py
@@ -0,0 +1,196 @@
+import importlib
+from contextlib import contextmanager
+from pathlib import Path
+
+from pyrio.utils.dict_item import DictItem
+from pyrio.streams.base_stream import BaseStream
+from pyrio.streams.stream import Stream
+from pyrio.utils.exception import UnsupportedFileTypeError
+
+
+TEMP_PATH = "{file_path}.bak"
+GENERIC_READ_CONFIG = {
+    ".toml": {
+        "import_mod": "tomllib",
+        "callable": "load",
+        "read_mode": "rb",
+    },
+    ".json": {
+        "import_mod": "json",
+        "callable": "load",
+        "read_mode": "r",
+    },
+    ".yaml": {
+        "import_mod": "yaml",
+        "callable": "safe_load",
+        "read_mode": "r",
+    },
+    ".xml": {
+        "import_mod": "xmltodict",
+        "callable": "parse",
+        "read_mode": "rb",
+    },
+}
+
+GENERIC_WRITE_CONFIG = {
+    ".toml": {
+        "import_mod": "tomli_w",
+        "callable": "dump",
+        "write_mode": "wb",
+        "default_null_handler": lambda x: DictItem(x.key, "N/A") if x.value is None else x,
+    },
+    ".json": {
+        "import_mod": "json",
+        "callable": "dump",
+        "write_mode": "w",
+        "default_null_handler": None,
+    },
+    ".yaml": {
+        "import_mod": "yaml",
+        "callable": "dump",
+        "write_mode": "w",
+        "default_null_handler": None,
+    },
+    ".xml": {
+        "import_mod": "xmltodict",
+        "callable": "unparse",
+        "write_mode": "w",
+        "default_null_handler": None,
+    },
+}
+
+
+class FileStream(BaseStream):
+    """Derived Stream class for querying files; maps file content to im-memory dict structures and vice versa"""
+    # NB: Dirty deeds for a nice-looking API
+    def __init__(self, file_path):  # noqa
+        """Creates Stream from a file"""
+        pass
+
+    def __new__(cls, file_path, f_open_options=None, f_read_options=None, **kwargs):
+        obj = super().__new__(cls)
+        iterable = cls._read_file(file_path, f_open_options, f_read_options, **kwargs)
+        super(cls, obj).__init__(iterable)
+        obj.file_path = file_path
+        return obj
+
+    @classmethod
+    def process(cls, file_path, f_open_options=None, f_read_options=None, **kwargs):
+        """Creates Stream from a file with advanced 'reading' options passed by the user"""
+        return cls.__new__(cls, file_path, f_open_options, f_read_options, **kwargs)
+
+    # ### reading from file ###
+    @classmethod
+    def _read_file(cls, file_path, f_open_options=None, f_read_options=None, **kwargs):
+        path = cls._get_file_path(file_path)
+
+        if path.suffix in {".csv", ".tsv"}:
+            return cls._read_csv(path, f_open_options, f_read_options, **kwargs)
+        return cls._read_generic(path, f_open_options, f_read_options, **kwargs)
+
+    @staticmethod
+    def _read_csv(path, f_open_options=None, f_read_options=None, **kwargs):
+        import csv
+
+        if f_open_options is None:
+            f_open_options = {}
+        f_open_options["newline"] = ""
+        if f_read_options is None:
+            f_read_options = {}
+        f_read_options["delimiter"] = "\t" if path.suffix == ".tsv" else ","
+        with open(path, **(f_open_options or {})) as f:
+            return tuple(csv.DictReader(f, **f_read_options))
+
+    @staticmethod
+    def _read_generic(path, f_open_options=None, f_read_options=None, **kwargs):
+        suffix = ".yaml" if path.suffix == ".yml" else path.suffix
+        if suffix not in GENERIC_READ_CONFIG:
+            raise UnsupportedFileTypeError(f"Unsupported file type: '{suffix}'")
+
+        config = GENERIC_READ_CONFIG[suffix]
+        load = getattr(importlib.import_module(config["import_mod"]), config["callable"])
+        with open(path, config["read_mode"], **(f_open_options or {})) as f:
+            content = load(f, **(f_read_options or {}))
+            if suffix == ".xml":
+                if kwargs.get("include_root", None):
+                    return content
+                # NB: return dict (instead of dict_view) to re-map it later as DictItem records
+                return next(iter(content.values()))
+            return content
+
+    # ### writing to file ###
+    def save(self, file_path=None, null_handler=None, f_open_options=None, f_write_options=None, **kwargs):
+        """Writes Stream to a new file (or updates an existing one) with advanced 'writing' options passed by the user"""
+        path, tmp_path = self._prepare_file_paths(file_path)
+        if path.suffix in {".csv", ".tsv"}:
+            return self._write_csv(path, tmp_path, null_handler, f_open_options, f_write_options, **kwargs)
+        return self._write_generic(path, tmp_path, null_handler, f_open_options, f_write_options, **kwargs)
+
+    def _write_csv(
+        self, path, tmp_path, null_handler=None, f_open_options=None, f_write_options=None, **kwargs
+    ):
+        import csv
+
+        if null_handler:
+            self.map(null_handler)
+        output = self.map(lambda x: Stream(x).to_dict()).to_list()
+
+        if f_write_options is None:
+            f_write_options = {}
+        f_write_options["delimiter"] = "\t" if path.suffix == ".tsv" else ","
+        f_write_options["fieldnames"] = output[0].keys() if output else ()
+        with self._atomic_write(path, tmp_path, "w", f_open_options) as f:
+            writer = csv.DictWriter(f, **f_write_options)
+            writer.writeheader()
+            writer.writerows(output)
+
+    def _write_generic(
+        self, path, tmp_path, null_handler=None, f_open_options=None, f_write_options=None, **kwargs
+    ):
+        suffix = ".yaml" if path.suffix == ".yml" else path.suffix
+        if suffix not in GENERIC_WRITE_CONFIG:
+            raise UnsupportedFileTypeError(f"Unsupported file type: '{suffix}'")
+        config = GENERIC_WRITE_CONFIG[suffix]
+        if existing_null_handler := null_handler or config["default_null_handler"]:
+            self.map(existing_null_handler)
+
+        output = self.to_dict()
+        if suffix == ".xml":
+            root = kwargs.get("xml_root", "root")
+            output = {root: output}
+            if f_write_options is None:
+                f_write_options = {}
+            f_write_options["pretty"] = True
+
+        dump = getattr(importlib.import_module(config["import_mod"]), config["callable"])
+        with self._atomic_write(path, tmp_path, config["write_mode"], f_open_options) as f:
+            dump(output, f, **(f_write_options or {}))
+
+    # ### helpers ###
+    @staticmethod
+    def _get_file_path(file_path, read_mode=True):
+        path = Path(file_path)
+        if read_mode and not path.exists():
+            raise FileNotFoundError(f"No such file or directory: '{file_path}'")
+        if path.is_dir():
+            raise IsADirectoryError(f"Given path '{file_path}' is a directory")
+        return path
+
+    def _prepare_file_paths(self, file_path):
+        if file_path is None:
+            file_path = self.file_path
+        path = self._get_file_path(file_path, read_mode=False)
+        tmp_path = Path(TEMP_PATH.format(file_path=self.file_path))
+        if tmp_path.exists():
+            raise FileExistsError(f"Temporary file {tmp_path} already exists")
+        return path, tmp_path
+
+    @contextmanager
+    def _atomic_write(self, path, tmp_path, mode="w", f_open_options=None):
+        try:
+            with open(tmp_path, mode, **(f_open_options or {})) as f:
+                yield f
+            tmp_path.replace(path)
+        except (IOError, Exception) as e:
+            tmp_path.unlink(missing_ok=True)
+            raise e
