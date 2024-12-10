b833b8d93417fcfd30819acf0eb4315766979b01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 14:04:21 +0000

    Add package flask-http-middleware/0.4.4/flask_http_middleware-0.4.4.tar.gz

---
+++ packages/flask-http-middleware/flask_http_middleware-0.4.4.tar.gz/flask_http_middleware-0.4.4/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+VERSION = "0.4.4"
+DESCRIPTION = "A module to create middleware with direct access to `request` and `response`"
+
+# Setting up
+setup(
+    name="flask-http-middleware",
+    version=VERSION,
+    author="danangjoyoo (Agus Danangjoyo)",
+    author_email="<agus.danangjoyo.blog@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=["flask","werkzeug"],
+    keywords=['flask', 'middleware', 'http', 'request', "response"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Environment :: Web Environment",
+        "Operating System :: OS Independent",
+        "Typing :: Typed"
+    ],
+    url="https://github.com/Danangjoyoo/flask-http-middleware"
+)
