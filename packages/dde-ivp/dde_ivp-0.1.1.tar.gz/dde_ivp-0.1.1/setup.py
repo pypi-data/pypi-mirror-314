ec37069c82d0e71f28e30ea1a8edd3d417d3dacf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 14:04:22 +0000

    Add package dde-ivp/0.1.1/dde_ivp-0.1.1.tar.gz

---
+++ packages/dde-ivp/dde_ivp-0.1.1.tar.gz/dde_ivp-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dde_ivp",
+    version="0.1.1",
+    author="Dhamdhawach  Horsuwan",
+    author_email="meng.inventor@gmail.com",
+    description="A package for solving delay differential equations",
+    long_description=open("README.md").read(),  # Read from README.md
+    long_description_content_type="text/markdown",  # Specify Markdown format
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "scipy",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
