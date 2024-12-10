6e2e2e227da0ec2f515c28552a268d3b9ed3d814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 16:06:33 +0000

    Add package slickconf/0.1.8/slickconf-0.1.8.tar.gz

---
+++ packages/slickconf/slickconf-0.1.8.tar.gz/slickconf-0.1.8/slickconf/__init__.py
@@ -0,0 +1,15 @@
+from slickconf.builder import (
+    F,
+    Field,
+    L,
+    placeholder,
+    single,
+    function,
+    tag,
+    call,
+    config_fn,
+)
+from slickconf.container import field
+from slickconf.config import Config, Instance, MainConfig, instantiate
+from slickconf.loader import build_config, load_arg_config, load_config
+from slickconf.selector import select
