721ac66c63b0a1eaea8f754c4da45d38e883b830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 15:04:52 +0000

    Add package python-template-x/0.4.1/python_template_x-0.4.1-py3-none-any.whl

---
+++ packages/python-template-x/python_template_x-0.4.1-py3-none-any.whl/python_template/__init__.py
@@ -0,0 +1,12 @@
+"""Handle initialization routines for the app module.
+
+Attributes
+----------
+    __version__: The version of the this module.
+
+
+"""
+
+from __future__ import annotations
+
+__version__: str = "0.4.1"
