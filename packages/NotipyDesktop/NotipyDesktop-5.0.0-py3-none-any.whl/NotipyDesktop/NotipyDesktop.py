0e28d042672c94545302a447ea4418de6daf2c52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 04:05:24 +0000

    Add package NotipyDesktop/5.0.0/NotipyDesktop-5.0.0-py3-none-any.whl

---
+++ packages/NotipyDesktop/NotipyDesktop-5.0.0-py3-none-any.whl/NotipyDesktop/NotipyDesktop.py
@@ -0,0 +1,18 @@
+import sys
+import gi
+gi.require_version('Notify', '0.7')  # Version festlegen
+from gi.repository import Notify
+
+def notify(app_name, title, message):
+    Notify.init(app_name)
+    notification = Notify.Notification.new(title, message)
+    notification.show()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:  # sys.argv[0] (Skriptname) + 3 Argumente
+        print("Usage: python3 PyNotifier.py <app_name> <title> <message>")
+    else:
+        app_name = sys.argv[1]
+        title = sys.argv[2]
+        message = sys.argv[3]
+        notify(app_name, title, message)
