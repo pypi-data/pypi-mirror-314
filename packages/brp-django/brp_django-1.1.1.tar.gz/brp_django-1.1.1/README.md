4086fa9bef087473e0a18479a12f8592902a63eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 16:06:30 +0000

    Add package brp-django/1.1.1/brp_django-1.1.1.tar.gz

---
+++ packages/brp-django/brp_django-1.1.1.tar.gz/brp_django-1.1.1/README.md
@@ -0,0 +1,9 @@
+#Building my django package
+Check : https://pypi.org/project/brp-django/1.0.0/
+
+i.authentication 
+ii.base for model and usecase
+iii.utilities
+
+
+# django-my_package
---
+++ packages/brp-django/brp_django-1.1.1.tar.gz/brp_django-1.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+    
+setup(
+    name="brp_django",
+    version="1.1.1",
+    author="Binay Raj Parajuli",
+    author_email="binayaparajuli17@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "Django>=4.0",
+        "djangorestframework>=3.14",
+    ],
+    entry_points={
+        # "console_scripts":[
+        #     "brp_django = brp_django:my_name",
+        # ],
+    },
+    long_description=description,
+    long_description_content_type="text/markdown"
+)
