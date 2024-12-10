16a3fd8a593dfecac127c90e0374742f1efd24ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 05:04:30 +0000

    Add package pypaystack2/2.1.1/pypaystack2-2.1.1-py3-none-any.whl

---
+++ packages/pypaystack2/pypaystack2-2.1.1-py3-none-any.whl/pypaystack2/_metadata.py
@@ -0,0 +1,5 @@
+__title__ = "pypaystack2"
+__version__ = "2.1.1"
+__author__ = [{"name": "Gbenga Adeyi", "email": "adeyigbenga005@gmail.com"}]
+__license__ = "MIT"
+__copyright__ = "Copyright 2024."
