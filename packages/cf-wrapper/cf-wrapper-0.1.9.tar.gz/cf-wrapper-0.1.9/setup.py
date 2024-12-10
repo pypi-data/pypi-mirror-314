98b80790f99c922ea80a3561a4e7b489c4940a25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 03:18:53 +0000

    Add package cf-wrapper/0.1.9/cf-wrapper-0.1.9.tar.gz

---
+++ packages/cf-wrapper/cf-wrapper-0.1.9.tar.gz/cf-wrapper-0.1.9/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cf-wrapper",
+    version="0.1.9",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "boto3>=1.26.0",
+        "requests>=2.28.0",
+        "python-dotenv>=0.19.0",
+        "click>=8.0.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "cfw=cf_wrapper.cli:cli",
+        ],
+    },
+)
