99785bff30cd0cbfc569b8cb3faf5c21a1d0e81f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 04:05:24 +0000

    Add package lensecam/0.2.9/lensecam-0.2.9-py3-none-any.whl

---
+++ packages/lensecam/lensecam-0.2.9-py3-none-any.whl/lensecam/ids/__init__.py
@@ -0,0 +1,5 @@
+__all__ = [
+    "camera_ids",           # refers to the 'camera_ids.py' file
+    "camera_ids_widget",    # refers to the 'camera_ids_widget.py' file
+    "camera_list",          # refers to the 'camera_list.py' file
+]
