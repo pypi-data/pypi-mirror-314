3a8f77ab79757ac7b68456bfc22497a7f5b998b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 04:04:13 +0000

    Add package NTkpp/0.0.3.2/ntkpp-0.0.3.2.tar.gz

---
+++ packages/NTkpp/ntkpp-0.0.3.2.tar.gz/ntkpp-0.0.3.2/NTPKKP/__init__.py
@@ -0,0 +1,5 @@
+from .layers import DenseLayer
+from .network import NeuralNetwork
+from .activations import ReLU, Sigmoid, Tanh
+from .losses import MeanSquaredError, CrossEntropyLoss, HingeLoss
+from .train import train
