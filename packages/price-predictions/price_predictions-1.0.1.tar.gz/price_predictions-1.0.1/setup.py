478caf61b0c4a25e6da5ce153b0768657dbdead7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 03:18:39 +0000

    Add package price-predictions/1.0.1/price_predictions-1.0.1.tar.gz

---
+++ packages/price-predictions/price_predictions-1.0.1.tar.gz/price_predictions-1.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='price_predictions',
+    version="1.0.1",
+    author="Milad",
+    author_email="heregoesnothingowo@gmail.com",
+    url="https://github.com/awkwarrd/ds-project-milad-almasri.git",
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scikit-learn==1.5.1',
+        'category_encoders',
+        'Boruta',
+        'hyperopt',
+    ],
+    python_requires=">=3.6",
+)
