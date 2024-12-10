145ade434c371959eb457d9bdf329cd7379de0fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 15:05:16 +0000

    Add package evenv/1.0.0/evenv-1.0.0.tar.gz

---
+++ packages/evenv/evenv-1.0.0.tar.gz/evenv-1.0.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="evenv",
+    version="1.0.0",
+    author="artemdorozhkin",
+    author_email="aa.dorozhkin@ya.ru",
+    description="Creates virtual embeddable Python "
+        "environments in one or "
+        "more target "
+        "directories.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/artemdorozhkin/evenv",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    package_data={
+        "": ["bundled/*.zip"],
+    },
+    include_package_data=True, 
+)
