e027f1181f50211df0ea39c68f987a1fb3102238
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 16:04:44 +0000

    Add package django-success-response/1.0.1/django_success_response-1.0.1-py3-none-any.whl

---
+++ packages/django-success-response/django_success_response-1.0.1-py3-none-any.whl/success_response/views.py
@@ -0,0 +1,33 @@
+from rest_framework.views import exception_handler
+from .response import SuccessResponse
+
+
+def success_exception_handler(exc, context):
+    """
+    Custom exception handler to format all error responses using SuccessResponse.
+
+    :param exc: The exception instance.
+    :param context: The context in which the exception occurred.
+    :return: Formatted error response.
+    """
+    # Call the default DRF exception handler for the initial response.
+    response = exception_handler(exc, context)
+
+    if response is not None:
+        data = response.data
+
+        # Ensure the response data is a dictionary for consistent formatting.
+        if not isinstance(data, dict):
+            # Extract the first error message if the data isn't already a dictionary.
+            data = {'detail': str(data)}
+
+        # Wrap the error response in a standardized format.
+        response = SuccessResponse(data, success=False)
+    else:
+        # Return a generic internal server error response if no response was created.
+        response = SuccessResponse(
+            {'detail': 'Internal Server Error'},
+            success=False,
+        )
+
+    return response
