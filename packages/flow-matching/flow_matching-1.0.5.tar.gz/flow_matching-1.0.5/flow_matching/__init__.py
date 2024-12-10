e926e289c210ded2447a6ca785edd5c9df8ec40c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 15:05:22 +0000

    Add package flow-matching/1.0.5/flow_matching-1.0.5.tar.gz

---
+++ packages/flow-matching/flow_matching-1.0.5.tar.gz/flow_matching-1.0.5/flow_matching/__init__.py
@@ -0,0 +1,16 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the CC-by-NC license found in the
+# LICENSE file in the root directory of this source tree.
+
+from flow_matching import loss, path, solver, utils
+
+__all__ = [
+    "loss",
+    "path",
+    "solver",
+    "utils"
+]
+
+__version__ = "1.0.5"
