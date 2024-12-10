c3c160f53bced20eedd282f1860e54c30e670099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 07:04:11 +0000

    Add package quill/1.0.0/quill-1.0.0-py3-none-any.whl

---
+++ packages/quill/quill-1.0.0-py3-none-any.whl/quill/logger/__init__.py
@@ -0,0 +1,19 @@
+from .logger import (
+    error,
+    success,
+    warning,
+    get_progress,
+    debug,
+    is_verbose,
+    set_verbose
+)
+
+__all__ = [
+    'error',
+    'success',
+    'warning',
+    'get_progress',
+    'debug',
+    'is_verbose',
+    'set_verbose'
+]
