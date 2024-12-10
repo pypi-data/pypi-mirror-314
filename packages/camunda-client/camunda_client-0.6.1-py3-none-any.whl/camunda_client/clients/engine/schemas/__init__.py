21092f0dcd1873f116bda3c2c4b936099142a653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 04:04:46 +0000

    Add package camunda-client/0.6.1/camunda_client-0.6.1-py3-none-any.whl

---
+++ packages/camunda-client/camunda_client-0.6.1-py3-none-any.whl/camunda_client/clients/engine/schemas/__init__.py
@@ -0,0 +1,30 @@
+from .body import (
+    GetHistoryTasksFilterSchema,
+    GetTasksFilterSchema,
+    StartProcessInstanceSchema,
+    SendCorrelationMessageSchema,
+)
+from .query import ProcessInstanceQuerySchema
+from .response import (
+    HistoricTaskInstanceSchema,
+    LinkSchema,
+    ProcessDefinitionSchema,
+    ProcessInstanceSchema,
+    TaskSchema,
+    VariableInstanceSchema,
+)
+
+
+__all__ = [
+    "StartProcessInstanceSchema",
+    "ProcessInstanceQuerySchema",
+    "GetHistoryTasksFilterSchema",
+    "ProcessInstanceSchema",
+    "GetTasksFilterSchema",
+    "LinkSchema",
+    "ProcessDefinitionSchema",
+    "TaskSchema",
+    "HistoricTaskInstanceSchema",
+    "VariableInstanceSchema",
+    "SendCorrelationMessageSchema",
+]
