dc94971822b3a1148165a7ecea8ca29d27bfbec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 03:19:06 +0000

    Add package sparse/0.16.0b2/sparse-0.16.0b2-py2.py3-none-any.whl

---
+++ packages/sparse/sparse-0.16.0b2-py2.py3-none-any.whl/sparse/mlir_backend/__init__.py
@@ -0,0 +1,56 @@
+try:
+    import mlir_finch  # noqa: F401
+
+    del mlir_finch
+except ModuleNotFoundError as e:
+    raise ImportError(
+        "MLIR Python bindings not installed. Run "
+        "`conda install conda-forge::mlir-python-bindings` "
+        "to enable MLIR backend."
+    ) from e
+
+from . import formats
+from ._array import Array
+from ._conversions import asarray, from_constituent_arrays, to_numpy, to_scipy
+from ._dtypes import (
+    asdtype,
+    complex64,
+    complex128,
+    float16,
+    float32,
+    float64,
+    int8,
+    int16,
+    int32,
+    int64,
+    uint8,
+    uint16,
+    uint32,
+    uint64,
+)
+from ._ops import add, reshape
+
+__all__ = [
+    "Array",
+    "add",
+    "asarray",
+    "asdtype",
+    "to_numpy",
+    "to_scipy",
+    "formats",
+    "reshape",
+    "from_constituent_arrays",
+    "int8",
+    "int16",
+    "int32",
+    "int64",
+    "uint8",
+    "uint16",
+    "uint32",
+    "uint64",
+    "float16",
+    "float32",
+    "float64",
+    "complex64",
+    "complex128",
+]
