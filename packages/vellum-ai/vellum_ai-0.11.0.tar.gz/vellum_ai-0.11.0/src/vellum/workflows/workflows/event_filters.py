8b3bc1ad0e0f3f054cf05267b04ba0b62648bc3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 04:05:25 +0000

    Add package vellum-ai/0.11.0/vellum_ai-0.11.0.tar.gz

---
+++ packages/vellum-ai/vellum_ai-0.11.0.tar.gz/vellum_ai-0.11.0/src/vellum/workflows/workflows/event_filters.py
@@ -0,0 +1,53 @@
+from typing import TYPE_CHECKING, Type
+
+from vellum.workflows.events.types import CodeResourceDefinition
+
+if TYPE_CHECKING:
+    from vellum.workflows.events.workflow import WorkflowEvent
+    from vellum.workflows.workflows.base import BaseWorkflow
+
+
+def workflow_event_filter(workflow_definition: Type["BaseWorkflow"], event: "WorkflowEvent") -> bool:
+    """
+    Filters for only Workflow events that were emitted by the `workflow_definition` parameter.
+    """
+
+    if (
+        event.name == "workflow.execution.initiated"
+        or event.name == "workflow.execution.resumed"
+        or event.name == "workflow.execution.fulfilled"
+        or event.name == "workflow.execution.rejected"
+        or event.name == "workflow.execution.paused"
+        or event.name == "workflow.execution.streaming"
+    ):
+        return event.workflow_definition == workflow_definition
+
+    return False
+
+
+def root_workflow_event_filter(workflow_definition: Type["BaseWorkflow"], event: "WorkflowEvent") -> bool:
+    """
+    Filters for Workflow and Node events that were emitted by the `workflow_definition` parameter.
+    """
+
+    if (
+        event.name == "workflow.execution.initiated"
+        or event.name == "workflow.execution.resumed"
+        or event.name == "workflow.execution.fulfilled"
+        or event.name == "workflow.execution.rejected"
+        or event.name == "workflow.execution.paused"
+        or event.name == "workflow.execution.streaming"
+    ):
+        return event.workflow_definition == workflow_definition
+
+    if not event.parent:
+        return False
+
+    if event.parent.type != "WORKFLOW":
+        return False
+
+    return event.parent.workflow_definition == CodeResourceDefinition.encode(workflow_definition)
+
+
+def all_workflow_event_filter(workflow_definition: Type["BaseWorkflow"], event: "WorkflowEvent") -> bool:
+    return True
