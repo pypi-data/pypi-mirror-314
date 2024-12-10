7abac26a546077e5819c957d095dd2468105f2dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 05:03:49 +0000

    Add package sputchedtools/0.20.0/sputchedtools-0.20.0.tar.gz

---
+++ packages/sputchedtools/sputchedtools-0.20.0.tar.gz/sputchedtools-0.20.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+	name = 'sputchedtools',
+	version = '0.20.0',
+	packages = find_packages(),
+	py_modules = ['sputchedtools'],
+	install_requires = [
+		'aiohttp>=3.11.10',
+		'aiofiles>=24.1.0',
+		# uvloop/winloop
+	],
+	author = 'Sputchik',
+	author_email = 'sputchik@gmail.com',
+	url = 'https://github.com/Sputchik/sputchedtools',
+	python_requires = '>=3.8',
+)
