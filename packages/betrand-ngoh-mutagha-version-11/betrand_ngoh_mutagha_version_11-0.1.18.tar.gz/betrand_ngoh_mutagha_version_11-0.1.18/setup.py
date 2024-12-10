f755a8b39466d933f67ba0fc4b5821e26fbd7bbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 15:05:38 +0000

    Add package betrand-ngoh-mutagha-version-11/0.1.18/betrand_ngoh_mutagha_version_11-0.1.18.tar.gz

---
+++ packages/betrand-ngoh-mutagha-version-11/betrand_ngoh_mutagha_version_11-0.1.18.tar.gz/betrand_ngoh_mutagha_version_11-0.1.18/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="betrand_ngoh_mutagha_version_11",  # Updated to reflect your project name
+    version="0.1.18",
+    description="A simple CI/CD Flask application",
+    author="BETRAND MUTAGHA",  # Replace with your name if needed
+    author_email="mutagha2@gmail.com",  # Replace with your email
+    url="https://github.com/Betrand1999/cicd",  # Updated with your repo URL
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "flask",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
+
+#
