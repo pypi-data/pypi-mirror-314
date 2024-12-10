d8a249cd112397ce73f23245a483051e8e1111e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 03:18:40 +0000

    Add package six-dxcses/1.0.0/six_dxcses-1.0.0.tar.gz

---
+++ packages/six-dxcses/six_dxcses-1.0.0.tar.gz/six_dxcses-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="six-dxcses",
+    version="1.0.0",
+    packages=find_packages(),
+    include_package_data=True,
+    description="A customized version of six module limited to AWSPE",
+    author="AWS Platform Engineering",
+    author_email="abc@example.com",
+    url="https://github.com/benjaminp/six",
+    install_requires=[],
+)
