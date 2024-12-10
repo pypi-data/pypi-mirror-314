5310b48545db34ac46d1e55ce0f9722a5da57627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 06:04:09 +0000

    Add package iencoder/0.1/iencoder-0.1.tar.gz

---
+++ packages/iencoder/iencoder-0.1.tar.gz/iencoder-0.1/README.md
@@ -0,0 +1,7 @@
+# i-encoding
+
+## Authors
+
+- [Anida Nezovic](https://github.com/anezovic1)
+- [Dr Aida Brankovic](https://scholar.google.it/citations?user=Lh3kj1MAAAAJ&hl=en)
+- [Dr Jin Yoon](https://scholar.google.com.au/citations?user=Ol5i7bcAAAAJ&hl=en)
---
+++ packages/iencoder/iencoder-0.1.tar.gz/iencoder-0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='iencoder',  
+    version='0.1',  
+    packages=find_packages(),  
+    description='Custom encoder for handling categorical variables with special encoding techniques.',  
+    long_description=open('README.md').read(),  
+    long_description_content_type='text/markdown',  
+    author='Anida Nezovic',  
+    author_email='nezovicanida@gmail.com', 
+    url='https://github.com/anezovic1/i-encoding',  
+    license='MIT',  
+    classifiers=[  
+        'Development Status :: 3 - Alpha',  
+        'Intended Audience :: Science/Research',  
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    python_requires='>=3.7',  
+    install_requires=[  
+        'numpy',  
+        'pandas',
+        'scikit-learn',
+    ],
+    include_package_data=True,  
+)
