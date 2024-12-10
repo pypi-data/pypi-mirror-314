ee1e23de0611da21f58334c7a94d0bc45f42bddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 16:05:41 +0000

    Add package wwttoolbox/0.15.0/wwttoolbox-0.15.0-py3-none-any.whl

---
+++ packages/wwttoolbox/wwttoolbox-0.15.0-py3-none-any.whl/wwttoolbox/qaqc/anomaly_detection/__init__.py
@@ -0,0 +1,11 @@
+from wwttoolbox.qaqc.anomaly_detection.anomaly_detection_algorithm import (
+    AnomalyDetectionAlgorithm,
+)
+from wwttoolbox.qaqc.anomaly_detection.anomaly_detection_config import (
+    AnomalyDetectionConfig,
+)
+
+from wwttoolbox.qaqc.anomaly_detection.quality_codes import QualityCodes
+from wwttoolbox.qaqc.anomaly_detection.anomaly_detection_validation import (
+    plot_dataframe,
+)
