dea8ddf35c17441100202fdd7722f72ee37f0961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 17:04:58 +0000

    Add package ivtmetrics/0.1.5/ivtmetrics-0.1.5.tar.gz

---
+++ packages/ivtmetrics/ivtmetrics-0.1.5.tar.gz/ivtmetrics-0.1.5/setup.py
@@ -0,0 +1,44 @@
+from distutils.core import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name='ivtmetrics',
+    version='0.1.5',    
+    packages=['ivtmetrics'],
+    author='Chinedu Nwoye',
+    author_email='nwoye.chinedu@gmail.com',    
+    description='A Python evaluation metrics package for action triplet recognition',
+    keywords = ['triplet', 'average precision', 'AP', 'mAP'], 
+    long_description = long_description,
+    # long_description_content_type ='text/x-rst',
+    long_description_content_type ='text/markdown',
+    url='https://github.com/CAMMA-public/ivtmetrics',
+    download_url = 'https://github.com/CAMMA-public/ivtmetrics/archive/refs/tags/v0.1.5.tar.gz', 
+    include_package_data=True,
+    license='BSD 2-clause', # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    install_requires=['scikit-learn>=1.0.2',
+                      'numpy>=1.21',
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning', # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Science/Research', # Define that your audience are developers
+        'License :: OSI Approved :: BSD License',  
+        'Topic :: Software Development :: Build Tools',  
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',   
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)    
+
+# guide @ https://medium.com/@joel.barmettler/how-to-upload-your-python-package-to-pypi-65edc5fe9c56
+# 'Operating System :: POSIX :: Linux :: Windows :: Mac :: Unix',  
+#    package_data={'': ['ivtmetrics/maps.txt', 'README.md', 'README.rst']}, 
+#    
