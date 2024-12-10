179a1b59a542463e7bcb631b27041e78206044ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 15:05:11 +0000

    Add package qq/0.1.5/qq-0.1.5.tar.gz

---
+++ packages/qq/qq-0.1.5.tar.gz/qq-0.1.5/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="qq",
+    version="0.1.5",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.31.0",
+        "rich>=13.7.0",
+        "anthropic>=0.18.1"
+    ],
+    entry_points={
+        'console_scripts': [
+            'qq=quickquestion.qq:main',
+        ],
+    },
+    author="Cristian Vyhmeister",
+    author_email="cv@southbrucke.com",
+    description="A CLI tool for getting quick command-line suggestions using any LLM potentially available",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://southbrucke.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Topic :: Utilities",
+        "Topic :: System :: Shells",
+        "Topic :: Terminals",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: End Users/Desktop"
+    ],
+    python_requires=">=3.6",
+    license="Proprietary",
+    license_files=("LICENSE",),
+)
