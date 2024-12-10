64a64c4b8ddeeec1222fc906074895af1d0c7dca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 05:04:24 +0000

    Add package TANN/0.1/tann-0.1.tar.gz

---
+++ packages/TANN/tann-0.1.tar.gz/tann-0.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+setuptools.setup(
+    name= 'TANN',
+    version= '0.1',
+    author= 'TAN',
+    description= 'MT FIRST LIB TEST IT ',
+    packages=setuptools.find_packages(),
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License"
+    ]
+)
