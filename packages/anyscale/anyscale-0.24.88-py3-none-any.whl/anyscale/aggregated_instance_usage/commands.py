620b82ddc6e4ed789a337442ea767332833b24a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 14:04:33 +0000

    Add package anyscale/0.24.88/anyscale-0.24.88-py3-none-any.whl

---
+++ packages/anyscale/anyscale-0.24.88-py3-none-any.whl/anyscale/aggregated_instance_usage/commands.py
@@ -0,0 +1,42 @@
+from anyscale._private.sdk import sdk_command
+from anyscale.aggregated_instance_usage._private.aggregated_instance_usage_sdk import (
+    PrivateAggregatedInstanceUsageSDK,
+)
+from anyscale.aggregated_instance_usage.models import DownloadCSVFilters
+
+
+_AGGREGATED_INSTANCE_USAGE_SDK_SINGLETON_KEY = "aggregated_instance_usage_sdk"
+
+_DOWNLOAD_CSV_EXAMPLE = """
+import anyscale
+from anyscale.aggregated_instance_usage.models import DownloadCSVFilters
+
+anyscale.aggregated_instance_usage.download_csv(
+    DownloadCSVFilters(
+        start_date="2024-10-01",
+        end_date="2024-10-02",
+        cloud="cloud_name",
+        project="project_name",
+        directory="/directory",
+        hide_progress_bar=False,
+    ),
+)
+"""
+
+_DOWNLOAD_ARG_DOCSTRINGS = {
+    "filters": "The filter of the instance usage to be downloaded."
+}
+
+
+@sdk_command(
+    _AGGREGATED_INSTANCE_USAGE_SDK_SINGLETON_KEY,
+    PrivateAggregatedInstanceUsageSDK,
+    doc_py_example=_DOWNLOAD_CSV_EXAMPLE,
+    arg_docstrings=_DOWNLOAD_ARG_DOCSTRINGS,
+)
+def download_csv(
+    filters: DownloadCSVFilters, *, _sdk: PrivateAggregatedInstanceUsageSDK
+) -> str:
+    """Downloads aggregated instance usage CSV.
+    """
+    return _sdk.download_csv(filters)
