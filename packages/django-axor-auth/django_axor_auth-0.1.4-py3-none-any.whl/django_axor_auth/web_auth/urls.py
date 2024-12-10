1486957c5be8ffc3d612c7065dfc55adcc51d7bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 16:06:12 +0000

    Add package django-axor-auth/0.1.4/django_axor_auth-0.1.4-py3-none-any.whl

---
+++ packages/django-axor-auth/django_axor_auth-0.1.4-py3-none-any.whl/django_axor_auth/web_auth/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.sign_in_page, name='login'),
+    path('sign-out/', views.sign_out_page, name='logout'),
+    path('forgot-password/', views.forgot_password_page, name='forgot_password'),
+    path('forgot-password/process/', views.process_forgot_password, name='process_forgot_password'),
+    path('magic-link/process/', views.process_magic_link, name='process_magic_link'),
+    path('verify-email/process/', views.process_verify_email, name='process_verify_email'),
+]
