ae1cf738f0ef55fce5a11834da6ab19e73fb5c54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 05:04:02 +0000

    Add package hspylib-askai/1.2.9/hspylib_askai-1.2.9-py3-none-any.whl

---
+++ packages/hspylib-askai/hspylib_askai-1.2.9-py3-none-any.whl/askai/core/model/model_result.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+   @project: HsPyLib-AskAI
+   @package: askai.core.model
+      @file: summary_result.py
+   @created: Tue, 11 Mar 2024
+    @author: <B>H</B>ugo <B>S</B>aporetti <B>J</B>unior
+      @site: https://github.com/yorevs/askai
+   @license: MIT - Please refer to <https://opensource.org/licenses/MIT>
+
+   Copyright (c) 2024, AskAI
+"""
+from dataclasses import dataclass
+from functools import lru_cache
+
+
+@dataclass
+class ModelResult:
+    """Keep track of the model select responses."""
+
+    mid: str = None
+    goal: str = None
+    reason: str = None
+
+    @staticmethod
+    @lru_cache
+    def default() -> "ModelResult":
+        """Track and store the responses from the selected model.
+        This class is used to encapsulate the model selection returned by the LLM, including any relevant data
+        associated with the model's response.
+        """
+        return ModelResult(
+            "ASK_000", "Default model", "Provide the answer as received by the AI"
+        )
+
+    def __str__(self):
+        return f"{self.mid} -> {self.reason}"
---
+++ packages/hspylib-askai/hspylib_askai-1.2.9-py3-none-any.whl/askai/core/support/__init__.py
@@ -0,0 +1,19 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-askai v1.2.9
+#
+# Package: main.askai.core.support
+"""Package initialization."""
+
+__all__ = [
+    'chat_context', 
+    'langchain_support', 
+    'llm_parser', 
+    'os_utils', 
+    'presets', 
+    'shared_instances', 
+    'spinner', 
+    'text_formatter', 
+    'utilities'
+]
+__version__ = '1.2.9'
