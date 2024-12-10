2817fe4a93861b78fa37f897c3809d04f9d3cdcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 04:05:25 +0000

    Add package tensorlink/0.1.0.post1/tensorlink-0.1.0.post1.tar.gz

---
+++ packages/tensorlink/tensorlink-0.1.0.post1.tar.gz/tensorlink-0.1.0.post1/tensorlink/mpc/nodes.py
@@ -0,0 +1,289 @@
+from tensorlink.roles.user import User
+from tensorlink.roles.validator import Validator
+from tensorlink.roles.worker import Worker
+from tensorlink.ml.module import DistributedModel
+from tensorlink.ml.worker import DistributedWorker
+from tensorlink.ml.optim import create_distributed_optimizer
+from tensorlink.ml.graphing import handle_layers
+from tensorlink.ml.utils import access_module
+
+import multiprocessing
+import threading
+import miniupnpc
+import itertools
+import logging
+import signal
+import atexit
+import torch
+import time
+import sys
+
+
+def spinning_cursor():
+    """Generator for a spinning cursor animation."""
+    for cursor in "|/-\\":
+        yield cursor
+
+
+def show_spinner(stop_event, message="Processing"):
+    """
+    Displays a spinner in the console.
+
+    Args:
+        stop_event (threading.Event): Event to signal when to stop the spinner.
+        message (str): The message to display alongside the spinner.
+    """
+    spinner = spinning_cursor()
+    while not stop_event.is_set():
+        sys.stdout.write(f"\r{message} {next(spinner)}")
+        sys.stdout.flush()
+        time.sleep(0.1)
+    sys.stdout.write("\r" + " " * (len(message) + 2) + "\r")  # Clear the line
+    sys.stdout.flush()
+
+
+# Set the start method to 'spawn'
+multiprocessing.set_start_method('spawn', force=True)
+
+
+class BaseNode:
+    def __init__(
+            self,
+            upnp=True,
+            max_connections: int = 0,
+            off_chain_test=False,
+            local_test=False,
+            print_level=logging.WARNING
+    ):
+        self.node_requests = multiprocessing.Queue()
+        self.node_responses = multiprocessing.Queue()
+        self.mpc_lock = multiprocessing.Lock()
+
+        self.init_kwargs = {
+            "print_level": print_level,
+            "max_connections": max_connections,
+            "upnp": upnp,
+            "off_chain_test": off_chain_test,
+            "local_test": local_test
+        }
+
+        self.upnp_enabled = upnp
+
+        self.node_process = None
+
+        signal.signal(signal.SIGINT, self.signal_handler)  # Handle Ctrl+C
+        # signal.signal(signal.SIGTERM, self.signal_handler)
+        # atexit.register(self.cleanup)
+
+        self._initialized = True
+        self.setup()
+
+    def setup(self):
+        self.node_process = multiprocessing.Process(target=self.run_role, daemon=True)
+        self.node_process.start()
+
+    def signal_handler(self, sig, frame):
+        """Handle termination signals and call cleanup, most importantly removing open port mappings"""
+        print(f"Received signal {sig}. Cleaning up...")
+        self.cleanup()
+        sys.exit(0)
+
+    def cleanup(self):
+        # Process cleanup
+        if self.node_process is not None and self.node_process.exitcode is None:
+            # Send a stop request to the role instance
+            response = self.send_request("stop", (None,), timeout=3)
+            if response:
+                self.node_process.join(timeout=10)
+
+            # If the process is still alive, terminate it
+            if self.node_process.is_alive():
+                print("Forcing termination for node process.")
+                self.node_process.terminate()
+
+            # Final join to ensure it's completely shut down
+            self.node_process.join()
+            self.node_process = None  # Reset to None after cleanup
+
+        # UPnP port mapping cleanup
+        if self.upnp_enabled:
+            try:
+                upnp = miniupnpc.UPnP()
+                upnp.discoverdelay = 200
+
+                # Discover UPnP devices
+                devices_found = upnp.discover()
+                if devices_found == 0:
+                    # print("No UPnP devices found.")
+                    return
+
+                upnp.selectigd()  # Select Internet Gateway Device
+                local_ip = upnp.lanaddr
+                removed_count = 0
+
+                # print("Scanning existing UPnP port mappings...")
+                i = 0
+                while True:
+                    try:
+                        mapping = upnp.getgenericportmapping(i)
+                        if mapping is None:
+                            break  # No more mappings
+
+                        ext_port, protocol, (int_ip, int_port), desc = mapping[:4]
+
+                        # Check if mapping matches our application
+                        if int_ip == local_ip and desc == "SmartNode":
+                            # print(f"Removing UPnP mapping: {ext_port}/{protocol} -> {int_ip}:{int_port}")
+                            upnp.deleteportmapping(ext_port, protocol)
+                            removed_count += 1
+
+                        i += 1
+
+                    except Exception:
+                        break  # End of list or error during retrieval
+
+                # print(f"Cleanup complete. Removed {removed_count} UPnP mappings.")
+
+            except Exception as e:
+                print(f"Error during UPnP cleanup: {e}")
+
+    def send_request(self, request_type, args, timeout=None):
+        """
+        Sends a request to the roles and waits for the response.
+        """
+        request = {"type": request_type, "args": args}
+        response = None
+        try:
+            self.mpc_lock.acquire(timeout=timeout)
+            self.node_requests.put(request)
+            response = self.node_responses.get()  # Blocking call, waits for response
+
+        except Exception as e:
+            print(f"Error sending request: {e}")
+            response = {"return": str(e)}
+
+        finally:
+            self.mpc_lock.release()
+
+        return response["return"]
+
+    def run_role(self):
+        raise NotImplementedError("Subclasses must implement this method")
+
+
+class WorkerNode(BaseNode):
+    distributed_worker = None
+
+    def run_role(self):
+        kwargs = self.init_kwargs.copy()
+        kwargs.update({
+            'upnp': kwargs.get('upnp', True),
+            'off_chain_test': kwargs.get('off_chain_test', False)
+        })
+        role_instance = Worker(
+            self.node_requests,
+            self.node_responses,
+            **kwargs
+        )
+        role_instance.start()
+        role_instance.activate()
+        role_instance.join()
+
+    def setup(self):
+        super().setup()
+        distributed_worker = DistributedWorker(self.node_requests, self.node_responses, self.mpc_lock)
+        t = threading.Thread(target=distributed_worker.run)
+        t.start()
+
+
+class ValidatorNode(BaseNode):
+    def run_role(self):
+        kwargs = self.init_kwargs.copy()
+        kwargs.update({
+            'upnp': kwargs.get('upnp', True),
+            'off_chain_test': kwargs.get('off_chain_test', False)
+        })
+        role_instance = Validator(
+            self.node_requests,
+            self.node_responses,
+            **kwargs
+        )
+        role_instance.start()
+        role_instance.join()
+
+
+class UserNode(BaseNode):
+    def run_role(self):
+        kwargs = self.init_kwargs.copy()
+
+        role_instance = User(
+            self.node_requests,
+            self.node_responses,
+            **kwargs
+        )
+        role_instance.start()
+        role_instance.join()
+
+    def create_distributed_model(self, model, training, n_pipelines=1, optimizer_type=None, dp_factor=None):
+        # stop_spinner = threading.Event()
+        # spinner_thread = threading.Thread(target=show_spinner, args=(stop_spinner, "Creating distributed model"))
+
+        try:
+            # Start the spinner
+            # spinner_thread.start()
+
+            dist_model = DistributedModel(self.node_requests, self.node_responses, self.mpc_lock, model, n_pipelines)
+            # self.send_request("request_workers", None)
+            # time.sleep(3)
+            # workers = self.send_request("check_workers", None)
+            # if len(workers) == 0:
+            #     self.send_request("request_workers", None)
+            #     time.sleep(5)
+            #     workers = self.send_request("check_workers", None)
+
+            if optimizer_type is None:
+                optimizer_type = torch.optim.Adam
+
+            # dist_model.worker_info = workers
+            # if len(workers) == 0:
+            #     self.send_request("debug_print", (
+            #         "Job creation failed: network at capacity (not enough workers)!", "bright_red", logging.CRITICAL))
+            #     return None, None
+
+            dist_model.training = training
+            distribution = dist_model.parse_model(model, handle_layer=False)
+
+            if training:
+                for module_id, module in distribution.items():
+                    if module["type"] == "offloaded":
+                        module["optimizer"] = f"{optimizer_type.__module__}.{optimizer_type.__name__}"
+                        module["training"] = training
+
+            distributed_config = self.send_request("request_job", (n_pipelines, 1, distribution))
+
+            if not distributed_config:
+                print("Could not obtain job from network... Please try again.")
+                return False
+
+            dist_model.distribute_model(distributed_config)
+
+            def _create_distributed_optimizer(**optimizer_kwargs):
+                return create_distributed_optimizer(dist_model, optimizer_type, **optimizer_kwargs)
+
+            setattr(self, "distributed_model", dist_model)
+
+            return dist_model, _create_distributed_optimizer
+
+        finally:
+            pass
+            # Stop the spinner
+            # stop_spinner.set()
+            # spinner_thread.join()
+
+    def cleanup(self):
+        """Downloads parameters from workers before shutting down"""
+        if hasattr(self, "distributed_model"):
+            if self.distributed_model.training:
+                self.distributed_model.parameters(distributed=True, load=False)
+
+        super().cleanup()
---
+++ packages/tensorlink/tensorlink-0.1.0.post1.tar.gz/tensorlink-0.1.0.post1/tensorlink/roles/worker.py
@@ -0,0 +1,214 @@
+import time
+
+from tensorlink.ml.torch_node import TorchNode
+from tensorlink.p2p.connection import Connection
+from tensorlink.ml.utils import estimate_memory, handle_output, get_gpu_memory
+from tensorlink.crypto.rsa import get_rsa_pub_key
+
+from dotenv import get_key
+import torch.nn as nn
+import threading
+import logging
+import hashlib
+import torch
+import json
+
+
+class Worker(TorchNode):
+    """
+    Todo:
+        - convert pickling to json for security (?)
+        - process other jobs/batches while waiting for worker response (?)
+        - link workers to database or download training data for complete offloading
+        - different subclasses of Worker for mpc requirements to designate mpc-specific
+            tasks, ie distributing a model too large to handle on a single computer / user
+        - function that detaches the huggingface wrapped outputs tensor without modifying the rest
+    """
+
+    def __init__(
+        self,
+        request_queue,
+        response_queue,
+        print_level=logging.INFO,
+        max_connections: int = 0,
+        upnp=True,
+        off_chain_test=False,
+        local_test=False
+    ):
+        super(Worker, self).__init__(
+            request_queue,
+            response_queue,
+            "W",
+            max_connections=max_connections,
+            upnp=upnp,
+            off_chain_test=off_chain_test,
+            local_test=local_test
+        )
+
+        self.training = False
+        self.role = "W"
+        self.print_level = print_level
+        self.loss = None
+        self.public_key = get_key(".env", "PUBLIC_KEY")
+        self.store_value(hashlib.sha256(b"ADDRESS").hexdigest(), self.public_key)
+
+        self.debug_print(f"Launching Worker: {self.rsa_key_hash} ({self.host}:{self.port})", level=logging.INFO)
+        self.available_memory = get_gpu_memory()
+
+        if self.off_chain_test is False:
+            self.public_key = get_key(".env", "PUBLIC_KEY")
+            self.store_value(hashlib.sha256(b"ADDRESS").hexdigest(), self.public_key)
+            self.bootstrap()
+
+    def handle_data(self, data: bytes, node: Connection):
+        """
+        Handle incoming tensors from connected roles and new job requests
+        Todo:
+            - ensure correct roles sending data
+            - potentially move/forward method directly to Connection to save data via identifying data
+                type and relevancy as its streamed in, saves bandwidth if we do not need the data / spam
+        """
+
+        try:
+            handled = super().handle_data(data, node)
+            ghost = 0
+
+            # Try worker-related tags if not found in parent class
+            if not handled:
+                # Try worker-related tags
+                if b"STATS-REQUEST" == data[:13]:
+                    self.debug_print(f"Worker -> Received stats request from: {node.node_id}")
+                    self.handle_statistics_request(node)
+
+                elif b"SHUTDOWN-JOB" == data[:12]:
+                    if node.role == "V":
+                        module_id = data[12:76].decode()
+                        self.modules[module_id]["termination"] = True
+
+                elif b"JOB-REQ" == data[:7]:
+                    try:
+                        if node.role == "V":
+                            # Accept job request from validator if we can handle it
+                            user_id, job_id, module_id, module_size, module_name, optimizer_name = json.loads(data[7:])
+
+                            if (
+                                self.available_memory >= module_size
+                            ):  # TODO Ensure were active?
+                                # Respond to validator that we can accept the job
+                                if module_name is None:
+                                    module_name = ""
+
+                                # Store a request to wait for the user connection
+                                self._store_request(user_id, module_id + module_name)
+                                self._store_request(user_id, "OPTIMIZER" + optimizer_name)
+                                data = b"ACCEPT-JOB" + job_id.encode() + module_id.encode()
+
+                                # Update available memory
+                                self.available_memory -= module_size
+
+                            else:
+                                data = b"DECLINE-JOB"
+
+                        else:
+                            node.stop()
+
+                        self.send_to_node(node, data)
+
+                    except Exception as e:
+                        print(data)
+                        print(node.main_port)
+                        raise e
+
+                # elif b"PoL" == data[:3]:
+                #     self.debug_print(f"RECEIVED PoL REQUEST")
+                #     if self.training and self.model:
+                #         dummy_input = pickle.loads(data[3:])
+                #
+                #         proof_of_learning = self.proof_of_learning(dummy_input)
+                #
+                #         self.send_to_node(roles, proof_of_learning)
+                #
+                # elif b"TENSOR" == data[:6]:
+                #     if self.training:
+                #         tensor = pickle.loads(data[6:])
+                #
+                #         # Confirm identity/role of roles
+                #         if roles in self.inbound:
+                #             self.forward_relays.put(tensor)
+                #         elif roles in self.outbound:
+                #             self.backward_relays.put(tensor)
+
+                else:
+                    ghost += 1
+
+            if ghost > 0:
+                node.ghosts += ghost
+                # TODO: potentially some form of reporting mechanism via ip and port
+                return False
+
+            return True
+
+        except Exception as e:
+            self.debug_print(f"Worker -> stream data exception: {e}", colour="bright_red",
+                             level=logging.ERROR)
+            raise e
+
+    def run(self):
+        # Accept users and back-check history
+        # Get proposees from SC and send our state to them
+        super().run()
+
+        while not self.terminate_flag.is_set():
+            # Handle job oversight, and inspect other jobs (includes job verification and reporting)
+            time.sleep(3)
+
+        self.stop()
+
+    def load_distributed_module(self, module: nn.Module, graph: dict = None):
+        pass
+
+    def proof_of_learning(self, dummy_input: torch.Tensor):
+        proof = {
+            "node_id": self.name,
+            "memory": self.available_memory,
+            "learning": self.training,
+            "model": self.model,
+        }
+
+        if self.training:
+            proof["output"] = handle_output(self.model(dummy_input)).sum()
+
+    def handle_statistics_request(self, callee, additional_context: dict = None):
+        """When a validator requests a stats request, return stats"""
+        stats = {
+            "id": self.rsa_key_hash,
+            "memory": self.available_memory,
+            "role": self.role,
+            "training": self.training,
+            # "connection": self.connections[i], "latency_matrix": self.connections[i].latency
+        }
+
+        if additional_context is not None:
+            for k, v in additional_context.items():
+                if k not in stats.keys():
+                    stats[k] = v
+
+        stats_bytes = json.dumps(stats).encode()
+        stats_bytes = b"STATS-RESPONSE" + stats_bytes
+        self.send_to_node(callee, stats_bytes)
+
+    def activate(self):
+        self.training = True
+
+    """Key Methods to Implement"""
+
+    # def host_job(self, model: nn.Module):
+    #     """
+    #     Todo:
+    #         - connect to master roles via SC and load in model
+    #         - attempt to assign and relay model to other idle connected workers
+    #         - determine relevant connections
+    #     """
+    #     self.optimizer = torch.optim.Adam
+    #     self.training = True
+    #     self.distribute_model(model)  # Add master vs worker functionality
