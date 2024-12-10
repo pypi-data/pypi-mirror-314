34eb3600a89c1e8f7c5f574512b8f6aaea695f1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 03:18:53 +0000

    Add package aiohttp-deps/1.1.4/aiohttp_deps-1.1.4.tar.gz

---
+++ packages/aiohttp-deps/aiohttp_deps-1.1.4.tar.gz/aiohttp_deps-1.1.4/README.md
@@ -0,0 +1,415 @@
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/aiohttp-deps?style=for-the-badge)](https://pypi.org/project/aiohttp-deps/)
+[![PyPI](https://img.shields.io/pypi/v/aiohttp-deps?style=for-the-badge)](https://pypi.org/project/aiohttp-deps/)
+[![PyPI - Downloads](https://img.shields.io/pypi/dm/aiohttp-deps?style=for-the-badge)](https://pypistats.org/packages/aiohttp-deps)
+
+# AioHTTP deps
+
+
+This project was initially created to show the abillities of [taskiq-dependencies](https://github.com/taskiq-python/taskiq-dependencies) project, which is used by [taskiq](https://github.com/taskiq-python/taskiq) to provide you with the best experience of sending distributed tasks.
+
+This project adds [FastAPI](https://github.com/tiangolo/fastapi)-like dependency injection to your [AioHTTP](https://github.com/aio-libs/aiohttp) application and swagger documentation based on types.
+
+To start using dependency injection, just initialize the injector.
+
+```python
+from aiohttp import web
+from aiohttp_deps import init as deps_init
+
+
+app = web.Application()
+
+
+app.on_startup.append(deps_init)
+
+web.run_app(app)
+
+```
+
+
+If you use mypy, then we have a custom router with propper types.
+
+
+```python
+from aiohttp import web
+from aiohttp_deps import init as deps_init
+from aiohttp_deps import Router
+
+router = Router()
+
+
+@router.get("/")
+async def handler():
+    return web.json_response({})
+
+
+app = web.Application()
+
+app.router.add_routes(router)
+
+app.on_startup.append(deps_init)
+
+web.run_app(app)
+
+```
+
+Also, you can nest routers with prefixes,
+
+```python
+api_router = Router()
+
+memes_router = Router()
+
+main_router = Router()
+
+main_router.add_routes(api_router, prefix="/api")
+main_router.add_routes(memes_router, prefix="/memes")
+```
+
+## Swagger
+
+If you use dependencies in you handlers, we can easily generate swagger for you.
+We have some limitations:
+1. We don't support resolving type aliases if hint is a string.
+    If you define variable like this: `myvar = int | None` and then in handler
+    you'd create annotation like this: `param: "str | myvar"` it will fail.
+    You need to unquote type hint in order to get it work.
+
+We will try to fix these limitations later.
+
+To enable swagger, just add it to your startup.
+
+```python
+from aiohttp_deps import init, setup_swagger
+
+app = web.Application()
+
+app.on_startup.extend([init, setup_swagger()])
+```
+
+### Responses
+
+You can define schema for responses using dataclasses or
+pydantic models. This would not affect handlers in any way,
+it's only for documentation purposes, if you want to actually
+validate values your handler returns, please write your own wrapper.
+
+```python
+from dataclasses import dataclass
+
+from aiohttp import web
+from pydantic import BaseModel
+
+from aiohttp_deps import Router, openapi_response
+
+router = Router()
+
+
+@dataclass
+class Success:
+    data: str
+
+
+class Unauthorized(BaseModel):
+    why: str
+
+
+@router.get("/")
+@openapi_response(200, Success, content_type="application/xml")
+@openapi_response(200, Success)
+@openapi_response(401, Unauthorized, description="When token is not correct")
+async def handler() -> web.Response:
+    ...
+```
+
+This example illustrates how much you can do with this decorator. You
+can have multiple content-types for a single status, or you can have different
+possble statuses. This function is pretty simple and if you want to make
+your own decorator for your responses, it won't be hard.
+
+
+## Default dependencies
+
+By default this library provides only two injectables. `web.Request` and `web.Application`.
+
+```python
+
+async def handler(app: web.Application = Depends()): ...
+
+async def handler2(request: web.Request = Depends()): ...
+
+```
+
+It's super useful, because you can use these dependencies in
+any other dependency. Here's a more complex example of how you can use this library.
+
+
+```python
+from aiohttp_deps import Router, Depends
+from aiohttp import web
+
+router = Router()
+
+
+async def get_db_session(app: web.Application = Depends()):
+    async with app[web.AppKey("db")] as sess:
+        yield sess
+
+
+class MyDAO:
+    def __init__(self, session=Depends(get_db_session)):
+        self.session = session
+
+    async def get_objects(self) -> list[object]:
+        return await self.session.execute("SELECT 1")
+
+
+@router.get("/")
+async def handler(db_session: MyDAO = Depends()):
+    objs = await db_session.get_objects()
+    return web.json_response({"objects": objs})
+
+```
+
+If you do something like this, you would never think about initializing your DAO. You can just inject it and that's it.
+
+
+# Built-in dependencies
+
+This library also provides you with some default dependencies that can help you in building the best web-service.
+
+## Json
+
+To parse json, create a pydantic model and add a dependency to your handler.
+
+
+```python
+from aiohttp import web
+from pydantic import BaseModel
+from aiohttp_deps import Router, Json, Depends
+
+router = Router()
+
+
+class UserInfo(BaseModel):
+    name: str
+
+
+@router.post("/users")
+async def new_data(user: UserInfo = Depends(Json())):
+    return web.json_response({"user": user.model_dump()})
+
+```
+
+This dependency automatically validates data and send
+errors if the data doesn't orrelate with schema or body is not a valid json.
+
+If you want to make this data optional, just mark it as optional.
+
+```python
+@router.post("/users")
+async def new_data(user: Optional[UserInfo] = Depends(Json())):
+    if user is None:
+        return web.json_response({"user": None})
+    return web.json_response({"user": user.model_dump()})
+
+```
+
+## Headers
+
+You can get and validate headers using `Header` dependency.
+
+Let's try to build simple example for authorization.
+
+```python
+from aiohttp_deps import Router, Header, Depends
+from aiohttp import web
+
+router = Router()
+
+
+def decode_token(authorization: str = Depends(Header())) -> str:
+    if authorization == "secret":
+        # Let's pretend that here we
+        # decode our token.
+        return authorization
+    raise web.HTTPUnauthorized()
+
+
+@router.get("/secret_data")
+async def new_data(token: str = Depends(decode_token)) -> web.Response:
+    return web.json_response({"secret": "not a secret"})
+
+```
+
+As you can see, header name to parse is equal to the
+name of a parameter that introduces Header dependency.
+
+If you want to use some name that is not allowed in python, or just want to have different names, you can use alias. Like this:
+
+```python
+def decode_token(auth: str = Depends(Header(alias="Authorization"))) -> str:
+```
+
+Headers can also be parsed to types. If you want a header to be parsed as int, just add the typehint.
+
+```python
+def decode_token(meme_id: int = Depends(Header())) -> str:
+```
+
+If you want to get list of values of one header, use parameter `multiple=True`.
+
+```python
+def decode_token(meme_id: list[int] = Depends(Header(multiple=True))) -> str:
+
+```
+
+And, of course, you can provide this dependency with default value if the value from user cannot be parsed for some reason.
+
+```python
+def decode_token(meme_id: str = Depends(Header(default="not-a-secret"))) -> str:
+```
+
+
+## Queries
+
+You can depend on `Query` to get and parse query parameters.
+
+```python
+from aiohttp_deps import Router, Query, Depends
+from aiohttp import web
+
+router = Router()
+
+
+@router.get("/shop")
+async def shop(item_id: str = Depends(Query())) -> web.Response:
+    return web.json_response({"id": item_id})
+
+```
+
+the name of the parameter is the same as the name of function parameter.
+
+The Query dependency is acually the same as the Header dependency, so everything about the `Header` dependency also applies to `Query`.
+
+## Views
+
+If you use views as handlers, please use View class from `aiohttp_deps`, otherwise the magic won't work.
+
+```python
+from aiohttp_deps import Router, View, Depends
+from aiohttp import web
+
+router = Router()
+
+
+@router.view("/view")
+class MyView(View):
+    async def get(self, app: web.Application = Depends()):
+        return web.json_response({"app": str(app)})
+
+```
+
+
+## Forms
+
+Now you can easiy get and validate form data from your request.
+To make the magic happen, please add `arbitrary_types_allowed` to the config of your model.
+
+
+```python
+import pydantic
+from aiohttp_deps import Router, Depends, Form
+from aiohttp import web
+
+router = Router()
+
+
+class MyForm(pydantic.BaseModel):
+    id: int
+    file: web.FileField
+
+    model_config = pydantic.ConfigDict(arbitrary_types_allowed=True)
+
+
+@router.post("/")
+async def handler(my_form: MyForm = Depends(Form())):
+    with open("my_file", "wb") as f:
+        f.write(my_form.file.file.read())
+    return web.json_response({"id": my_form.id})
+
+```
+
+## Path
+
+If you have path variables, you can also inject them in your handler.
+
+```python
+from aiohttp_deps import Router, Path, Depends
+from aiohttp import web
+
+router = Router()
+
+
+@router.get("/view/{var}")
+async def my_handler(var: str = Depends(Path())):
+    return web.json_response({"var": var})
+
+```
+
+
+## ExtraOpenAPI
+
+This dependency is used to add additional swagger fields to the endpoint's swagger
+that is using this dependency. It might be even indirect dependency.
+
+You can check how this thing can be used in our [examples/swagger_auth.py](https://github.com/taskiq-python/aiohttp-deps/tree/master/examples/swagger_auth.py).
+
+
+## Overriding dependencies
+
+Sometimes for tests you don't want to calculate actual functions
+and you want to pass another functions instead.
+
+To do so, you can add "dependency_overrides" or "values_overrides" to the aplication's state.
+These values should be dicts. The keys for these values can be found in `aiohttp_deps.keys` module.
+
+Here's an example.
+
+```python
+def original_dep() -> int:
+    return 1
+
+class MyView(View):
+    async def get(self, num: int = Depends(original_dep)):
+        """Nothing."""
+        return web.json_response({"request": num})
+```
+
+Imagine you have a handler that depends on some function,
+but instead of `1` you want to have `2` in your tests.
+
+To do it, jsut add `dependency_overrides` somewhere,
+where you create your application. And make sure that keys
+of that dict are actual function that are being replaced.
+
+```python
+from aiohttp_deps import VALUES_OVERRIDES_KEY
+
+my_app[VALUES_OVERRIDES_KEY] = {original_dep: 2}
+```
+
+But `values_overrides` only overrides returned values. If you want to
+override functions, you have to use `dependency_overrides`. Here's an example:
+
+```python
+from aiohttp_deps import DEPENDENCY_OVERRIDES_KEY
+
+def replacing_function() -> int:
+    return 2
+
+
+my_app[DEPENDENCY_OVERRIDES_KEY] = {original_dep: replacing_function}
+```
+
+The cool point about `dependency_overrides`, is that it recalculates graph and
+you can use dependencies in function that replaces the original.
---
+++ packages/aiohttp-deps/aiohttp_deps-1.1.4.tar.gz/aiohttp_deps-1.1.4/aiohttp_deps/swagger.py
@@ -0,0 +1,403 @@
+import inspect
+from collections import defaultdict
+from logging import getLogger
+from typing import (
+    Any,
+    Awaitable,
+    Callable,
+    Dict,
+    List,
+    Optional,
+    Tuple,
+    TypeVar,
+    get_type_hints,
+)
+
+import pydantic
+from aiohttp import web
+from deepmerge import always_merger
+from taskiq_dependencies import DependencyGraph
+
+from aiohttp_deps.initializer import InjectableFuncHandler, InjectableViewHandler
+from aiohttp_deps.keys import SWAGGER_SCHEMA_KEY
+from aiohttp_deps.utils import ExtraOpenAPI, Form, Header, Json, Path, Query
+
+_T = TypeVar("_T")
+
+REF_TEMPLATE = "#/components/schemas/{model}"
+SWAGGER_HTML_TEMPALTE = """
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="SwaggerUI" />
+    <title>SwaggerUI</title>
+    <link rel="stylesheet"
+        href="https://unpkg.com/swagger-ui-dist/swagger-ui.css"
+    />
+</head>
+
+<body>
+    <div id="swagger-ui"></div>
+    <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"
+    crossorigin></script>
+    <script>
+        window.onload = () => {
+            window.ui = SwaggerUIBundle({
+                url: '{schema_url}',
+                dom_id: '#swagger-ui',
+            });
+        };
+    </script>
+</body>
+</html>
+"""
+METHODS_WITH_BODY = {"POST", "PUT", "PATCH"}
+
+logger = getLogger()
+
+
+async def _schema_handler(
+    request: web.Request,
+) -> web.Response:
+    return web.json_response(request.app[SWAGGER_SCHEMA_KEY])
+
+
+def _get_swagger_handler(
+    swagger_html: str,
+) -> Callable[[web.Request], Awaitable[web.Response]]:
+    async def swagger_handler(_: web.Request) -> web.Response:
+        return web.Response(text=swagger_html, content_type="text/html")
+
+    return swagger_handler
+
+
+def _is_optional(annotation: Optional[inspect.Parameter]) -> bool:
+    # If it's an empty annotation,
+    # we guess that the value can be optional.
+    if annotation is None or annotation.annotation == annotation.empty:
+        return True
+
+    def dummy(_var: annotation.annotation) -> None:  # type: ignore
+        """Dummy function to use for type resolution."""
+
+    var = get_type_hints(dummy).get("_var")
+    return var == Optional[var]
+
+
+def _get_param_schema(annotation: Optional[inspect.Parameter]) -> Dict[str, Any]:
+    if annotation is None or annotation.annotation == annotation.empty:
+        return {}
+
+    def dummy(_var: annotation.annotation) -> None:  # type: ignore
+        """Dummy function to use for type resolution."""
+
+    var = get_type_hints(dummy).get("_var")
+    return pydantic.TypeAdapter(var).json_schema(
+        ref_template=REF_TEMPLATE,
+        mode="validation",
+    )
+
+
+def _add_route_def(  # noqa: C901, PLR0912
+    openapi_schema: Dict[str, Any],
+    route: web.ResourceRoute,
+    method: str,
+    graph: DependencyGraph,
+    extra_openapi: Dict[str, Any],
+    extra_openapi_schemas: Dict[str, Any],
+) -> None:
+    route_info: Dict[str, Any] = {
+        "description": inspect.getdoc(graph.target),
+        "responses": {},
+        "parameters": [],
+    }
+    if route.resource is None:  # pragma: no cover
+        return
+
+    if extra_openapi_schemas:
+        openapi_schema["components"]["schemas"].update(extra_openapi_schemas)
+
+    params: Dict[Tuple[str, str], Any] = {}
+    updaters: List[Callable[[Dict[str, Any]], None]] = []
+
+    def _insert_in_params(data: Dict[str, Any]) -> None:
+        element = params.get((data["name"], data["in"]))
+        if element is None:
+            params[(data["name"], data["in"])] = data
+            return
+        element["required"] = element.get("required") or data.get("required")
+        element["allowEmptyValue"] = bool(element.get("allowEmptyValue")) and bool(
+            data.get("allowEmptyValue"),
+        )
+        params[(data["name"], data["in"])] = element
+
+    for dependency in graph.ordered_deps:
+        if isinstance(dependency.dependency, (Json, Form)):
+            content_type = "application/json"
+            if isinstance(dependency.dependency, Form):
+                content_type = "application/x-www-form-urlencoded"
+            if (
+                dependency.signature
+                and dependency.signature.annotation != inspect.Parameter.empty
+            ):
+                input_schema = pydantic.TypeAdapter(
+                    dependency.signature.annotation,
+                ).json_schema(ref_template=REF_TEMPLATE, mode="validation")
+                openapi_schema["components"]["schemas"].update(
+                    input_schema.pop("$defs", {}),
+                )
+                route_info["requestBody"] = {
+                    "content": {content_type: {"schema": input_schema}},
+                }
+            else:
+                route_info["requestBody"] = {
+                    "content": {content_type: {}},
+                }
+        elif isinstance(dependency.dependency, Query):
+            schema = _get_param_schema(dependency.signature)
+            openapi_schema["components"]["schemas"].update(schema.pop("$defs", {}))
+            _insert_in_params(
+                {
+                    "name": dependency.dependency.alias or dependency.param_name,
+                    "in": "query",
+                    "description": dependency.dependency.description,
+                    "required": not _is_optional(dependency.signature),
+                    "schema": schema,
+                },
+            )
+        elif isinstance(dependency.dependency, Header):
+            name = dependency.dependency.alias or dependency.param_name
+            schema = _get_param_schema(dependency.signature)
+            openapi_schema["components"]["schemas"].update(schema.pop("$defs", {}))
+            _insert_in_params(
+                {
+                    "name": name.capitalize(),
+                    "in": "header",
+                    "description": dependency.dependency.description,
+                    "required": not _is_optional(dependency.signature),
+                    "schema": schema,
+                },
+            )
+        elif isinstance(dependency.dependency, Path):
+            schema = _get_param_schema(dependency.signature)
+            openapi_schema["components"]["schemas"].update(schema.pop("$defs", {}))
+            _insert_in_params(
+                {
+                    "name": dependency.dependency.alias or dependency.param_name,
+                    "in": "path",
+                    "description": dependency.dependency.description,
+                    "required": not _is_optional(dependency.signature),
+                    "allowEmptyValue": _is_optional(dependency.signature),
+                    "schema": schema,
+                },
+            )
+        elif isinstance(dependency.dependency, ExtraOpenAPI):
+            if dependency.dependency.updater is not None:
+                updaters.append(dependency.dependency.updater)
+            if dependency.dependency.extra_openapi is not None:
+                extra_openapi = always_merger.merge(
+                    extra_openapi,
+                    dependency.dependency.extra_openapi,
+                )
+
+    route_info["parameters"] = list(params.values())
+    for updater in updaters:
+        updater(route_info)
+    openapi_schema["paths"][route.resource.canonical].update(
+        {method.lower(): always_merger.merge(route_info, extra_openapi)},
+    )
+
+
+def setup_swagger(  # noqa: C901
+    schema_url: str = "/openapi.json",
+    swagger_ui_url: str = "/docs",
+    enable_ui: bool = True,
+    hide_heads: bool = True,
+    hide_options: bool = True,
+    title: str = "AioHTTP",
+    description: Optional[str] = None,
+    version: str = "1.0.0",
+    extra_openapi: Optional[Dict[str, Any]] = None,
+) -> Callable[[web.Application], Awaitable[None]]:
+    """
+    Add swagger documentation.
+
+    This function creates new function,
+    that can be used in on_startup.
+
+    Add outputs of this function in on_startup array
+    to enable swagger.
+
+    >>> app.on_startup.append(setup_swagger())
+
+    This function will generate swagger schema based
+    on dependencies that were used.
+
+    :param schema_url: URL where schema will be served.
+    :param swagger_ui_url: URL where swagger ui will be served.
+    :param enable_ui: whether you want to enable bundled swagger ui.
+    :param hide_heads: hide HEAD requests.
+    :param hide_options: hide OPTIONS requests.
+    :param title: Title of an application.
+    :param description: description of an application.
+    :param version: version of an application.
+    :param extra_openapi: extra openAPI dict that will be merged with generated schema.
+    :return: startup event handler.
+    """
+    if extra_openapi is None:
+        extra_openapi = {}
+
+    async def event_handler(app: web.Application) -> None:  # noqa: C901
+        openapi_schema = {
+            "openapi": "3.0.0",
+            "info": {
+                "title": title,
+                "description": description,
+                "version": version,
+            },
+            "components": {"schemas": {}},
+            "paths": defaultdict(dict),
+        }
+        for route in app.router.routes():
+            if route.resource is None:  # pragma: no cover
+                continue
+            if hide_heads and route.method.upper() == "HEAD":
+                continue
+            if hide_options and route.method.upper() == "OPTIONS":
+                continue
+            if isinstance(route._handler, InjectableFuncHandler):
+                route_extra_openapi = getattr(
+                    route._handler.original_handler,
+                    "__extra_openapi__",
+                    {},
+                )
+                route_extra_schemas = getattr(
+                    route._handler.original_handler,
+                    "__extra_openapi_schemas__",
+                    {},
+                )
+                try:
+                    _add_route_def(
+                        openapi_schema,
+                        route,  # type: ignore
+                        route.method,
+                        route._handler.graph,
+                        extra_openapi=route_extra_openapi,
+                        extra_openapi_schemas=route_extra_schemas,
+                    )
+                except Exception as exc:  # pragma: no cover
+                    logger.warning(
+                        "Cannot add route info: %s",
+                        exc,
+                        exc_info=True,
+                    )
+
+            elif isinstance(route._handler, InjectableViewHandler):
+                for key, graph in route._handler.graph_map.items():
+                    route_extra_openapi = getattr(
+                        getattr(route._handler.original_handler, key),
+                        "__extra_openapi__",
+                        {},
+                    )
+                    route_extra_schemas = getattr(
+                        getattr(route._handler.original_handler, key),
+                        "__extra_openapi_schemas__",
+                        {},
+                    )
+                    try:
+                        _add_route_def(
+                            openapi_schema,
+                            route,  # type: ignore
+                            key,
+                            graph,
+                            extra_openapi=route_extra_openapi,
+                            extra_openapi_schemas=route_extra_schemas,
+                        )
+                    except Exception as exc:  # pragma: no cover
+                        logger.warning(
+                            "Cannot add route info: %s",
+                            exc,
+                            exc_info=True,
+                        )
+
+        app[SWAGGER_SCHEMA_KEY] = always_merger.merge(openapi_schema, extra_openapi)
+
+        app.router.add_get(
+            schema_url,
+            _schema_handler,
+        )
+
+        if enable_ui:
+            app.router.add_get(
+                swagger_ui_url,
+                _get_swagger_handler(
+                    SWAGGER_HTML_TEMPALTE.replace("{schema_url}", schema_url),
+                ),
+            )
+
+    return event_handler
+
+
+def extra_openapi(additional_schema: Dict[str, Any]) -> Callable[[_T], _T]:
+    """
+    Add extra openapi schema.
+
+    This function just adds a parameter for later use
+    by openapi schema generator.
+
+    :param additional_schema: dict with updates.
+    :return: same function with new attributes.
+    """
+
+    def decorator(func: _T) -> _T:
+        func.__extra_openapi__ = additional_schema  # type: ignore
+        return func
+
+    return decorator
+
+
+def openapi_response(
+    status: int,
+    model: Any,
+    *,
+    content_type: str = "application/json",
+    description: Optional[str] = None,
+) -> Callable[[_T], _T]:
+    """
+    Add response schema to the endpoint.
+
+    This function takes a status and model,
+    which is going to represent the response.
+
+    :param status: Status of a response.
+    :param model: Response model.
+    :param content_type: Content-type of a response.
+    :param description: Response's description.
+
+    :returns: decorator that modifies your function.
+    """
+
+    def decorator(func: _T) -> _T:
+        openapi = getattr(func, "__extra_openapi__", {})
+        openapi_schemas = getattr(func, "__extra_openapi_schemas__", {})
+        adapter: "pydantic.TypeAdapter[Any]" = pydantic.TypeAdapter(model)
+        responses = openapi.get("responses", {})
+        status_response = responses.get(status, {})
+        if not status_response:
+            status_response["description"] = description
+        status_response["content"] = status_response.get("content", {})
+        response_schema = adapter.json_schema(
+            ref_template=REF_TEMPLATE,
+            mode="serialization",
+        )
+        openapi_schemas.update(response_schema.pop("$defs", {}))
+        status_response["content"][content_type] = {"schema": response_schema}
+        responses[status] = status_response
+        openapi["responses"] = responses
+        func.__extra_openapi__ = openapi  # type: ignore
+        func.__extra_openapi_schemas__ = openapi_schemas  # type: ignore
+        return func
+
+    return decorator
