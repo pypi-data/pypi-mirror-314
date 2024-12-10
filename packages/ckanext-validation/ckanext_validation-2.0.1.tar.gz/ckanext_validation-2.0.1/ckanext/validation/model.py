17aea2742e3ff30081d801385443b0f32b415935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 15:07:39 +0000

    Add package ckanext-validation/2.0.1/ckanext_validation-2.0.1.tar.gz

---
+++ packages/ckanext-validation/ckanext_validation-2.0.1.tar.gz/ckanext_validation-2.0.1/ckanext/validation/model.py
@@ -0,0 +1,42 @@
+# encoding: utf-8
+
+import datetime
+import uuid
+import logging
+
+from sqlalchemy import Column, Unicode, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.dialects.postgresql import JSON
+
+from ckan.model.meta import metadata
+
+log = logging.getLogger(__name__)
+
+
+def make_uuid():
+    return str(uuid.uuid4())
+
+
+Base = declarative_base(metadata=metadata)
+
+
+class Validation(Base):
+    __tablename__ = u'validation'
+
+    id = Column(Unicode, primary_key=True, default=make_uuid)
+    resource_id = Column(Unicode)
+    status = Column(Unicode, default=u'created')
+    created = Column(DateTime, default=datetime.datetime.utcnow)
+    finished = Column(DateTime)
+    report = Column(JSON)
+    error = Column(JSON)
+
+
+def create_tables():
+    Validation.__table__.create()
+
+    log.info(u'Validation database tables created')
+
+
+def tables_exist():
+    return Validation.__table__.exists()
---
+++ packages/ckanext-validation/ckanext_validation-2.0.1.tar.gz/ckanext_validation-2.0.1/ckanext/validation/validators.py
@@ -0,0 +1,69 @@
+# encoding: utf-8
+import json
+
+import tableschema
+
+from ckantoolkit import Invalid, config
+
+
+# Input validators
+
+def resource_schema_validator(value, context):
+
+    if not value:
+        return
+
+    msg = None
+
+    if isinstance(value, dict):
+        descriptor = value
+    else:
+        value = str(value)
+
+        if value.lower().startswith('http'):
+            return value
+
+        try:
+            descriptor = json.loads(str(value))
+            if not isinstance(descriptor, dict):
+                msg = u'Invalid Table Schema descriptor: {}'.format(value)
+                raise Invalid(msg)
+
+        except ValueError as e:
+            msg = u'JSON error in Table Schema descriptor: {}'.format(e)
+            raise Invalid(msg)
+
+    try:
+        tableschema.validate(descriptor)
+    except tableschema.exceptions.ValidationError as e:
+        errors = []
+        for error in e.errors:
+            errors.append(str(error))
+        msg = u'Invalid Table Schema: {}'.format(u', '.join(errors))
+
+    if msg:
+        raise Invalid(msg)
+
+    return json.dumps(descriptor)
+
+
+def validation_options_validator(value, context):
+    '''Add default validation options if not already present
+
+    At this point the value should already be a valid JSON string (ie
+    `scheming_valid_json_object` has been run).
+    '''
+
+    default_options = config.get(
+        'ckanext.validation.default_validation_options')
+
+    if default_options:
+        default_options = json.loads(default_options)
+
+        provided_options = json.loads(value)
+
+        default_options.update(provided_options)
+
+        value = json.dumps(default_options, indent=None, sort_keys=True)
+
+    return value
