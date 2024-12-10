ed521c5bb29d7cf3004a9418e6bed58b2854b532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 14:04:25 +0000

    Add package hdf5extractor/1.0.4.dev4/hdf5extractor-1.0.4.dev4.tar.gz

---
+++ packages/hdf5extractor/hdf5extractor-1.0.4.dev4.tar.gz/hdf5extractor-1.0.4.dev4/hdf5extractor/common.py
@@ -0,0 +1,6 @@
+#
+# Copyright (c) 2022-2023 Geosiris.
+# SPDX-License-Identifier: Apache-2.0
+#
+UUID_REGEX = r"[0-9a-fA-F]{8}-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}"
+FILE_NAME_REGEX = r"^[\w]+_" + UUID_REGEX + r"\.xml$"
---
+++ packages/hdf5extractor/hdf5extractor-1.0.4.dev4.tar.gz/hdf5extractor-1.0.4.dev4/hdf5extractor/etp/__init__.py
@@ -0,0 +1,4 @@
+#
+# Copyright (c) 2022-2023 Geosiris.
+# SPDX-License-Identifier: Apache-2.0
+#
