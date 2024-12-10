29e3ce2419a497e27adb6309c5785f1293360652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 07:04:31 +0000

    Add package oteltest/0.37.0/oteltest-0.37.0.tar.gz

---
+++ packages/oteltest/oteltest-0.37.0.tar.gz/oteltest-0.37.0/example_scripts/trace_loop_http_splk.py
@@ -0,0 +1,28 @@
+from lib import trace_loop
+
+
+if __name__ == "__main__":
+    trace_loop(1)
+
+
+class MyOtelTest:
+    def requirements(self):
+        return ("splunk-opentelemetry[all]",)
+
+    def environment_variables(self):
+        return {
+            "OTEL_SERVICE_NAME": "my-otel-test",
+            "OTEL_EXPORTER_OTLP_PROTOCOL": "http/protobuf",
+        }
+
+    def wrapper_command(self):
+        return "splunk-py-trace"
+
+    def on_start(self):
+        return None
+
+    def on_stop(self, telemetry, stdout: str, stderr: str, returncode: int) -> None:
+        pass
+
+    def is_http(self):
+        return True
