ca16371c26cc92fd96303d7476158ce1a9292f49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 15:05:15 +0000

    Add package SigProfilerExtractor/1.1.25/SigProfilerExtractor-1.1.25-py3-none-any.whl

---
+++ packages/SigProfilerExtractor/SigProfilerExtractor-1.1.25-py3-none-any.whl/SigProfilerExtractor/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEREXTRACTOR SETUP.PY
+short_version = '1.1.25'
+version = '1.1.25'
+Update = 'Upgrade v1.1.25: Add CLI support, README formatting udpate, and CI/CD badge fix.'
+    
+    
