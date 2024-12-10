445dd36547531016aa2ec1abcb837f4911c1934d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 15:05:26 +0000

    Add package cadica-data-set/1.0.0/cadica_data_set-1.0.0.tar.gz

---
+++ packages/cadica-data-set/cadica_data_set-1.0.0.tar.gz/cadica_data_set-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages 
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name="cadica_data_set",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.6",
+) 
