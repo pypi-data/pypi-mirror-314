ed64b41541f8ed36dfec3032becddd3e20ac7e32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 07:04:19 +0000

    Add package Frac-to-cart-coordinates/1.1.2/Frac_to_cart_coordinates-1.1.2-py3-none-any.whl

---
+++ packages/Frac-to-cart-coordinates/Frac_to_cart_coordinates-1.1.2-py3-none-any.whl/Fractocart/__init__.py
@@ -0,0 +1,9 @@
+"""
+Convert between fractional and Cartesian coordinates.
+"""
+
+__version__ = "1.1.2"
+__author__ = "Noah Deveaux"
+__maintainer__ = "Noah Deveaux"
+__email__ = "noah.deveaux@unamur.be"
+__status__ = "Development"
