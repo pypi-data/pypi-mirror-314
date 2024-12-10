27e0c0cbb87d6fce1ed2bba3584d1ff06edc4d04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 16:04:36 +0000

    Add package nshtrainer/1.0.0b27/nshtrainer-1.0.0b27-py3-none-any.whl

---
+++ packages/nshtrainer/nshtrainer-1.0.0b27-py3-none-any.whl/nshtrainer/configs/loggers/__init__.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+__codegen__ = True
+
+from nshtrainer.loggers import ActSaveLoggerConfig as ActSaveLoggerConfig
+from nshtrainer.loggers import BaseLoggerConfig as BaseLoggerConfig
+from nshtrainer.loggers import CSVLoggerConfig as CSVLoggerConfig
+from nshtrainer.loggers import TensorboardLoggerConfig as TensorboardLoggerConfig
+from nshtrainer.loggers import WandbLoggerConfig as WandbLoggerConfig
+from nshtrainer.loggers.wandb import CallbackConfigBase as CallbackConfigBase
+from nshtrainer.loggers.wandb import (
+    WandbUploadCodeCallbackConfig as WandbUploadCodeCallbackConfig,
+)
+from nshtrainer.loggers.wandb import (
+    WandbWatchCallbackConfig as WandbWatchCallbackConfig,
+)
+
+from . import _base as _base
+from . import actsave as actsave
+from . import csv as csv
+from . import tensorboard as tensorboard
+from . import wandb as wandb
+
+__all__ = [
+    "ActSaveLoggerConfig",
+    "BaseLoggerConfig",
+    "CSVLoggerConfig",
+    "CallbackConfigBase",
+    "TensorboardLoggerConfig",
+    "WandbLoggerConfig",
+    "WandbUploadCodeCallbackConfig",
+    "WandbWatchCallbackConfig",
+    "_base",
+    "actsave",
+    "csv",
+    "tensorboard",
+    "wandb",
+]
---
+++ packages/nshtrainer/nshtrainer-1.0.0b27-py3-none-any.whl/nshtrainer/configs/util/config/duration/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+__codegen__ = True
+
+from nshtrainer.util.config.duration import EpochsConfig as EpochsConfig
+from nshtrainer.util.config.duration import StepsConfig as StepsConfig
+
+__all__ = [
+    "EpochsConfig",
+    "StepsConfig",
+]
