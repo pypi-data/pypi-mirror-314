1486079ecadc004e3eba5f2c733eba1c158c2d9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 06:04:00 +0000

    Add package IndovinaLaParola/1.1.3/indovinalaparola-1.1.3.tar.gz

---
+++ packages/IndovinaLaParola/indovinalaparola-1.1.3.tar.gz/indovinalaparola-1.1.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='IndovinaLaParola',
+    version='1.1.3',
+    description='Un gioco semplice di indovina la parola',
+    author='Gasa industries',
+    packages=find_packages(),
+    python_requires='>=3.6',  
+    install_requires=[
+        'setuptools',  
+    ],
+    entry_points={
+        'console_scripts': [
+            'indovina_la_parola = IndovinaLaParola:IndovinaLaParola',
+        ],
+    },
+)
