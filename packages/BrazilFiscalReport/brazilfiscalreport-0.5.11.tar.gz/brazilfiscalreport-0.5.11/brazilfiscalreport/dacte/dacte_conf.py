7b8cc2876035b6cc6f112f908ae4f1a5623b7e25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 03:18:31 +0000

    Add package BrazilFiscalReport/0.5.11/brazilfiscalreport-0.5.11.tar.gz

---
+++ packages/BrazilFiscalReport/brazilfiscalreport-0.5.11.tar.gz/brazilfiscalreport-0.5.11/brazilfiscalreport/dacte/dacte_conf.py
@@ -0,0 +1,40 @@
+URL = ".//{http://www.portalfiscal.inf.br/cte}"
+
+TP_MODAL = {
+    "01": "RODOVIÁRIO",
+    "02": "AÉREO",
+    "03": "AQUAVIÁRIO",
+    "04": "FERROVIÁRIO",
+    "05": "DUTOVIÁRIO",
+    "06": "MULTIMODAL",
+}
+TP_TOMADOR = {
+    "0": "REMETENTE",
+    "1": "EXPEDIDOR",
+    "2": "RECEBEDOR",
+    "3": "DESTINATÁRIO",
+    "4": "OUTRO",
+}
+
+TP_CTE = {
+    "0": "NORMAL",
+    "1": "COMPLEMENTAR",
+    "2": "ANULADO",
+    "3": "SUSBTITUTO",
+}
+
+TP_SERVICO = {
+    "0": "NORMAL",
+    "1": "SUBCONTRATAÇÃO",
+    "2": "REDESPACHO",
+    "3": "REDESPACHO INTERMEDIÁRIO",
+}
+
+TP_CODIGO_MEDIDA = {
+    "00": "M3",
+    "01": "KG",
+    "02": "TON",
+    "03": "UNIDADE",
+    "04": "LITROS",
+    "05": "MMBTU",
+}
