601b51a76bdc20101ebd2b60f23b0e2e1ade2457
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 16:06:08 +0000

    Add package chanjo/4.7.2/chanjo-4.7.2.tar.gz

---
+++ packages/chanjo/chanjo-4.7.2.tar.gz/chanjo-4.7.2/tests/init/test_init_bootstrap.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from mock import patch
+
+from chanjo.init import bootstrap
+from chanjo.testutils import FakeZipFile, fake_urlretrieve
+
+
+@patch("urllib.request.urlretrieve", fake_urlretrieve)
+@patch("zipfile.ZipFile", FakeZipFile)
+def test_pull(tmp_path):
+    # GIVEN a target directory
+    target_dir = str(tmp_path)
+    # WHEN downloading resources
+    bootstrap.pull(target_dir)
+    # THEN BED resource should be in place
+    out_bed = tmp_path.joinpath(bootstrap.BED_NAME)
+    assert out_bed.exists()
+    assert len(list(tmp_path.iterdir())) == 1
+
+    # GIVEN the resources already exists
+    bootstrap.pull(target_dir)
+    # THEN nothing happens :)
