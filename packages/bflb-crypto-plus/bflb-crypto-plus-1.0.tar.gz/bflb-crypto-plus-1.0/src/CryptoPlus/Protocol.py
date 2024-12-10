cc13efa171db05895f3ceee19b269afdc2134b45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 15:07:41 +0000

    Add package bflb-crypto-plus/1.0/bflb-crypto-plus-1.0.tar.gz

---
+++ packages/bflb-crypto-plus/bflb-crypto-plus-1.0.tar.gz/bflb-crypto-plus-1.0/src/CryptoPlus/Protocol.py
@@ -0,0 +1,15 @@
+
+"""
+Licensed under BSD license
+"""
+
+"""Imports Crypto Protocol
+
+Now you can do:
+    >>> from CryptoPlus.Protocol import *
+OR:
+    >>> from CryptoPlus.Protocol import XXX
+but not:
+    >>> import CryptoPlus.Protocol.XXX
+"""
+from Crypto.Protocol import *
