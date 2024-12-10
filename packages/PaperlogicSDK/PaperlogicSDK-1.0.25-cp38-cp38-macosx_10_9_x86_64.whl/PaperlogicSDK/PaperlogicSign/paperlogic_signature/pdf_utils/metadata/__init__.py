863077a4bc2350d42f270745446999fb9ec9834e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 03:19:57 +0000

    Add package PaperlogicSDK/1.0.25/PaperlogicSDK-1.0.25-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/PaperlogicSDK/PaperlogicSDK-1.0.25-cp38-cp38-macosx_10_9_x86_64.whl/PaperlogicSDK/PaperlogicSign/paperlogic_signature/pdf_utils/metadata/__init__.py
@@ -0,0 +1,8 @@
+from .info import update_info_dict, view_from_info_dict
+from .model import DocumentMetadata
+
+__all__ = [
+    'update_info_dict',
+    'view_from_info_dict',
+    'DocumentMetadata'
+]
