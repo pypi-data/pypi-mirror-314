b6a30e97149f88bb6ba9e0926a6e6a488a7be3c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 03:20:15 +0000

    Add package flask-traffic/0.5.0/flask_traffic-0.5.0-py3-none-any.whl

---
+++ packages/flask-traffic/flask_traffic-0.5.0-py3-none-any.whl/flask_traffic/stores/__init__.py
@@ -0,0 +1,10 @@
+"""
+This module contains the different store classes that can be used to store the traffic data.
+"""
+
+from .csv_store import CSVStore
+from .json_store import JSONStore
+from .sql_orm_store import SQLORMStore, SQLORMModelMixin
+from .sql_store import SQLStore
+
+__all__ = ["JSONStore", "CSVStore", "SQLStore", "SQLORMStore", "SQLORMModelMixin"]
