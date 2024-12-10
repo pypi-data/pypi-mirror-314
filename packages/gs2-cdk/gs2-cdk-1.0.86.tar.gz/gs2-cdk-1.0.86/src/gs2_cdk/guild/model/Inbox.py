bf266ffa9f5c8cbb0b2a5a8e2e20f3c7c859d775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 16:06:06 +0000

    Add package gs2-cdk/1.0.86/gs2-cdk-1.0.86.tar.gz

---
+++ packages/gs2-cdk/gs2-cdk-1.0.86.tar.gz/gs2-cdk-1.0.86/src/gs2_cdk/guild/model/Inbox.py
@@ -0,0 +1,43 @@
+# Copyright 2016- Game Server Services, Inc. or its affiliates. All Rights
+# Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+from __future__ import annotations
+from typing import *
+from .options.InboxOptions import InboxOptions
+
+
+class Inbox:
+    guild_name: str
+    from_user_ids: Optional[List[str]] = None
+    revision: Optional[int] = None
+
+    def __init__(
+        self,
+        guild_name: str,
+        options: Optional[InboxOptions] = InboxOptions(),
+    ):
+        self.guild_name = guild_name
+        self.from_user_ids = options.from_user_ids if options.from_user_ids else None
+        self.revision = options.revision if options.revision else None
+
+    def properties(
+        self,
+    ) -> Dict[str, Any]:
+        properties: Dict[str, Any] = {}
+
+        if self.guild_name is not None:
+            properties["guildName"] = self.guild_name
+        if self.from_user_ids is not None:
+            properties["fromUserIds"] = self.from_user_ids
+
+        return properties
