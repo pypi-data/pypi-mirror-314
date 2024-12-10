eb7c4f2c2f23ec5218346d5fd2ebfaaf7f910b23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 03:18:26 +0000

    Add package dynamicaug/0.0.1/dynamicaug-0.0.1.tar.gz

---
+++ packages/dynamicaug/dynamicaug-0.0.1.tar.gz/dynamicaug-0.0.1/README.md
@@ -0,0 +1,4 @@
+# DynamicAugmentation
+
+- Pytorch implementation of very simply applicable Cutout and GridCutOut augmentation
+- New torch pipeline of DynamicAugmentation
---
+++ packages/dynamicaug/dynamicaug-0.0.1.tar.gz/dynamicaug-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dynamicaug",
+    version="0.0.1",
+    author="JeonghyunKim",
+    author_email="kr.jeonghyun.kim@gmail.com",
+    description="schooldevops sample lib",
+    long_description="will be added",
+    long_description_content_type="text/markdown",
+    url="https://github.com/krjeo/DynamicAugmentation",
+    project_urls={},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.9",
+)
