3980e7e31ddc5dd771c887f6a323eda1808bb10b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 16:06:58 +0000

    Add package serafimNum/0.0.1/serafimnum-0.0.1.tar.gz

---
+++ packages/serafimNum/serafimnum-0.0.1.tar.gz/serafimnum-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='serafimNum',
+    version='0.0.1',
+    description='Перевод систем счисления',
+    license='MIT',
+    packages=find_packages(),
+    author='Serafim H',
+    author_email='i@drseraf.ru',
+    keywords=['sis'],
+    url='https://github.com'
+)
