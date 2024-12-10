c97c1e76ba60c61723aa27ed023b15be7c1e6c52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 16:05:15 +0000

    Add package yjsubscription/1.0.0.5/yjsubscription-1.0.0.5-py3-none-any.whl

---
+++ packages/yjsubscription/yjsubscription-1.0.0.5-py3-none-any.whl/subscriptionv1/client/subscription_api.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright(C) 2024 baidu, Inc. All Rights Reserved
+
+# @Time    : 2024/12/3 上午11:45
+# @Author  : haosixu
+# @File    : subscription_api.py
+# @Software: PyCharm
+"""
+from pydantic import BaseModel, Field
+
+class CheckEquityRequest(BaseModel):
+    """
+    Check equity request.
+    """
+    equty_id: str = Field(alias="equityId")
+
+class CheckEquityResponse(BaseModel):
+    """
+    Check equity response.
+    """
+    check_result: str = Field(alias="checkResult")
+    validity_end_date: int = Field(alias="validityEndDate")
+    total: int = Field(alias="total")
+    used: int = Field(alias="used")
+
