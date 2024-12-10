ea3432a61463353df8fbc5b96b4b13b51dfc4318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 17:04:55 +0000

    Add package oarepo-model-builder-drafts-files/4.0.16/oarepo_model_builder_drafts_files-4.0.16.tar.gz

---
+++ packages/oarepo-model-builder-drafts-files/oarepo_model_builder_drafts_files-4.0.16.tar.gz/oarepo_model_builder_drafts_files-4.0.16/oarepo_model_builder_drafts_files/builders/__init__.py
@@ -0,0 +1,4 @@
+TEMPLATES = {
+    "service-config-disable-upload": "templates/service_config_disable_upload.py.jinja2",
+    "published-service-files-link": "templates/published_service_files_link.py.jinja2",
+}
