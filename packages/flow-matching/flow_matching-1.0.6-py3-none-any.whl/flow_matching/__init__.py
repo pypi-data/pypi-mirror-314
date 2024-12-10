6482e50047e6fd3ea8b24526913a88b40f7c2c6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 15:06:05 +0000

    Add package flow-matching/1.0.6/flow_matching-1.0.6-py3-none-any.whl

---
+++ packages/flow-matching/flow_matching-1.0.6-py3-none-any.whl/flow_matching/__init__.py
@@ -0,0 +1,13 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the CC-by-NC license found in the
+# LICENSE file in the root directory of this source tree.
+
+import loss
+
+__all__ = [
+    "loss",
+]
+
+__version__ = "1.0.6"
