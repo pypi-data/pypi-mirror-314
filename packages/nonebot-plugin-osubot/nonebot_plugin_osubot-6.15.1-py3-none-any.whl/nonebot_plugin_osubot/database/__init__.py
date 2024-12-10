31076ef0930fbe384502f304779ebcea656790f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 14:04:57 +0000

    Add package nonebot-plugin-osubot/6.15.1/nonebot_plugin_osubot-6.15.1-py3-none-any.whl

---
+++ packages/nonebot-plugin-osubot/nonebot_plugin_osubot-6.15.1-py3-none-any.whl/nonebot_plugin_osubot/database/__init__.py
@@ -0,0 +1,3 @@
+from .models import InfoData, UserData
+
+__all__ = ["UserData", "InfoData"]
---
+++ packages/nonebot-plugin-osubot/nonebot_plugin_osubot-6.15.1-py3-none-any.whl/nonebot_plugin_osubot/matcher/bind.py
@@ -0,0 +1,34 @@
+import asyncio
+
+from nonebot import on_command
+from nonebot.params import CommandArg
+from nonebot_plugin_alconna import UniMessage
+from nonebot.internal.adapter import Event, Message
+
+from ..database import UserData
+from ..info import bind_user_info
+
+bind = on_command("bind", priority=11, block=True)
+unbind = on_command("unbind", priority=11, block=True)
+lock = asyncio.Lock()
+
+
+@bind.handle()
+async def _bind(event: Event, name: Message = CommandArg()):
+    name = name.extract_plain_text().strip()
+    if not name:
+        await UniMessage.text("请在指令后输入您的 osuid").finish(reply_to=True)
+    async with lock:
+        if user := await UserData.get_or_none(user_id=event.get_user_id()):
+            await UniMessage.text(f"您已绑定{user.osu_name}，如需要解绑请输入/unbind").finish(reply_to=True)
+        msg = await bind_user_info("bind", name, event.get_user_id(), True)
+    await UniMessage.text(msg).finish(reply_to=True)
+
+
+@unbind.handle()
+async def _unbind(event: Event):
+    if _ := await UserData.get_or_none(user_id=event.get_user_id()):
+        await UserData.filter(user_id=event.get_user_id()).delete()
+        await UniMessage.text("解绑成功！").send(reply_to=True)
+    else:
+        await UniMessage.text("尚未绑定，无需解绑").send(reply_to=True)
