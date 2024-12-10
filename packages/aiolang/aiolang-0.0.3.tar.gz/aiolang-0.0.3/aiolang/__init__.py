a729acc5bc02942dad39ed3fe6740074517382a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 17:05:02 +0000

    Add package aiolang/0.0.3/aiolang-0.0.3.tar.gz

---
+++ packages/aiolang/aiolang-0.0.3.tar.gz/aiolang-0.0.3/aiolang/__init__.py
@@ -0,0 +1,6 @@
+# aiolang/__init__.py
+from .aiolang import Aiolang
+from .exceptions import TranslationError
+
+
+__all__ = ["TranslationError", "Aiolang"]
---
+++ packages/aiolang/aiolang-0.0.3.tar.gz/aiolang-0.0.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Translate Google Framework For Python'
+LONG_DESCRIPTION = 'Simple, modern, asynchronous for API building use or normal user use.'
+
+# Setting up
+setup(
+    name="aiolang",
+    version=VERSION,
+    author="",
+    author_email="aiolang.python@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['aiohttp','aiofiles'],
+    keywords=['python', 'translate'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
