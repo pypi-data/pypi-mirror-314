8660c69fcccdf48bb43991917e13daa7df82e006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 05:04:38 +0000

    Add package sibi-dst/0.3.18/sibi_dst-0.3.18-py3-none-any.whl

---
+++ packages/sibi-dst/sibi_dst-0.3.18-py3-none-any.whl/sibi_dst/df_helper/backends/django/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from ._django_db_connection import DjangoConnectionConfig
+from ._io_dask import ReadFrameDask
+#from ._io_dask_alt import ReadFrameDask
+from ._django_load_from_db import DjangoLoadFromDb
+
+__all__ = [
+    "DjangoConnectionConfig",
+    "ReadFrameDask",
+    "DjangoLoadFromDb"
+]
