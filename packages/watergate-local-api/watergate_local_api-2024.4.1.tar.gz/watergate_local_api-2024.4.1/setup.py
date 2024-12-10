7b8f2329dacff1099e3f245154a649f76a0b166d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 05:04:33 +0000

    Add package watergate-local-api/2024.4.1/watergate_local_api-2024.4.1.tar.gz

---
+++ packages/watergate-local-api/watergate_local_api-2024.4.1.tar.gz/watergate_local_api-2024.4.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+with open("version.txt", "r") as f:
+    version = f.read()
+
+setup(
+    name="watergate_local_api", # Replace with your own username
+    version=version,
+    author="Watergate",
+    author_email="hi@watergate.ai",
+    description="Python package to interact with the Watergate Local API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hero-laboratories/watergate-local-api-python",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ]
+)
