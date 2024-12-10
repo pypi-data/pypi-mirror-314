f249ab0166a25aa4157696672774131f789c7fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 03:18:47 +0000

    Add package instinct-py/1.2.0/instinct-py-1.2.0.tar.gz

---
+++ packages/instinct-py/instinct-py-1.2.0.tar.gz/instinct-py-1.2.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="instinct-py",
+    version="1.2.0",
+    author="Nexstem India Private Limited",
+    author_email="developers@nexstem.ai",
+    description="Control and interact with your Instinct headset.",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 1 - Planning",
+        "License :: Free To Use But Restricted",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
