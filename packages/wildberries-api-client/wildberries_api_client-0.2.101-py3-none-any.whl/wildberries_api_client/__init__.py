7f0f1f2e7d9f39b4fe8f438eec645154b363c444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 16:05:30 +0000

    Add package wildberries-api-client/0.2.101/wildberries_api_client-0.2.101-py3-none-any.whl

---
+++ packages/wildberries-api-client/wildberries_api_client-0.2.101-py3-none-any.whl/wildberries_api_client/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+
+from . import analytics
+from . import content
+from . import feedbacks_questions
+from . import marketplace
+from . import prices
+from . import promotion
+from . import recommendations
+from . import statistics
+from . import tariffs
+from . import chats
+from . import supplies
+from wildberries_api_client._configuration import Configuration
+from wildberries_api_client._api_client import ApiClient
---
+++ packages/wildberries-api-client/wildberries_api_client-0.2.101-py3-none-any.whl/wildberries_api_client/prices/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from wildberries_api_client.prices.api.sostoania_zagruzok_api import SostoaniaZagruzokApi
+from wildberries_api_client.prices.api.spiski_tovarov_api import SpiskiTovarovApi
+from wildberries_api_client.prices.api.ustanovka_cen_i_skidok_api import UstanovkaCenISkidokApi
