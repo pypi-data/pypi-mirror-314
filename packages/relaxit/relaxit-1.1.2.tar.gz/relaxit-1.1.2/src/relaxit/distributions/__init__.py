d2169a36c9928741e24e5e436b5aa60f98e08360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 17:05:00 +0000

    Add package relaxit/1.1.2/relaxit-1.1.2.tar.gz

---
+++ packages/relaxit/relaxit-1.1.2.tar.gz/relaxit-1.1.2/src/relaxit/distributions/__init__.py
@@ -0,0 +1,20 @@
+from .CorrelatedRelaxedBernoulli import CorrelatedRelaxedBernoulli
+from .GaussianRelaxedBernoulli import GaussianRelaxedBernoulli
+from .GumbelSoftmaxTopK import GumbelSoftmaxTopK
+from .HardConcrete import HardConcrete
+from .InvertibleGaussian import InvertibleGaussian
+from .LogisticNormalSoftmax import LogisticNormalSoftmax
+from .StochasticTimesSmooth import StochasticTimesSmooth
+from .StraightThroughBernoulli import StraightThroughBernoulli
+
+
+__all__ = [
+    "CorrelatedRelaxedBernoulli",
+    "GaussianRelaxedBernoulli",
+    "GumbelSoftmaxTopK",
+    "HardConcrete",
+    "InvertibleGaussian",
+    "LogisticNormalSoftmax",
+    "StochasticTimesSmooth",
+    "StraightThroughBernoulli"
+]
