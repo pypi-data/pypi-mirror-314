c49493b9ce37b18b552c4c9765528f2dd38ee295
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 14:04:17 +0000

    Add package boto3-stubs-full/1.35.77/boto3_stubs_full-1.35.77.tar.gz

---
+++ packages/boto3-stubs-full/boto3_stubs_full-1.35.77.tar.gz/boto3_stubs_full-1.35.77/mypy_boto3_artifact/__init__.pyi
@@ -0,0 +1,28 @@
+"""
+Main interface for artifact service.
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_artifact import (
+        ArtifactClient,
+        Client,
+        ListReportsPaginator,
+    )
+
+    session = Session()
+    client: ArtifactClient = session.client("artifact")
+
+    list_reports_paginator: ListReportsPaginator = client.get_paginator("list_reports")
+    ```
+
+Copyright 2024 Vlad Emelianov
+"""
+
+from .client import ArtifactClient
+from .paginator import ListReportsPaginator
+
+Client = ArtifactClient
+
+__all__ = ("ArtifactClient", "Client", "ListReportsPaginator")
---
+++ packages/boto3-stubs-full/boto3_stubs_full-1.35.77.tar.gz/boto3_stubs_full-1.35.77/mypy_boto3_artifact/paginator.pyi
@@ -0,0 +1,58 @@
+"""
+Type annotations for artifact service client paginators.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_artifact/paginators/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+
+    from mypy_boto3_artifact.client import ArtifactClient
+    from mypy_boto3_artifact.paginator import (
+        ListReportsPaginator,
+    )
+
+    session = Session()
+    client: ArtifactClient = session.client("artifact")
+
+    list_reports_paginator: ListReportsPaginator = client.get_paginator("list_reports")
+    ```
+
+Copyright 2024 Vlad Emelianov
+"""
+
+import sys
+from typing import Generic, Iterator, TypeVar
+
+from botocore.paginate import PageIterator, Paginator
+
+from .type_defs import ListReportsRequestListReportsPaginateTypeDef, ListReportsResponseTypeDef
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+__all__ = ("ListReportsPaginator",)
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+class _PageIterator(PageIterator, Generic[_ItemTypeDef]):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+class ListReportsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/artifact/paginator/ListReports.html#Artifact.Paginator.ListReports)
+    [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_artifact/paginators/#listreportspaginator)
+    """
+    def paginate(
+        self, **kwargs: Unpack[ListReportsRequestListReportsPaginateTypeDef]
+    ) -> _PageIterator[ListReportsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/artifact/paginator/ListReports.html#Artifact.Paginator.ListReports.paginate)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_artifact/paginators/#listreportspaginator)
+        """
