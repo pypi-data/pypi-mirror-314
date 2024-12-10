778527851ac098b77f0bc4ea050db2fdf0578b81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-10 15:06:05 +0000

    Add package DesignPR/0.2.3/designpr-0.2.3.tar.gz

---
+++ packages/DesignPR/designpr-0.2.3.tar.gz/designpr-0.2.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DesignPR",  
+    version="0.2.3",  # Update as you make changes
+    description="A GUI tool for designing PCR primers",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="",
+    author_email="",
+    url="https://github.com/aidamo1824/DesignPR",  
+    packages=find_packages(), 
+        install_requires=[
+        'biopython>=1.80',
+        'tkinter',  
+    ],
+    py_modules=["main"],
+    entry_points={
+        "console_scripts": [
+            "run_DesignPR=designPR.main:main", 
+        ],
+    },
+    license="MIT"
+)
