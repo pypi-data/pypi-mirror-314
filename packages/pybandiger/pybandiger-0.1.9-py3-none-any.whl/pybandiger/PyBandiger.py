664759f55a15f0b2c39701eadb1ae7a972e49227
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 16:05:16 +0000

    Add package pybandiger/0.1.9/pybandiger-0.1.9-py3-none-any.whl

---
+++ packages/pybandiger/pybandiger-0.1.9-py3-none-any.whl/pybandiger/PyBandiger.py
@@ -0,0 +1,49 @@
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+import pandas as pd
+
+class PyBandiger:
+    def __init__(self):
+        self.le = {}
+        self.ss = StandardScaler()
+
+    def clean(self, data):
+        data = data.copy()
+        cat_col = data.select_dtypes(include='object').columns
+        num_col = data.select_dtypes(include=['int', 'float']).columns
+        for col in cat_col:
+            data[col] = data[col].fillna('Missing')
+        for col in num_col:
+            data[col] = data[col].fillna(data[col].mean())
+        return data
+
+    def EncodeAndScale_fit(self, data):
+        data = data.copy()
+        cat_col = data.select_dtypes(include='object').columns
+        num_col = data.select_dtypes(include=['int', 'float']).columns
+        for col in cat_col:
+            encoder = LabelEncoder()
+            data[col] = encoder.fit_transform(data[col])
+            self.le[col] = encoder
+        data[num_col] = self.ss.fit_transform(data[num_col])
+        return data
+
+    def EncodeAndScale_transform(self, data):
+        data = data.copy()
+        cat_col = data.select_dtypes(include='object').columns
+        num_col = data.select_dtypes(include=['int', 'float']).columns
+        for col in cat_col:
+            if col in self.le:
+                data[col] = self.le[col].transform(data[col])
+            else:
+                raise ValueError(f"Column '{col}' was not fitted with an encoder.")
+        data[num_col] = self.ss.transform(data[num_col])
+        return data
+    def time_transform(self, data, date_col = 'date', prefix = ''):
+        data = data.copy()
+        data[date_col] = pd.to_datetime(data[date_col])
+        data[prefix+'year'] = data[date_col].dt.year
+        data[prefix+'month'] = data[date_col].dt.month
+        data[prefix+'day'] = data[date_col].dt.day
+        data[prefix+'dayofweek'] = data[date_col].dt.dayofweek
+        data = data.drop(date_col, axis=1)
+        return data
---
+++ packages/pybandiger/pybandiger-0.1.9-py3-none-any.whl/pybandiger/__init__.py
@@ -0,0 +1,7 @@
+from .PyBandiger import PyBandiger
+
+__version__ = '0.1.9'
+
+__all__ = [
+    'PyBandiger',
+]
