d67dc4984759c2811cd6b2c983c19dd9c1c15e31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 06:04:38 +0000

    Add package ccrestoration/0.2.0/ccrestoration-0.2.0-py3-none-any.whl

---
+++ packages/ccrestoration/ccrestoration-0.2.0-py3-none-any.whl/ccrestoration/type/arch.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+
+# Enum for the architecture type, use capital letters
+class ArchType(str, Enum):
+    # ------------------------------------- Single Image Super-Resolution ----------------------------------------------
+
+    RRDB = "RRDB"
+    SRVGG = "SRVGG"
+    UPCUNET = "UPCUNET"
+    EDSR = "EDSR"
+    SWINIR = "SWINIR"
+    SCUNET = "SCUNET"
+    DAT = "DAT"
+    SRCNN = "SRCNN"
+
+    # ------------------------------------- Auxiliary Network ----------------------------------------------------------
+
+    SPYNET = "SPYNET"
+    EDVRFEATUREEXTACTOR = "EDVRFEATUREEXTACTOR"
+
+    # ------------------------------------- Video Super-Resolution -----------------------------------------------------
+
+    EDVR = "EDVR"
+    BASICVSR = "BASICVSR"
+    ICONVSR = "ICONVSR"
+    MSRSWVSR = "MSRSWVSR"
