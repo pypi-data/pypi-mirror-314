fb55717249933fc0dac28fe26c2af003651bafb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 06:04:19 +0000

    Add package waii-sdk-py/1.25.5/waii_sdk_py-1.25.5-py3-none-any.whl

---
+++ packages/waii-sdk-py/waii_sdk_py-1.25.5-py3-none-any.whl/waii_sdk_py/database/database.py
@@ -0,0 +1,527 @@
+import inspect
+import warnings
+
+from waii_sdk_py.waii_http_client import WaiiHttpClient
+from ..common import LLMBasedRequest, CommonRequest, CheckOperationStatusResponse, CheckOperationStatusRequest
+from ..my_pydantic import BaseModel, PrivateAttr
+import re
+from typing import Optional, List, Dict, Any, Union
+from urllib.parse import urlparse, parse_qs
+from enum import Enum
+
+from ..user import CommonResponse
+from ..utils.utils import to_async, wrap_methods_with_async
+
+MODIFY_DB_ENDPOINT = "update-db-connect-info"
+GET_CATALOG_ENDPOINT = "get-table-definitions"
+UPDATE_TABLE_DESCRIPTION_ENDPOINT = "update-table-description"
+UPDATE_SCHEMA_DESCRIPTION_ENDPOINT = "update-schema-description"
+UPDATE_COLUMN_DESCRIPTION_ENDPOINT = "update-column-description"
+UPDATE_CONSTRAINT_ENDPOINT = "update-constraint"
+UPDATE_TABLE_DEFINITION_ENDPOINT = "update-table-definitions"
+UPDATE_SIMILARITY_SEARCH_INDEX_ENDPOINT = "update-similarity-search-index"
+GET_SIMILARITY_SEARCH_INDEX_ENDPOINT = "get-similarity-search-index"
+DELETE_SIMILARITY_SEARCH_INDEX_ENDPOINT = "delete-similarity-search-index"
+GET_SIMILARITY_SEARCH_INDEX_TABLE_ENDPOINT = "get-similarity-search-index-table"
+CHECK_SIMILARITY_SEARCH_INDEX_STATUS_ENDPOINT = "check-similarity-search-index-status"
+
+
+class SchemaName(BaseModel):
+    schema_name: str
+    database_name: Optional[str]
+
+
+class TableName(BaseModel):
+    table_name: str
+    schema_name: Optional[str]
+    database_name: Optional[str]
+
+
+class ColumnName(BaseModel):
+    table_name: TableName
+    column_name: str
+
+
+class ColumnSampleValues(BaseModel):
+    values: Optional[Dict[str, int]]
+
+
+class ColumnDefinition(BaseModel):
+    name: str
+    type: str
+    comment: Optional[str]
+    description: Optional[str]
+    sample_values: Optional[ColumnSampleValues]
+
+    def __lt__(self, other):
+        return self.name < other.name
+
+
+class ConstraintType(str, Enum):
+    primary = "primary"
+    foreign = "foreign"
+
+
+class ConstraintDetectorType(str, Enum):
+    database = "database"
+    inferred_llm = "inferred_llm"
+    inferred_static = "inferred_static"
+    inferred_query_history = "inferred_query_history"
+    manual = "manual"
+
+
+class TableReference(BaseModel):
+    src_table: Optional[TableName]  # table name
+    src_cols: Optional[List[str]]  # source table columns
+    ref_table: Optional[TableName]  # ref table name
+    ref_cols: Optional[List[str]]  # ref table columns
+    source: Optional[ConstraintDetectorType]
+
+
+class TableNameToDescription(BaseModel):
+    name: str
+    description: str
+
+
+class SchemaDescription(BaseModel):
+    summary: Optional[str]
+    common_questions: Optional[List[str]]
+    common_tables: Optional[List[TableNameToDescription]]
+
+
+class Constraint(BaseModel):
+    source: Optional[ConstraintDetectorType]
+    table: Optional[TableName]
+    cols: Optional[List[str]]
+    constraint_type: Optional[ConstraintType]
+
+    # for foreign key, it is the table and columns that the source table and cols
+    src_table: Optional[TableName]  # table name
+    src_cols: Optional[List[str]]  # source table columns
+
+    # comment for the constraint
+    comment: Optional[str]
+
+    def __repr__(self):
+        if self.constraint_type == ConstraintType.primary:
+            return f"PK {self.table} ({self.cols} [{self.source.name}]"
+        else:
+            return f"FK {self.src_table} ({self.src_cols}) -> {self.table} ({self.cols}) [{self.source.name}]"
+
+
+
+class TableDefinition(BaseModel):
+    name: TableName
+    columns: Optional[List[ColumnDefinition]]
+    comment: Optional[str]
+    last_altered_time: Optional[int]
+    _refs: Optional[List[TableReference]] = PrivateAttr(default=[])
+    constraints: Optional[List[Constraint]]
+    inferred_refs: Optional[List[TableReference]]
+    inferred_constraints: Optional[List[Constraint]]
+    description: Optional[str]
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        refs_data = data.get("refs",None)
+        refs = []
+        if refs_data:
+            for ref in refs_data:
+                if type(ref) == TableReference:
+                    refs.append(ref)
+                else:
+                    refs.append(TableReference(**ref))
+
+        self._refs = refs
+
+    @property
+    def refs(self):
+        warnings.warn(
+            "The 'refs' attribute is deprecated and will be removed in a future release. Use 'constraints' instead.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        return self._refs
+
+    @refs.setter
+    def refs(self, value):
+        warnings.warn(
+            "The 'refs' attribute is deprecated and will be removed in a future release. Use 'constraints' instead.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        self._refs = value
+
+class SchemaDefinition(BaseModel):
+    name: SchemaName
+    tables: Optional[List[TableDefinition]]
+    description: Optional[SchemaDescription]
+
+
+class CatalogDefinition(BaseModel):
+    name: str
+    schemas: Optional[List[SchemaDefinition]]
+
+class DBContentFilterScope(str, Enum):
+    schema = "schema"
+    table = "table"
+    column = "column"
+
+class DBContentFilterType(str, Enum):
+    include = "include"
+    exclude = "exclude"
+
+class DBContentFilterActionType(str, Enum):
+    # this means we will include/exclude the content
+    visibility = "visibility"
+
+    # this means we will do sample value or not
+    sample_values = "sample_values"
+
+
+class FilterType(str, Enum):
+    INCLUSION = "inclusion"
+    EXCLUSION = "exclusion"
+
+
+class SearchContext(BaseModel):
+    type: Optional[FilterType] = FilterType.INCLUSION
+    db_name: Optional[str] = '*'
+    schema_name: Optional[str] = '*'
+    table_name: Optional[str] = '*'
+    column_name: Optional[str] = '*'
+    ignore_case: Optional[bool] = True
+
+class DBContentFilter(BaseModel):
+    filter_scope: DBContentFilterScope
+    filter_type: DBContentFilterType = DBContentFilterType.include
+    filter_action_type: DBContentFilterActionType = DBContentFilterActionType.visibility
+    ignore_case: Optional[bool] = True
+    pattern: str  # regex pattern
+    search_context: Optional[List[SearchContext]]
+
+
+class DBAccessPolicy(BaseModel):
+    read_only: Optional[bool] = False
+    allow_access_beyond_db_content_filter: Optional[bool] = True
+    allow_access_beyond_search_context: Optional[bool] = True
+
+
+class DBConnection(BaseModel):
+    key: Optional[str]
+    db_type: str
+    description: Optional[str] = None
+    account_name: Optional[str] = None
+    username: Optional[str] = None
+    password: Optional[str] = None
+    database: Optional[str] = None
+    warehouse: Optional[str] = None
+    role: Optional[str] = None
+    path: Optional[str] = None
+    host: Optional[str] = None
+    port: Optional[int] = None
+    parameters: Optional[Dict[str, Any]] = None
+    sample_col_values: Optional[bool]
+    push:Optional[bool] = False
+    db_content_filters: Optional[List[DBContentFilter]]
+    embedding_model: Optional[str]
+    always_include_tables: Optional[List[TableName]]
+    alias: Optional[str]
+    db_access_policy: Optional[DBAccessPolicy] = DBAccessPolicy()
+    host_alias : Optional[str] = None
+    user_alias: Optional[str] = None
+    db_alias: Optional[str] = None
+    content_filters: Optional[List[SearchContext]]
+    sample_filters: Optional[List[SearchContext]]
+
+class ModifyDBConnectionRequest(BaseModel):
+    updated: Optional[List[DBConnection]] = None
+    removed: Optional[List[str]] = None
+    validate_before_save: Optional[bool] = None
+    user_id: Optional[str] = None
+    default_db_connection_key: Optional[str] = None
+
+
+class SchemaIndexingStatus(BaseModel):
+    n_pending_indexing_tables: int
+    n_total_tables: int
+    status: str
+
+
+class DBConnectionIndexingStatus(BaseModel):
+    status: Optional[str]
+    schema_status: Optional[Dict[str, SchemaIndexingStatus]]
+
+
+class ModifyDBConnectionResponse(BaseModel):
+    connectors: Optional[List[DBConnection]]
+    diagnostics: Optional[str]
+    default_db_connection_key: Optional[str]
+    connector_status: Optional[Dict[str, DBConnectionIndexingStatus]]
+
+
+class GetCatalogRequest(LLMBasedRequest):
+    search_context: Optional[List[SearchContext]]
+
+    # ask is to perform semantic search, if it is provided, then we will use it to search the table definition
+    # we will try to find the same tables which can answer the ask
+    ask: Optional[str]
+
+    internal: bool = False
+
+
+class GetDBConnectionRequest(BaseModel):
+    pass
+
+
+class GetDBConnectionResponse(BaseModel):
+    connectors: Optional[List[DBConnection]]
+    diagnostics: Optional[str]
+    default_db_connection_key: Optional[str]
+    connector_status: Optional[Dict[str, DBConnectionIndexingStatus]]
+
+
+class GetCatalogResponse(BaseModel):
+    catalogs: Optional[List[CatalogDefinition]] = None
+    debug_info: Optional[Dict[str, Any]]
+
+
+class UpdateTableDescriptionRequest(BaseModel):
+    table_name: TableName
+    description: str
+
+
+class UpdateTableDefinitionRequest(BaseModel):
+    updated_tables: Optional[List[TableDefinition]]
+    removed_tables: Optional[List[TableName]]
+
+
+class ColumnDescription(BaseModel):
+    column_name: str
+    description: Optional[str]
+
+
+class TableToColumnDescription(BaseModel):
+    table_name: TableName
+    column_descriptions: Optional[List[ColumnDescription]]
+
+
+class UpdateColumnDescriptionRequest(BaseModel):
+    col_descriptions: Optional[List[TableToColumnDescription]]
+
+
+class UpdatedTableToCol(BaseModel):
+    table_name: TableName
+    column_names: Optional[List[str]]
+
+
+class UpdateColumnDescriptionResponse(BaseModel):
+    updated_table_to_cols: Optional[List[UpdatedTableToCol]]
+
+class UpdateTableDefinitionResponse(BaseModel):
+    updated_tables: Optional[List[TableName]]
+
+
+class UpdateSchemaDescriptionRequest(BaseModel):
+    schema_name: SchemaName
+    description: str
+
+
+class UpdateTableDescriptionResponse(BaseModel):
+    pass
+
+
+class UpdateSchemaDescriptionResponse(BaseModel):
+    pass
+
+
+class TableConstraints(BaseModel):
+    table_name: TableName
+    constraints: Optional[List[Constraint]]
+    constraint_type: ConstraintType
+
+
+class UpdateConstraintRequest(BaseModel):
+    # updated constraints, it will replace the existing constraints
+    updated_constraints: Optional[List[TableConstraints]]
+
+
+class UpdateConstraintResponse(BaseModel):
+    updated_tables: Optional[List[TableName]]
+
+
+class RefreshDBConnectionRequest(BaseModel):
+    db_conn_key: str
+
+
+class ColumnValue(BaseModel):
+    value: str
+    additional_info: Optional[List[str]]
+
+
+class UpdateSimilaritySearchIndexRequest(CommonRequest):
+    values: Optional[List[ColumnValue]]
+    column: ColumnName
+    enable_llm_rerank: Optional[bool] = True
+    similarity_score_threshold: Optional[float] = None
+    max_matched_values: Optional[int] = None
+    min_matched_values: Optional[int] = None
+
+
+class UpdateSimilaritySearchIndexResponse(CommonResponse):
+    op_id: str
+
+
+class GetSimilaritySearchIndexOnTableRequest(CommonRequest):
+    table: TableName
+
+
+class GetSimilaritySearchIndexOnTableResponse(CommonResponse):
+    table: TableName
+    columns: List[ColumnName]
+
+
+class DeleteSimilaritySearchIndexRequest(CommonRequest):
+    column: ColumnName
+
+class DeleteSimilaritySearchIndexResponse(CommonResponse):
+    op_id: Optional[str]
+
+
+class GetSimilaritySearchIndexRequest(CommonRequest):
+    column: ColumnName
+
+
+class GetSimilaritySearchIndexResponse(CommonRequest):
+    column: ColumnName
+    values: Optional[List[ColumnValue]]
+
+
+class DatabaseImpl:
+
+    def __init__(self, http_client: WaiiHttpClient):
+        self.http_client = http_client
+
+    def modify_connections(
+        self, params: ModifyDBConnectionRequest
+    ) -> ModifyDBConnectionResponse:
+        return self.http_client.common_fetch(
+            MODIFY_DB_ENDPOINT, params.__dict__, ModifyDBConnectionResponse
+        )
+
+    def get_connections(
+        self, params: GetDBConnectionRequest = GetDBConnectionRequest()
+    ) -> GetDBConnectionResponse:
+        return self.http_client.common_fetch(
+            MODIFY_DB_ENDPOINT,
+            params.__dict__,
+            GetDBConnectionResponse,
+            need_scope=False,
+        )
+
+    def activate_connection(self, key: str):
+        self.http_client.set_scope(key)
+
+    def get_activated_connection(self):
+        return self.http_client.get_scope()
+
+    def get_default_connection(self):
+        return self.http_client.get_scope()
+
+    def get_catalogs(
+        self, params: GetCatalogRequest = GetCatalogRequest()
+    ) -> GetCatalogResponse:
+        return self.http_client.common_fetch(
+            GET_CATALOG_ENDPOINT, params.__dict__, GetCatalogResponse
+        )
+
+    def update_table_description(
+        self, params: UpdateTableDescriptionRequest
+    ) -> UpdateTableDescriptionResponse:
+        return self.http_client.common_fetch(
+            UPDATE_TABLE_DESCRIPTION_ENDPOINT, params.__dict__, GetCatalogResponse
+        )
+
+    def update_table_definition(
+        self, params:UpdateTableDefinitionRequest
+    ) -> UpdateTableDefinitionResponse:
+        return self.http_client.common_fetch(
+            UPDATE_TABLE_DEFINITION_ENDPOINT, params.__dict__,UpdateTableDefinitionResponse
+        )
+
+    def update_schema_description(
+        self, params: UpdateSchemaDescriptionRequest
+    ) -> UpdateSchemaDescriptionResponse:
+        return self.http_client.common_fetch(
+            UPDATE_SCHEMA_DESCRIPTION_ENDPOINT, params.__dict__, GetCatalogResponse
+        )
+
+    def update_column_description(
+        self, params: UpdateColumnDescriptionRequest
+    ) -> UpdateColumnDescriptionResponse:
+        return self.http_client.common_fetch(
+            UPDATE_COLUMN_DESCRIPTION_ENDPOINT,
+            params.__dict__,
+            UpdateColumnDescriptionResponse,
+        )
+
+    def update_constraint(
+        self, params: UpdateConstraintRequest
+    ) -> UpdateConstraintResponse:
+        return self.http_client.common_fetch(
+            UPDATE_CONSTRAINT_ENDPOINT, params.__dict__, UpdateConstraintResponse
+        )
+
+    def refresh_db_connection(self):
+        return self.http_client.common_fetch(
+            "refresh-db-connection",
+            {
+                "db_conn_key": self.get_activated_connection(),
+            },
+            CommonResponse,
+        )
+
+    def update_similarity_search_index(
+            self, params: UpdateSimilaritySearchIndexRequest
+    ) -> CommonResponse:
+        return self.http_client.common_fetch(
+            UPDATE_SIMILARITY_SEARCH_INDEX_ENDPOINT, params.__dict__, UpdateSimilaritySearchIndexResponse
+        )
+
+    def get_similarity_search_index(
+            self, params: GetSimilaritySearchIndexRequest
+    ) -> GetSimilaritySearchIndexResponse:
+        return self.http_client.common_fetch(
+            GET_SIMILARITY_SEARCH_INDEX_ENDPOINT, params.__dict__, GetSimilaritySearchIndexResponse
+        )
+
+    def delete_similarity_search_index(
+            self, params: DeleteSimilaritySearchIndexRequest
+    ) -> CommonResponse:
+        return self.http_client.common_fetch(
+            DELETE_SIMILARITY_SEARCH_INDEX_ENDPOINT, params.__dict__, DeleteSimilaritySearchIndexResponse
+        )
+
+    def get_similarity_search_index_on_table(
+            self, params: GetSimilaritySearchIndexOnTableRequest
+    ) -> GetSimilaritySearchIndexOnTableResponse:
+        return self.http_client.common_fetch(
+            GET_SIMILARITY_SEARCH_INDEX_TABLE_ENDPOINT, params.__dict__, GetSimilaritySearchIndexOnTableResponse
+        )
+
+    def get_similarity_search_index_status(
+            self, params: CheckOperationStatusRequest
+    ) -> CheckOperationStatusResponse:
+        return self.http_client.common_fetch(
+            CHECK_SIMILARITY_SEARCH_INDEX_STATUS_ENDPOINT, params.__dict__, CheckOperationStatusResponse
+        )
+
+
+class AsyncDatabaseImpl:
+    def __init__(self, http_client: WaiiHttpClient):
+        self._database_impl = DatabaseImpl(http_client)
+        wrap_methods_with_async(self._database_impl, self)
+
+
+
+Database = DatabaseImpl(WaiiHttpClient.get_instance())
---
+++ packages/waii-sdk-py/waii_sdk_py-1.25.5-py3-none-any.whl/waii_sdk_py/user/user.py
@@ -0,0 +1,236 @@
+from typing import Optional, List, Dict, Any
+
+from waii_sdk_py.common import CommonRequest, CommonResponse
+from waii_sdk_py.waii_http_client import WaiiHttpClient
+from ..my_pydantic import BaseModel
+from waii_sdk_py.utils import wrap_methods_with_async
+
+LIST_ACCESS_KEY_ENDPOINT = "list-access-keys"
+DELETE_ACCESS_KEY_ENDPOINT = "delete-access-keys"
+CREATE_KEY_ENDPOINT = "create-key"
+GET_USER_INFO_ENDPOINT = "get-user-info"
+UPDATE_CONFIG_ENDPOINT = "update-config"
+CREATE_USER_ENDPOINT = "create-user"
+DELETE_USER_ENDPOINT = "delete-user"
+UPDATE_USER_ENDPOINT = "update-user"
+LIST_USERS_ENDPOINT = "list-users"
+CREATE_TENANT_ENDPOINT = "create-tenant"
+UPDATE_TENANT_ENDPOINT = "update-tenant"
+DELETE_TENANT_ENDPOINT = "delete-tenant"
+LIST_TENANTS_ENDPOINT = "list-tenants"
+CREATE_ORG_ENDPOINT = "create-org"
+UPDATE_ORG_ENDPOINT = "update-org"
+DELETE_ORG_ENDPOINT = "delete-org"
+LIST_ORGS_ENDPOINT = "list-orgs"
+
+
+class CreateAccessKeyRequest(BaseModel):
+    name: str
+
+
+class AccessKey(BaseModel):
+    access_key: str
+    user_id: str
+    description: Optional[str]
+    name: Optional[str]
+    created_at: Optional[int]
+
+
+class GetAccessKeyResponse(BaseModel):
+    access_keys: Optional[List[AccessKey]]
+
+
+class GetAccessKeyRequest(BaseModel):
+    pass
+
+
+class DelAccessKeyRequest(BaseModel):
+    names: List[str]
+
+
+class DelAccessKeyResponse(BaseModel):
+    pass
+
+
+class GetUserInfoRequest:
+    pass
+
+
+class GetUserInfoResponse(BaseModel):
+    id: str
+    name: str
+    email: str
+    roles: List[str]
+    permissions: List[str]
+
+
+class UpdateConfigRequest(BaseModel):
+    updated: Optional[Dict[str, Any]]
+    deleted: Optional[List[str]]
+
+
+class UpdateConfigResponse(BaseModel):
+    configs: Dict[str, Any]
+
+
+class Organization(BaseModel):
+    id: str  # unique id for the organization
+    name: str  # display name for the organization
+    variables: Optional[Dict[str, Any]]  # variables for the organization
+
+
+class Tenant(BaseModel):
+    id: str  # unique id for the tenant
+    name: str  # display name for the tenant
+    org_id: Optional[str]  # org id for the tenant
+    variables: Optional[Dict[str, Any]]  # variables for the tenant
+
+
+class User(BaseModel):
+    id: str  # unique id for the user
+    name: Optional[str]  # display name for the user
+    tenant_id: Optional[str]  # tenant id for the user
+    org_id: Optional[str]  # org id for the user
+    variables: Optional[Dict[str, Any]]  # variables for the user
+    roles: Optional[List[str]] = []  # roles for the user
+
+# Roles of the user
+class WaiiRoles:
+    WAII_TRIAL_USER = 'waii-trial-user'
+    WAII_USER = 'waii-user'
+    WAII_API_USER = 'waii-api-user'
+    WAII_ADMIN_USER = 'waii-admin-user'
+    WAII_ORG_ADMIN_USER = 'waii-org-admin-user'
+    WAII_SUPER_ADMIN_USER = 'waii-super-admin-user'
+    WAII_TRAIL_API_USER = 'waii-trial-api-user'
+
+class CreateUserRequest(CommonRequest):
+    user: User
+
+
+class UpdateUserRequest(CommonRequest):
+    user: User
+
+
+class DeleteUserRequest(CommonRequest):
+    id: str
+
+
+class ListUsersRequest(CommonRequest):
+    # lookup by org, by default it will lookup users within the same org
+    lookup_org_id: Optional[str]
+
+    # lookup by tenant, by default it will lookup all users within the org (depends on lookup_org_id)
+    lookup_tenant_id: Optional[str]
+
+    # lookup by user, by default it will return all users after apply the above filters
+    lookup_user_id: Optional[str]
+
+
+class ListUsersResponse(CommonResponse):
+    users: List[User]
+
+
+class CreateTenantRequest(BaseModel):
+    tenant: Tenant
+
+
+class UpdateTenantRequest(BaseModel):
+    tenant: Tenant
+
+
+class DeleteTenantRequest(BaseModel):
+    id: str
+
+
+class ListTenantsRequest(CommonRequest):
+    lookup_org_id: Optional[str]
+
+
+class ListTenantsResponse(CommonResponse):
+    tenants: List[Tenant]
+
+
+class CreateOrganizationRequest(BaseModel):
+    organization: Organization
+
+
+class UpdateOrganizationRequest(BaseModel):
+    organization: Organization
+
+
+class DeleteOrganizationRequest(BaseModel):
+    id: str
+
+
+class ListOrganizationsRequest(CommonRequest):
+    pass
+
+
+class ListOrganizationsResponse(CommonResponse):
+    organizations: List[Organization]
+
+
+class UserImpl:
+
+    def __init__(self, http_client: WaiiHttpClient):
+        self.http_client = http_client
+
+    def create_access_key(self, params: CreateAccessKeyRequest):
+        return self.http_client.common_fetch(
+            CREATE_KEY_ENDPOINT, params.__dict__, GetAccessKeyResponse
+        )
+
+    def list_access_keys(self, params: GetAccessKeyRequest):
+        return self.http_client.common_fetch(LIST_ACCESS_KEY_ENDPOINT, params.__dict__, GetAccessKeyResponse)
+
+    def delete_access_key(self, params: DelAccessKeyRequest):
+        return self.http_client.common_fetch(DELETE_ACCESS_KEY_ENDPOINT, params.__dict__, DelAccessKeyResponse)
+
+    def get_user_info(self, params: GetUserInfoRequest):
+        return self.http_client.common_fetch(GET_USER_INFO_ENDPOINT, params.__dict__, GetUserInfoResponse)
+
+    def update_config(self, params: UpdateConfigRequest):
+        return self.http_client.common_fetch(UPDATE_CONFIG_ENDPOINT, params.__dict__, UpdateConfigResponse)
+
+    def create_user(self, params: CreateUserRequest):
+        return self.http_client.common_fetch(CREATE_USER_ENDPOINT, params.__dict__, CommonResponse)
+
+    def delete_user(self, params: DeleteUserRequest):
+        return self.http_client.common_fetch(DELETE_USER_ENDPOINT, params.__dict__, CommonResponse)
+
+    def update_user(self, params: UpdateUserRequest):
+        return self.http_client.common_fetch(UPDATE_USER_ENDPOINT, params.__dict__, CommonResponse)
+
+    def list_users(self, params: ListUsersRequest):
+        return self.http_client.common_fetch(LIST_USERS_ENDPOINT, params.__dict__, ListUsersResponse)
+
+    def create_tenant(self, params: CreateTenantRequest):
+        return self.http_client.common_fetch(CREATE_TENANT_ENDPOINT, params.__dict__, CommonResponse)
+
+    def update_tenant(self, params: UpdateTenantRequest):
+        return self.http_client.common_fetch(UPDATE_TENANT_ENDPOINT, params.__dict__, CommonResponse)
+
+    def delete_tenant(self, params: DeleteTenantRequest):
+        return self.http_client.common_fetch(DELETE_TENANT_ENDPOINT, params.__dict__, CommonResponse)
+
+    def list_tenants(self, params: ListTenantsRequest):
+        return self.http_client.common_fetch(LIST_TENANTS_ENDPOINT, params.__dict__, ListTenantsResponse)
+
+    def create_org(self, params: CreateOrganizationRequest):
+        return self.http_client.common_fetch(CREATE_ORG_ENDPOINT, params.__dict__, CommonResponse)
+
+    def update_org(self, params: UpdateOrganizationRequest):
+        return self.http_client.common_fetch(UPDATE_ORG_ENDPOINT, params.__dict__, CommonResponse)
+
+    def delete_org(self, params: DeleteOrganizationRequest):
+        return self.http_client.common_fetch(DELETE_ORG_ENDPOINT, params.__dict__, CommonResponse)
+
+    def list_orgs(self, params: ListOrganizationsRequest):
+        return self.http_client.common_fetch(LIST_ORGS_ENDPOINT, params.__dict__, ListOrganizationsResponse)
+
+
+class AsyncUserImpl:
+    def __init__(self, http_client: WaiiHttpClient):
+        self._user_impl = UserImpl(http_client)
+        wrap_methods_with_async(self._user_impl, self)
