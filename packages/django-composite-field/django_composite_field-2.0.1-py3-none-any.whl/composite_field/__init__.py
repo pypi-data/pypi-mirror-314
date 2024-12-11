558d6e5b3be1081ed5e10c9a5b4dc9bbd6647eaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 07:05:09 +0000

    Add package django-composite-field/2.0.1/django_composite_field-2.0.1-py3-none-any.whl

---
+++ packages/django-composite-field/django_composite_field-2.0.1-py3-none-any.whl/composite_field/__init__.py
@@ -0,0 +1,11 @@
+from .base import CompositeField
+from .l10n import LocalizedField, LocalizedCharField, LocalizedTextField
+from .complex import ComplexField
+
+__all__ = [
+    'CompositeField',
+    'LocalizedField',
+    'LocalizedCharField',
+    'LocalizedTextField',
+    'ComplexField',
+]
