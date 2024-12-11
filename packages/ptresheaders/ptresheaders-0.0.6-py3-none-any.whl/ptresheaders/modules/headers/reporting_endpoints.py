312f98cee4cd869122f838d147358de53edbad8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 05:06:36 +0000

    Add package ptresheaders/0.0.6/ptresheaders-0.0.6-py3-none-any.whl

---
+++ packages/ptresheaders/ptresheaders-0.0.6-py3-none-any.whl/ptresheaders/modules/headers/reporting_endpoints.py
@@ -0,0 +1,7 @@
+from modules.headers._header_test_base import HeaderTestBase
+from ptlibs.ptprinthelper import ptprint
+
+class ReportingEndpoints(HeaderTestBase):
+
+    def test_header(self, header_value: str):
+        pass
---
+++ packages/ptresheaders/ptresheaders-0.0.6-py3-none-any.whl/ptresheaders/modules/headers/x_dns_prefetch_control.py
@@ -0,0 +1,6 @@
+from modules.headers._header_test_base import HeaderTestBase
+from ptlibs.ptprinthelper import ptprint
+
+class XDNSPrefetchControl(HeaderTestBase):
+    def test_header(self, header_value: str):
+        pass
