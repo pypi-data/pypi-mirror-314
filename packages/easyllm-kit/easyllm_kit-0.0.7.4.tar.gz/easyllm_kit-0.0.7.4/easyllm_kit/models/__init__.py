8be2fd707f5440d54306912e0f089d57d1b9573e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 15:05:23 +0000

    Add package easyllm-kit/0.0.7.4/easyllm_kit-0.0.7.4.tar.gz

---
+++ packages/easyllm-kit/easyllm_kit-0.0.7.4.tar.gz/easyllm_kit-0.0.7.4/easyllm_kit/models/__init__.py
@@ -0,0 +1,19 @@
+from easyllm_kit.models.base import LLM
+from easyllm_kit.models.openai import GPT4o
+from easyllm_kit.models.llama import Llama3
+from easyllm_kit.models.perplexity import Perplexity
+from easyllm_kit.models.claude import Claude35Sonnet
+from easyllm_kit.models.llava import Llava
+from easyllm_kit.models.cpm import MiniCPM
+from easyllm_kit.models.gemini import Gemini
+from easyllm_kit.models.flan import FlanT5
+
+__all__ = ['LLM',
+           'GPT4o',
+           'Llama3',
+           'Perplexity',
+           'Claude35Sonnet',
+           'Llava',
+           'MiniCPM',
+           'Gemini',
+           'FlanT5']
