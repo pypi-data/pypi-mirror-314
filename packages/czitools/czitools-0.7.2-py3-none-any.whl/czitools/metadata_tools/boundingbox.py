83bb27112bb08646451fb47d6ba2509b893dd6fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 16:05:58 +0000

    Add package czitools/0.7.2/czitools-0.7.2-py3-none-any.whl

---
+++ packages/czitools/czitools-0.7.2-py3-none-any.whl/czitools/metadata_tools/boundingbox.py
@@ -0,0 +1,58 @@
+from typing import Optional, Union, Dict
+from dataclasses import dataclass, field
+from box import Box
+import os
+from czitools.utils import logging_tools
+from pylibCZIrw import czi as pyczi
+import validators
+
+logger = logging_tools.set_logging()
+
+
+@dataclass
+class CziBoundingBox:
+    czisource: Union[str, os.PathLike[str], Box]
+    scenes_bounding_rect: Optional[Dict[int, pyczi.Rectangle]] = field(
+        init=False, default_factory=lambda: []
+    )
+    total_rect: Optional[pyczi.Rectangle] = field(init=False, default=None)
+    total_bounding_box: Optional[Dict[str, tuple]] = field(
+        init=False, default_factory=lambda: []
+    )
+    verbose: bool = False
+    
+    # TODO Is this really needed as a separate class or better integrate directly into CziMetadata class?
+
+    def __post_init__(self):
+        if self.verbose:
+            logger.info("Reading BoundingBoxes from CZI image data.")
+
+        pyczi_readertype = pyczi.ReaderFileInputTypes.Standard
+
+        if isinstance(self.czisource, Box):
+            self.czisource = self.czisource.filepath
+
+        if validators.url(str(self.czisource)):
+            pyczi_readertype = pyczi.ReaderFileInputTypes.Curl
+            logger.info(
+                "FilePath is a valid link. Only pylibCZIrw functionality is available."
+            )
+
+        with pyczi.open_czi(str(self.czisource), pyczi_readertype) as czidoc:
+            try:
+                self.scenes_bounding_rect = czidoc.scenes_bounding_rectangle
+            except Exception as e:
+                self.scenes_bounding_rect = None
+                logger.info("Scenes Bounding rectangle not found.")
+
+            try:
+                self.total_rect = czidoc.total_bounding_rectangle
+            except Exception as e:
+                self.total_rect = None
+                logger.info("Total Bounding rectangle not found.")
+
+            try:
+                self.total_bounding_box = czidoc.total_bounding_box
+            except Exception as e:
+                self.total_bounding_box = None
+                logger.info("Total Bounding Box not found.")
