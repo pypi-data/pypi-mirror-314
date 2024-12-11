e9ed17e33a1b117050a15bb9527fb12b62fa1dfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 14:05:05 +0000

    Add package ebtorch/0.28.3/ebtorch-0.28.3-py3-none-any.whl

---
+++ packages/ebtorch/ebtorch-0.28.3-py3-none-any.whl/ebtorch/__init__.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ==============================================================================
+#
+# Copyright 2020-* Emanuele Ballarin <emanuele@ballarin.cc>
+# All Rights Reserved. Unless otherwise explicitly stated.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ==============================================================================
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Imports (wildcard)
+from .data import *
+from .distributed import *
+from .logging import *
+from .nn import *
+from .optim import *
+from .typing import *
+
+# Deletions (from .data)
+del FastCollateMixup
+del Mixup
+del cifarhundred_dataloader_dispatcher
+del cifarten_dataloader_dispatcher
+del data_prep_dispatcher_1ch
+del data_prep_dispatcher_3ch
+del fashionmnist_dataloader_dispatcher
+del imagenette_dataloader_dispatcher
+del mnist_dataloader_dispatcher
+del kmnist_dataloader_dispatcher
+del pathmnist_dataloader_dispatcher
+del octmnist_dataloader_dispatcher
+del tissuemnist_dataloader_dispatcher
+del tinyimagenet_dataloader_dispatcher
+
+# Deletions (from .distributed)
+del reduce_accumulate_keepalive
+del slurm_nccl_env
+
+# Deletions (from .nn)
+del ArgMaxLayer
+del BasicAE
+del BasicVAE
+del SingleNeckVAE
+del beta_gaussian_kldiv
+del var_of_lap
+del BinarizeLayer
+del BrokenReLU
+del CausalConv1d
+del Clamp
+del ConvolutionalFlattenLayer
+del CoordConv1d
+del CoordConv2d
+del CoordConv3d
+del DeepRBL
+del FCBlock
+del FieldTransform
+del FlatChannelize2DLayer
+del GaussianReparameterizerSampler
+del GaussianReparameterizerSamplerLegacy
+del InnerProduct
+del KWTA1d
+del KWTA2d
+del Mish
+del multilasso
+del multiridge
+del MultiSolvePoissonTensor
+del NNEnsemble
+del PoissonNetCifar
+del PreActResNet
+del ProbePrintLayer
+del RBLinear
+del ResBlock
+del ReshapeLayer
+del SERLU
+del SGRUHCell
+del ScaledERF
+del SinLU
+del SirenSine
+del SmeLU
+del SolvePoisson
+del SolvePoissonTensor
+del SwiGLU
+del TupleDecouple
+del TupleSelect
+del StatefulTupleSelect
+del SilhouetteScore
+del Concatenate
+del DuplexLinearNeck
+del SharedDuplexLinearNeck
+del SimpleDuplexLinearNeck
+del GenerAct
+del WideResNet
+del beta_reco_bce
+del beta_reco_bce_splitout
+del lexsemble
+del build_repeated_sequential
+del field_transform
+del mishlayer_init
+del pixelwise_bce_mean
+del pixelwise_bce_sum
+del pixelwise_mse_sum
+del pixelwise_mse_mean
+del beta_reco_mse
+del beta_reco_mse_splitout
+del oldtranspose
+del silhouette_score
+del cummatmul
+del tensor_replicate
+del logit_to_prob
+del bisided_thresholding
+del ConvNeXtStem
+del ConvStem
+del GRNorm
+del MetaAILayerNorm
+del ViTStem
+
+# Deletions (from .optim)
+del AdaBound
+del AdamP
+del Adan
+del CosineLRScheduler
+del Lamb
+del Lion
+del Lookahead
+del Lookaround
+del RAdam
+del ADOPT
+del SAM
+del SGDP
+del epochwise_onecycle
+del onecycle_lincos
+del onecycle_linlin
+del onecycle_linlin_updown
+del warmed_up_linneal
+del warmed_up_annealer
+del make_beta_scheduler
+del alah_optim
+del ralah_optim
+del tricyc1c
+del wfneal
+
+# Deletions (from .logging)
+del AverageMeter
+del LogCSV
+
+# Deletions (from .typing)
+del numlike
+del realnum
+del strdev
---
+++ packages/ebtorch/ebtorch-0.28.3-py3-none-any.whl/ebtorch/nn/__init__.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ──────────────────────────────────────────────────────────────────────────────
+#
+#  Copyright (c) 2020-2024 Emanuele Ballarin <emanuele@ballarin.cc>
+#  Released under the terms of the MIT License
+#  (see: https://url.ballarin.cc/mitlicense)
+#
+# ──────────────────────────────────────────────────────────────────────────────
+#
+# SPDX-License-Identifier: MIT
+#
+# ──────────────────────────────────────────────────────────────────────────────
+# Imports
+from .architectures import ArgMaxLayer
+from .architectures import BasicAE
+from .architectures import BasicVAE
+from .architectures import beta_reco_bce
+from .architectures import beta_reco_bce_splitout
+from .architectures import beta_reco_mse
+from .architectures import beta_reco_mse_splitout
+from .architectures import BinarizeLayer
+from .architectures import build_repeated_sequential
+from .architectures import CausalConv1d
+from .architectures import Clamp
+from .architectures import Concatenate
+from .architectures import DeepRBL
+from .architectures import DuplexLinearNeck
+from .architectures import FCBlock
+from .architectures import GaussianReparameterizerSampler
+from .architectures import GaussianReparameterizerSamplerLegacy
+from .architectures import GenerAct
+from .architectures import InnerProduct
+from .architectures import lexsemble
+from .architectures import pixelwise_bce_mean
+from .architectures import pixelwise_bce_sum
+from .architectures import pixelwise_mse_mean
+from .architectures import pixelwise_mse_sum
+from .architectures import RBLinear
+from .architectures import ResBlock
+from .architectures import SGRUHCell
+from .architectures import SharedDuplexLinearNeck
+from .architectures import SilhouetteScore
+from .architectures import SimpleDuplexLinearNeck
+from .architectures import SingleNeckVAE
+from .architectures import SirenSine
+from .architectures import StatefulTupleSelect
+from .architectures import SwiGLU
+from .architectures import TupleDecouple
+from .architectures import TupleSelect
+from .architectures_resnets_dm import PreActResNet
+from .architectures_resnets_dm import WideResNet
+from .convolutional_flatten import ConvolutionalFlattenLayer
+from .convstems import ConvNeXtStem
+from .convstems import ConvStem
+from .convstems import GRNorm
+from .convstems import MetaAILayerNorm
+from .convstems import ViTStem
+from .coordconv import CoordConv1d
+from .coordconv import CoordConv2d
+from .coordconv import CoordConv3d
+from .debuglayers import ProbePrintLayer
+from .fieldtransform import FieldTransform
+from .functional import bisided_thresholding
+from .functional import cummatmul
+from .functional import field_transform
+from .functional import logit_to_prob
+from .functional import mish
+from .functional import oldtranspose
+from .functional import silhouette_score
+from .functional import tensor_replicate
+from .kwta import BrokenReLU
+from .kwta import KWTA1d
+from .kwta import KWTA2d
+from .laplacenet import MultiSolvePoissonTensor
+from .laplacenet import PoissonNetCifar
+from .laplacenet import SolvePoisson
+from .laplacenet import SolvePoissonTensor
+from .mish import Mish
+from .mish import mishlayer_init
+from .nnsemble import NNEnsemble
+from .penalties import beta_gaussian_kldiv
+from .penalties import multilasso
+from .penalties import multiridge
+from .penalties import var_of_lap
+from .reshapelayers import FlatChannelize2DLayer
+from .reshapelayers import ReshapeLayer
+from .serf import ScaledERF
+from .serlu import SERLU
+from .sinlu import SinLU
+from .smelu import SmeLU
+from .utils import *
+
+# Deletions (from .)
+del architectures
+del architectures_resnets_dm
+del convolutional_flatten
+del convstems
+del coordconv
+del debuglayers
+del fieldtransform
+del kwta
+del laplacenet
+del mish
+del nnsemble
+del penalties
+del reshapelayers
+del serlu
+del sinlu
+del smelu
+del serf
+
+# Deletions (from .functional)
+# del mish  # (already done by chance!)
+# del serf  # (already done by chance!)
+
+# Deletions (from .utils)
+del AdverApply
+del AutoClipper
+del TA2ATAdapter
+del eval_model_on_test
+del extract_conv_filters
+del argser_f
+del find_lr
+del fxfx2module
+del act_opclone
+del suppress_std
+del TelegramBotEcho
+del stablediv
+del argsink
+del no_op
+del download_gdrive
+del emplace_kv
+del show_filters
+del subset_state_dict
+del gather_model_repr
+del model_reqgrad
+del model_reqgrad_
+del patchify_2d
+del patchify_batch
+del patchify_dataset
+del store_repr_autohook
+del store_repr_fx
+del store_repr_hook
+del petroff_2021_color
+del tableau10_color
+del petroff_2021_cycler
+del tableau10_cycler
+del petroff_2021_cmap
+del tableau10_cmap
+del variadic_attrs
+del set_petroff_2021_colors
+del set_tableau10_colors
+del custom_plot_setup
+del plot_out
+del fromcache
+del repr_sizes_flat_adapter
+del repr_fx_flat_adapter
+del act_auto_broadcast
+del broadcast_in_dim
+del matched_apply
+del tensor_module_matched_apply
+del realnum
+del strdev
+del actvt
+del numlike
