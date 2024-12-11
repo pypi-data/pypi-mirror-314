da69941e011524b547c6325458536c39253d2533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 16:05:22 +0000

    Add package twjcrawl/0.0.2/twjcrawl-0.0.2.tar.gz

---
+++ packages/twjcrawl/twjcrawl-0.0.2.tar.gz/twjcrawl-0.0.2/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="twjcrawl",
+    version="0.0.2",
+    author="twj",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
