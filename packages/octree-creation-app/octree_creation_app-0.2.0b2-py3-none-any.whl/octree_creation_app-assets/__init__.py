521b98cee0969b337ca8646da7c9da286fe22bb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 06:05:35 +0000

    Add package octree-creation-app/0.2.0b2/octree_creation_app-0.2.0b2-py3-none-any.whl

---
+++ packages/octree-creation-app/octree_creation_app-0.2.0b2-py3-none-any.whl/octree_creation_app-assets/__init__.py
@@ -0,0 +1,6 @@
+#  Copyright (c) 2024 Mira Geoscience Ltd.
+#
+#  This file is part of octree-creation-app package.
+#
+#  octree-creation-app is distributed under the terms and conditions of the MIT License
+#  (see LICENSE file at the root of this source code package).
