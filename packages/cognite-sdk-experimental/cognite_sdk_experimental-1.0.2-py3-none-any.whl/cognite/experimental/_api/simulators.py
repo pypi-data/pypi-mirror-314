ba78229030392e0ce95a86750c97963c2cf8e521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 16:05:41 +0000

    Add package cognite-sdk-experimental/1.0.2/cognite_sdk_experimental-1.0.2-py3-none-any.whl

---
+++ packages/cognite-sdk-experimental/cognite_sdk_experimental-1.0.2-py3-none-any.whl/cognite/experimental/_api/simulators.py
@@ -0,0 +1,111 @@
+from __future__ import annotations
+
+from cognite.client._api_client import APIClient
+from cognite.client.utils._validation import assert_type
+from cognite.experimental.data_classes.simulators import SimulationRun, SimulationRunFilter, SimulationRunList
+
+
+class SimulationRunsAPI(APIClient):
+    _RESOURCE_PATH = "/simulators/run"
+    _LIST_CLASS = SimulationRunList
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def run(
+        self,
+        items: SimulationRun | list[SimulationRun],
+    ) -> SimulationRun | SimulationRunList:
+        assert_type(items, "items", [SimulationRun, list])
+        return self._create_multiple(
+            items=items,
+            resource_path=self._RESOURCE_PATH,
+            list_cls=SimulationRunList,
+            resource_cls=SimulationRun,
+        )
+
+    def list_runs(
+        self,
+        simulator_name: str | None = None,
+        model_name: str | None = None,
+        routine_name: str | None = None,
+        status: str | None = None,
+    ) -> SimulationRunList:
+        filter = SimulationRunFilter(
+            simulator_name=simulator_name,
+            model_name=model_name,
+            routine_name=routine_name,
+            status=status,
+        ).dump(camel_case=True)
+
+        return self._list(
+            method="POST",
+            filter=filter,
+            list_cls=SimulationRunList,
+            resource_path="/simulators/runs",
+            resource_cls=SimulationRun,
+        )
+
+
+class SimulatorsAPI(APIClient):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.simulations = SimulationRunsAPI(*args, **kwargs)
+
+    def run(
+        self,
+        items: SimulationRun | list[SimulationRun],
+    ) -> SimulationRun | SimulationRunList:
+        """Run a simulation
+
+        Args:
+            items (SimulationRun | List[SimulationRun]): simulation(s) to run
+
+        Returns:
+            SimulationRun | SimulationRunList: simulation run(s)
+
+        Examples:
+
+            Run a simulation::
+
+                >>> from cognite.experimental import CogniteClient
+                >>> from cognite.experimental.data_classes.simulators import SimulationRun
+                >>> c = CogniteClient()
+                >>> client.config.headers = {"cdf-version": "alpha"}
+                >>> test_run = SimulationRun(simulator_name="my_simulator", model_name="my_model", routine_name="my_routine")
+                >>> c.simulators.run(items=test_run)
+        """
+        return self.simulations.run(items)
+
+    def list_runs(
+        self,
+        simulator_name: str | None = None,
+        model_name: str | None = None,
+        routine_name: str | None = None,
+        status: str | None = None,
+    ) -> SimulationRunList:
+        """List simulation runs
+
+        Args:
+            simulator_name: name of the simulator
+            model_name: name of the model
+            routine_name: name of the routine
+            status: status of the simulation run. One of ("ready", "running", "success", "failure")
+
+        Returns:
+            SimulationRunList: list of simulation runs
+
+        Examples:
+
+            List simulation runs::
+
+                >>> from cognite.experimental import CogniteClient
+                >>> c = CogniteClient()
+                >>> client.config.headers = {"cdf-version": "alpha"}
+                >>> c.simulators.list_runs(
+                >>>     simulator_name="my_simulator", model_name="my_model", routine_name="my_routine", status="success"
+                >>> )
+        """
+        return self.simulations.list_runs(
+            simulator_name=simulator_name, model_name=model_name, routine_name=routine_name, status=status
+        )
---
+++ packages/cognite-sdk-experimental/cognite_sdk_experimental-1.0.2-py3-none-any.whl/cognite/experimental/_client.py
@@ -0,0 +1,62 @@
+import os
+from typing import Optional
+
+from cognite.client import ClientConfig, global_config
+from cognite.client._api_client import APIClient
+from cognite.client.beta import CogniteClient as Client
+from cognite.client.credentials import OAuthClientCredentials
+from cognite.experimental._api.alerts import AlertsAPI
+from cognite.experimental._api.extractionpipelines import ExperimentalExtractionPipelinesAPI
+from cognite.experimental._api.geospatial import ExperimentalGeospatialAPI
+from cognite.experimental._api.hosted_extractors import HostedExtractorsAPI
+from cognite.experimental._api.simulators import SimulatorsAPI
+
+try:
+    APIClient._RETRYABLE_POST_ENDPOINT_REGEX_PATTERNS |= {
+        "^" + path + r"(\?.*)?$"
+        for path in (
+            "/(types|labels|templates)/(list|byids|search)",
+            "/alerts/deduplicate",
+        )
+    }
+except TypeError:
+    # We want to move the python-level-loop down into the compiled land of regex:
+    (pattern,) = APIClient._RETRYABLE_POST_ENDPOINT_REGEX_PATTERNS
+    additional = {
+        "^" + path + r"(\?.*)?$" for path in ("/(types|labels|templates)/(list|byids|search)", "/alerts/deduplicate")
+    }
+    APIClient._RETRYABLE_POST_ENDPOINT_REGEX_PATTERNS = "|".join((pattern, *additional))
+    del pattern, additional
+
+
+class CogniteClient(Client):
+    """Initializes cognite client, with experimental extensions.
+
+    Args:
+        config (ClientConfig): The configuration for this client.
+    """
+
+    def __init__(self, config: Optional[ClientConfig] = None):
+        if (client_config := config or global_config.default_client_config) is None:
+            credentials = OAuthClientCredentials(
+                token_url=os.environ["COGNITE_TOKEN_URL"],
+                client_id=os.environ["COGNITE_CLIENT_ID"],
+                client_secret=os.environ["COGNITE_CLIENT_SECRET"],
+                scopes=os.environ["COGNITE_TOKEN_SCOPES"].split(","),
+            )
+            client_config = ClientConfig(
+                os.environ["COGNITE_CLIENT_NAME"],
+                os.environ["COGNITE_PROJECT"],
+                credentials,
+                base_url=os.environ["COGNITE_BASE_URL"],
+            )
+        self._config = client_config
+        super().__init__(self._config)
+        self.geospatial = ExperimentalGeospatialAPI(self._config, api_version="v1", cognite_client=self)
+        self.alerts = AlertsAPI(self._config, api_version="v1", cognite_client=self)
+        self.simulators = SimulatorsAPI(self._config, api_version="v1", cognite_client=self)
+
+        self.extraction_pipelines = ExperimentalExtractionPipelinesAPI(
+            self._config, api_version="playground", cognite_client=self
+        )
+        self.hosted_extractors = HostedExtractorsAPI(self._config, api_version=self._API_VERSION, cognite_client=self)
