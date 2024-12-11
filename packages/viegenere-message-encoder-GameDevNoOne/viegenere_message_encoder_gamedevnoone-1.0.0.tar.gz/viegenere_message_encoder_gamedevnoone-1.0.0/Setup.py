8f80f605e7ba4ec840c842c5811cb9cf73f16b9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 05:07:05 +0000

    Add package viegenere-message-encoder-GameDevNoOne/1.0.0/viegenere_message_encoder_gamedevnoone-1.0.0.tar.gz

---
+++ packages/viegenere-message-encoder-GameDevNoOne/viegenere_message_encoder_gamedevnoone-1.0.0.tar.gz/viegenere_message_encoder_gamedevnoone-1.0.0/Setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name= "Viegenere_Coder_and_Encoder",
+    version= "1.0",
+    description= "Codes and decodes messages",
+    author= "No One",
+    author_email= "noonegamedev1@gmail.com",
+    packages= find_packages(),
+    install_requires= ["alive_progress"],
+)
