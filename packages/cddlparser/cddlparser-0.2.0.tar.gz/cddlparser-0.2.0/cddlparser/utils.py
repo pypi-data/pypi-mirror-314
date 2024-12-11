e8cc01b872690ea87bbea86e37d79557681ae9fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 14:04:53 +0000

    Add package cddlparser/0.2.0/cddlparser-0.2.0.tar.gz

---
+++ packages/cddlparser/cddlparser-0.2.0.tar.gz/cddlparser-0.2.0/cddlparser/utils.py
@@ -0,0 +1,14 @@
+import re
+from .tokens import Tokens
+
+
+def isAlpha(ch: str) -> bool:
+    return ("a" <= ch <= "z") or ("A" <= ch <= "Z")
+
+
+def isExtendedAlpha(ch: str) -> bool:
+    return isAlpha(ch) or ch in {Tokens.ATSIGN, Tokens.UNDERSCORE, Tokens.DOLLAR}
+
+
+def isUint(literal: str) -> bool:
+    return re.match(r"^[1-9]\d*$", literal) is not None
