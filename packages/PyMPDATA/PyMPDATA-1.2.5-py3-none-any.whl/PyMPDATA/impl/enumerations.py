513f4999130f1dc183a1fb4d1e97775f79a1e2cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 16:05:57 +0000

    Add package PyMPDATA/1.2.5/PyMPDATA-1.2.5-py3-none-any.whl

---
+++ packages/PyMPDATA/PyMPDATA-1.2.5-py3-none-any.whl/PyMPDATA/impl/enumerations.py
@@ -0,0 +1,30 @@
+""" common constants (mostly integer indices used for indexing tuples) """
+
+import numpy as np
+
+ARG_FOCUS, ARG_DATA, ARG_DATA_OUTER, ARG_DATA_MID3D, ARG_DATA_INNER = 0, 1, 1, 2, 3
+
+MAX_DIM_NUM = 3
+
+OUTER, MID3D, INNER = 0, 1, -1
+
+IMPL_META_AND_DATA = 0
+IMPL_BC = 1
+
+META_AND_DATA_META = 0
+META_AND_DATA_DATA = 1
+
+SIGN_LEFT, SIGN_RIGHT = +1, -1
+
+RNG_START, RNG_STOP, RNG_STEP = 0, 1, 2
+
+INVALID_INDEX = -44
+
+INVALID_INIT_VALUE = np.nan
+BUFFER_DEFAULT_VALUE = np.nan
+
+INVALID_HALO_VALUE = 666
+
+INVALID_NULL_VALUE = 0.0
+
+ONE_FOR_STAGGERED_GRID = 1
