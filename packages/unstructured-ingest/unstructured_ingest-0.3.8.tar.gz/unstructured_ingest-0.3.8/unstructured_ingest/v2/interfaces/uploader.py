0376c5d2f9123024ab4a291f07a4edf544ed27fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 14:05:11 +0000

    Add package unstructured-ingest/0.3.8/unstructured_ingest-0.3.8.tar.gz

---
+++ packages/unstructured-ingest/unstructured_ingest-0.3.8.tar.gz/unstructured_ingest-0.3.8/unstructured_ingest/v2/interfaces/uploader.py
@@ -0,0 +1,44 @@
+from abc import ABC
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Any, TypeVar
+
+from pydantic import BaseModel
+
+from unstructured_ingest.v2.interfaces.connector import BaseConnector
+from unstructured_ingest.v2.interfaces.file_data import FileData
+from unstructured_ingest.v2.interfaces.process import BaseProcess
+
+
+class UploaderConfig(BaseModel):
+    pass
+
+
+UploaderConfigT = TypeVar("UploaderConfigT", bound=UploaderConfig)
+
+
+@dataclass
+class UploadContent:
+    path: Path
+    file_data: FileData
+
+
+@dataclass
+class Uploader(BaseProcess, BaseConnector, ABC):
+    upload_config: UploaderConfigT
+    connector_type: str
+
+    def is_async(self) -> bool:
+        return False
+
+    def is_batch(self) -> bool:
+        return False
+
+    def run_batch(self, contents: list[UploadContent], **kwargs: Any) -> None:
+        raise NotImplementedError()
+
+    def run(self, path: Path, file_data: FileData, **kwargs: Any) -> None:
+        raise NotImplementedError()
+
+    async def run_async(self, path: Path, file_data: FileData, **kwargs: Any) -> None:
+        return self.run(contents=[UploadContent(path=path, file_data=file_data)], **kwargs)
