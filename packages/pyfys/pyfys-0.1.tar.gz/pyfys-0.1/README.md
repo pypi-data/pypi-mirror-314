31f02c14e8f02b5c09ecb276a7a01bdfbc177bff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 07:04:55 +0000

    Add package pyfys/0.1/pyfys-0.1.tar.gz

---
+++ packages/pyfys/pyfys-0.1.tar.gz/pyfys-0.1/README.md
@@ -0,0 +1,29 @@
+
+# PyFys: Python library to speed up physics work
+
+1. Quickly graph equations by removeing customizability and focusing on simplicity.
+2. Store all useful physics constants and equations.
+3. Convert units easily.
+
+This projects focus is on simplicity and speed.
+
+## Documentation
+Visist the [documentation](https://pyfys.readthedocs.io/en/latest/) TODO:
+
+## Installation
+
+```sh
+pip install pyfys
+```
+
+## Basic Usage
+
+## Dependencies
+TODO:
+
+## TODO
+- [ ] Add more equations
+- [ ] Add more constants
+- [ ] Add more unit conversions
+- [ ] Create documentation
+- [ ] Update functions to work with both lists and single values
---
+++ packages/pyfys/pyfys-0.1.tar.gz/pyfys-0.1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+DESCRIPTION = "A library that was made to quickly make physics projects."
+LONG_DESCRIPTION = """This library was made to: 
+1. Quickly graph equations by removeing customizability and focusing on simplicity.
+2. Store all useful physics constants and equations.
+3. Convert units easily.
+
+This projects focus is heavily on simplicity and speed.
+"""
+
+# Setting up
+setup(
+    name="PyFys",
+    version=0.1,
+    author="Nikolai G. Borbe",
+    url="https://github.com/nikolaiborbe",
+    author_email="nikolaiborbe@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[], #TODO:
+    keywords=['python', 'physics', 'graphing', 'constants'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
