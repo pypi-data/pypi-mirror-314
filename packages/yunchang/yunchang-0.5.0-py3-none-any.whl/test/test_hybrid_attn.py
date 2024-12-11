14b547a9917858a1313cd54e5e4411acbadc8c4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 16:05:19 +0000

    Add package yunchang/0.5.0/yunchang-0.5.0-py3-none-any.whl

---
+++ packages/yunchang/yunchang-0.5.0-py3-none-any.whl/test/test_hybrid_attn.py
@@ -0,0 +1,232 @@
+from yunchang import (
+    AsyncLongContextAttention,
+    LongContextAttention,
+    set_seq_parallel_pg,
+    EXTRACT_FUNC_DICT
+)
+import torch
+import torch.distributed as dist
+from flash_attn import flash_attn_func
+from yunchang.kernels import FlashAttentionImpl
+from test_utils import attention_ref
+
+def log(msg, a, rank0_only=False):
+    world_size = dist.get_world_size()
+    rank = dist.get_rank()
+    if rank0_only:
+        if rank == 0:
+            print(
+                f"[Rank#0] {msg}: "
+                f"max {a.abs().max().item()}, "
+                f"mean {a.abs().mean().item()}",
+                flush=True,
+            )
+        return
+
+    for i in range(world_size):
+        if i == rank:
+            if rank == 0:
+                print(f"{msg}:")
+            print(
+                f"[Rank#{rank}] "
+                f"max {a.abs().max().item()}, "
+                f"mean {a.abs().mean().item()}",
+                flush=True,
+            )
+        dist.barrier()
+
+# test it with:
+# torchrun --nproc_per_node=4  test/test_hybrid_attn.py
+if __name__ == "__main__":
+
+    torch.random.manual_seed(0)
+    use_bwd = True
+    dist.init_process_group("nccl")
+
+    rank = dist.get_rank()
+    world_size = dist.get_world_size()
+
+    assert world_size == 4, f"torchrun --nproc_per_node=4  test/test_hybrid_attn.py"
+    # Inference mainly uses fp16; ROCM flash attention with bf16 precision is slightly larger, will be fixed soon 
+    dtype = torch.bfloat16
+    device = torch.device(f"cuda:{rank}")
+
+    batch_size = 2
+    seqlen = 1024
+    nheads = 4
+    d = 128
+    dropout_p = 0
+    causal = True
+    deterministic = False
+
+    assert seqlen % world_size == 0
+    assert d % 8 == 0
+
+    ring_impl_type = "basic"  # You can change this to "basic" or "zigzag" if needed
+
+    # Prepare inputs
+    q = torch.randn(
+        batch_size, seqlen, nheads, d, device=device, dtype=dtype, requires_grad=True
+    )
+    k = torch.randn(
+        batch_size, seqlen, nheads, d, device=device, dtype=dtype, requires_grad=True
+    )
+    v = torch.randn(
+        batch_size, seqlen, nheads, d, device=device, dtype=dtype, requires_grad=True
+    )
+    dout = torch.randn(batch_size, seqlen, nheads, d, device=device, dtype=dtype)
+
+    dist.broadcast(q, src=0)
+    dist.broadcast(k, src=0)
+    dist.broadcast(v, src=0)
+    dist.broadcast(dout, src=0)
+
+    # prepare process group for hybrid sequence parallelism
+    use_ring_low_dim = True
+
+    sp_ulysses_degree = 2
+    sp_ring_degree = world_size // sp_ulysses_degree
+
+    print(
+        f"rank {rank}, sp_ulysses_degree: {sp_ulysses_degree}, sp_ring_degree: {sp_ring_degree}"
+    )
+
+    set_seq_parallel_pg(sp_ulysses_degree, sp_ring_degree, rank, world_size)
+
+    # Use EXTRACT_FUNC_DICT to shard the tensors
+    local_q = EXTRACT_FUNC_DICT[ring_impl_type](
+        q, rank, world_size=world_size, rd=sp_ring_degree, ud=sp_ulysses_degree
+    ).detach().clone()
+    local_q.requires_grad = True
+
+    local_k = EXTRACT_FUNC_DICT[ring_impl_type](
+        k, rank, world_size=world_size, rd=sp_ring_degree, ud=sp_ulysses_degree
+    ).detach().clone()
+    local_k.requires_grad = True
+
+    local_v = EXTRACT_FUNC_DICT[ring_impl_type](
+        v, rank, world_size=world_size, rd=sp_ring_degree, ud=sp_ulysses_degree
+    ).detach().clone()
+    local_v.requires_grad = True
+    usp_attn = LongContextAttention(ring_impl_type=ring_impl_type, attn_type=FlashAttentionImpl.FA)
+
+    if rank == 0:
+        print("#" * 30)
+        print("# ds-ulysses forward:")
+        print("#" * 30)
+
+    # common test parameters
+    window_size=(-1, -1)
+    alibi_slopes, attn_bias = None, None
+    dropout_mask = None
+
+    print(f"before usp attn forward: {local_q.shape} {local_k.shape} {local_v.shape}")
+    # usp attn forward
+    local_out = usp_attn(
+        local_q,
+        local_k,
+        local_v,
+        dropout_p=dropout_p,
+        causal=causal,
+        window_size=window_size,
+        softcap=0.0,
+        alibi_slopes=alibi_slopes,
+        deterministic=deterministic,
+        return_attn_probs=True,
+    )
+
+    # extract local dout
+    local_dout = EXTRACT_FUNC_DICT[ring_impl_type](
+        dout, rank, world_size=world_size, rd=sp_ring_degree, ud=sp_ulysses_degree
+    ).detach().clone()
+
+    if rank == 0:
+        print("#" * 30)
+        print("# ds-ulysses backward:")
+        print("#" * 30)
+
+    # usp attn backward
+    if use_bwd:
+        local_out.backward(local_dout)
+
+    dist.barrier()
+
+    if rank == 0:
+        print("#" * 30)
+        print("# local forward:")
+        print("#" * 30)
+    # reference, a local flash attn
+    out_ref, _, _ = flash_attn_func(
+        q,
+        k,
+        v,
+        dropout_p=dropout_p,
+        causal=causal,
+        window_size=window_size,
+        softcap=0.0,
+        alibi_slopes=alibi_slopes,
+        deterministic=deterministic,
+        return_attn_probs=True,
+    )
+
+    out_pt_ref, attn_pt_ref = attention_ref(
+        q,
+        k,
+        v,
+        None,
+        None,
+        attn_bias,
+        dropout_p,
+        dropout_mask,
+        causal=causal,
+        window_size=window_size,
+    )
+
+    if rank == 0:
+        print("#" * 30)
+        print("# local forward:")
+        print("#" * 30)
+
+    if use_bwd:
+        out_ref.backward(dout)
+
+    dist.barrier()
+
+    # check correctness
+    # When checking correctness, use EXTRACT_FUNC_DICT for reference outputs
+    local_out_ref = EXTRACT_FUNC_DICT[ring_impl_type](
+        out_ref, rank, world_size=world_size, rd=sp_ring_degree, ud=sp_ulysses_degree
+    )
+    local_out_pt_ref = EXTRACT_FUNC_DICT[ring_impl_type](
+        out_pt_ref, rank, world_size=world_size, rd=sp_ring_degree, ud=sp_ulysses_degree
+    )
+
+    log("local (rank) out", local_out, rank0_only=True)
+    log("out (distributed) - out_ref (non-distributed) diff", local_out_ref - local_out)
+
+    # log("out_ref (non-distributed) - out_pt_ref (gpu) diff", local_out_ref - local_out_pt_ref)
+
+    torch.testing.assert_close(local_out, local_out_ref, atol=1e-2, rtol=0)
+    # torch.testing.assert_close(out_ref, out_pt_ref, atol=1e-2, rtol=0)
+
+    if use_bwd:
+        local_dq_ref = EXTRACT_FUNC_DICT[ring_impl_type](
+            q.grad, rank, world_size=world_size, rd=sp_ring_degree, ud=sp_ulysses_degree
+        )
+        log("load_dq", local_q.grad)
+        log("dq diff", local_dq_ref - local_q.grad)
+
+        local_dk_ref = EXTRACT_FUNC_DICT[ring_impl_type](
+            k.grad, rank, world_size=world_size, rd=sp_ring_degree, ud=sp_ulysses_degree
+        )
+        log("load_dk", local_k.grad)
+        log("dk diff", local_dk_ref - local_k.grad)
+
+        local_dv_ref = EXTRACT_FUNC_DICT[ring_impl_type](
+            v.grad, rank, world_size=world_size, rd=sp_ring_degree, ud=sp_ulysses_degree
+        )
+        log("load_dv", local_v.grad)
+        log("dv diff", local_dv_ref - local_v.grad)
+
+    if dist.is_initialized():
+        dist.destroy_process_group()
---
+++ packages/yunchang/yunchang-0.5.0-py3-none-any.whl/yunchang/kernels/attention.py
@@ -0,0 +1,158 @@
+from yunchang.globals import HAS_FLASH_ATTN, HAS_FLASH_ATTN_HOPPER
+
+if HAS_FLASH_ATTN:
+    import flash_attn
+    from flash_attn.flash_attn_interface import _flash_attn_forward, _flash_attn_backward
+
+
+if HAS_FLASH_ATTN_HOPPER:
+    from flash_attn_interface import _flash_attn_forward as flash_attn_forward_hopper
+    from flash_attn_interface import _flash_attn_backward as flash_attn_func_hopper_backward
+    from flash_attn_interface import flash_attn_func as flash3_attn_func
+else:
+    flash_attn_forward_hopper = None
+    flash_attn_func_hopper_backward = None
+    flash3_attn_func = None
+
+import torch.nn.functional as F
+
+def torch_attn(q, k, v, dropout_p = 0.0, 
+            softmax_scale = None, 
+            causal=False, 
+            *args, **kwargs):
+    batch_size, seq_len, hs, hd = q.size()
+    query = q.view(batch_size, -1, hs, hd).transpose(1, 2)
+    key = k.view(batch_size, -1, hs, hd).transpose(1, 2)
+    value = v.view(batch_size, -1, hs, hd).transpose(1, 2)
+
+    # the output of sdp = (batch, num_heads, seq_len, head_dim)
+    # TODO: add support for attn.scale when we move to Torch 2.1
+    hidden_states = F.scaled_dot_product_attention(
+        query, key, value, dropout_p=dropout_p, is_causal=causal
+    )
+
+    hidden_states = hidden_states.transpose(1, 2).reshape(
+        batch_size, -1, hs, hd
+    )
+    hidden_states = hidden_states.to(query.dtype)
+    return hidden_states,
+
+def flash_attn_forward(q, k, v, 
+        dropout_p = 0.0, 
+        softmax_scale = None, 
+        causal=False, 
+        window_size=(-1, -1), 
+        softcap=None, 
+        alibi_slopes=None, 
+        return_softmax=False):
+    assert HAS_FLASH_ATTN, "FlashAttention is not available"
+    if softmax_scale is None:
+        softmax_scale = q.shape[-1] ** (-0.5)
+    if flash_attn.__version__ <= '2.6.3':
+        block_out, _, _, _, _, block_lse, _, _ = _flash_attn_forward(
+            q,
+            k,
+            v,
+            dropout_p = dropout_p,
+            softmax_scale = softmax_scale,
+            causal=causal,
+            window_size=window_size,
+            softcap=softcap,
+            alibi_slopes=alibi_slopes,
+            return_softmax=return_softmax,
+        )
+    else:
+        block_out, block_lse, _, _ = _flash_attn_forward(
+            q,
+            k,
+            v,
+            dropout_p = dropout_p,
+            softmax_scale = softmax_scale,
+            causal=causal,
+            window_size_left=window_size[0],
+            window_size_right=window_size[1],
+            softcap=softcap,
+            alibi_slopes=alibi_slopes,
+            return_softmax=return_softmax,
+        )
+    return block_out, block_lse
+
+def flash_attn_backward(dout, q, k, v, out, softmax_lse, block_dq_buffer, block_dk_buffer, block_dv_buffer, dropout_p, softmax_scale, 
+    bwd_causal, window_size, softcap, alibi_slopes, deterministic, rng_state):
+    if softmax_scale is None:
+        softmax_scale = q.shape[-1] ** (-0.5)
+    assert HAS_FLASH_ATTN
+    if flash_attn.__version__ <= '2.6.3':
+        _flash_attn_backward(
+            dout,
+            q,
+            k,
+            v,
+            out,
+            softmax_lse,
+            block_dq_buffer,
+            block_dk_buffer,
+            block_dv_buffer,
+            dropout_p,
+            softmax_scale,
+            bwd_causal,
+            window_size,
+            softcap,
+            alibi_slopes,
+            deterministic,
+            rng_state,
+        )
+    else:
+        _flash_attn_backward(
+            dout,
+            q,
+            k,
+            v,
+            out,
+            softmax_lse,
+            block_dq_buffer,
+            block_dk_buffer,
+            block_dv_buffer,
+            dropout_p,
+            softmax_scale,
+            bwd_causal,
+            window_size[0],  # Pass window_size_left
+            window_size[1],  # Pass window_size_right
+            softcap,
+            alibi_slopes,
+            deterministic,
+            rng_state,
+        )
+    
+
+def flash_attn3_func_forward(q, k, v, dropout_p, softmax_scale, causal, window_size, softcap, alibi_slopes, return_softmax):
+    assert HAS_FLASH_ATTN_HOPPER
+    # current signature of flash_attn_forward_hopper:
+    # (q, k, v, softmax_scale, causal, window_size, descale_q=None, descale_k=None, descale_v=None, gqa_parallel=False)
+    out, q, k, v, out_padded, softmax_lse, S_dmask = flash_attn_forward_hopper(
+        q, k, v, softmax_scale, causal, window_size
+    )
+    return out, softmax_lse
+
+def flash_attn3_func_backward(dout, q, k, v, out, softmax_lse, 
+                                    block_dq_buffer, block_dk_buffer, block_dv_buffer, 
+                                    dropout_p, softmax_scale, 
+                                    bwd_causal, window_size, softcap, alibi_slopes, deterministic, rng_state):
+    # (dout, q, k, v, out, softmax_lse, dq, dk, dv, softmax_scale, causal):
+    assert HAS_FLASH_ATTN_HOPPER, f"FlashAttention Hopper is not available"
+
+    flash_attn_func_hopper_backward(
+        dout,
+        q,
+        k,
+        v,
+        out,
+        softmax_lse,
+        block_dq_buffer,
+        block_dk_buffer,
+        block_dv_buffer,
+        softmax_scale,
+        bwd_causal,
+        window_size,
+        deterministic,
+    )
