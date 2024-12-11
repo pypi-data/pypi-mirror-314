749d2065c860256784b50110990c6e2f7b04e672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 14:05:14 +0000

    Add package meltanolabs-target-postgres/0.2.0/meltanolabs_target_postgres-0.2.0.tar.gz

---
+++ packages/meltanolabs-target-postgres/meltanolabs_target_postgres-0.2.0.tar.gz/meltanolabs_target_postgres-0.2.0/target_postgres/tests/test_types.py
@@ -0,0 +1,38 @@
+"""Test custom types and the type hierarchy."""
+
+import pytest
+import sqlalchemy as sa
+
+from target_postgres.connector import NOTYPE, PostgresConnector
+
+
+@pytest.fixture
+def connector():
+    """Create a PostgresConnector instance."""
+    return PostgresConnector(
+        config={
+            "dialect+driver": "postgresql+psycopg",
+            "host": "localhost",
+            "port": "5432",
+            "user": "postgres",
+            "password": "postgres",
+            "database": "postgres",
+            "ssl_enable": False,
+            "ssl_client_certificate_enable": False,
+        },
+    )
+
+
+@pytest.mark.parametrize(
+    ("types", "expected"),
+    [
+        pytest.param([sa.Integer(), sa.String()], sa.String, id="int+str=str"),
+        pytest.param([sa.Boolean(), sa.String()], sa.String, id="bool+str=str"),
+        pytest.param([sa.Integer(), sa.DateTime()], sa.Integer, id="int+datetime=int"),
+        pytest.param([NOTYPE(), sa.String()], sa.String, id="none+str=str"),
+        pytest.param([NOTYPE(), sa.Integer()], NOTYPE, id="none+int=none"),
+    ],
+)
+def test_type_hierarchy(connector, types, expected):
+    """Test that types are merged correctly."""
+    assert type(connector.merge_sql_types(types)) is expected
