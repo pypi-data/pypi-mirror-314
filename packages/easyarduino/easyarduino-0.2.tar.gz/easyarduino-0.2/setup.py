7f44e4bda3a91cbc0a5057a87ccb6817dcef9ad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 05:07:05 +0000

    Add package easyarduino/0.2/easyarduino-0.2.tar.gz

---
+++ packages/easyarduino/easyarduino-0.2.tar.gz/easyarduino-0.2/setup.py
@@ -0,0 +1,24 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="easyarduino",
+    version="0.2",
+    description="A simple and easy-to-use Arduino control library using PyFirmata",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="AKM Korishee Apurbo",
+    author_email="bandinvisible8@gmail.com",
+    url="https://github.com/IMApurbo/easyarduino",
+    packages=find_packages(),
+    install_requires=[
+        "pyfirmata",
+        "pyserial",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
