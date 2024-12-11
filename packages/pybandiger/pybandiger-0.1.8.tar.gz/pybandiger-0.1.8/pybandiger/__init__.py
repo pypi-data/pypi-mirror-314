1b64f5f39e561eca5955b46f42bada8dbef6186e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 14:05:01 +0000

    Add package pybandiger/0.1.8/pybandiger-0.1.8.tar.gz

---
+++ packages/pybandiger/pybandiger-0.1.8.tar.gz/pybandiger-0.1.8/pybandiger/__init__.py
@@ -0,0 +1,7 @@
+from .PyBandiger import PyBandiger
+
+__version__ = '0.1.8'
+
+__all__ = [
+    'PyBandiger',
+]
---
+++ packages/pybandiger/pybandiger-0.1.8.tar.gz/pybandiger-0.1.8/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pybandiger',
+    version='0.1.8',
+    packages=find_packages(),
+    install_requires=[
+        'scikit-learn',
+        'pandas',
+    ],
+    author='Lansari Fedi',
+    author_email='lansarifedi7@gmail.com',
+    description='A data wrangling library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/LansariFedi/PyBandiger',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
