14515ea37d5380dfe103c0a9029e2ff67f84ff26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 15:05:11 +0000

    Add package tc-neo4j-lib/2.0.4/tc_neo4j_lib-2.0.4.tar.gz

---
+++ packages/tc-neo4j-lib/tc_neo4j_lib-2.0.4.tar.gz/tc_neo4j_lib-2.0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+
+setup(
+    name="tc-neo4j-lib",
+    version="2.0.4",
+    author="Mohammad Amin Dadgar, TogetherCrew",
+    maintainer="Mohammad Amin Dadgar",
+    maintainer_email="dadgaramin96@gmail.com",
+    packages=find_packages(),
+    description="A neo4j shared library to use across togethercrew python projects.",
+    long_description=open("README.md").read(),
+    install_requires=requirements,
+)
