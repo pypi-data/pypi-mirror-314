559457077edd37220d8abea5cb6415ddc2a6a85a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 04:06:18 +0000

    Add package jupyter-collaboration-ui/1.1.0/jupyter_collaboration_ui-1.1.0.tar.gz

---
+++ packages/jupyter-collaboration-ui/jupyter_collaboration_ui-1.1.0.tar.gz/jupyter_collaboration_ui-1.1.0/packages/collaboration/src/index.ts
@@ -0,0 +1,13 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+/**
+ * @packageDocumentation
+ * @module collaboration
+ */
+
+export * from './tokens';
+export * from './collaboratorspanel';
+export * from './cursors';
+export * from './menu';
+export * from './sharedlink';
+export * from './userinfopanel';
