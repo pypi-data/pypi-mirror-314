840e3113db2110446d09eaa2c35d2706567b083e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 04:07:10 +0000

    Add package register-value/0.0.1/register_value-0.0.1.tar.gz

---
+++ packages/register-value/register_value-0.0.1.tar.gz/register_value-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='register_value',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires = [],
+    author='Kelvin',
+    author_email='yan18771989297@gmail.com',
+    description='A simple global value manager',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
