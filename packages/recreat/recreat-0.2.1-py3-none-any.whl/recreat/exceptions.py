f40cd8c95c138d7563ee6e2604c7be2a5c988c20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 04:06:42 +0000

    Add package recreat/0.2.1/recreat-0.2.1-py3-none-any.whl

---
+++ packages/recreat/recreat-0.2.1-py3-none-any.whl/recreat/exceptions.py
@@ -0,0 +1,14 @@
+class ModelValidationError(Exception):
+    def __init__(self, message):            
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
+
+class MethodNotImplemented(Exception):
+    def __init__(self, message):            
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
+
+class DisaggregationError(Exception):
+    def __init__(self, message):            
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
