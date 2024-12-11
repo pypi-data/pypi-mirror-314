1dd4bab0f8710f30b6bb6377352a0615af1bd8b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 14:05:05 +0000

    Add package ebtorch/0.28.3/ebtorch-0.28.3.tar.gz

---
+++ packages/ebtorch/ebtorch-0.28.3.tar.gz/ebtorch-0.28.3/ebtorch/typing/customtypes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ──────────────────────────────────────────────────────────────────────────────
+# Imports
+from typing import List
+from typing import Union
+
+import numpy as np
+import torch
+from torch import Tensor
+
+# ──────────────────────────────────────────────────────────────────────────────
+__all__: List[str] = ["realnum", "strdev", "numlike", "tensorlike"]
+# ──────────────────────────────────────────────────────────────────────────────
+realnum = Union[int, float]
+strdev = Union[str, torch.device]
+tensorlike = Union[Tensor, np.ndarray]
+numlike = Union[realnum, tensorlike]
+# ──────────────────────────────────────────────────────────────────────────────
