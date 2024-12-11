cf50231c661b48976d53cff5f142151d5bb9b8d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 15:04:55 +0000

    Add package conda-build-test/0.4.2/conda_build_test-0.4.2.tar.gz

---
+++ packages/conda-build-test/conda_build_test-0.4.2.tar.gz/conda_build_test-0.4.2/setup.py
@@ -0,0 +1,11 @@
+import os
+from setuptools import setup, Extension
+
+EXT_MODULES = [
+    Extension(
+        'hello.hello',
+        sources=[os.path.join('src', 'hello.c')],
+    )
+]
+
+setup(ext_modules=EXT_MODULES)
