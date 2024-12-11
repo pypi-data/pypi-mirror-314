1cea093df59c4cd9f2ca5d957127c1057725f0a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 05:07:06 +0000

    Add package TexTOM/0.1/TexTOM-0.1-py3-none-any.whl

---
+++ packages/TexTOM/TexTOM-0.1-py3-none-any.whl/textom/config.py
@@ -0,0 +1,9 @@
+# ### MacBook ### 
+# # Define how many cores you want to use 
+n_threads = 8 
+### Greenland ### 
+# Define how many CPU cores you want to use 
+# n_threads = 128 
+
+# Choose if you want to use a GPU for integration and alignment
+use_gpu = True
