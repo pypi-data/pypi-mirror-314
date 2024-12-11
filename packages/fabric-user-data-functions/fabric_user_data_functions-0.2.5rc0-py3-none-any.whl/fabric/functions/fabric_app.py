14a76232daedf587911e20ed428faca4e387b5f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 06:05:31 +0000

    Add package fabric-user-data-functions/0.2.5rc0/fabric_user_data_functions-0.2.5rc0-py3-none-any.whl

---
+++ packages/fabric-user-data-functions/fabric_user_data_functions-0.2.5rc0-py3-none-any.whl/fabric/functions/fabric_app.py
@@ -0,0 +1,10 @@
+# flake8: noqa: I005
+from typing_extensions import deprecated
+from fabric.functions.user_data_functions import UserDataFunctions
+
+
+@deprecated("This class is deprecated. Please use 'UserDataFunctions' instead.")
+class FabricApp(UserDataFunctions):
+
+    def __init__(self):
+        super().__init__()
---
+++ packages/fabric-user-data-functions/fabric_user_data_functions-0.2.5rc0-py3-none-any.whl/fabric/functions/middleware/__init__.py
@@ -0,0 +1,3 @@
+# flake8: noqa: F401
+from .invocationIdMiddleware import *
+from .removeReqMiddleware import *
