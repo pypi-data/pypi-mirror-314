ccb42452afab2783267e238bd4d185ab96412fd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 06:05:02 +0000

    Add package pyzerox-impacte/0.0.8/pyzerox_impacte-0.0.8-py3-none-any.whl

---
+++ packages/pyzerox-impacte/pyzerox_impacte-0.0.8-py3-none-any.whl/pyzerox/constants/prompts.py
@@ -0,0 +1,11 @@
+class Prompts:
+    """Class for storing prompts for the Zerox system."""
+
+    DEFAULT_SYSTEM_PROMPT = """
+    Convert the following PDF page to markdown.
+    Return only the markdown with no explanation text.
+    Do not exclude any content from the page.
+    If there is more than one document in the same PDF
+    or image, separate the Markdown for each document as
+    separate sections.
+    """
