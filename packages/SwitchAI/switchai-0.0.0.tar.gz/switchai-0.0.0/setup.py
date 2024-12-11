7f01705cd3bd8555ba8e7c5c8004230d2307c8ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 06:05:39 +0000

    Add package SwitchAI/0.0.0/switchai-0.0.0.tar.gz

---
+++ packages/SwitchAI/switchai-0.0.0.tar.gz/switchai-0.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SwitchAI',
+    version='0.0.0',
+    description='',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Yassine El Boudouri',
+    author_email='boudouriyassine@gmail.com',
+    url='https://github.com/yelboudouri/SwitchAI',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.6',
+)
