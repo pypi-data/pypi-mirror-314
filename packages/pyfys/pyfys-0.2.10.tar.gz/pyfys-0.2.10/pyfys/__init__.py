540a2bb27026b11225f0892c54d4ae473e8c8ce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 14:04:56 +0000

    Add package pyfys/0.2.10/pyfys-0.2.10.tar.gz

---
+++ packages/pyfys/pyfys-0.2.10.tar.gz/pyfys-0.2.10/pyfys/__init__.py
@@ -0,0 +1,5 @@
+from .constants import Constants
+from .masses import Masses
+from .main import fys
+
+__all__ = ['Constants', 'Masses', 'fys']
---
+++ packages/pyfys/pyfys-0.2.10.tar.gz/pyfys-0.2.10/pyfys/test.py
@@ -0,0 +1,5 @@
+from main import fys
+import math
+
+f = fys()
+f.plot((math.sin, math.cos), 0, math.pi*2)
