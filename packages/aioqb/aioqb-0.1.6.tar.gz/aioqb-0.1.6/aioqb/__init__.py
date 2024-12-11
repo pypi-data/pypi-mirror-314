697c39e02e371931fdcb482bb08bb5e4298a6040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 03:18:50 +0000

    Add package aioqb/0.1.6/aioqb-0.1.6.tar.gz

---
+++ packages/aioqb/aioqb-0.1.6.tar.gz/aioqb-0.1.6/aioqb/__init__.py
@@ -0,0 +1,21 @@
+"""
+Copyright (c) 2008-2021 synodriver <synodriver@gmail.com>
+"""
+# https://github.com/qbittorrent/qBittorrent/wiki/WebUI-API-(qBittorrent-4.1)#general-information
+
+from aioqb.client import QbittorrentClient as Client
+from aioqb.exceptions import (
+    ApiFailedException,
+    BaseQbittorrentException,
+    HashNotFoundException,
+    IPBanedException,
+)
+
+__version__ = "0.1.6"
+__all__ = [
+    "Client",
+    "BaseQbittorrentException",
+    "IPBanedException",
+    "HashNotFoundException",
+    "ApiFailedException",
+]
