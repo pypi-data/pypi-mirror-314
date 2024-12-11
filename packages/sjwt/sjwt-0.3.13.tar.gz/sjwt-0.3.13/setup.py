f16562e1b7348ee31f206f40d60377d5e4a91833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 03:18:05 +0000

    Add package sjwt/0.3.13/sjwt-0.3.13.tar.gz

---
+++ packages/sjwt/sjwt-0.3.13.tar.gz/sjwt-0.3.13/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='sjwt',
+      version='0.3.13',
+      description='Generate, check token, and get payload from token',
+      packages=['sjwt'],
+      author_email='sagrityanin4@gmail.com',
+      zip_safe=False)
