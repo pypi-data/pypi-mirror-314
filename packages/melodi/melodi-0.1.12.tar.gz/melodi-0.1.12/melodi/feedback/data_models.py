222609c982de17367e5c3c9cfba3061c42ad3788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 06:04:29 +0000

    Add package melodi/0.1.12/melodi-0.1.12.tar.gz

---
+++ packages/melodi/melodi-0.1.12.tar.gz/melodi-0.1.12/melodi/feedback/data_models.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from typing import List, Literal, Optional
+
+from pydantic import BaseModel
+
+from melodi.users.data_models import User, UserResponse
+
+
+class Feedback(BaseModel):
+    externalThreadId: Optional[str] = None
+    externalMessageId: Optional[str] = None
+
+    feedbackType: Optional[Literal['POSITIVE', 'NEGATIVE']] = None
+    feedbackText: Optional[str] = None
+
+    externalUser: Optional[User] = None
+
+    attributes: Optional[dict[str, str]] = {}
+
+class Attribute(BaseModel):
+    id: int
+    name: str
+
+class AttributeOption(BaseModel):
+    id: int
+    name: str
+    attribute: Attribute
+
+class FeedbackResponse(Feedback):
+    id: int
+
+    externalUserId: Optional[int] = None
+    externalUser: Optional[UserResponse] = None
+
+    attributeOptions: List[AttributeOption] = []
+    createdAt: datetime
+    updatedAt: datetime
