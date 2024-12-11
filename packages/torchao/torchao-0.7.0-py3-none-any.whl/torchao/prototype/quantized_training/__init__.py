7e0c6865fdf78589487ad3ee586ee7de76d482f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 14:04:48 +0000

    Add package torchao/0.7.0/torchao-0.7.0-py3-none-any.whl

---
+++ packages/torchao/torchao-0.7.0-py3-none-any.whl/torchao/prototype/quantized_training/__init__.py
@@ -0,0 +1,12 @@
+from .bitnet import BitNetTrainingLinearWeight, bitnet_training, precompute_bitnet_scale_for_fsdp
+from .int8 import (
+    Int8QuantizedTrainingLinearWeight,
+    int8_weight_only_quantized_training,
+    quantize_int8_rowwise,
+)
+from .int8_mixed_precision import (
+    Int8MixedPrecisionTrainingConfig,
+    Int8MixedPrecisionTrainingLinear,
+    Int8MixedPrecisionTrainingLinearWeight,
+    int8_mixed_precision_training,
+)
---
+++ packages/torchao/torchao-0.7.0-py3-none-any.whl/torchao/sparsity/prototype/superblock/supermask.py
@@ -0,0 +1,11 @@
+from torchao.prototype.sparsity.superblock.supermask import (
+    GetSubnet,
+    SupermaskConv2d,
+    SupermaskLinear,
+)
+
+__all__ = [
+    "GetSubnet",
+    "SupermaskConv2d",
+    "SupermaskLinear",
+]
