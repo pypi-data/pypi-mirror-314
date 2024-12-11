848df759b2cc1cac9160a2bacffc8eebb02703ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 14:04:45 +0000

    Add package wagtail-hallo/0.5.0/wagtail_hallo-0.5.0.tar.gz

---
+++ packages/wagtail-hallo/wagtail_hallo-0.5.0.tar.gz/wagtail_hallo-0.5.0/wagtail_hallo/static/js/hallo-plugins/hallo-hr.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// original source - https://github.com/wagtail/wagtail/blob/stable/2.16.x/client/src/entrypoints/admin/hallo-plugins/hallo-hr.js
+
+(function () {
+  // hallo-hr
+  $.widget('IKS.hallohr', {
+    options: {
+      editable: null,
+      toolbar: null,
+      uuid: '',
+      buttonCssClass: null,
+    },
+    populateToolbar(toolbar) {
+      const buttonset = $('<span class="' + this.widgetName + '"></span>');
+      const buttonElement = $('<span></span>');
+      buttonElement.hallobutton({
+        uuid: this.options.uuid,
+        editable: this.options.editable,
+        label: 'Horizontal rule',
+        command: 'insertHorizontalRule',
+        icon: 'icon-horizontalrule',
+        cssClass: this.options.buttonCssClass,
+      });
+      buttonset.append(buttonElement);
+      buttonset.hallobuttonset();
+      return toolbar.append(buttonset);
+    },
+  });
+})();
---
+++ packages/wagtail-hallo/wagtail_hallo-0.5.0.tar.gz/wagtail_hallo-0.5.0/wagtail_hallo/static/js/hallo-plugins/hallo-wagtailembeds.js
@@ -0,0 +1,57 @@
+'use strict';
+
+// original source - https://github.com/wagtail/wagtail/blob/stable/2.16.x/wagtail/embeds/static_src/wagtailembeds/js/hallo-plugins/hallo-wagtailembeds.js
+
+(function () {
+  // hallo-wagtailembeds
+  $.widget('IKS.hallowagtailembeds', {
+    options: {
+      uuid: '',
+      editable: null,
+    },
+    populateToolbar: function (toolbar) {
+      var button;
+      var widget;
+
+      widget = this;
+      button = $('<span class="' + this.widgetName + '"></span>');
+      button.hallobutton({
+        uuid: this.options.uuid,
+        editable: this.options.editable,
+        label: 'Embed',
+        icon: 'icon-media',
+        command: null,
+      });
+
+      toolbar.append(button);
+
+      return button.on('click', function (event) {
+        var insertionPoint;
+        var lastSelection;
+
+        lastSelection = widget.options.editable.getSelection();
+        insertionPoint = $(lastSelection.endContainer)
+          .parentsUntil('[data-hallo-editor]')
+          .last();
+
+        return ModalWorkflow({
+          url: window.chooserUrls.embedsChooser,
+          onload: EMBED_CHOOSER_MODAL_ONLOAD_HANDLERS,
+          responses: {
+            embedChosen: function (embedData) {
+              var elem;
+
+              elem = $(embedData).get(0);
+              lastSelection.insertNode(elem);
+              if (elem.getAttribute('contenteditable') === 'false') {
+                insertRichTextDeleteControl(elem);
+              }
+
+              return widget.options.editable.element.trigger('change');
+            },
+          },
+        });
+      });
+    },
+  });
+})();
