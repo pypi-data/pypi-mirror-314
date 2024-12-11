44bdbfc39756a10be9ba343459eb04a9709cd876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 15:05:14 +0000

    Add package prunepytest/0.5.0/prunepytest-0.5.0-cp37-abi3-win32.whl

---
+++ packages/prunepytest/prunepytest-0.5.0-cp37-abi3-win32.whl/prunepytest/__init__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: © 2024 Hugues Bruant <hugues.bruant@gmail.com>
+# SPDX-License-Identifier: GPL-3.0-only
+
+from ._prunepytest import *  # type: ignore[import-not-found]  # noqa: F403
