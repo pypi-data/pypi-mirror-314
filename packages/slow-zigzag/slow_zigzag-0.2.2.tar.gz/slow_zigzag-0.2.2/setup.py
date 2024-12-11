7f9685fde88352b3d98e2442867b7ffdcf3606bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 14:05:11 +0000

    Add package slow-zigzag/0.2.2/slow_zigzag-0.2.2.tar.gz

---
+++ packages/slow-zigzag/slow_zigzag-0.2.2.tar.gz/slow_zigzag-0.2.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='slow_zigzag',
+    version='0.2.2',
+    description='Zig Zag indicator',
+    url='https://github.com/pakchu/zigzag',
+    author=['hjkim17', 'pakchu'],
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    install_requires=[
+        'numpy',
+        'pandas',
+        'pandas-ta'
+    ],
+    python_requires='>=3.9',
+    long_description=open('README.md').read(),
+)
