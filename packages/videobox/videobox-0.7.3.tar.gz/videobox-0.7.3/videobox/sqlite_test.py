46ee21cfc59c498858e147bd9d4f9967c649ee7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 04:06:21 +0000

    Add package videobox/0.7.3/videobox-0.7.3.tar.gz

---
+++ packages/videobox/videobox-0.7.3.tar.gz/videobox-0.7.3/videobox/sqlite_test.py
@@ -0,0 +1,35 @@
+def max_sql_variables():
+    """Get the maximum number of arguments allowed in a query by the current
+    sqlite3 implementation. Based on `this question
+    `_
+
+    Returns
+    -------
+    int
+        inferred SQLITE_MAX_VARIABLE_NUMBER
+    """
+    import sqlite3
+    db = sqlite3.connect(':memory:')
+    cur = db.cursor()
+    cur.execute('CREATE TABLE t (test)')
+    low, high = 0, 100000
+    while (high - 1) > low: 
+        guess = (high + low) // 2
+        query = 'INSERT INTO t VALUES ' + ','.join(['(?)' for _ in
+                                                    range(guess)])
+        args = [str(i) for i in range(guess)]
+        try:
+            cur.execute(query, args)
+        except sqlite3.OperationalError as e:
+            if "too many SQL variables" in str(e):
+                high = guess
+            else:
+                raise
+        else:
+            low = guess
+    cur.close()
+    db.close()
+    return low
+
+SQLITE_MAX_VARIABLE_NUMBER = max_sql_variables()
+print(SQLITE_MAX_VARIABLE_NUMBER)
