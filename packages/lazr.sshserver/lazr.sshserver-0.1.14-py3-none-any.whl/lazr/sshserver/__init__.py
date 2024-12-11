72d6b36de3f30b63e235ff1226954e91f36a7597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 03:18:32 +0000

    Add package lazr.sshserver/0.1.14/lazr.sshserver-0.1.14-py3-none-any.whl

---
+++ packages/lazr.sshserver/lazr.sshserver-0.1.14-py3-none-any.whl/lazr/sshserver/__init__.py
@@ -0,0 +1,26 @@
+# Copyright 2009-2015 Canonical Ltd.  All rights reserved.
+#
+# This file is part of lazr.sshserver
+#
+# lazr.sshserver is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# lazr.sshserver is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+# License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with lazr.sshserver.  If not, see <http://www.gnu.org/licenses/>.
+
+"The lazr.sshserver package."
+
+__all__ = []
+
+try:
+    import importlib.metadata as importlib_metadata
+except ImportError:
+    import importlib_metadata
+
+__version__ = importlib_metadata.version("lazr.sshserver")
