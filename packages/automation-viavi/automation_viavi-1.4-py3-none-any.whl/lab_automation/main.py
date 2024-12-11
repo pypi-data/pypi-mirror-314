47bf065bb21043cf5aa79bd307351242b0050f86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 16:05:37 +0000

    Add package automation-viavi/1.4/automation_viavi-1.4-py3-none-any.whl

---
+++ packages/automation-viavi/automation_viavi-1.4-py3-none-any.whl/lab_automation/main.py
@@ -0,0 +1,12 @@
+from serial_utils import check_all_ports
+import os
+
+def main():
+    print("Starting automation process\n")
+    check_all_ports()
+    page_ID = os.getenv('PAGE_ID')
+    api_token = os.getenv('API_TOKEN')
+    print(page_ID, api_token)
+
+if __name__ == "__main__":
+    main()
