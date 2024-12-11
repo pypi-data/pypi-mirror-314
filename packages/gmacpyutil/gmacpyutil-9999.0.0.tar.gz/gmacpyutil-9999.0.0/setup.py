a3c117c1c1c6b20bf7695020d34fc24c4558b73d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 16:05:34 +0000

    Add package gmacpyutil/9999.0.0/gmacpyutil-9999.0.0.tar.gz

---
+++ packages/gmacpyutil/gmacpyutil-9999.0.0.tar.gz/gmacpyutil-9999.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="gmacpyutil",
+    version="9999.0.0",
+    description="Utilities, tools, and scripts for managing and tracking a fleet of Macintoshes in a corporate environment",
+    author="System Engineer",
+    author_email="miguelangel4b+bugbounty@gmail.com",
+    packages=["gmacpyutil"],
+    install_requires=["requests"],
+)
