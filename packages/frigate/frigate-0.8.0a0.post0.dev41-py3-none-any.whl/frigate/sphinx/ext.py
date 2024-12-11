8e4043f9d526c7a6b920d52fa1a4e219c3174a27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 03:18:32 +0000

    Add package frigate/0.8.0a0.post0.dev41/frigate-0.8.0a0.post0.dev41-py3-none-any.whl

---
+++ packages/frigate/frigate-0.8.0a0.post0.dev41-py3-none-any.whl/frigate/sphinx/ext.py
@@ -0,0 +1,36 @@
+import os
+
+from docutils import nodes
+from docutils.parsers import rst
+from docutils.parsers.rst.directives import unchanged
+from docutils.statemachine import ViewList
+from sphinx.util.nodes import nested_parse_with_titles
+
+from frigate.gen import gen
+
+
+class FrigateDirective(rst.Directive):
+    has_content = True
+    required_arguments = 1
+    option_spec = {
+        'output_format': unchanged,
+    }
+
+    def run(self):
+        chart_path = os.path.join(
+            self.state.document.settings.env.srcdir,
+            self.arguments[0],
+        )
+        if self.options.get('output_format') is None:
+            self.options.update({'output_format': 'rst'})
+        output = ViewList(gen(chart_path, output_format=self.options.get('output_format')).split("\n"))
+
+        node = nodes.section()
+        node.document = self.state.document
+        nested_parse_with_titles(self.state, output, node)
+
+        return node.children
+
+
+def setup(app):
+    app.add_directive("frigate", FrigateDirective)
