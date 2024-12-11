a94170e246fe76f53206496580e3a090d1b67e19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 04:05:39 +0000

    Add package cosmotech-run-orchestrator/1.5.2/cosmotech_run_orchestrator-1.5.2.tar.gz

---
+++ packages/cosmotech-run-orchestrator/cosmotech_run_orchestrator-1.5.2.tar.gz/cosmotech_run_orchestrator-1.5.2/cosmotech/orchestrator/utils/logger.py
@@ -0,0 +1,30 @@
+# Copyright (C) - 2023 - 2024 - Cosmo Tech
+# This document and all information contained herein is the exclusive property -
+# including all intellectual property rights pertaining thereto - of Cosmo Tech.
+# Any use, reproduction, translation, broadcasting, transmission, distribution,
+# etc., to any person is prohibited unless it has been previously and
+# specifically authorized by written means by Cosmo Tech.
+
+import logging
+import os
+
+from rich.logging import RichHandler
+
+LOGGER = logging.getLogger("csm.run.orchestrator")
+HANDLER = RichHandler(rich_tracebacks=True,
+                      omit_repeated_times=False,
+                      show_path=False,
+                      markup=True)
+_format = "%(message)s"
+
+if "PAILLETTES" in os.environ:
+    paillettes = "[bold yellow blink]***[/]"
+    _format = f"{paillettes} {_format} {paillettes}"
+
+FORMATTER = logging.Formatter(fmt=_format,
+                              datefmt="[%Y/%m/%d-%H:%M:%S]",
+                              )
+
+HANDLER.setFormatter(FORMATTER)
+LOGGER.addHandler(HANDLER)
+LOGGER.setLevel(logging.INFO)
