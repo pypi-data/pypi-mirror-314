8ad2a495190ab610c91436dcb0b9d1b216e05a0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 07:05:18 +0000

    Add package autoevals/0.0.109/autoevals-0.0.109-py3-none-any.whl

---
+++ packages/autoevals/autoevals-0.0.109-py3-none-any.whl/autoevals/oai.py
@@ -0,0 +1,147 @@
+import asyncio
+import os
+import sys
+import textwrap
+import time
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Any
+
+PROXY_URL = "https://api.braintrust.dev/v1/proxy"
+
+
+@dataclass
+class OpenAIWrapper:
+    complete: Any
+    embed: Any
+    moderation: Any
+    RateLimitError: Exception
+
+
+def prepare_openai(is_async=False, api_key=None, base_url=None):
+    if api_key is None:
+        api_key = os.environ.get("OPENAI_API_KEY") or os.environ.get("BRAINTRUST_API_KEY")
+    if base_url is None:
+        base_url = os.environ.get("OPENAI_BASE_URL", PROXY_URL)
+
+    try:
+        import openai
+    except Exception as e:
+        print(
+            textwrap.dedent(
+                f"""\
+            Unable to import openai: {e}
+
+            Please install it, e.g. with
+
+              pip install 'openai'
+            """
+            ),
+            file=sys.stderr,
+        )
+        raise
+
+    openai_obj = openai
+    is_v1 = False
+
+    if hasattr(openai, "OpenAI"):
+        # This is the new v1 API
+        is_v1 = True
+        if is_async:
+            openai_obj = openai.AsyncOpenAI(api_key=api_key, base_url=base_url)
+        else:
+            openai_obj = openai.OpenAI(api_key=api_key, base_url=base_url)
+    else:
+        if api_key:
+            openai.api_key = api_key
+        openai.api_base = base_url
+
+    wrapped = False
+    try:
+        from braintrust.oai import wrap_openai
+
+        openai_obj = wrap_openai(openai_obj)
+        wrapped = True
+    except ImportError:
+        pass
+
+    complete_fn = None
+    rate_limit_error = None
+    if is_v1:
+        wrapper = OpenAIWrapper(
+            complete=openai_obj.chat.completions.create,
+            embed=openai_obj.embeddings.create,
+            moderation=openai_obj.moderations.create,
+            RateLimitError=openai.RateLimitError,
+        )
+    else:
+        rate_limit_error = openai.error.RateLimitError
+        if is_async:
+            complete_fn = openai_obj.ChatCompletion.acreate
+            embedding_fn = openai_obj.Embedding.acreate
+            moderation_fn = openai_obj.Moderations.acreate
+        else:
+            complete_fn = openai_obj.ChatCompletion.create
+            embedding_fn = openai_obj.Embedding.create
+            moderation_fn = openai_obj.Moderations.create
+        wrapper = OpenAIWrapper(
+            complete=complete_fn,
+            embed=embedding_fn,
+            moderation=moderation_fn,
+            RateLimitError=rate_limit_error,
+        )
+
+    return wrapper, wrapped
+
+
+def post_process_response(resp):
+    # This normalizes against craziness in OpenAI v0 vs. v1
+    if hasattr(resp, "to_dict"):
+        # v0
+        return resp.to_dict()
+    else:
+        # v1
+        return resp.dict()
+
+
+def set_span_purpose(kwargs):
+    kwargs.setdefault("span_info", {}).setdefault("span_attributes", {})["purpose"] = "scorer"
+
+
+def run_cached_request(request_type="complete", api_key=None, base_url=None, **kwargs):
+    wrapper, wrapped = prepare_openai(is_async=False, api_key=api_key, base_url=base_url)
+    if wrapped:
+        set_span_purpose(kwargs)
+
+    retries = 0
+    sleep_time = 0.1
+    while retries < 100:
+        try:
+            resp = post_process_response(getattr(wrapper, request_type)(**kwargs))
+            break
+        except wrapper.RateLimitError:
+            sleep_time *= 1.5
+            time.sleep(sleep_time)
+            retries += 1
+
+    return resp
+
+
+async def arun_cached_request(request_type="complete", api_key=None, base_url=None, **kwargs):
+    wrapper, wrapped = prepare_openai(is_async=True, api_key=api_key, base_url=base_url)
+    if wrapped:
+        set_span_purpose(kwargs)
+
+    retries = 0
+    sleep_time = 0.1
+    while retries < 100:
+        try:
+            resp = post_process_response(await getattr(wrapper, request_type)(**kwargs))
+            break
+        except wrapper.RateLimitError:
+            # Just assume it's a rate limit error
+            sleep_time *= 1.5
+            await asyncio.sleep(sleep_time)
+            retries += 1
+
+    return resp
---
+++ packages/autoevals/autoevals-0.0.109-py3-none-any.whl/autoevals/test_llm.py
@@ -0,0 +1,138 @@
+import asyncio
+
+import chevron
+
+from autoevals.llm import *
+from autoevals.llm import build_classification_tools
+
+
+def test_template_html():
+    template_double = "{{output}}"
+    template_triple = "{{{output}}}"
+
+    assert chevron.render(template_double, dict(output="Template<Foo>")) == "Template&lt;Foo&gt;"
+    assert chevron.render(template_triple, dict(output="Template<Foo>")) == "Template<Foo>"
+
+
+def test_openai():
+    e = OpenAILLMClassifier(
+        "title",
+        messages=[
+            {
+                "role": "system",
+                "content": """\
+You are a technical project manager who helps software engineers generate better titles for their GitHub issues.
+You will look at the issue description, and pick which of two titles better describes it.""",
+            },
+            {
+                "role": "user",
+                "content": """\
+I'm going to provide you with the issue description, and two possible titles.
+
+Issue Description: {{page_content}}
+
+1: {{output}}
+2: {{expected}}
+
+Please discuss each title briefly (one line for pros, one for cons), and then answer the question by calling
+the select_choice function with "1" or "2".""",
+            },
+        ],
+        model="gpt-3.5-turbo",
+        choice_scores={"1": 1, "2": 0},
+        classification_tools=build_classification_tools(useCoT=True, choice_strings=["1", "2"]),
+        max_tokens=500,
+    )
+
+    page_content = """
+As suggested by Nicolo, we should standardize the error responses coming from GoTrue, postgres, and realtime (and any other/future APIs) so that it's better DX when writing a client,
+
+We can make this change on the servers themselves, but since postgrest and gotrue are fully/partially external may be harder to change, it might be an option to transform the errors within the client libraries/supabase-js, could be messy?
+
+Nicolo also dropped this as a reference: http://spec.openapis.org/oas/v3.0.3#openapi-specification"""
+
+    gen_title = "Standardize error responses from GoTrue, Postgres, and Realtime APIs for better DX"
+    original_title = "This title has nothing to do with the content"
+
+    response = e(gen_title, original_title, page_content=page_content)
+    print(response.as_json(indent=2))
+    assert response.score == 1
+    assert response.error is None
+
+
+def test_llm_classifier():
+    for use_cot in [True, False]:
+        e = LLMClassifier(
+            "title",
+            """
+You are a technical project manager who helps software engineers generate better titles for their GitHub issues.
+You will look at the issue description, and pick which of two titles better describes it.
+
+I'm going to provide you with the issue description, and two possible titles.
+
+Issue Description: {{page_content}}
+
+1: {{output}}
+2: {{expected}}""",
+            {"1": 1, "2": 0},
+            use_cot=use_cot,
+        )
+
+        page_content = """
+As suggested by Nicolo, we should standardize the error responses coming from GoTrue, postgres, and realtime (and any other/future APIs) so that it's better DX when writing a client,
+
+We can make this change on the servers themselves, but since postgrest and gotrue are fully/partially external may be harder to change, it might be an option to transform the errors within the client libraries/supabase-js, could be messy?
+
+Nicolo also dropped this as a reference: http://spec.openapis.org/oas/v3.0.3#openapi-specification"""
+
+        gen_title = "Standardize error responses from GoTrue, Postgres, and Realtime APIs for better DX"
+        original_title = "This title has nothing to do with the content"
+
+        response = e(gen_title, original_title, page_content=page_content)
+        print(response.as_json(indent=2))
+        assert response.score == 1
+        assert response.error is None
+
+        response = e(original_title, gen_title, page_content=page_content)
+        print(response.as_json(indent=2))
+        assert response.score == 0
+        assert response.error is None
+
+
+def test_nested_async():
+    async def nested_async():
+        e = Battle()
+        e(instructions="Add the following numbers: 1, 2, 3", output="600", expected="6")
+
+    asyncio.run(nested_async())
+
+
+def test_battle():
+    for use_cot in [True, False]:
+        print("use_cot", use_cot)
+        e = Battle(use_cot=use_cot)
+        response = e(
+            instructions="Add the following numbers: 1, 2, 3",
+            output="600",
+            expected="6",
+        )
+
+        print(response.as_json(indent=2))
+        assert response.score == 0
+        assert response.error is None
+
+        response = e(
+            instructions="Add the following numbers: 1, 2, 3",
+            output="6",
+            expected="600",
+        )
+
+        print(response.as_json(indent=2))
+        assert response.score == 1
+        assert response.error is None
+
+        response = e(instructions="Add the following numbers: 1, 2, 3", output="6", expected="6")
+
+        print(response.as_json(indent=2))
+        assert response.score == 0
+        assert response.error is None
