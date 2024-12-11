d976963c48ee2ac6b7109b02e6fd5a05afb7f430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 03:18:26 +0000

    Add package tutor/18.2.2/tutor-18.2.2.tar.gz

---
+++ packages/tutor/tutor-18.2.2.tar.gz/tutor-18.2.2/tutor/commands/upgrade/__init__.py
@@ -0,0 +1,13 @@
+# Note: don't forget to change this when we upgrade from redwood
+OPENEDX_RELEASE_NAMES = [
+    "ironwood",
+    "juniper",
+    "koa",
+    "lilac",
+    "maple",
+    "nutmeg",
+    "olive",
+    "palm",
+    "quince",
+    "redwood",
+]
