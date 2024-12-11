ba78407ab0b94160a5b5d32f01f705807a9c4430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 14:04:46 +0000

    Add package arelle-release/2.35.17/arelle_release-2.35.17.tar.gz

---
+++ packages/arelle-release/arelle_release-2.35.17.tar.gz/arelle_release-2.35.17/arelle/plugin/validate/DBA/ValidationPluginExtension.py
@@ -0,0 +1,84 @@
+"""
+See COPYRIGHT.md for copyright information.
+"""
+from __future__ import annotations
+
+from arelle.ModelValue import qname
+from arelle.ValidateXbrl import ValidateXbrl
+from arelle.typing import TypeGetText
+from arelle.utils.validate.ValidationPlugin import ValidationPlugin
+from .PluginValidationDataExtension import PluginValidationDataExtension
+
+_: TypeGetText
+
+NAMESPACE_ARR = 'http://xbrl.dcca.dk/arr'
+NAMESPACE_CMN = 'http://xbrl.dcca.dk/cmn'
+NAMESPACE_FSA = 'http://xbrl.dcca.dk/fsa'
+NAMESPACE_GSD = 'http://xbrl.dcca.dk/gsd'
+NAMESPACE_SOB = 'http://xbrl.dcca.dk/sob'
+
+
+class ValidationPluginExtension(ValidationPlugin):
+    def newPluginData(self, validateXbrl: ValidateXbrl) -> PluginValidationDataExtension:
+        return PluginValidationDataExtension(
+            self.name,
+            annualReportTypes=frozenset([
+                'Årsrapport',
+                'årsrapport',
+                'Annual report'
+            ]),
+            assetsQn=qname(f'{{{NAMESPACE_FSA}}}Assets'),
+            classOfReportingEntityQn=qname(f'{{{NAMESPACE_FSA}}}ClassOfReportingEntity'),
+            consolidatedMemberQn=qname(f'{{{NAMESPACE_CMN}}}ConsolidatedMember'),
+            consolidatedSoloDimensionQn=qname(f'{{{NAMESPACE_CMN}}}ConsolidatedSoloDimension'),
+            dateOfApprovalOfAnnualReportQn=qname(f'{{{NAMESPACE_SOB}}}DateOfApprovalOfAnnualReport'),
+            dateOfExtraordinaryDividendDistributedAfterEndOfReportingPeriod=
+            qname(f'{{{NAMESPACE_FSA}}}DateOfExtraordinaryDividendDistributedAfterEndOfReportingPeriod'),
+            dateOfGeneralMeetingQn=qname(f'{{{NAMESPACE_GSD}}}DateOfGeneralMeeting'),
+            descriptionOfQualificationsOfAssuranceEngagementPerformedQn=qname(f'{{{NAMESPACE_ARR}}}DescriptionOfQualificationsOfAssuranceEngagementPerformed'),
+            distributionOfResultsQns=frozenset([
+                qname(f'{{{NAMESPACE_FSA}}}DistributionsResultDistribution'),
+                qname(f'{{{NAMESPACE_FSA}}}ExtraordinaryDistributions'),
+                qname(f'{{{NAMESPACE_FSA}}}ProposedDividendRecognisedInEquity'),
+                qname(f'{{{NAMESPACE_FSA}}}ProposedExtraordinaryDividendRecognisedInEquity'),
+                qname(f'{{{NAMESPACE_FSA}}}ProposedExtraordinaryDividendRecognisedInLiabilities'),
+                qname(f'{{{NAMESPACE_FSA}}}TransferredFromToHedgeFund'),
+                qname(f'{{{NAMESPACE_FSA}}}TransferredFromToReserveFund'),
+                qname(f'{{{NAMESPACE_FSA}}}TransferredFromToReservesAvailable'),
+                qname(f'{{{NAMESPACE_FSA}}}TransferredToFromEquityAttributableToParent'),
+                qname(f'{{{NAMESPACE_FSA}}}TransferredToFromMinorityInterests'),
+                qname(f'{{{NAMESPACE_FSA}}}TransferredToFromOtherStatutoryReserves'),
+                qname(f'{{{NAMESPACE_FSA}}}TransferredToFromReserveAccordingToArticlesOfAssociation'),
+                qname(f'{{{NAMESPACE_FSA}}}TransferredToFromReserveForNetRevaluationAccordingToEquityMethod'),
+                qname(f'{{{NAMESPACE_FSA}}}TransferredToFromReserveForNetRevaluationOfInvestmentAssets'),
+                qname(f'{{{NAMESPACE_FSA}}}TransferredToFromRestOfOtherReserves'),
+                qname(f'{{{NAMESPACE_FSA}}}TransferredToFromRetainedEarnings'),
+                qname(f'{{{NAMESPACE_FSA}}}TransferredToReserveForCurrentValueAdjustmentsOfCurrencyGains'),
+                qname(f'{{{NAMESPACE_FSA}}}TransferredToReserveForCurrentValueOfHedging'),
+                qname(f'{{{NAMESPACE_FSA}}}TransferredToReserveForDevelopmentExpenditure'),
+                qname(f'{{{NAMESPACE_FSA}}}TransferredToReserveForEntrepreneurialCompany'),
+            ]),
+            equityQn=qname(f'{{{NAMESPACE_FSA}}}Equity'),
+            extraordinaryCostsQn=qname(f'{{{NAMESPACE_FSA}}}ExtraordinaryCosts'),
+            extraordinaryIncomeQn=qname(f'{{{NAMESPACE_FSA}}}ExtraordinaryIncome'),
+            extraordinaryResultBeforeTaxQn=qname(f'{{{NAMESPACE_FSA}}}ExtraordinaryResultBeforeTax'),
+            fr37RestrictedText='has not given rise to reservations',
+            informationOnTypeOfSubmittedReportQn=qname(f'{{{NAMESPACE_GSD}}}InformationOnTypeOfSubmittedReport'),
+            liabilitiesQn=qname(f'{{{NAMESPACE_FSA}}}LiabilitiesAndEquity'),
+            liabilitiesAndEquityQn=qname(f'{{{NAMESPACE_FSA}}}LiabilitiesAndEquity'),
+            liabilitiesOtherThanProvisionsQn=qname(f'{{{NAMESPACE_FSA}}}LiabilitiesOtherThanProvisions'),
+            longtermLiabilitiesOtherThanProvisionsQn=qname(f'{{{NAMESPACE_FSA}}}LongtermLiabilitiesOtherThanProvisions'),
+            noncurrentAssetsQn=qname(f'{{{NAMESPACE_FSA}}}NoncurrentAssets'),
+            nameAndSurnameOfChairmanOfGeneralMeetingQn=qname(f'{{{NAMESPACE_GSD}}}NameAndSurnameOfChairmanOfGeneralMeeting'),
+            positiveProfitThreshold=1000,
+            precedingReportingPeriodEndDateQn=qname(f'{{{NAMESPACE_GSD}}}PredingReportingPeriodEndDate'),  # Typo in taxonomy
+            precedingReportingPeriodStartDateQn=qname(f'{{{NAMESPACE_GSD}}}PrecedingReportingPeriodStartDate'),
+            profitLossQn=qname(f'{{{NAMESPACE_FSA}}}ProfitLoss'),
+            proposedDividendRecognisedInEquityQn=qname(f'{{{NAMESPACE_FSA}}}ProposedDividendRecognisedInEquity'),
+            reportingPeriodEndDateQn=qname(f'{{{NAMESPACE_GSD}}}ReportingPeriodEndDate'),
+            reportingPeriodStartDateQn=qname(f'{{{NAMESPACE_GSD}}}ReportingPeriodStartDate'),
+            shorttermLiabilitiesOtherThanProvisionsQn=qname(f'{{{NAMESPACE_FSA}}}ShorttermLiabilitiesOtherThanProvisions'),
+            taxExpenseOnOrdinaryActivitiesQn=qname(f'{{{NAMESPACE_FSA}}}TaxExpenseOnOrdinaryActivities'),
+            taxExpenseQn=qname(f'{{{NAMESPACE_FSA}}}TaxExpense'),
+            typeOfReportingPeriodDimensionQn=qname(f'{{{NAMESPACE_GSD}}}TypeOfReportingPeriodDimension'),
+        )
