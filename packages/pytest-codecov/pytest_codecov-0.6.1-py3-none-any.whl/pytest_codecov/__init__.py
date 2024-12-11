bbf8c0a5599f1ddcdfe7dcc6882b1573eaf2775f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 16:05:25 +0000

    Add package pytest-codecov/0.6.1/pytest_codecov-0.6.1-py3-none-any.whl

---
+++ packages/pytest-codecov/pytest_codecov-0.6.1-py3-none-any.whl/pytest_codecov/__init__.py
@@ -0,0 +1,183 @@
+import argparse
+import os
+import pytest
+import re
+
+import pytest_codecov.git as git
+import pytest_codecov.codecov as codecov
+
+
+__version__ = '0.6.1'
+token_regex = re.compile(
+    r'^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$'
+)
+slug_regex = re.compile(
+    r'^[0-9a-zA-Z_.-]+/[0-9a-zA-Z_.-]+$'
+)
+
+
+def validate_token(arg):
+    if not token_regex.match(arg):
+        msg = 'Invalid Codecov.io repository token supplied.'
+        raise argparse.ArgumentTypeError(msg)
+    return arg
+
+
+def validate_slug(arg):
+    if not slug_regex.match(arg):
+        msg = 'Invalid repository slug supplied.'
+        raise argparse.ArgumentTypeError(msg)
+    return arg
+
+
+def pytest_addoption(parser, pluginmanager):
+    group = parser.getgroup('codecov')
+    group.addoption(
+        '--codecov',
+        action='store_true',
+        dest='codecov',
+        default=False,
+        help='Upload coverage results to codecov.io'
+    )
+    group.addoption(
+        '--codecov-token',
+        action='store',
+        dest='codecov_token',
+        default=os.environ.get('CODECOV_TOKEN') or None,
+        metavar='TOKEN',
+        type=validate_token,
+        help='Set the codecov token for private repositories.'
+    )
+    group.addoption(
+        '--codecov-slug',
+        action='store',
+        dest='codecov_slug',
+        default=os.environ.get('CODECOV_SLUG') or git.slug,
+        metavar='SLUG',
+        type=validate_slug,
+        help='Set the git repository slug manually.'
+    )
+    group.addoption(
+        '--codecov-branch',
+        action='store',
+        dest='codecov_branch',
+        default=os.environ.get('CODECOV_BRANCH') or git.branch,
+        help='Set the git branch manually.'
+    )
+    group.addoption(
+        '--codecov-commit',
+        action='store',
+        dest='codecov_commit',
+        default=os.environ.get('CODECOV_COMMIT') or git.commit,
+        help='Set the git commit hash manually.'
+    )
+    group.addoption(
+        '--codecov-dump',
+        action='store_true',
+        dest='codecov_dump',
+        default=False,
+        help='Dump codecov payload to terminal instead of uploading it'
+    )
+    group.addoption(
+        '--no-codecov-on-failure',
+        action='store_false',
+        dest='codecov_upload_on_failure',
+        default=True,
+        help='Don\'t upload coverage results on test failure'
+    )
+
+
+class CodecovPlugin:
+
+    def upload_report(self, terminalreporter, config, cov):
+        uploader = codecov.CodecovUploader(
+            config.option.codecov_slug,
+            commit=config.option.codecov_commit,
+            branch=config.option.codecov_branch,
+            token=config.option.codecov_token,
+        )
+        uploader.write_network_files(git.ls_files())
+        from coverage.misc import CoverageException
+        try:
+            uploader.add_coverage_report(cov)
+        except CoverageException as exc:
+            terminalreporter.section('Codecov.io payload')
+            terminalreporter.write_line(
+                f'ERROR: Failed to generate XML report: {exc}',
+                red=True,
+                bold=True,
+            )
+            terminalreporter.line('')
+            return
+
+        if config.option.codecov_dump:
+            terminalreporter.section('Prepared Codecov.io payload')
+            terminalreporter.write_line(uploader.get_payload())
+            return
+
+        terminalreporter.section('Codecov.io upload')
+
+        if not config.option.codecov_slug:
+            terminalreporter.write_line(
+                'ERROR: Failed to determine git repository slug. '
+                'Cannot upload without a valid slug.',
+                red=True,
+                bold=True,
+            )
+            terminalreporter.line('')
+            return
+        if not config.option.codecov_branch:
+            terminalreporter.write_line(
+                'WARNING: Failed to determine git repository branch.',
+                yellow=True,
+                bold=True,
+            )
+        if not config.option.codecov_commit:
+            terminalreporter.write_line(
+                'WARNING: Failed to determine git commit.',
+                yellow=True,
+                bold=True,
+            )
+        terminalreporter.write_line(
+            'Environment:\n'
+            f'Slug:   {config.option.codecov_slug}\n'
+            f'Branch: {config.option.codecov_branch}\n'
+            f'Commit: {config.option.codecov_commit}\n'
+        )
+        try:
+            terminalreporter.write_line('Pinging codecov API...')
+            uploader.ping()
+            terminalreporter.line('')
+            terminalreporter.write_line(
+                'Uploading reports to storage endpoint...'
+            )
+            uploader.upload()
+            terminalreporter.line('')
+            terminalreporter.write_line(
+                'Successfully queued reports for processing.',
+                green=True
+            )
+            terminalreporter.line('')
+        except codecov.CodecovError as error:
+            terminalreporter.write_line(f'ERROR: {error}', red=True, bold=True)
+
+    @pytest.hookimpl(trylast=True)
+    def pytest_terminal_summary(self, terminalreporter, exitstatus, config):
+        cov_plugin = config.pluginmanager.get_plugin('_cov')
+        if cov_plugin.cov_controller is None:
+            return
+
+        cov = cov_plugin.cov_controller.cov
+        if cov is None:
+            return
+
+        if exitstatus != 0 and not config.option.codecov_upload_on_failure:
+            return
+
+        self.upload_report(terminalreporter, config, cov)
+
+
+def pytest_configure(config):  # pragma: no cover
+    # NOTE: if cov is missing we fail silently
+    if config.option.codecov and config.pluginmanager.has_plugin('_cov'):
+        config.pluginmanager.register(CodecovPlugin())
---
+++ packages/pytest-codecov/pytest_codecov-0.6.1-py3-none-any.whl/pytest_codecov/codecov.py
@@ -0,0 +1,101 @@
+import gzip
+import io
+import requests
+import tempfile
+from urllib.parse import urljoin
+
+
+def package():
+    from pytest_codecov import __version__ as version
+    return f'pytest_codecov-{version}'
+
+
+class CodecovError(Exception):
+    pass
+
+
+class CodecovUploader:
+    api_endpoint = 'https://codecov.io'
+    storage_endpoint = 'https://storage.googleapis.com/codecov-production/'
+
+    def __init__(self, slug, commit=None, branch=None, token=None):
+        self.slug = slug
+        self.commit = commit
+        self.branch = branch
+        self.token = token
+        self.store_url = None
+        self._buffer = io.StringIO()
+
+    def write_network_files(self, files):
+        self._buffer.write(
+            '\n'.join(files + ['<<<<<< network'])
+        )
+
+    def add_coverage_report(self, cov, filename='coverage.xml', **kwargs):
+        with tempfile.NamedTemporaryFile(mode='r') as xml_report:
+            # embed xml report
+            self._buffer.write(f'\n# path=./{filename}\n')
+            cov.xml_report(outfile=xml_report.name)
+            xml_report.seek(0)
+            self._buffer.write(xml_report.read())
+            self._buffer.write('\n<<<<<< EOF')
+
+    def get_payload(self):
+        return self._buffer.getvalue()
+
+    def ping(self):
+        if not self.slug:
+            raise CodecovError(
+                'Failed to determine git repository slug. '
+                'Cannot upload without a valid slug.'
+            )
+        api_url = urljoin(self.api_endpoint, '/upload/v4')
+        headers = {
+            'X-Reduced-Redundancy': 'false',
+            'X-Content-Type': 'application/x-gzip',
+            'Content-Length': '0',
+        }
+        params = {
+            'package': package(),
+            'token': self.token or '',
+            'branch': self.branch or '',
+            'commit': self.commit or '',
+            'build': '',  # TODO: support builds?
+            'build_url': '',
+            'name': '',
+            'tag': '',  # TODO: support tags?
+            'slug': self.slug,
+            'service': '',
+            'flags': '',  # TODO: support flags?
+            'pr': '',  # TODO: support pull requests?
+            'job': '',
+            'cmd_args': '',
+        }
+        response = requests.post(api_url, headers=headers, params=params)
+        lines = response.text.splitlines()
+        if len(lines) != 2 or not lines[1].startswith(self.storage_endpoint):
+            raise CodecovError(
+                f'Invalid response from codecov API:\n{response.text}'
+            )
+        self.store_url = lines[1]
+
+    def upload(self):
+        if not self.store_url:
+            raise CodecovError('Need to ping API before upload.')
+
+        headers = {
+            'Content-Type': 'application/x-gzip',
+            'Content-Encoding': 'gzip',
+        }
+        gz_payload = io.BytesIO()
+        with gzip.open(gz_payload, 'wb', 9) as payload:
+            payload.write(self.get_payload().encode('utf-8'))
+        gz_payload.seek(0)
+        response = requests.put(
+            self.store_url, headers=headers, data=gz_payload
+        )
+
+        if not response.ok:
+            raise CodecovError('Failed to upload report to storage endpoint.')
+
+        self.store_url = None  # NOTE: Invalidate store url after upload
