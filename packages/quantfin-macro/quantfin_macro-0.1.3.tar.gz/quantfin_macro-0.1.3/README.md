d954e2e108d6f6ac8d91f2808b2a402c3c7b3182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 14:05:04 +0000

    Add package quantfin-macro/0.1.3/quantfin_macro-0.1.3.tar.gz

---
+++ packages/quantfin-macro/quantfin_macro-0.1.3.tar.gz/quantfin_macro-0.1.3/README.md
@@ -0,0 +1,10 @@
+# QuantFin Macro
+
+## What this is
+
+This project is a personal project. Feel free to explore but no support
+will be provided, as it is meant for personal use.
+
+## What this is NOT
+
+It's not something for public consumption.
---
+++ packages/quantfin-macro/quantfin_macro-0.1.3.tar.gz/quantfin_macro-0.1.3/src/quantfin/macro/charts/__init__.py
@@ -0,0 +1,7 @@
+"""
+This package contains useful function to produce standardised charts 
+used in the context of macroeconomics.
+
+"""
+from .components_and_line_chart import draw_components_and_line_chart
+from .line_and_shade_chart import draw_line_and_shade_chart
