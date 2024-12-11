3294159d3bf5bada991308f03c0cc8054f3306a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 05:06:47 +0000

    Add package edu-eldoc-registry/0.1.2/edu_eldoc_registry-0.1.2-py3-none-any.whl

---
+++ packages/edu-eldoc-registry/edu_eldoc_registry-0.1.2-py3-none-any.whl/edu_eldoc_registry/constants.py
@@ -0,0 +1,11 @@
+import datetime
+
+
+APP_VERBOSE_NAME = 'Электронные документы'
+
+# Возможные расширения для файла подписи
+EXT_SIG = 'sig'
+EXT_P7S = 'p7s'
+EXT_SGN = 'sgn'
+
+MIN_DATE = datetime.date(1917, 1, 1)
