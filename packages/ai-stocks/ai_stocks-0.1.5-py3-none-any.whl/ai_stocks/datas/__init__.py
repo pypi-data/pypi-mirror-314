c38c290bb5bf81e04c29e3457bb73e5eff0b002d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 14:05:12 +0000

    Add package ai-stocks/0.1.5/ai_stocks-0.1.5-py3-none-any.whl

---
+++ packages/ai-stocks/ai_stocks-0.1.5-py3-none-any.whl/ai_stocks/datas/__init__.py
@@ -0,0 +1,4 @@
+from .base_dataloader import BaseDataloader
+from .operate_dataloader import OperateDataloader
+from .price_dataloader import PriceDataloader
+from .stock_dataset import StockDataset
---
+++ packages/ai-stocks/ai_stocks-0.1.5-py3-none-any.whl/ai_stocks/predictions/__init__.py
@@ -0,0 +1,5 @@
+from .base_predictiion import BasePrediction
+from .operate_prediction import OperatePrediction
+from .price_prediction import PricePrediction
+
+predictions_list = [OperatePrediction, PricePrediction]
