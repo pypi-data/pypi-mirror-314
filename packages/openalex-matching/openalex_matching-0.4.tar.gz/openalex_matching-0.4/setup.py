d7b89db0a13c634d36147455650bafdc5de48850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 07:05:34 +0000

    Add package openalex-matching/0.4/openalex_matching-0.4.tar.gz

---
+++ packages/openalex-matching/openalex_matching-0.4.tar.gz/openalex_matching-0.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name= "openalex_matching",  
+    version = "0.4",  
+    author="Bryan Yuk", 
+    author_email= "dzg5cg@virginia.edu",  
+    license = "MIT",
+    long_description= open('README.md').read(),
+    long_description_content_type='text/markdown', 
+    url="https://github.com/byuk729/openalex_matching",  
+    packages=find_packages(),  
+    install_requires=[
+        "requests",  
+        "pandas",
+        "tqdm",
+        "fuzzywuzzy",
+        "nicknames"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",  
+        "License :: OSI Approved :: MIT License",  
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Information Analysis"
+    ],
+    python_requires='>=3.8',  # Minimum Python version required
+)
