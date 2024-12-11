ededea954a4444cf8fae2478bc0ba533122c653d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 14:04:48 +0000

    Add package torchao/0.7.0/torchao-0.7.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/torchao/torchao-0.7.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_28_x86_64.whl/torchao/__init__.py
@@ -0,0 +1,48 @@
+import torch
+import logging
+
+# torch/nested/_internal/nested_tensor.py:417: UserWarning: Failed to initialize NumPy: No module named 'numpy'
+import warnings
+warnings.filterwarnings("ignore", message="Failed to initialize NumPy: No module named 'numpy'")
+
+
+# We use this "hack" to set torchao.__version__ correctly
+# the version of ao is dependent on environment variables for multiple architectures
+# For local development this will default to whatever is version.txt
+# For release builds this will be set the version+architecture_postfix
+from importlib.metadata import version, PackageNotFoundError
+try:
+    __version__ = version("torchao")
+except PackageNotFoundError:
+    __version__ = 'unknown'  # In case this logic breaks don't break the build
+
+_IS_FBCODE = (
+    hasattr(torch._utils_internal, "IS_FBSOURCE") and
+    torch._utils_internal.IS_FBSOURCE
+)
+if not _IS_FBCODE:
+    try:
+        from pathlib import Path
+        so_files = list(Path(__file__).parent.glob("_C*.so"))
+        assert len(so_files) == 1, f"Expected one _C*.so file, found {len(so_files)}"
+        torch.ops.load_library(so_files[0])
+        from . import ops
+    except:
+        logging.debug("Skipping import of cpp extensions")
+
+from torchao.quantization import (
+    autoquant,
+    quantize_,
+)
+from . import dtypes
+from . import testing
+
+__all__ = [
+    "dtypes",
+    "autoquant",
+    "quantize_",
+    "testing",
+]
+
+# test-pytorchbot
+# test-codev
