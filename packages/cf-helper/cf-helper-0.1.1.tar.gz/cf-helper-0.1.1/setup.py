8fdb33bf3960184cf06b6a0563badf6852b0a2dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 15:05:31 +0000

    Add package cf-helper/0.1.1/cf-helper-0.1.1.tar.gz

---
+++ packages/cf-helper/cf-helper-0.1.1.tar.gz/cf-helper-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cf-helper",  
+    version="0.1.1",  
+    description="A Python library for interacting with Codeforces API and automating tasks.",
+    long_description=open("README.md").read(),  
+    long_description_content_type="text/markdown",
+    author="Shubham Kumar Verma",
+    author_email="skv.iitroorkee@gmail.com",
+    url="https://github.com/skv-1180/CF-Automate",
+    packages=find_packages(), 
+    install_requires=[
+        "requests", 
+        "beautifulsoup4",
+        "python-dotenv==0.20.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
