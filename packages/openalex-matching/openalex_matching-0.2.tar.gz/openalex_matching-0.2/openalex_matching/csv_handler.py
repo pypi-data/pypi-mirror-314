a5c9be26314911449b18a44c8bf8285d67f6a80d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 06:05:20 +0000

    Add package openalex-matching/0.2/openalex_matching-0.2.tar.gz

---
+++ packages/openalex-matching/openalex_matching-0.2.tar.gz/openalex_matching-0.2/openalex_matching/csv_handler.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+def name_csv_reader(inputFileName, columnName):
+    try:
+        if not inputFileName.lower().endswith(".csv"):
+            raise ValueError("Invalid input file format, only csv files are acceptable")
+        
+        df = pd.read_csv(inputFileName)
+
+        # Check if inputted column exists
+        if columnName in df.columns:
+            namesArray = df[columnName].to_numpy()
+        else:
+            raise ValueError(columnName + "column not found in input CSV file")
+        
+        return namesArray
+    
+    except FileNotFoundError:
+        print("No file found")
+
+def name_csv_writer(outputFileName, dataToWrite):
+
+    if not isinstance(dataToWrite, dict):
+        raise TypeError("Invalid Input, only can write from dictionary inputs")
+    
+    if not outputFileName.lower().endswith(".csv"):
+        raise ValueError("Invalid output file format, only csv files are acceptable")
+    
+    df = pd.DataFrame.from_dict(dataToWrite)
+    df.to_csv(outputFileName, index=False)
+    print("data appended to csv")
+
+
+       
