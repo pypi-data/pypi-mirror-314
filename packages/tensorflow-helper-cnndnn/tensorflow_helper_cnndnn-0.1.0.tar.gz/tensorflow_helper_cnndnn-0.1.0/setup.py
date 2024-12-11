814883305e457c3f7f3b92df10b3ba6f8351c79f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 06:05:24 +0000

    Add package tensorflow-helper-cnndnn/0.1.0/tensorflow_helper_cnndnn-0.1.0.tar.gz

---
+++ packages/tensorflow-helper-cnndnn/tensorflow_helper_cnndnn-0.1.0.tar.gz/tensorflow_helper_cnndnn-0.1.0/setup.py
@@ -0,0 +1,25 @@
+import pathlib
+from setuptools import setup, find_packages
+
+setup(
+    name="tensorflow_helper_cnndnn",
+    version="0.1.0",
+    description="Helper package for TensorFlow CNN and DNN (dummy)",
+    long_description=pathlib.Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/SA-Ahmed-W/tensorflow_helper_cnndnn",
+    author="saaw",
+    author_email="aasimahmedsiddiqui45666@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10,<=3.12",
+    install_requires=[
+        "tensorflow",
+        "scikit-learn",
+        "matplotlib"
+    ]
+)
