464d5dd0a00c2736ea2c7e531f46ab19df669000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 16:06:01 +0000

    Add package BeatPrints/1.0.1/beatprints-1.0.1-py3-none-any.whl

---
+++ packages/BeatPrints/beatprints-1.0.1-py3-none-any.whl/BeatPrints/lyrics.py
@@ -0,0 +1,97 @@
+"""
+Module: lyrics.py
+
+Provide lyrics from LRClib API.
+"""
+
+import re
+from lrclib import LrcLibAPI
+
+from .spotify import TrackMetadata
+from .errors import (
+    NoLyricsAvailable,
+    InvalidFormatError,
+    InvalidSelectionError,
+    LineLimitExceededError,
+)
+
+
+class Lyrics:
+    """
+    This class helps to retrieve lyrics through LRClib API.
+    """
+
+    def get_lyrics(self, metadata: TrackMetadata) -> str:
+        """
+        Retrieve lyrics from LRClib.net for a given track and artist.
+
+        Args:
+            metadata (TrackMetadata): Instance that holds the metadata about a track.
+
+        Returns:
+            str: The lyrics in plain text if found.
+
+        Raises:
+            NoLyricsAvailable: If no lyrics are found for the given track and artist.
+        """
+        user_agent = (
+            "Mozilla/5.0 (X11; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0"
+        )
+
+        # Prepare the API
+        api = LrcLibAPI(user_agent=user_agent)
+        id = api.search_lyrics(track_name=metadata.name, artist_name=metadata.artist)
+
+        # Check if lyrics are available
+        if len(id) != 0:
+            lyrics = api.get_lyrics_by_id(id[0].id)
+            return str(lyrics.plain_lyrics)
+        else:
+            raise NoLyricsAvailable
+
+    def select_lines(self, lyrics: str, selection: str) -> str:
+        """
+        Selects a range of lines from the song lyrics.
+
+        Args:
+            lyrics (str): The lyrics of the song.
+            selection (str): Line range to extract in "start-end" format.
+
+        Returns:
+            str: The selected lines.
+
+        Raises:
+            InvalidFormatError: If the selection format is invalid.
+            InvalidSelectionError: If the selection range is invalid.
+            LineLimitExceededError: If the selected range doesn't contain exactly 4 lines.
+        """
+        lines = [line for line in lyrics.split("\n")]
+        line_count = len(lines)
+
+        try:
+            pattern = r"^\d+-\d+$"
+
+            if not re.match(pattern, selection):
+                raise InvalidFormatError
+
+            selected = [int(num) for num in selection.split("-")]
+
+            if (
+                len(selected) != 2
+                or selected[0] >= selected[1]
+                or selected[0] <= 0
+                or selected[1] > line_count
+            ):
+                raise InvalidSelectionError
+
+            extracted = lines[selected[0] - 1 : selected[1]]
+            selected_lines = [line for line in extracted if line != ""]
+
+            if len(selected_lines) != 4:
+                raise LineLimitExceededError
+
+            quatrain = "\n".join(selected_lines).strip()
+            return quatrain
+
+        except Exception as e:
+            raise e
---
+++ packages/BeatPrints/beatprints-1.0.1-py3-none-any.whl/BeatPrints/spotify.py
@@ -0,0 +1,218 @@
+"""
+Module: spotify.py
+
+Provides functionalities related to interacting with the Spotify API.
+"""
+
+import requests, datetime
+from dataclasses import dataclass
+
+from typing import List
+from .errors import NoMatchingTrackFound, NoMatchingAlbumFound, InvalidSearchLimit
+
+
+@dataclass
+class TrackMetadata:
+    name: str
+    artist: str
+    album: str
+    released: str
+    duration: str
+    image: str
+    label: str
+    id: str
+
+
+@dataclass
+class AlbumMetadata:
+    name: str
+    artist: str
+    released: str
+    image: str
+    label: str
+    id: str
+    tracks: List[str]
+
+
+class Spotify:
+    """
+    Uses Spotify's API to search and retrieve information about a track.
+    """
+
+    def __init__(self, CLIENT_ID: str, CLIENT_SECRET: str) -> None:
+        """
+        Initializes the Spotify client with credentials and obtains an access token.
+
+        Args:
+            CLIENT_ID (str): Spotify API client ID.
+            CLIENT_SECRET (str): Spotify API client secret.
+        """
+        self.__CLIENT_ID = CLIENT_ID
+        self.__CLIENT_SECRET = CLIENT_SECRET
+        self.__BASE_URL = "https://api.spotify.com/v1"
+        self.__authorization_header()
+
+    def __authorization_header(self) -> None:
+        """
+        Constructs the authorization header required for API requests.
+        """
+        endpoint = "https://accounts.spotify.com/api/token"
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        payload = {
+            "grant_type": "client_credentials",
+            "client_id": self.__CLIENT_ID,
+            "client_secret": self.__CLIENT_SECRET,
+        }
+
+        # Requesting token from Spotify API
+        data = requests.post(endpoint, headers=headers, params=payload)
+        token = data.json()["access_token"]
+
+        self.__AUTH_HEADER = {"Authorization": f"Bearer {token}"}
+
+    def _format_release_date(self, release_date: str, precision: str) -> str:
+        """
+        Formats the release date of a track.
+
+        Args:
+            release_date (str): Release date string from Spotify API.
+            precision (str): Precision of the release date
+                ('day', 'month', 'year').
+
+        Returns:
+            str: Formatted release date.
+        """
+        date_format = {"day": "%Y-%m-%d", "month": "%Y-%m", "year": "%Y"}.get(
+            precision, ""
+        )
+        return datetime.datetime.strptime(release_date, date_format).strftime(
+            "%B %d, %Y"
+        )
+
+    def _format_duration(self, duration_ms: int) -> str:
+        """
+        Formats the duration of a track from milliseconds to MM:SS format.
+
+        Args:
+            duration_ms (int): Duration of the track in milliseconds.
+
+        Returns:
+            str: Formatted duration in MM:SS format.
+        """
+        minutes = duration_ms // 60000
+        seconds = (duration_ms // 1000) % 60
+        return f"{minutes:02d}:{seconds:02d}"
+
+    def get_track(self, query: str, limit: int = 6) -> List[TrackMetadata]:
+        """
+        Searches for tracks matching the given query.
+
+        Args:
+            query (str): The search query for the track.
+            limit (int, optional): Maximum number of tracks to retrieve. Defaults to 6.
+
+        Returns:
+            List[TrackMetadata]: A list of the track's metadata.
+
+        Raises:
+            InvalidSearchLimit: If the limit is less than 1.
+            NoMatchingTrackFound: If no matching songs are found.
+        """
+        if limit < 1:
+            raise InvalidSearchLimit
+
+        tracklist = []
+        params = {"q": query, "type": "track", "limit": limit}
+        response = requests.get(
+            f"{self.__BASE_URL}/search", params=params, headers=self.__AUTH_HEADER
+        ).json()
+
+        tracks = response.get("tracks", {}).get("items", [])
+
+        if not tracks:
+            raise NoMatchingTrackFound
+
+        for track in tracks:
+
+            # Retrieve album details
+            id = track["album"]["id"]
+            album_details = requests.get(
+                f"{self.__BASE_URL}/albums/{id}", headers=self.__AUTH_HEADER
+            ).json()
+
+            # Format metadata
+            metadata = {
+                "name": track["name"],
+                "artist": track["artists"][0]["name"],
+                "album": track["album"]["name"],
+                "released": self._format_release_date(
+                    track["album"]["release_date"],
+                    track["album"]["release_date_precision"],
+                ),
+                "duration": self._format_duration(track["duration_ms"]),
+                "image": track["album"]["images"][0]["url"],
+                "label": album_details["label"],
+                "id": track["id"],
+            }
+
+            tracklist.append(TrackMetadata(**metadata))
+
+        return tracklist
+
+    def get_album(self, query: str, limit: int = 6) -> List[AlbumMetadata]:
+        """
+        Retrieves album metadata and track listing.
+
+        Args:
+            query (str): The search query for the album.
+            limit (int, optional): Maximum number of albums to retrieve. Defaults to 6.
+
+        Returns:
+            List[AlbumMetadata]: A list of album metadata, including track listings.
+
+        Raises:
+            InvalidSearchLimit: If the limit is less than 1.
+            NoMatchingAlbumFound: If no matching albums are found.
+        """
+        if limit < 1:
+            raise InvalidSearchLimit
+
+        albumlist = []
+        params = {"q": query, "type": "album", "limit": limit}
+        response = requests.get(
+            f"{self.__BASE_URL}/search", params=params, headers=self.__AUTH_HEADER
+        ).json()
+
+        albums = response.get("albums", {}).get("items", [])
+
+        if not albums:
+            raise NoMatchingAlbumFound
+
+        # Process albums in one loop
+        for album in albums:
+            id = album["id"]
+            album_details = requests.get(
+                f"{self.__BASE_URL}/albums/{id}", headers=self.__AUTH_HEADER
+            ).json()
+
+            # Fetch track names directly from album details
+            tracks = [
+                track["name"]
+                for track in album_details.get("tracks", {}).get("items", [])
+            ]
+
+            metadata = {
+                "name": album["name"],
+                "artist": album["artists"][0]["name"],
+                "released": self._format_release_date(
+                    album["release_date"], album["release_date_precision"]
+                ),
+                "image": album["images"][0]["url"],
+                "label": album_details.get("label", "Unknown"),
+                "id": album["id"],
+                "tracks": tracks,
+            }
+
+            albumlist.append(AlbumMetadata(**metadata))
+
+        return albumlist
