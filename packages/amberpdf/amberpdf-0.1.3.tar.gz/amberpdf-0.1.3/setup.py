797206f880c3c613229e3cca22e3e73003dbde73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-11 04:05:33 +0000

    Add package amberpdf/0.1.3/amberpdf-0.1.3.tar.gz

---
+++ packages/amberpdf/amberpdf-0.1.3.tar.gz/amberpdf-0.1.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="amberpdf",
+    version="0.1.3",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=[
+        "PyMuPDF>=1.18.0",
+        "boto3>=1.26.0",
+        "pandas"
+    ],
+    author="Paulo Suclly",
+    author_email="paulo.suclly@pucp.edu.pe",
+    description="Librería que procesa un PDF mixto (texto e imágenes/tablas) y extrae el contenido en orden",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/amberpdf",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
---
+++ packages/amberpdf/amberpdf-0.1.3.tar.gz/amberpdf-0.1.3/src/amberpdf/__init__.py
@@ -0,0 +1,3 @@
+from .core import credentials, process_pdf
+
+__version__ = "0.1.3"
