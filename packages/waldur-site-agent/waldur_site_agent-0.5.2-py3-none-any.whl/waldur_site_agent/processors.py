af29e2795eed8f4725471586a20568396e57749d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 04:05:51 +0000

    Add package waldur-site-agent/0.5.2/waldur_site_agent-0.5.2-py3-none-any.whl

---
+++ packages/waldur-site-agent/waldur_site_agent-0.5.2-py3-none-any.whl/waldur_site_agent/processors.py
@@ -0,0 +1,36 @@
+"""Module for abstract offering processor."""
+
+import abc
+
+from waldur_client import WaldurClient
+
+from waldur_site_agent import Offering, common_utils
+from waldur_site_agent.backends import BackendType
+from waldur_site_agent.backends.exceptions import BackendError
+
+
+class OfferingBaseProcessor(abc.ABC):
+    """Abstract class for an offering processing."""
+
+    def __init__(self, offering: Offering, user_agent: str = "") -> None:
+        """Constructor."""
+        self.offering: Offering = offering
+        self.waldur_rest_client: WaldurClient = WaldurClient(
+            offering.api_url, offering.api_token, user_agent
+        )
+        self.resource_backend = common_utils.get_backend_for_offering(offering)
+        if self.resource_backend.backend_type == BackendType.UNKNOWN.value:
+            raise BackendError(f"Unable to create backend for {self.offering}")
+
+        self._print_current_user()
+
+        waldur_offering = self.waldur_rest_client._get_offering(self.offering.uuid)
+        common_utils.extend_backend_components(self.offering, waldur_offering["components"])
+
+    def _print_current_user(self) -> None:
+        current_user = self.waldur_rest_client.get_current_user()
+        common_utils.print_current_user(current_user)
+
+    @abc.abstractmethod
+    def process_offering(self) -> None:
+        """Pulls data form Mastermind using REST client and creates objects on the backend."""
