cb0e76938422a2aa2a54b41b13cf1578777c927e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 03:19:21 +0000

    Add package SwingedgeCore/0.3/SwingedgeCore-0.3-py3-none-any.whl

---
+++ packages/SwingedgeCore/SwingedgeCore-0.3-py3-none-any.whl/SwingedgeCore/strategies/DB.py
@@ -0,0 +1,13 @@
+# Class which contains database specific information
+# this class contains methods to actually process the data
+# the sql to be processed will be passed by the calling
+# class/function
+
+class DB:
+    
+    # initialize the database here with data provided in db_creds
+    def __init__(self,db_creds):
+        pass
+    
+    def getData(sql):
+        pass
