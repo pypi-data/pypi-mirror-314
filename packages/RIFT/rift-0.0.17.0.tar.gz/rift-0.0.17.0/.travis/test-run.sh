745c55e966b89f1629c49e7dabfb31112d6bd48d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 05:06:17 +0000

    Add package RIFT/0.0.17.0/rift-0.0.17.0.tar.gz

---
+++ packages/RIFT/rift-0.0.17.0.tar.gz/rift-0.0.17.0/.travis/test-run.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# GETTING_STARTED.md example
+
+git  clone https://github.com/oshaughn/ILE-GPU-Paper.git
+cd ILE-GPU-Paper/demos/
+make test_workflow_batch_gpu_lowlatency
+cd test_workflow_batch_gpu_lowlatency
+# force standard code path
+switcheroo '--maximize-only '  ' --force-xpy ' command-single.sh
+# Reduce number of analyses for this worker to 1, to reduce runtime
+switcheroo '\$\(macrongroup\)' 1  command-single.sh
+# new format for n-events-to-analyze.  Backstop
+alias macrongroup='echo 1'
+echo 'echo 1' > macrongroup; chmod a+x macrongroup; PATH=${PATH}:`pwd`
+# Reduce the number of points investigated by x100
+switcheroo 'n-max 2000000' 'n-max 50000' command-single.sh
+./command-single.sh
