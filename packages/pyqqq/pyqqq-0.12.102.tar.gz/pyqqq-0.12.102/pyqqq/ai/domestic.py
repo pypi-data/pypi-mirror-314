a604f58859e3c3a56770aee449f490c72db729be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 15:07:41 +0000

    Add package pyqqq/0.12.102/pyqqq-0.12.102.tar.gz

---
+++ packages/pyqqq/pyqqq-0.12.102.tar.gz/pyqqq-0.12.102/pyqqq/ai/domestic.py
@@ -0,0 +1,46 @@
+import datetime
+import pandas
+from typing import List, Optional
+import pyqqq.data.domestic as domestic
+
+
+def get_tickers(date: Optional[datetime.date] = None, market: Optional[str] = None) -> pandas.DataFrame:
+    if isinstance(date, str):
+        date = datetime.date.fromisoformat(date)
+
+    df = domestic.get_tickers(date, market)
+    if df.empty:
+        if date:
+            assert False, f"No tickers found for {date}"
+        else:
+            assert False, "No tickers found"
+    else:
+        return df
+
+
+def get_market_cap(date: datetime.date = None) -> pandas.DataFrame:
+    if isinstance(date, str):
+        date = datetime.date.fromisoformat(date)
+
+    df = domestic.get_market_cap(date)
+    if df.empty:
+        if date:
+            assert False, f"No market cap data found for {date}"
+        else:
+            assert False, "No market cap data found"
+    else:
+        return df.rename(columns={"value": "market_cap", "shares": "outstanding_shares"}, inplace=True)
+
+
+def get_market_cap_by_codes(codes: List[str], date: datetime.date = None) -> pandas.DataFrame:
+    if isinstance(date, str):
+        date = datetime.date.fromisoformat(date)
+
+    df = domestic.get_market_cap_by_codes(codes, date)
+    if df.empty:
+        if date:
+            assert False, f"No market cap data found for {date}"
+        else:
+            assert False, "No market cap data found"
+    else:
+        return df.rename(columns={"value": "market_cap", "shares": "outstanding_shares"}, inplace=True)
