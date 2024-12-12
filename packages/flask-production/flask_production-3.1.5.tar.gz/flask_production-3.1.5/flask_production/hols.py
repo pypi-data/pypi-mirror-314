b9dec7e9229e662f07625c6a53521e1845af00a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 06:05:55 +0000

    Add package flask-production/3.1.5/flask_production-3.1.5.tar.gz

---
+++ packages/flask-production/flask_production-3.1.5.tar.gz/flask_production-3.1.5/flask_production/hols.py
@@ -0,0 +1,29 @@
+import holidays
+from datetime import date
+from dateutil.relativedelta import relativedelta, MO#, FR
+# from dateutil.easter import easter
+
+
+class TradingHolidays(holidays.countries.UnitedStates):
+	def _populate(self, year):
+		# Populate the holiday list with the default US holidays
+		holidays.UnitedStates._populate(self, year)
+		# Remove Columbus Day
+		# Edit: in holidays==0.61, Columbus day was removed as a US countrywide holiday
+		# 	- https://github.com/vacanza/holidays/pull/2106
+		columbus_day = date(year, 10, 1) + relativedelta(weekday=MO(+2))
+		if columbus_day in self:
+			self.pop(columbus_day, None)
+
+		# Remove Veterans Day
+		self.pop(date(year, 11, 11), None)
+		if year==2023:
+			self.pop(date(year, 11, 10), None)
+
+		# # Add Good Friday # NOTE: removed this since Good Friday is not a bond market holiday - [04/01/2021]
+		# self[easter(year) + relativedelta(weekday=FR(-1))] = "Good Friday"
+
+		# 2021-12-31 is not a holiday apparently :( and windows version seems to think it is
+		nye_2021 = date(2021, 12, 31)
+		if nye_2021 in self:
+			self.pop(nye_2021, None)
