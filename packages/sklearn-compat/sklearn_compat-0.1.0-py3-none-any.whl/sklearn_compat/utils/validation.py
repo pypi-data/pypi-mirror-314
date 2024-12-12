1a71a74eed4e964cdea565279b9a8d51e8d447ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 05:05:30 +0000

    Add package sklearn-compat/0.1.0/sklearn_compat-0.1.0-py3-none-any.whl

---
+++ packages/sklearn-compat/sklearn_compat-0.1.0-py3-none-any.whl/sklearn_compat/utils/validation.py
@@ -0,0 +1,7 @@
+from sklearn_compat._sklearn_compat import (
+    _check_feature_names,  # noqa: F401
+    _check_n_features,  # noqa: F401
+    _is_fitted,  # noqa: F401
+    _to_object_array,  # noqa: F401
+    validate_data,  # noqa: F401
+)
