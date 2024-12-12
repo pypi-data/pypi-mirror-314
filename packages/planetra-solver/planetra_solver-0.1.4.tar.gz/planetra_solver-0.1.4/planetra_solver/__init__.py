c312c505fcd4f83fa764e30c335d19e4a9ff9e92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 03:18:53 +0000

    Add package planetra-solver/0.1.4/planetra_solver-0.1.4.tar.gz

---
+++ packages/planetra-solver/planetra_solver-0.1.4.tar.gz/planetra_solver-0.1.4/planetra_solver/__init__.py
@@ -0,0 +1,23 @@
+from .api.ks_api_classes import *
+from .api.ks_api_models import *
+from .api.ks_api_objects import *
+
+__all__ = [
+    'create_object',
+    'create_objects',
+    'delete_object',
+    'delete_objects',
+    'rename_object',
+    'rename_objects',
+
+    'create_model',
+    'delete_model',
+    'rename_model',
+
+    'create_class',
+    'create_classes',
+    'delete_class',
+    'update_class',
+    'update_class_policy',
+    'get_class_tree',
+]
