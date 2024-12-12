39869c5f4608fc07bc7d5bc18b903eed64a99534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 03:18:30 +0000

    Add package helical/0.0.1a12/helical-0.0.1a12.tar.gz

---
+++ packages/helical/helical-0.0.1a12.tar.gz/helical-0.0.1a12/examples/run_models/run_hyena_dna.py
@@ -0,0 +1,16 @@
+from helical.models.hyena_dna.model import HyenaDNA, HyenaDNAConfig
+import hydra
+from omegaconf import DictConfig
+
+@hydra.main(version_base=None, config_path="configs", config_name="hyena_dna_config")
+def run(cfg: DictConfig):
+
+    hyena_config = HyenaDNAConfig(**cfg)
+    model = HyenaDNA(configurer = hyena_config)   
+    sequence = ['ACTG' * int(1024/4)]
+    tokenized_sequence = model.process_data(sequence)
+    embeddings = model.get_embeddings(tokenized_sequence)
+    print(embeddings.shape)
+
+if __name__ == "__main__":
+    run()
