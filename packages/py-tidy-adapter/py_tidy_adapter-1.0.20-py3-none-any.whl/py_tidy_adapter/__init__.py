4f7c317395b7c5c077d13cdf6b77e74762fd6604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 03:18:24 +0000

    Add package py-tidy-adapter/1.0.20/py_tidy_adapter-1.0.20-py3-none-any.whl

---
+++ packages/py-tidy-adapter/py_tidy_adapter-1.0.20-py3-none-any.whl/py_tidy_adapter/__init__.py
@@ -0,0 +1,6 @@
+from py_tidy_adapter.adapter import Adapter
+from py_tidy_adapter.tidy import *
+from py_tidy_adapter.document import Document
+from py_tidy_adapter.exception import TidyException
+from py_tidy_adapter.bsc_elements import Query,Updater,Component,Key,Xslt,Validator
+from py_tidy_adapter.class_business import ClassBusiness
