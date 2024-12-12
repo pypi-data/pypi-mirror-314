52676f5c5314916f6aac39c9a62c0b1c5824bd37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 03:18:54 +0000

    Add package cloxz/0.5.0/cloxz-0.5.0-py3-none-any.whl

---
+++ packages/cloxz/cloxz-0.5.0-py3-none-any.whl/clock/main.py
@@ -0,0 +1,260 @@
+import os
+import pathlib
+import pkg_resources
+import subprocess
+import tempfile
+import typer
+import typer.completion
+from datetime import datetime
+from rich import print
+from rich.table import Table
+from typing import Annotated, Optional
+from .local_db import LocalDatabase
+from .utils import (
+    add_entry,
+    ClockStatus,
+    create_directories,
+    create_file,
+    find_status_by_date,
+    get_rows,
+    get_sum,
+    validate_month,
+    get_table_name,
+)
+from statistics import median
+
+CONFIG_DIR = pathlib.Path.home() / ".config/clockz"
+DATA_DIR = CONFIG_DIR / "data"
+CSV_FILE = f"{datetime.now().strftime('%B')}.csv"
+CSV_FILE_PATH = DATA_DIR / CSV_FILE
+DEFAULT_TABLE_NAME = (
+    f'data_{datetime.now().strftime("%Y")}_{datetime.now().strftime("%m")}'
+)
+
+
+app = typer.Typer(name="cxz")
+config_app = typer.Typer(name="config", help="Configuration reletad commands.")
+app.add_typer(config_app)
+
+
+# TODO: Support time and date input / picker
+@app.command(name="in")
+def clock_in(customer: str = typer.Argument(None)):
+    """Clock in for the day."""
+    add_entry(customer, "in", CONFIG_DIR, DEFAULT_TABLE_NAME)
+
+
+# TODO: Support time and date input / picker
+@app.command(name="out")
+def clock_out(customer: str = typer.Argument(None)):
+    """Clock out for the day."""
+    add_entry(customer, "out", CONFIG_DIR, DEFAULT_TABLE_NAME)
+
+
+@app.command(name="task")
+def clock_task(customer: str = typer.Argument(None)):
+    """Mark a task in the timetable."""
+    add_entry(customer, "task", CONFIG_DIR, DEFAULT_TABLE_NAME)
+
+
+@app.command(name="show")
+def clock_show(
+    month: str = typer.Option(str(datetime.now().strftime("%m"))),
+    year: str = typer.Option(str(datetime.now().strftime("%Y"))),
+):
+    """Display clock-in/clock-out records."""
+    table_name = get_table_name(month, year)
+    try:
+        print(get_rows(CONFIG_DIR, table_name))
+    except Exception:
+        _date = table_name.lstrip("data_").split("_")
+        print(f"Failed to retrieve data for {_date[1]}.{_date[0]}")
+
+
+@app.command(name="sum")
+def clock_sum(
+    customer: str = None,
+    month: str = typer.Option(str(datetime.now().strftime("%m"))),
+    year: str = typer.Option(str(datetime.now().strftime("%Y"))),
+):
+    """Summerize clocked time for a customer"""
+    print(
+        get_sum(
+            customer=customer or typer.prompt("Customer"),
+            config_dir=CONFIG_DIR,
+            table_name=get_table_name(month, year),
+        )
+    )
+
+
+@config_app.command("dir")
+def config_dir_command():
+    """
+    Print the DATA directory path.
+    """
+    print(DATA_DIR)
+
+
+@config_app.command("show-tables")
+def show_tables():
+    """
+    List all tables in the database.
+    """
+    with LocalDatabase.Database(database_file=f"{CONFIG_DIR}/database.db") as db:
+        table = Table()
+        table.add_column("Table")
+        for row in db.get_all_tables():
+            table.add_row(row[0])
+        print(table)
+
+
+@config_app.command("create-db")
+def create_db():
+    """Create the local database file."""
+    with LocalDatabase.Database(database_file=f"{CONFIG_DIR}/database.db") as db:
+        db.create_database()
+
+
+@config_app.command("create-table")
+def create_db_table(
+    month: Annotated[str, typer.Option(..., prompt=True)] = str(
+        datetime.now().strftime("%m")
+    ),
+    year: Annotated[str, typer.Option(..., prompt=True)] = str(
+        datetime.now().strftime("%Y")
+    ),
+):
+    """Create a new database table."""
+    with LocalDatabase.Database(database_file=f"{CONFIG_DIR}/database.db") as db:
+        if not db.create_table(
+            table_name=f"data_{year}_{month}",
+            columns=["date TEXT", "time TEXT", "action TEXT", "customer TEXT"],
+        ):
+            print("[red]Database table failed to create[/red]")
+
+
+@config_app.command()
+def drop_table(month: str, year: str):
+    """Erase all months' records."""
+    table_name = f"data_{year}_{month}"
+    with LocalDatabase.Database(database_file=f"{CONFIG_DIR}/database.db") as db:
+        typer.confirm(
+            f"You sure you want to delete all entries for the month {month}.{year}?",
+            abort=True,
+        )
+        if db.delete_table(table_name):
+            print(f"[green]Table {table_name} dropped[/green]")
+        else:
+            print(f"[red]Could not drop table [{table_name}][/red]")
+
+
+@app.command()
+def delete():
+    """Delete a specific clock-in/clock-out record."""
+    print(get_rows(CONFIG_DIR, DEFAULT_TABLE_NAME, True))
+
+    entries = []
+    with LocalDatabase.Database(database_file=f"{CONFIG_DIR}/database.db") as db:
+        reader = db.read_all_rows(DEFAULT_TABLE_NAME)
+        for i, row in enumerate(reader, start=1):
+            entries.append((i,) + row)
+
+        line_number = typer.prompt(
+            "\nEnter the line number of the entry you want to delete", type=int
+        )
+
+        for entry in entries:
+            if entry[0] == line_number:
+                _, date, time, action, customer = entry
+
+        typer.confirm("Are you sure you want to delete?", abort=True)
+
+        db.delete_row(
+            DEFAULT_TABLE_NAME,
+            f"date = '{date}' AND time = '{time}' AND action = '{action}' AND customer = '{customer}'",
+        )
+
+
+@app.command()
+def status():
+    """Display the clock-in/out status for today."""
+    date = datetime.now().strftime("%Y-%m-%d")
+    match find_status_by_date(date, CONFIG_DIR, DEFAULT_TABLE_NAME):
+        case ClockStatus.NONE:
+            print("No clocking entry found for today")
+        case ClockStatus.IN:
+            print("Found a clock-in entry for today")
+        case ClockStatus.OUT:
+            print("found clock-out entry for today")
+
+
+@app.command("edit")
+def edit_table(
+    month: Annotated[str, typer.Option(..., prompt=True)] = str(
+        datetime.now().strftime("%m")
+    ),
+    year: Annotated[str, typer.Option(..., prompt=True)] = str(
+        datetime.now().strftime("%Y")
+    ),
+    editor: Annotated[str, typer.Option(..., prompt=True)] = None,
+):
+    """Edit a database's table."""
+    table_name = f"data_{year}_{month}"
+
+    with LocalDatabase.Database(database_file=f"{CONFIG_DIR}/database.db") as db:
+        with tempfile.NamedTemporaryFile(mode="w", delete=False) as temp_file:
+            reader = db.read_all_rows(table_name)
+            for row in reader:
+                temp_file.write("\t".join(row) + "\n")
+
+        if editor == None:
+            editor = os.environ.get("CXZ_EDITOR", "nano")
+
+        match editor:
+            case "code":
+                subprocess.Popen([editor, temp_file.name])
+                subprocess.call([editor, "--wait", temp_file.name])
+            case _:
+                os.system(f"{editor} {temp_file.name}")
+
+        with open(temp_file.name, "r") as updated_file:
+            updated_rows = [
+                line.strip().split("\t") for line in updated_file.readlines()
+            ]
+
+        db.delete_table(table_name)
+        db.create_table(
+            table_name, ["date TEXT", "time TEXT", "action TEXT", "customer TEXT"]
+        )
+        for row in updated_rows:
+            db.insert_row(table_name, row)
+
+        print(f"[green]Table {table_name} updated[/green]")
+
+
+def _version_callback(value: bool) -> None:
+    if value:
+        print(pkg_resources.get_distribution("cloxz").version)
+        raise typer.Exit()
+
+
+@app.callback()
+def main(
+    version: Optional[bool] = typer.Option(
+        None,
+        "--version",
+        "-v",
+        help="Show the app's version.",
+        callback=_version_callback,
+        is_eager=True,
+    )
+) -> None:
+    create_directories(CONFIG_DIR, DATA_DIR)
+    if not os.path.exists(CSV_FILE_PATH):
+        create_file(CSV_FILE_PATH)
+    create_db()
+    create_db_table()
+
+
+if __name__ == "__main__":
+    app()
---
+++ packages/cloxz/cloxz-0.5.0-py3-none-any.whl/clock/utils.py
@@ -0,0 +1,171 @@
+from datetime import datetime, timedelta
+import calendar
+import os
+import typer
+from datetime import datetime
+from enum import Enum
+from rich.table import Table
+from statistics import median
+from .local_db import LocalDatabase
+
+
+class ClockStatus(Enum):
+    NONE = 0
+    IN = 1
+    OUT = 2
+
+
+def create_directories(config_dir: str, data_dir: str):
+    """Create the necessary directories if they don't exist."""
+    if not os.path.exists(config_dir):
+        os.makedirs(config_dir)
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+
+
+def create_file(filename: str):
+    if not os.path.exists(filename):
+        try:
+            with open(filename, "w+"):
+                pass
+        except:
+            print("Failed to create clock file")
+            return
+        print(f"Created {filename}")
+    else:
+        print(f"{filename} already exists.")
+
+
+def validate_month(month: str) -> int:
+    if month.lower() == "current":
+        return datetime.now().month
+    try:
+        if month.startswith("-"):
+            month = datetime.now().month - int(month[1:])
+
+        month_num = int(month)
+        if 1 <= month_num <= 12:
+            return month_num
+        else:
+            print(f"Invalid month number: {month}")
+            raise typer.Exit(code=1)
+    except ValueError:
+        month_num = list(calendar.month_name).index(month.capitalize())
+        if month_num > 0:
+            return month_num
+        else:
+            print(f"Invalid month name: {month}")
+            raise typer.Exit(code=1)
+
+
+def add_entry(customer: str, action: str, config_dir: str, table_name: str):
+    if customer is None:
+        customer = typer.prompt("Customer")
+    with LocalDatabase.Database(database_file=f"{config_dir}/database.db") as db:
+        db.insert_row(
+            table_name,
+            (
+                str(datetime.now().strftime("%Y-%m-%d")),
+                str(datetime.now().strftime("%H:%M")),
+                action,
+                customer,
+            ),
+        )
+
+
+def get_rows(config_dir: str, table_name: str, print_line_num: bool = False):
+    with LocalDatabase.Database(database_file=f"{config_dir}/database.db") as db:
+        table = Table()
+        if print_line_num:
+            table.add_column("")
+        table.add_column("Date")
+        table.add_column("Time")
+        table.add_column("Action")
+        table.add_column("Customer")
+        for i, row in enumerate(db.read_all_rows(table_name), start=1):
+            id_, timestamp, action, customer = row
+            match action:
+                case "in":
+                    action = "[green]in[/green]"
+                case "out":
+                    action = "[red]out[/red]"
+                case "task":
+                    action = "[blue]task[/blue]"
+            if print_line_num:
+                table.add_row(str(i), str(id_), timestamp, action, customer)
+            else:
+                table.add_row(str(id_), timestamp, action, customer)
+        return table
+
+
+def find_status_by_date(date: str, config_dir: str, table_name: str) -> ClockStatus:
+    with LocalDatabase.Database(database_file=f"{config_dir}/database.db") as db:
+        entries = db.read_all_rows(table_name)
+
+    status = ClockStatus.NONE
+
+    for row in entries:
+        if row[0] == date:
+            if row[2] == "out":
+                status = ClockStatus.OUT
+            elif row[2] == "in" and status != ClockStatus.OUT:
+                status = ClockStatus.IN
+    return status
+
+
+def get_sum(customer: str, config_dir: str, table_name: str) -> str:
+    with LocalDatabase.Database(database_file=f"{config_dir}/database.db") as db:
+        # Check if the number of clock-ins and clock-outs are equal
+        check_query = """
+            SELECT
+                COUNT(CASE WHEN "action" = 'in' THEN 1 END) AS total_in,
+                COUNT(CASE WHEN "action" = 'out' THEN 1 END) AS total_out
+            FROM
+                {table_name}
+            WHERE
+                "customer" = '{customer}';
+        """
+        check_query = check_query.format(table_name=table_name, customer=customer)
+        check_result = db.execute_query(check_query)
+        if check_result:
+            total_in = check_result[0][0]
+            total_out = check_result[0][1]
+            if total_in != total_out:
+                return "Error: Unequal number of clock-ins and clock-outs"
+
+        # Calculate the total time
+        # FIXME: Needs refactoring, does not calculate time over 24 hours well
+        query = """
+            SELECT
+                "customer",
+                SUM(CASE WHEN "action" = 'in' THEN CAST(SUBSTR("time", 1, 2) AS INTEGER) * 60 + CAST(SUBSTR("time", 4, 2) AS INTEGER) ELSE 0 END) AS total_in_minutes,
+                SUM(CASE WHEN "action" = 'out' THEN CAST(SUBSTR("time", 1, 2) AS INTEGER) * 60 + CAST(SUBSTR("time", 4, 2) AS INTEGER) ELSE 0 END) AS total_out_minutes
+            FROM
+                {table_name}
+            WHERE
+                "customer" = '{customer}'
+            GROUP BY
+                "customer";
+        """
+        query = query.format(table_name=table_name, customer=customer)
+        result = db.execute_query(query)
+        if result:
+            total_in_minutes = result[0][1]
+            total_out_minutes = result[0][2]
+            total_time_minutes = total_out_minutes - total_in_minutes
+            hours = int(total_time_minutes // 60)
+            minutes = int(total_time_minutes % 60)
+            return f"{hours}:{minutes:02d}"
+        else:
+            return "0:00"
+
+
+def get_table_name(month: str | int, year: str | int) -> str:
+    valid_month = validate_month(month)
+    _month = (
+        valid_month
+        if valid_month == median([1, 12, valid_month])
+        else datetime.now().strftime("%m")
+    )
+    _year = year if year not in (None, "") else datetime.now().strftime("%Y")
+    return f"data_{_year}_{_month}"
