3e21fb28cdb71ab40ca30b1a87f02889163f2896
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 17:06:02 +0000

    Add package polars-ta/0.4.2/polars_ta-0.4.2.tar.gz

---
+++ packages/polars-ta/polars_ta-0.4.2.tar.gz/polars_ta-0.4.2/polars_ta/tdx/pressure_support.py
@@ -0,0 +1,18 @@
+from polars import Expr
+
+from polars_ta.tdx.reference import MA
+from polars_ta.tdx.statistic import STDP
+
+
+def BOLL(close: Expr, M: int = 20, N: int = 2) -> Expr:
+    """
+    BOLL:MA(CLOSE,M);
+    UB:BOLL+2*STD(CLOSE,M);
+    LB:BOLL-2*STD(CLOSE,M);
+    """
+    ma = MA(close, M)
+    # it should be total standard deviation, the value is smaller than sample standard deviation.
+    # 这里是总体标准差，值比样本标准差小。部分软件使用样本标准差是错误的，
+    _std = STDP(close, M)
+    UB = ma + _std * N
+    return UB
