9fa3f6585d573982479065dd6bb6ab5bb538cb4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 16:06:24 +0000

    Add package base-aux/0.1.9/base_aux-0.1.9-py3-none-any.whl

---
+++ packages/base-aux/base_aux-0.1.9-py3-none-any.whl/base_aux/attrs/__init__.py
@@ -0,0 +1,46 @@
+# =====================================================================================================================
+from .annot_1_aux import (
+    AnnotAux,
+)
+from .annot_2_required import (
+    AnnotRequired,
+)
+from .annot_3_iter_values import (
+    AnnotValuesIter,
+)
+from .annot_4_cls_keys_as_values import (
+    AnnotClsKeysAsValues_Meta,
+    AnnotClsKeysAsValues,
+)
+
+
+# =====================================================================================================================
+from .attr_0_init_kwargs import (
+    AttrInitKwargs,
+)
+from .attr_1_aux import (
+    AttrAux,
+)
+from .attr_2_anycase import (
+    AttrAnycase,
+)
+from .attr_3_lambda_call import (
+    AttrLambdaCall,
+)
+
+
+# =====================================================================================================================
+from .getattr_0_echo import (
+    GetattrEcho,
+    GetattrEchoSpace,
+)
+from .getattr_1_prefix_1_inst import (
+    GetattrPrefixInst,
+    GetattrPrefixInst_RaiseIf,
+)
+from .getattr_1_prefix_2_cls import (
+    GetattrPrefixCls_MetaTemplate
+)
+
+
+# =====================================================================================================================
