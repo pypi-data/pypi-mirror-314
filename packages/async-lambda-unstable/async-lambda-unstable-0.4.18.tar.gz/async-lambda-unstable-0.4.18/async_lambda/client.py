c0be429380a129ff32edff2e849a02efa97e372c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 07:05:27 +0000

    Add package async-lambda-unstable/0.4.18/async-lambda-unstable-0.4.18.tar.gz

---
+++ packages/async-lambda-unstable/async-lambda-unstable-0.4.18.tar.gz/async-lambda-unstable-0.4.18/async_lambda/client.py
@@ -0,0 +1,35 @@
+import os
+from typing import Any, Optional
+
+import boto3
+
+
+class Clients:
+    s3_client: Optional[Any] = None
+    sqs_client: Optional[Any] = None
+
+    def reset(self):
+        self.s3_client = None
+        self.sqs_client = None
+
+
+clients = Clients()
+
+
+def get_client_kwargs() -> dict:
+    _kwargs = {}
+    if os.environ.get("MOTO_ENDPOINT_URL"):
+        _kwargs["endpoint_url"] = os.environ["MOTO_ENDPOINT_URL"]
+    return _kwargs
+
+
+def get_s3_client():
+    if clients.s3_client is None:
+        clients.s3_client = boto3.client("s3", **get_client_kwargs())
+    return clients.s3_client
+
+
+def get_sqs_client():
+    if clients.sqs_client is None:
+        clients.sqs_client = boto3.client("sqs", **get_client_kwargs())
+    return clients.sqs_client
