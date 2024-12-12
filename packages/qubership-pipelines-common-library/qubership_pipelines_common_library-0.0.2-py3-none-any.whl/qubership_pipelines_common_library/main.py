afc04a6c487f0faf0c1226af3a8efa048325d799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 17:05:57 +0000

    Add package qubership-pipelines-common-library/0.0.2/qubership_pipelines_common_library-0.0.2-py3-none-any.whl

---
+++ packages/qubership-pipelines-common-library/qubership_pipelines_common_library-0.0.2-py3-none-any.whl/qubership_pipelines_common_library/main.py
@@ -0,0 +1,50 @@
+# Copyright 2024 NetCracker Technology Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os, sys, logging, click, urllib3
+
+from v1.execution.exec_logger import ExecutionLogger
+from operations.git.get_file import GetFile
+from v1.utils.utils_file import UtilsFile
+from v1.utils.utils_context import init_context
+
+DEFAULT_CONTEXT_FILE_PATH = 'context.yaml'
+
+
+@click.group(chain=True)
+def cli():
+    logging.basicConfig(stream=sys.stdout, format=ExecutionLogger.DEFAULT_FORMAT, level=logging.INFO)
+    # add this folder to path
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    sys.path.insert(0, current_path)
+    logging.info(f"Current path: {current_path}")
+    # suppress InsecureRequestWarning: Unverified HTTPS request is being made to host
+    urllib3.disable_warnings()
+
+
+@cli.command("init_context")
+@click.option('--context_path', required=False, default=DEFAULT_CONTEXT_FILE_PATH, type=str, help="Path to context")
+def init_context_cmd(context_path):
+    logging.info("Default context.yaml creation starting")
+    init_context(context_path)
+    context = UtilsFile.read_yaml(context_path)
+    logging.info(context)
+
+
+@cli.command("get_file")
+@click.option('--context_path', required=True, default=DEFAULT_CONTEXT_FILE_PATH, type=str, help="Path to context")
+def _get_file(context_path):
+    logging.info("CLI get_file operation calling for %s", context_path)
+    command = GetFile(context_path)
+    command.run()
