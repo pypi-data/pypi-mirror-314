90909b6afd16d070313267a490c2ecf5bae0b8ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 14:05:56 +0000

    Add package accurate-timed-loop/0.1.0/accurate_timed_loop-0.1.0.tar.gz

---
+++ packages/accurate-timed-loop/accurate_timed_loop-0.1.0.tar.gz/accurate_timed_loop-0.1.0/src/accurate_timed_loop/build_info.py
@@ -0,0 +1,10 @@
+class BuildInfo:  # pylint: disable=too-few-public-methods
+    python_version = '3.10.15'
+    os_name = 'posix:ubuntu'
+    version = '0.1.0'
+    git_sha = '42e594dcc913b7fc6ff220efc655a1d6883cec7b'
+    git_branch = 'master'
+    git_uncommitted = [
+    ]
+    git_unpushed = [
+    ]
