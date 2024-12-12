01cd691af4bac45b4d80c30f63da68c9536051d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 06:06:04 +0000

    Add package hubmap-anndata-validator/0.1.0/hubmap_anndata_validator-0.1.0.tar.gz

---
+++ packages/hubmap-anndata-validator/hubmap_anndata_validator-0.1.0.tar.gz/hubmap_anndata_validator-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="hubmap-anndata-validator",
+    version="0.1.0",
+    description="A package to validate AnnData objects for HuBMAP EPICs.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Penny Cuda",
+    author_email="pcuda@andrew.cmu.edu",
+    license="GPLv3",
+    packages=find_packages(),
+    install_requires=[
+        "anndata",
+    ],
+    url="https://github.com/hubmapconsortium/anndata-validator",
+    python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
