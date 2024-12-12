820b00c26ef13263fa0bf35bdaaadca000b1bc32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 14:06:22 +0000

    Add package bitstory/0.1.4/bitstory-0.1.4-py3-none-any.whl

---
+++ packages/bitstory/bitstory-0.1.4-py3-none-any.whl/bitstory/__init__.py
@@ -0,0 +1,2 @@
+# bitstory/__init__.py
+from .version import __version__, print_version
---
+++ packages/bitstory/bitstory-0.1.4-py3-none-any.whl/bitstory/version.py
@@ -0,0 +1,5 @@
+# bitstory/version.py
+__version__ = "0.1.4"
+
+def print_version():
+    print(f"bitstory version: {__version__}")
