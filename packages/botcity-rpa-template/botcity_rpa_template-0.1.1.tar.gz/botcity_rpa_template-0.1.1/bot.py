0d03999e7efa89711881a809446d203e2a2f11e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 07:05:19 +0000

    Add package botcity-rpa-template/0.1.1/botcity_rpa_template-0.1.1.tar.gz

---
+++ packages/botcity-rpa-template/botcity_rpa_template-0.1.1.tar.gz/botcity_rpa_template-0.1.1/bot.py
@@ -0,0 +1,59 @@
+import argparse
+
+from decouple import config
+
+from botcity_aux.bot_runner import BotRunnerLocal, BotRunnerMaestro
+
+
+def parse_args() -> argparse.Namespace:
+    """
+    Configure command-line arguments.
+
+    Returns:
+        argparse.Namespace: Configured arguments.
+    """
+    parser = argparse.ArgumentParser(description="Bot Runner Configuration")
+    parser.add_argument(
+        "--environment",
+        type=str,
+        choices=["maestro", "local"],
+        default="maestro",  # Set 'maestro' as the default
+        help="Defines the execution environment: 'maestro' or 'local' (default: 'maestro')",
+    )
+    args, _ = parser.parse_known_args()  # Get only required arguments
+
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    BOT_NAME = "My Bot"
+    TELEGRAM_GROUP = "My Group"
+
+    if args.environment == "maestro":
+        bot_runner = BotRunnerMaestro(
+            bot_name=BOT_NAME,
+            use_telegram=True,
+            telegram_group=TELEGRAM_GROUP,
+        )
+    else:
+        server = config("SERVER_MAESTRO", cast=str)
+        login = config("LOGIN_MAESTRO", cast=str)
+        key = config("KEY_MAESTRO", cast=str)
+
+        if not server or not login or not key:
+            raise ValueError(
+                "SERVER_MAESTRO, LOGIN_MAESTRO, and KEY_MAESTRO must be provided in the .env file"
+            )
+
+        bot_runner = BotRunnerLocal(
+            bot_name=BOT_NAME,
+            server=server,
+            login=login,
+            key=key,
+            use_telegram=True,
+            telegram_group=TELEGRAM_GROUP,
+        )
+
+    bot_runner.run()
---
+++ packages/botcity-rpa-template/botcity_rpa_template-0.1.1.tar.gz/botcity_rpa_template-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='botcity-rpa-template',  # Name of your package
+    version='0.1.1',    # Version of your package
+    packages=find_packages(),  # This will automatically find all packages in the directory
+    install_requires=[   # List any dependencies here
+        'some_dependency', 
+    ],
+    entry_points={  # This allows setting an entry point (if needed)
+        'console_scripts': [
+            'my-bot=bot:main',  # You can point this to the main function in bot.py
+        ],
+    },
+    description='My bot project description',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Your Name',
+    author_email='your_email@example.com',
+    url='https://github.com/jpzinn654/my-package',  # Link to your project repository
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
