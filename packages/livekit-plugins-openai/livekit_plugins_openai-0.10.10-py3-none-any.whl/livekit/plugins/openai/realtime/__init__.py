5f48e4ed0808e75b89ba9eb835dd477abd998c75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 16:06:14 +0000

    Add package livekit-plugins-openai/0.10.10/livekit_plugins_openai-0.10.10-py3-none-any.whl

---
+++ packages/livekit-plugins-openai/livekit_plugins_openai-0.10.10-py3-none-any.whl/livekit/plugins/openai/realtime/__init__.py
@@ -0,0 +1,35 @@
+from . import api_proto
+from .realtime_model import (
+    DEFAULT_INPUT_AUDIO_TRANSCRIPTION,
+    DEFAULT_SERVER_VAD_OPTIONS,
+    InputTranscriptionCompleted,
+    InputTranscriptionFailed,
+    InputTranscriptionOptions,
+    RealtimeContent,
+    RealtimeError,
+    RealtimeModel,
+    RealtimeOutput,
+    RealtimeResponse,
+    RealtimeSession,
+    RealtimeToolCall,
+    ServerVadOptions,
+)
+
+__all__ = [
+    "InputTranscriptionCompleted",
+    "InputTranscriptionFailed",
+    "RealtimeContent",
+    "RealtimeOutput",
+    "RealtimeResponse",
+    "RealtimeToolCall",
+    "RealtimeSession",
+    "RealtimeModel",
+    "RealtimeError",
+    "ServerVadOptions",
+    "InputTranscriptionOptions",
+    "ConversationItemCreated",
+    "ConversationItemDeleted",
+    "api_proto",
+    "DEFAULT_INPUT_AUDIO_TRANSCRIPTION",
+    "DEFAULT_SERVER_VAD_OPTIONS",
+]
