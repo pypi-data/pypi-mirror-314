18a67796e88b3ec283aca356f8e8522d539e3b1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 03:18:47 +0000

    Add package rekuest-next/0.2.59/rekuest_next-0.2.59.tar.gz

---
+++ packages/rekuest-next/rekuest_next-0.2.59.tar.gz/rekuest_next-0.2.59/rekuest_next/__init__.py
@@ -0,0 +1,32 @@
+__version__ = "0.1.1"
+
+from .utils import (
+    acall,
+    afind,
+    areserve,
+    reserved,
+    call,
+    aiterate,
+    iterate,
+    call_raw,
+    acall_raw,
+)
+from .structures.model import model
+
+
+__all__ = [
+    "acall",
+    "afind",
+    "areserve",
+    "reserved",
+    "call",
+    "find",
+    "reserve",
+    "structur_reg",
+    "imported",
+    "iterate",
+    "aiterate",
+    "model",
+    "call_raw",
+    "acall_raw",
+]
