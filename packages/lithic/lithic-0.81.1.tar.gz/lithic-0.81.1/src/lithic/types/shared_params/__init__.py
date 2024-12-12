90589bfb9932f55d120c882b5557e480655d2c50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 04:06:11 +0000

    Add package lithic/0.81.1/lithic-0.81.1.tar.gz

---
+++ packages/lithic/lithic-0.81.1.tar.gz/lithic-0.81.1/src/lithic/types/shared_params/__init__.py
@@ -0,0 +1,7 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .address import Address as Address
+from .carrier import Carrier as Carrier
+from .shipping_address import ShippingAddress as ShippingAddress
+from .velocity_limit_params import VelocityLimitParams as VelocityLimitParams
+from .velocity_limit_params_period_window import VelocityLimitParamsPeriodWindow as VelocityLimitParamsPeriodWindow
