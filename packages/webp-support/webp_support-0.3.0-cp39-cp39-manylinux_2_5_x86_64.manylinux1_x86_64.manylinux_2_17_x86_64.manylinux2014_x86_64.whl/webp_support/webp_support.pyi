b0972850233db7da3cd08a0ea7a784ffbb737048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 04:06:31 +0000

    Add package webp-support/0.3.0/webp_support-0.3.0-cp39-cp39-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/webp-support/webp_support-0.3.0-cp39-cp39-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl/webp_support/webp_support.pyi
@@ -0,0 +1,10 @@
+def webp_supported(user_agent: bytes) -> bool:
+    """通过 UserAgent 判断是否支持 Webp
+
+    Args:
+        user_agent (bytes): UserAgent
+
+    Returns:
+        bool: 是否支持 Webp
+    """
+    ...
