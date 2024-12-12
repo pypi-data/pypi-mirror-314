5dcbc6a79948d7160279fbea0f39f69065f32c8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 05:06:15 +0000

    Add package clientfactory/0.5.4/clientfactory-0.5.4-py3-none-any.whl

---
+++ packages/clientfactory/clientfactory-0.5.4-py3-none-any.whl/clientfactory/clients/__init__.py
@@ -0,0 +1,14 @@
+# ~/ClientFactory/src/clientfactory/clients/__init__.py
+from .search import (
+    Search,
+    SearchResource,
+    SearchResourceConfig,
+    SearchClient,
+    ParameterType,
+    Parameter,
+    ProtocolType,
+    Protocol,
+    Payload,
+    searchmethod,
+    searchresource
+)
