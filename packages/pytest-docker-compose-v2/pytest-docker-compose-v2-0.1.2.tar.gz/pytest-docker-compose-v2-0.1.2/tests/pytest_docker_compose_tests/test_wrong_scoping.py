fc87af45f3c6f3a6079625fbe9110bcbfc4af010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 06:05:45 +0000

    Add package pytest-docker-compose-v2/0.1.2/pytest-docker-compose-v2-0.1.2.tar.gz

---
+++ packages/pytest-docker-compose-v2/pytest-docker-compose-v2-0.1.2.tar.gz/pytest-docker-compose-v2-0.1.2/tests/pytest_docker_compose_tests/test_wrong_scoping.py
@@ -0,0 +1,13 @@
+import pytest
+
+pytest_plugins = ["docker_compose"]
+
+
+@pytest.mark.should_fail
+def test_read_all_module(module_scoped_container_getter):
+    assert module_scoped_container_getter.get("my_api_service").network_info[0]
+
+
+@pytest.mark.should_fail
+def test_read_all_function(function_scoped_container_getter):
+    assert function_scoped_container_getter.get("my_api_service").network_info[0]
