e9821c2ebed6a3851665c168e10542a2e99d5b8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 14:06:08 +0000

    Add package streamlit-scroll-to-top/0.0.3/streamlit_scroll_to_top-0.0.3-py3-none-any.whl

---
+++ packages/streamlit-scroll-to-top/streamlit_scroll_to_top-0.0.3-py3-none-any.whl/streamlit_scroll_to_top/__init__.py
@@ -0,0 +1,35 @@
+import os
+import streamlit.components.v1 as components
+import uuid
+
+
+_RELEASE = True
+
+if not _RELEASE:
+    _component_func = components.declare_component(
+        "streamlit_scroll_to_top",
+        url="http://localhost:3001",  # Replace with your local dev server URL
+    )
+else:
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(parent_dir, "frontend/build")
+    _component_func = components.declare_component("streamlit_scroll_to_top", path=build_dir)
+
+
+def scroll_to_here(delay: int = 2000, key: str = None) -> None:
+    """
+    Scroll the parent window to the iframe location.
+
+    Parameters
+    ----------
+    delay : int, optional
+        Time in milliseconds before the scroll action. Defaults to 2000ms.
+    key : str, optional
+        A unique identifier for the component.
+
+    Returns
+    -------
+    None
+    """
+    unique_key = str(uuid.uuid4())
+    _component_func(delay=delay, key=f"key_{unique_key}")
