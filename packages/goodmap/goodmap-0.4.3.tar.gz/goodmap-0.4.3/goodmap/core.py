e48f49b535b223ffb20126e14ed17b8e6bac569b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 14:05:49 +0000

    Add package goodmap/0.4.3/goodmap-0.4.3.tar.gz

---
+++ packages/goodmap/goodmap-0.4.3.tar.gz/goodmap-0.4.3/goodmap/core.py
@@ -0,0 +1,19 @@
+# TODO move filtering to db site
+
+
+def does_fulfill_requirement(entry, requirements):
+    matches = []
+    for category, values in requirements:
+        if not values:
+            continue
+        matches.append(all(entry_value in entry[category] for entry_value in values))
+    return all(matches)
+
+
+def get_queried_data(all_data, categories, query_params):
+    requirements = []
+    for key in categories.keys():
+        requirements.append((key, query_params.get(key)))
+
+    filtered_data = [x for x in all_data if does_fulfill_requirement(x, requirements)]
+    return filtered_data
