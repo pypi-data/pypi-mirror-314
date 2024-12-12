b4ab04c5a9bb2edae7d8ccd1a6ec3ffd285a2e4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 05:05:20 +0000

    Add package do-data-utils/2.5.0/do_data_utils-2.5.0.tar.gz

---
+++ packages/do-data-utils/do_data_utils-2.5.0.tar.gz/do_data_utils-2.5.0/do_data_utils/azure/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+    azure sub-package
+    ~~~~
+    Provides to all the useful functionalities and allows you to interact with Azure.
+"""
+
+from .azureutils import (
+    databricks_to_df
+)
+
+__all__ = ["databricks_to_df"]
