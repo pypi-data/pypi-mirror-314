0bfcf719702484e60d5e1bbc68ee2235bcf67b97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 16:06:12 +0000

    Add package open-qr-plus/1.2.10/open_qr_plus-1.2.10.tar.gz

---
+++ packages/open-qr-plus/open_qr_plus-1.2.10.tar.gz/open_qr_plus-1.2.10/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="open_qr_plus",  # Name of your project
+    version="1.2.10",  # Initial version
+    author="Sandesh Kumar",
+    author_email="connect@sandeshai.in",
+    description="A perfect solution for qr generation with stunning ui...",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'open-qr-plus=open_qr_plus.server:start',  # This allows running `open-qr` from the command line
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    package_data={ '': ['templates/*'], 
+                   },
+    python_requires=">=3.10",  # Minimum Python version
+    install_requires=open("requirements.txt").readlines(),  # Dependencies
+)
+
