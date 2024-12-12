ab02b9f314a5bb1b9a49d0439947b930f8842a97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 17:06:22 +0000

    Add package aiopygismeteo/7.0.1/aiopygismeteo-7.0.1.tar.gz

---
+++ packages/aiopygismeteo/aiopygismeteo-7.0.1.tar.gz/aiopygismeteo-7.0.1/aiopygismeteo/_http.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from aiohttp import ClientSession, ClientTimeout
+from pygismeteo_base import types
+from pygismeteo_base.http import BaseHttpClient
+
+
+class AiohttpClient(BaseHttpClient[ClientSession]):
+    __slots__ = ()
+
+    async def get_response(
+        self, endpoint: str, /, *, params: types.Params
+    ) -> str:
+        params, headers = self._get_params_and_headers(params)
+        if self.session is None:
+            self.session = ClientSession(
+                timeout=ClientTimeout(total=60, connect=5)
+            )
+        async with self.session.get(
+            f"{self.base_url}/{endpoint}/",
+            params=params,
+            headers=headers,
+            raise_for_status=True,
+        ) as response:
+            await response.read()
+        return await response.text()
