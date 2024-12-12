fb3f60ef65a84c2ad7da270fa54984c6824ef39d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 04:06:26 +0000

    Add package vbvarsel/0.2.2/vbvarsel-0.2.2-py3-none-any.whl

---
+++ packages/vbvarsel/vbvarsel-0.2.2-py3-none-any.whl/vbvarsel/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about vbvarsel."""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Alan Nardo"
+__email__ = "bsu.software@mrc-bsu.cam.ac.uk"
+__version__ = "0.2.2"
