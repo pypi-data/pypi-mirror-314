6034509ef3ab13bcdfa0baf0f16a1e070f1b167f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 06:06:04 +0000

    Add package nshtrainer/1.0.0b36/nshtrainer-1.0.0b36-py3-none-any.whl

---
+++ packages/nshtrainer/nshtrainer-1.0.0b36-py3-none-any.whl/nshtrainer/configs/loggers/csv/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+__codegen__ = True
+
+from nshtrainer.loggers.csv import BaseLoggerConfig as BaseLoggerConfig
+from nshtrainer.loggers.csv import CSVLoggerConfig as CSVLoggerConfig
+
+__all__ = [
+    "BaseLoggerConfig",
+    "CSVLoggerConfig",
+]
---
+++ packages/nshtrainer/nshtrainer-1.0.0b36-py3-none-any.whl/nshtrainer/configs/nn/mlp/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+__codegen__ = True
+
+from nshtrainer.nn.mlp import BaseNonlinearityConfig as BaseNonlinearityConfig
+from nshtrainer.nn.mlp import MLPConfig as MLPConfig
+from nshtrainer.nn.mlp import NonlinearityConfig as NonlinearityConfig
+
+__all__ = [
+    "BaseNonlinearityConfig",
+    "MLPConfig",
+    "NonlinearityConfig",
+]
