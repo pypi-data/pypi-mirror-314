76b4f8d369aafcd0b40aaf66892c7c2931e7a57c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 03:18:51 +0000

    Add package django-lazy-srcset/1.0.5/django_lazy_srcset-1.0.5-py3-none-any.whl

---
+++ packages/django-lazy-srcset/django_lazy_srcset-1.0.5-py3-none-any.whl/lazy_srcset/conf.py
@@ -0,0 +1,54 @@
+from django.conf import settings as django_settings
+
+
+class LazySrcsetSettings:
+    """
+    Lazy settings are the best settings.  These can be defined in the project settings and LazySrcsetSettings will
+    return settings from django.conf.settings when possible, that includes any setting not defined here too.
+    """
+
+    # When disabled src, width and height attributes are returned so images still work but no srcset or image
+    # generation will happen. By default, lazy-srcset is disabled when debug is True.
+    LAZY_SRCSET_ENABLED = not django_settings.DEBUG
+
+    # List of file extensions that should be ignored (case-insensitive)
+    LAZY_SRCSET_IGNORED_EXTENSIONS = [".gif"]
+
+    # The default threshold to use when not specified in the config.
+    LAZY_SRCSET_THRESHOLD = 69
+
+    # The default generator to use when not specified in the config.
+    LAZY_SRCSET_GENERATOR_ID = "lazy_srcset:srcset_image"
+
+    # Configs
+    LAZY_SRCSET = {
+        "default": {
+            # breakpoints is the only setting you must define in each config
+            # a sizes entry for each breakpoint will be generated
+            "breakpoints": [1920, 1580, 1280, 1024, 640],
+            # max_width is the widest a generated image can be
+            # If max_width is not provided or the source image is smaller the source image width is used
+            "max_width": 2560,
+            # If quality is not provided PIL will choose a default
+            # You may want to tune this to your needs
+            "quality": 91,
+            # If format is not provided the source image format is used
+            "format": "WEBP",
+            # The difference in width (px) required to generate a new image
+            # This prevents images being created which are too similar in size
+            # If threshold is not provided LAZY_SRCSET_THRESHOLD is used
+            # Set to 0 to generate all images even if they are only 1 px smaller
+            "threshold": LAZY_SRCSET_THRESHOLD,
+            # If generator_id is not provided LAZY_SRCSET_GENERATOR_ID is used
+            "generator_id": LAZY_SRCSET_GENERATOR_ID,
+        }
+    }
+
+    def __getattribute__(self, item):
+        try:
+            return getattr(django_settings, item)
+        except AttributeError:
+            return super().__getattribute__(item)
+
+
+settings = LazySrcsetSettings()
---
+++ packages/django-lazy-srcset/django_lazy_srcset-1.0.5-py3-none-any.whl/lazy_srcset/management/commands/imagekit_cleanup.py
@@ -0,0 +1,109 @@
+"""
+Use at your own risk!
+
+* I don't know if this will work with any storage backend other than FileSystemStorage, but it might.
+* I don't expect this to work when using multiple sites with cache prefixes. In this case it's probably best to have a
+  separate cache for imagekit and flush the whole thing.
+* It only works with the source_name_dot_hash namer.
+
+For these reasons this remains an undocumented and untested feature.
+"""
+
+import re
+from pathlib import Path
+
+from django.conf import settings
+from django.core.cache import caches
+from django.core.files.storage import get_storage_class
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+    help = (
+        "Deletes generated files for sources that no longer exist.  Ie you clean up your media files and this will"
+        "clean up the imagekit files.  Suggest running once per day."
+    )
+    storage = None
+    static_storage = None
+    root_path = None
+    cache = None
+
+    def file_exists(self, path, file):
+        """
+        Check the storage and static storage for the file.
+        The file can have a different extension.
+        """
+        file_stem = Path(file).stem
+        for storage in [self.storage, self.static_storage]:
+            try:
+                directories, files = storage.listdir(path)
+            except FileNotFoundError:
+                continue
+
+            for f in files:
+                if Path(f).stem == file_stem:
+                    return True
+
+        return False
+
+    def process_file(self, file, path):
+        """
+        Delete images that no longer have a source.
+        """
+        source_path = path.relative_to(self.root_path)
+        source_file = re.sub(r"\.[^\.]+\.([^\.]+$)", ".\1", file)  # noqa
+
+        if not self.file_exists(source_path, source_file):
+            filepath = path / file
+
+            cache_key = f"{settings.IMAGEKIT_CACHE_PREFIX}{filepath.relative_to(self.root_path.parent)}-state"
+            self.cache.delete(cache_key)
+
+            self.storage.delete(filepath)
+            self.stdout.write(f"Deleted: {filepath}")
+
+    def process_directory(self, path):
+        """
+        Recursively walk through directories.
+        """
+        directories, files = self.storage.listdir(path)
+
+        for file in files:
+            self.process_file(file, path)
+
+        for directory in directories:
+            next_path = path / directory
+
+            self.process_directory(next_path)
+
+            # Delete empty dirs
+            if not any(self.storage.listdir(next_path)):
+                self.storage.delete(next_path)
+
+    def handle(self, *args, **options):
+        """
+        Using the appropriate storage walk through the directories in settings.IMAGEKIT_CACHEFILE_DIR
+        For each image file find its source name by removing the hash and settings.IMAGEKIT_CACHEFILE_DIR
+        Check if the source exists in media or static if it does continue
+        If the source doesn't exist delete the imagekit file, work out the cache key and delete from cache
+        """
+        compatible_namer = "imagekit.cachefiles.namers.source_name_dot_hash"
+        if not settings.IMAGEKIT_SPEC_CACHEFILE_NAMER == compatible_namer:
+            self.stdout.write(
+                "imagekit_cleanup will only work if you use the %s namer."
+                % compatible_namer
+            )
+            return
+
+        self.storage = get_storage_class(settings.IMAGEKIT_DEFAULT_FILE_STORAGE)()
+        self.static_storage = get_storage_class(settings.STATICFILES_STORAGE)()
+        self.cache = caches[settings.IMAGEKIT_CACHE_BACKEND]
+
+        try:
+            path = Path(self.storage.location)
+        except AttributeError:
+            path = Path("/")
+
+        self.root_path = path / settings.IMAGEKIT_CACHEFILE_DIR
+
+        self.process_directory(self.root_path)
