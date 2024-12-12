e85296e1910c54e16015bdfc0d1cd0c6597b97aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 14:06:15 +0000

    Add package pydantic-br-validator/0.7.0/pydantic_br_validator-0.7.0.tar.gz

---
+++ packages/pydantic-br-validator/pydantic_br_validator-0.7.0.tar.gz/pydantic_br_validator-0.7.0/pydantic_br_validator/__init__.py
@@ -0,0 +1,30 @@
+from typing import TYPE_CHECKING
+
+from .field_erros import *  # noqa
+
+__version__ = "0.7.0"
+
+
+if TYPE_CHECKING:
+    CPF = str
+    CNH = str
+    CPFMask = str
+    CPFDigits = str
+    CNPJ = str
+    CNPJDigits = str
+    CNPJMask = str
+    TE = str
+    PIS = str
+    PISMask = str
+    PISDigits = str
+    Certidao = str
+    CertidaoMask = str
+    CertidaoDigits = str
+    CEP = str
+    CEPMask = str
+    CEPDigits = str
+else:
+    from .fields.cnpj_field import *  # noqa
+    from .fields.cnh_field import *  # noqa
+    from .fields.cpf_field import *  # noqa
+    from .fields.cep_field import *  # noqa
