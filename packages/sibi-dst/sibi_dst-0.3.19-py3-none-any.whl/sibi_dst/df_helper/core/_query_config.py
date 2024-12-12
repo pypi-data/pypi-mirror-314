61e329856e34acc923a83c9771fcaf1050751099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 07:05:33 +0000

    Add package sibi-dst/0.3.19/sibi_dst-0.3.19-py3-none-any.whl

---
+++ packages/sibi-dst/sibi_dst-0.3.19-py3-none-any.whl/sibi_dst/df_helper/core/_query_config.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+import dask.dataframe as dd
+import pandas as pd
+from pydantic import BaseModel, model_validator
+
+
+class QueryConfig(BaseModel):
+    use_exclude: bool = False
+    n_records: int = 100
+    dt_field: Optional[str] = None
+    use_dask: bool = False
+    as_dask: bool = False
+
+    @model_validator(mode='after')
+    def check_n_records(self):
+        if self.n_records < 0:
+            raise ValueError('Number of records must be non-negative')
+        return self
