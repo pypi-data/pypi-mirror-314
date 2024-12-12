5c8af58ae69e762b6f6bf89ef432294bf4141dd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 15:07:53 +0000

    Add package passagemath-symbolics/10.5.5/passagemath_symbolics-10.5.5-cp39-cp39-macosx_13_0_x86_64.whl

---
+++ packages/passagemath-symbolics/passagemath_symbolics-10.5.5-cp39-cp39-macosx_13_0_x86_64.whl/sage/interfaces/sage-maxima.lisp
@@ -0,0 +1,8 @@
+; sage_setup: distribution = sagemath-symbolics
+
+;(setf *prompt-prefix* "<prompt>")
+;(setf *prompt-suffix* "</prompt>")
+;(setf *general-display-prefix* "<sage-display>")
+
+(setf *prompt-prefix* "<sage-display>")
+
