833f1246ed0800f85c9c7499d81f8d322004f57c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 03:18:24 +0000

    Add package weompy/0.6.139/weompy-0.6.139.tar.gz

---
+++ packages/weompy/weompy-0.6.139.tar.gz/weompy-0.6.139/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='WEOMPY',
+    version='0.6.139',
+    packages=find_packages(),
+    author='Workswell',
+    description='Python interface for WEOM module',
+)
