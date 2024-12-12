677eeb02cbe5ca23a22cabff31b4dc6a5ec8679a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 14:05:52 +0000

    Add package daft-meizter-ingestion/0.0.2/daft_meizter_ingestion-0.0.2.tar.gz

---
+++ packages/daft-meizter-ingestion/daft_meizter_ingestion-0.0.2.tar.gz/daft_meizter_ingestion-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+# Lê as dependências de requirements.txt
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="daft_meizter_ingestion",
+    version="0.0.2",
+    author="Douglas B. Martins",
+    author_email="douglas@meizter.com",
+    description="lakehouse with daft",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
