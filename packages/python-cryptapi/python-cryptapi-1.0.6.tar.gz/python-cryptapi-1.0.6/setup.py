1017692ac6e09d1c8671356339f01392506f2ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 04:06:19 +0000

    Add package python-cryptapi/1.0.6/python-cryptapi-1.0.6.tar.gz

---
+++ packages/python-cryptapi/python-cryptapi-1.0.6.tar.gz/python-cryptapi-1.0.6/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+
+    name='python-cryptapi',
+
+    version='1.0.6',
+
+    packages=find_packages(),
+
+    author="CryptAPI",
+
+    author_email="info@cryptapi.io",
+    install_requires=[
+        'requests'
+    ],
+    description="Python Library for CryptAPI payment gateway",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+
+    include_package_data=True,
+
+    url='https://github.com/cryptapi/python-cryptapi',
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+
+    license="MIT",
+
+    zip_safe=False,
+)
