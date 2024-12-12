f3a03d0590e632cd80881ac7f4687b856bfaf699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 07:05:35 +0000

    Add package sibi-dst/0.3.19/sibi_dst-0.3.19.tar.gz

---
+++ packages/sibi-dst/sibi_dst-0.3.19.tar.gz/sibi_dst-0.3.19/sibi_dst/df_helper/backends/parquet/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from ._parquet_options import *
+from ._parquet_filter_handler import ParquetFilterHandler
+
+__all__=[
+    "ParquetConfig",
+    "ParquetFilterHandler",
+]
