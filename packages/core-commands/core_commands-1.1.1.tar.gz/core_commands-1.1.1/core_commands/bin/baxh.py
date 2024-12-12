ac9ba073dc0b85ca9df8d9b7700a882dbd87bf7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 05:06:25 +0000

    Add package core-commands/1.1.1/core_commands-1.1.1.tar.gz

---
+++ packages/core-commands/core_commands-1.1.1.tar.gz/core_commands-1.1.1/core_commands/bin/baxh.py
@@ -0,0 +1,21 @@
+from ..bin._validate import _validateArguments,_validateCommand
+from subprocess import run
+from ..bin.exceptions import UnknownCommand
+
+def baxh(command,arguments):
+    _runCommand = run([command],
+                    capture_output=True,
+                    text=True,
+                    shell=True
+                    )
+    _runCommandArgument = run([command,arguments],
+                    capture_output=True,
+                    text=True,
+                    shell=True
+                    )
+    if _validateCommand(command):
+        raise UnknownCommand(command)
+    if _validateArguments(arguments):
+        print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+        return _runCommandArgument
+    return _runCommand
