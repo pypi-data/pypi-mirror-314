d0ed413644c274563903b3b0a56bb6bc40f08d74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 15:07:34 +0000

    Add package atomic-counter/0.1.4/atomic_counter-0.1.4-cp37-abi3-macosx_11_0_x86_64.whl

---
+++ packages/atomic-counter/atomic_counter-0.1.4-cp37-abi3-macosx_11_0_x86_64.whl/atomic_counter/tests/test_offset.py
@@ -0,0 +1,41 @@
+from datetime import datetime, timezone
+
+from atomic_counter import _BASE, Counter
+
+
+class TestOffset:
+    def test_base(self):
+        c = Counter(0, 0)
+        assert c.current() == 0
+        assert c.next() == 1
+        assert c.current() == 1
+
+    def test_interval(self):
+        c1 = Counter(None, _BASE, 2)
+        c2 = Counter(None, _BASE + 1, 2)
+        assert c1.current() == 0
+        assert c2.current() == 1
+        assert c1.next() == 2
+        assert c2.next() == 3
+        assert c1.current() == 2
+        assert c2.current() == 3
+
+    def test_offset(self):
+        nowish = datetime.now(timezone.utc)
+        base = datetime(nowish.year, nowish.month, nowish.day, nowish.hour, nowish.minute, nowish.second, nowish.microsecond, tzinfo=timezone.utc)
+        nowish2 = datetime(
+            nowish.year, nowish.month, nowish.day, nowish.hour, nowish.minute, nowish.second + 1, nowish.microsecond, tzinfo=timezone.utc
+        )
+        base = int(base.timestamp()) * 1_000_000_000
+        nowish = int(nowish.timestamp()) * 1_000_000_000
+        nowish2 = int(nowish2.timestamp()) * 1_000_000_000
+        c = Counter(base, nowish)
+
+        # if we offset to the nearest second, we should be within 1s
+        assert c.current() <= 1_000
+        assert c.next() <= 1_000
+
+        c = Counter(nowish, nowish2)
+
+        # if we offset to the nearest second, we should be within 1s
+        assert c.next() == 1_000_000_001
