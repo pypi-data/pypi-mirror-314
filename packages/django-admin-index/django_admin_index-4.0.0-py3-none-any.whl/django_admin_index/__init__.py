9c627ce8daaadb11f52957ae92f24bf366184d5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 17:05:58 +0000

    Add package django-admin-index/4.0.0/django_admin_index-4.0.0-py3-none-any.whl

---
+++ packages/django-admin-index/django_admin_index-4.0.0-py3-none-any.whl/django_admin_index/__init__.py
@@ -0,0 +1,7 @@
+# :copyright: (c) 2017, Maykin Media BV.
+#             All rights reserved.
+# :license:   BSD (3 Clause), see LICENSE for more details.
+from importlib.metadata import version
+
+__version__ = version("django-admin-index")
+__author__ = "Joeri Bekker"
