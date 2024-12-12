04ac3a514f83711a5d505572083d43772571aee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 04:06:17 +0000

    Add package canonical-transformer/0.1.2/canonical_transformer-0.1.2.tar.gz

---
+++ packages/canonical-transformer/canonical_transformer-0.1.2.tar.gz/canonical_transformer-0.1.2/canonical_transformer/save_utils.py
@@ -0,0 +1,24 @@
+import os
+import json
+from .dataframe_utils import map_df_to_data
+
+
+def save_df_as_csv(df, file_folder, file_name):
+    df.to_csv(os.path.join(file_folder, file_name), index=False)
+    print(f"| Saved csv to {os.path.join(file_folder, file_name)}")
+    return None
+
+def save_data_as_json(data, file_folder, file_name):
+    with open(os.path.join(file_folder, file_name), 'w') as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
+    print(f"| Saved json to {os.path.join(file_folder, file_name)}")
+    return None
+
+def save_df_as_json(df, file_folder, file_name):
+    data = map_df_to_data(df)
+    
+    print("| Transformed df to json data")
+    save_data_as_json(data, file_folder, file_name)
+    print(f"| Saved json to {os.path.join(file_folder, file_name)}")
+    return None
+
