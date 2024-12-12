7e8997ee24506c5eb011af59e6cf6e835ad316e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 16:06:47 +0000

    Add package testit-adapter-behave/3.3.2/testit_adapter_behave-3.3.2.tar.gz

---
+++ packages/testit-adapter-behave/testit_adapter_behave-3.3.2.tar.gz/testit_adapter_behave-3.3.2/src/testit_adapter_behave/models/option.py
@@ -0,0 +1,18 @@
+from attr import attrib, attrs
+
+
+# TODO: Add model to python-commons
+@attrs(kw_only=True)
+class Option(object):
+    set_url = attrib(default=None)
+    set_private_token = attrib(default=None)
+    set_project_id = attrib(default=None)
+    set_configuration_id = attrib(default=None)
+    set_test_run_id = attrib(default=None)
+    set_test_run_name = attrib(default=None)
+    set_tms_proxy = attrib(default=None)
+    set_adapter_mode = attrib(default=None)
+    set_config_file = attrib(default=None)
+    set_cert_validation = attrib(default=None)
+    set_automatic_creation_test_cases = attrib(default=None)
+    set_automatic_updation_links_to_test_cases = attrib(default=None)
