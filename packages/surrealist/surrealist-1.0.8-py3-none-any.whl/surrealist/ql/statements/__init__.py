2776f773b344c9a7d042f14929f6a7af92456381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 16:06:39 +0000

    Add package surrealist/1.0.8/surrealist-1.0.8-py3-none-any.whl

---
+++ packages/surrealist/surrealist-1.0.8-py3-none-any.whl/surrealist/ql/statements/__init__.py
@@ -0,0 +1,16 @@
+from .create import Create
+from .define import DefineEvent, DefineParam
+from .define_analyzer import DefineAnalyzer
+from .define_user import DefineUser
+from .delete import Delete
+from .insert import Insert
+from .live import Live
+from .rebuild_index import RebuildIndex
+from .remove import Remove
+from .select import Select
+from .show import Show
+from .transaction import Transaction
+from .update import Update
+
+__all__ = ("Create", "Delete", "Insert", "Live", "Remove", "Select", "Show", "Update", "Transaction", "DefineParam",
+           "DefineUser", "DefineEvent", "RebuildIndex", "DefineAnalyzer")
