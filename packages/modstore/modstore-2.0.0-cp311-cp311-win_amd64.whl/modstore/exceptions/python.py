6bf8f2f11e65e78101cf3ed834f3413f53fd06c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 06:06:09 +0000

    Add package modstore/2.0.0/modstore-2.0.0-cp311-cp311-win_amd64.whl

---
+++ packages/modstore/modstore-2.0.0-cp311-cp311-win_amd64.whl/modstore/exceptions/python.py
@@ -0,0 +1,50 @@
+# python exceptions
+
+# LIST
+# typecast error
+class TypeCastError(Exception):
+    pass
+
+# STACK
+# overflow
+class StackOverFlow(Exception):
+    pass
+# underflow
+class StackUnderFlow(Exception):
+    pass
+# Stack Func Err
+class StackError(Exception):
+    pass
+
+# LinkedList Node
+class NodeError(Exception):
+    pass
+class LinkedListError(Exception):
+    PARAM_GT_ZERO = "{} parameter must be an integer and greater than zero."
+    PARAM_NOT_ZERO_OR_LESS = "{} parameter cannot be less than zero."
+    PRAM_GT_EQ_ZERO = "{} parameter must be an integer and greater than or equal to zero"
+    PARAMS_NOT_NONE = "All parameters cannot be None."
+
+class HashMapError(Exception):
+    pass
+
+# all
+__all__ = [
+    "TypeCastError",
+    "StackError",
+    "StackOverFlow",
+    "StackUnderFlow",
+    "NodeError",
+    "LinkedListError",
+    "HashMapError"
+]
+
+__list__ = [
+    "TypeCastError"
+]
+
+__stack__ = [
+    "StackError",
+    "StackOverFlow",
+    "StackUnderFlow"
+]
