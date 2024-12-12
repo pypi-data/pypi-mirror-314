fa2d68e154b0ade80f1920870ad63f9328027ad2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 04:06:43 +0000

    Add package fau_colors/1.8.0/fau_colors-1.8.0.tar.gz

---
+++ packages/fau_colors/fau_colors-1.8.0.tar.gz/fau_colors-1.8.0/fau_colors/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "1.8.0"
+
+from fau_colors._utils import export_as_gpl, export_as_tex
+from fau_colors.fonts import register_fausans_font
+from fau_colors.v2021 import (
+    cmaps,
+    cmaps_with_names,
+    colors,
+    colors_all,
+    colors_dark,
+    colors_light,
+    register_cmaps,
+    unregister_cmaps,
+)
