3eb017e3fb11226158470abed88133d5f2cfd21d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 15:07:38 +0000

    Add package geo-espresso/0.3.19/geo_espresso-0.3.19-cp39-cp39-macosx_14_0_arm64.whl

---
+++ packages/geo-espresso/geo_espresso-0.3.19-cp39-cp39-macosx_14_0_arm64.whl/espresso/__init__.py
@@ -0,0 +1,85 @@
+r"""
+
+EspressoProblem
+---------------
+
+The essential idea of Espresso is to have exactly same interface for different 
+inversion test problems, so we list all the standard methods and attributes once in 
+this API reference page.
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    EspressoProblem
+
+
+Utility functions
+-----------------
+
+Some utility functions are there to help contributors load data and calculate things.
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+    
+    espresso.utils
+
+
+List the problems & capabilities
+--------------------------------
+
+To programatically see what problems are available in Espresso, these functions are 
+helpful:
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+    
+    espresso.capabilities
+
+Espresso exceptions
+-------------------
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+    
+    espresso.exceptions
+
+"""
+
+from ._espresso_problem import EspressoProblem
+
+from . import utils
+from . import exceptions
+
+
+from ._version import __version__
+
+
+__all__ = [
+    "EspressoProblem",
+]
+
+# from .example_name import ExampleName
+
+# from .capabilities import list_problem_names, list_problems
+# __all__ += list_problem_names()
+# __all__ += ["list_problem_names", "list_problems", "list_capabilities"]
+
+from ._surface_wave_tomography import SurfaceWaveTomography
+from ._simple_regression import SimpleRegression
+from ._receiver_function_inversion_knt import ReceiverFunctionInversionKnt
+from ._fmm_tomography import FmmTomography
+from ._receiver_function_inversion_shibutani import ReceiverFunctionInversionShibutani
+from ._pumping_test import PumpingTest
+from ._magnetotelluric_1D import Magnetotelluric1D
+from ._xray_tomography import XrayTomography
+from ._slug_test import SlugTest
+from ._gravity_inversion import GravityInversion
+
+from .capabilities import list_problem_names, list_problems, list_capabilities
+
+__all__ += list_problem_names()
+__all__ += ['list_problem_names', 'list_problems', 'list_capabilities']
