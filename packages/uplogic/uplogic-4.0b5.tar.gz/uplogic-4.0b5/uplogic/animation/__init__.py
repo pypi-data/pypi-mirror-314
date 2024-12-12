aac57c8ce346fafdf68a46dba7f3fab8154d94c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 17:06:16 +0000

    Add package uplogic/4.0b5/uplogic-4.0b5.tar.gz

---
+++ packages/uplogic/uplogic-4.0b5.tar.gz/uplogic-4.0b5/uplogic/animation/__init__.py
@@ -0,0 +1,10 @@
+'''The Uplogic Animation Module provides easy to use API for playing and
+controlling animations.
+'''
+
+from .actionsystem import ActionSystem  # noqa
+from .action import ACTION_FINISHED  # noqa
+from .action import ACTION_STARTED  # noqa
+from .action import ULAction, Action  # noqa
+from .sequence import ULSequence, Sequence  # noqa
+from .rig import Rig
