4e2a33ce3dcef4e09a48c8498af5ee7625ae9d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 03:18:33 +0000

    Add package Relation-Hallucination/0.0.1/relation_hallucination-0.0.1.tar.gz

---
+++ packages/Relation-Hallucination/relation_hallucination-0.0.1.tar.gz/relation_hallucination-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+# Read requirements from file
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="Relation_Hallucination",  # Package name
+    version="0.0.1",  # Initial version
+    description="Package for RHI Metrics evaluation",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Praveen Katwe, Naman Kabadi",
+    author_email="c121007@iiit-bh.ac.in, namankabadi50@gmail.com",
+    license="MIT",
+    packages=find_packages(),  # Automatically find all packages in the directory
+    install_requires=requirements,  # Dependencies from requirements file
+    python_requires=">=3.10",  # Minimum Python version
+    py_modules=["RH_Metrics"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,  # Include other files (like README.md)
+)
