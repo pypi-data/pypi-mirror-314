60844d138f20d524f6b5562da62075ff9b52c0f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 06:06:10 +0000

    Add package luna-variables/0.0.1/luna_variables-0.0.1.tar.gz

---
+++ packages/luna-variables/luna_variables-0.0.1.tar.gz/luna_variables-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='luna-variables',
+    version='0.0.1',
+    packages=find_packages(),
+    author='Rugved',
+    author_email='rugveddanej.mail@gmail.com',
+    description='A package of commonly used variables in luna',
+    url='https://github.com/username/your-repo',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
