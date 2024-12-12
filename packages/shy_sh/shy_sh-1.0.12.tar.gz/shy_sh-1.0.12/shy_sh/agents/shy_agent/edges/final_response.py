4316d74b7a201280db3780659f10acd59e3019fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 14:06:14 +0000

    Add package shy_sh/1.0.12/shy_sh-1.0.12.tar.gz

---
+++ packages/shy_sh/shy_sh-1.0.12.tar.gz/shy_sh-1.0.12/shy_sh/agents/shy_agent/edges/final_response.py
@@ -0,0 +1,26 @@
+from rich import print
+from rich.syntax import Syntax
+from langgraph.graph import END
+from shy_sh.models import State, ToolMeta
+from shy_sh.settings import settings
+
+
+def final_response_edge(state: State):
+    last_tool = state["tool_history"][-1]
+    artifact = getattr(last_tool, "artifact", None)
+    if isinstance(artifact, ToolMeta) and artifact.stop_execution:
+        message = last_tool.content
+        if settings.llm.agent_pattern == "react":
+            message = message.replace("Tool response:\n", "", 1)
+        if not artifact.skip_print:
+            print(
+                Syntax(
+                    f"🤖: {message}",
+                    "console",
+                    theme="one-dark",
+                    background_color="#181818",
+                )
+            )
+        return END
+    print()
+    return "chatbot"
