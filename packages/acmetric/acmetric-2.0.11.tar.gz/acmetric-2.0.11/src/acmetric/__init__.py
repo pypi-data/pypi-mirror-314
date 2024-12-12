12f94e7603134ff392a07347efdc30a8a9109bb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 05:05:17 +0000

    Add package acmetric/2.0.11/acmetric-2.0.11.tar.gz

---
+++ packages/acmetric/acmetric-2.0.11.tar.gz/acmetric-2.0.11/src/acmetric/__init__.py
@@ -0,0 +1,10 @@
+"""The main package of the project."""
+
+__version__ = "1.0.0"
+
+
+class Friday:
+    def __init__() -> None: ...
+
+    def drinks() -> None:
+        print("Who is in for a drink? 🍻")
