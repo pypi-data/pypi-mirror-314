5f476215b76add22997078dd4ec1e92d2d1d9844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 03:18:27 +0000

    Add package odoo-addon-product-name-with-attributes/16.0.1.0.0.2/odoo_addon_product_name_with_attributes-16.0.1.0.0.2-py3-none-any.whl

---
+++ packages/odoo-addon-product-name-with-attributes/odoo_addon_product_name_with_attributes-16.0.1.0.0.2-py3-none-any.whl/odoo/addons/product_name_with_attributes/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Ángel García de la Chica Herrera <angel.garcia@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product name with attributes",
+    "summary": "Product name with attributes",
+    "version": "16.0.1.0.0",
+    "category": "Inventory",
+    "website": "https://github.com/sygel-technology/sy-product-attribute",
+    "author": "Sygel, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["base", "product", "sale"],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+}
