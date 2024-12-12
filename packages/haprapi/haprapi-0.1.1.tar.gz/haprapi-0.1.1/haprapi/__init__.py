b561a1265b410e86fb4f3d0e9b9f7312eabdd393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 04:05:59 +0000

    Add package haprapi/0.1.1/haprapi-0.1.1.tar.gz

---
+++ packages/haprapi/haprapi-0.1.1.tar.gz/haprapi-0.1.1/haprapi/__init__.py
@@ -0,0 +1,3 @@
+from .Client import Client
+from .Models import Backend, Frontend, Server, ServerAdminState, ServerOperationalState, Stats, HAProxyInfo
+__all__ = ['Client', 'Backend', 'Frontend', 'Server', 'ServerAdminState', 'ServerOperationalState', 'Stats', 'HAProxyInfo']
