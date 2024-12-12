de41ec5891ae307a88dec4a2fac9039649194502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 16:06:17 +0000

    Add package streamlit-nightly/1.41.1.dev20241211/streamlit_nightly-1.41.1.dev20241211.tar.gz

---
+++ packages/streamlit-nightly/streamlit_nightly-1.41.1.dev20241211.tar.gz/streamlit_nightly-1.41.1.dev20241211/streamlit/static/static/js/Hooks.Bbqs8th_.js
@@ -0,0 +1,2 @@
+import{r as t}from"./index.LTr0pzEv.js";const o=r=>{const e=t.useRef();return t.useEffect(()=>{e.current=r},[r]),e.current};export{o as u};
+//# sourceMappingURL=Hooks.Bbqs8th_.js.map
