b6d883b82d88ca47f6feb072e9dbc145f1cbe918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 04:06:14 +0000

    Add package tonie_sync/0.1.9/tonie_sync-0.1.9.tar.gz

---
+++ packages/tonie_sync/tonie_sync-0.1.9.tar.gz/tonie_sync-0.1.9/src/tonie_sync/clients/__init__.py
@@ -0,0 +1,5 @@
+# from .spotify_client import SpotifyClient
+from .spotdl_client import SpotDLClient, SpotDLClientFactory
+from .tonie_client import TonieClient
+
+__all__ = ["TonieClient", "SpotDLClient", "SpotDLClientFactory"]
