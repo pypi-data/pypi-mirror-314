88eb9e8faf693f28c7c539f6df19a3c5481916ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 06:05:43 +0000

    Add package splight-lib/5.12.0/splight_lib-5.12.0.tar.gz

---
+++ packages/splight-lib/splight_lib-5.12.0.tar.gz/splight_lib-5.12.0/splight_lib/constants.py
@@ -0,0 +1,8 @@
+ENGINE_PREFIX: str = "v2/engine"
+HUB_PREFIX: str = "v2/hub"
+
+DL_BUFFER_SIZE = 500
+DL_BUFFER_TIMEOUT = 60  # seconds
+
+DEFAULT_COLLECTION = "default"
+DEFAULT_SORT_FIELD = "timestamp"
