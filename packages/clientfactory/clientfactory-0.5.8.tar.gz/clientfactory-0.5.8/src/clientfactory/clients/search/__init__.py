91b58dadc1e12b221f6d70895d5b96ff9b3aaec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 15:06:02 +0000

    Add package clientfactory/0.5.8/clientfactory-0.5.8.tar.gz

---
+++ packages/clientfactory/clientfactory-0.5.8.tar.gz/clientfactory-0.5.8/src/clientfactory/clients/search/__init__.py
@@ -0,0 +1,10 @@
+# ~/ClientFactory/src/clientfactory/clients/search/__init__.py
+from .core import (
+    Parameter, ParameterType, Payload, Protocol, ProtocolType, SearchResourceConfig
+)
+from .base import (
+    Search, SearchClient, SearchResource
+)
+from .decorators import (searchmethod, searchresource)
+
+from .adapters import *
