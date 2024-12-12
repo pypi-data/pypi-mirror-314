d328c906a488c83395cfbe74ff2fbffc861bc262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-12 17:06:25 +0000

    Add package mercuryclient/1.20.1/mercuryclient-1.20.1-py3-none-any.whl

---
+++ packages/mercuryclient/mercuryclient-1.20.1-py3-none-any.whl/mercuryclient/types/payments/enums.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+
+class Endpoints:
+    api_endpoints = {
+        "generate_qr_code": "generate/qr/code",
+        "plan_creation": "plan/create",
+        "subscription_creation": "subscription/create",
+        "subscription_fetch": "subscription/fetch",
+        "payment_link_creation": "payment/link/create",
+        "charge_subscription": "subscription/charge",
+        "manage_subscription": "subscription/manage",
+    }
+
+
+class PlanType(Enum):
+    PERIODIC = "PERIODIC"
+    ON_DEMAND = "ON_DEMAND"
+
+
+class SubscriptionActionType(Enum):
+    CANCEL = "CANCEL"
+    PAUSE = "PAUSE"
+    ACTIVATE = "ACTIVATE"
+    CHNAGE_PLAN = "CHANGE_PLAN"
