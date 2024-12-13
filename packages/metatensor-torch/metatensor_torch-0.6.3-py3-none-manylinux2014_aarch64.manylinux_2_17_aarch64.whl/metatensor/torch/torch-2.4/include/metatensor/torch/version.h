bcb8263b06317ee09f292df94d2f4f54896b8bd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 04:05:48 +0000

    Add package metatensor-torch/0.6.3/metatensor_torch-0.6.3-py3-none-manylinux2014_aarch64.manylinux_2_17_aarch64.whl

---
+++ packages/metatensor-torch/metatensor_torch-0.6.3-py3-none-manylinux2014_aarch64.manylinux_2_17_aarch64.whl/metatensor/torch/torch-2.4/include/metatensor/torch/version.h
@@ -0,0 +1,11 @@
+/** Full version of metatensor-torch as a string */
+#define METATENSOR_TORCH_VERSION "0.6.3"
+
+/** Major version number of metatensor-torch as an integer */
+#define METATENSOR_TORCH_VERSION_MAJOR 0
+
+/** Minor version number of metatensor-torch as an integer */
+#define METATENSOR_TORCH_VERSION_MINOR 6
+
+/** Patch version number of metatensor-torch as an integer */
+#define METATENSOR_TORCH_VERSION_PATCH 3
