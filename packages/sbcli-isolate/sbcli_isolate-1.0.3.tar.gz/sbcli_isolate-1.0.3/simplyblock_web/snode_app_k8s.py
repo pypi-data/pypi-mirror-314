2443c596e169311cf9539f2cb6eef962f983cf48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 04:05:39 +0000

    Add package sbcli-isolate/1.0.3/sbcli_isolate-1.0.3.tar.gz

---
+++ packages/sbcli-isolate/sbcli_isolate-1.0.3.tar.gz/sbcli_isolate-1.0.3/simplyblock_web/snode_app_k8s.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import logging
+from flask import Flask, request
+import math
+import os
+import sys
+import utils
+
+from simplyblock_core import utils as sbc_utils
+from blueprints import snode_ops_k8s
+
+logger_handler = logging.StreamHandler()
+logger_handler.setFormatter(logging.Formatter('%(asctime)s: %(levelname)s: %(message)s'))
+logger = logging.getLogger()
+logger.addHandler(logger_handler)
+logger.setLevel(logging.INFO)
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def status():
+    return utils.get_response("Live")
+
+
+@app.route('/prepare-cores', methods=['POST'])
+def prepare_cores_api():
+    """Handle prepare_cores via a web API."""
+    data = request.json
+    return prepare_cores(data)
+
+def prepare_cores(data):
+    data = request.json
+    cpu_count = os.cpu_count()
+    spdk_cpu_mask = data.get('spdk_cpu_mask')
+    if not spdk_cpu_mask:
+        spdk_cpu_mask = hex(int(math.pow(2, cpu_count))-2)
+    sbc_utils.prepare_cores(spdk_cpu_mask)
+    return utils.get_response("Done")
+
+
+app.register_blueprint(snode_ops_k8s.bp)
+
+
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+if __name__ == '__main__':
+    spdk_cpu_mask = None
+    if len(sys.argv) > 1:
+        spdk_cpu_mask = sys.argv[1]  # First argument after the script name
+    data = {"spdk_cpu_mask": spdk_cpu_mask}
+    app.run(host='0.0.0.0')
+    prepare_cores(data)
