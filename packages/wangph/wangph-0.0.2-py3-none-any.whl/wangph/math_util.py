63f28291cdc00043bcdfc1776f9cc0cd33a13ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 03:20:38 +0000

    Add package wangph/0.0.2/wangph-0.0.2-py3-none-any.whl

---
+++ packages/wangph/wangph-0.0.2-py3-none-any.whl/wangph/math_util.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def np_plus(a, b):
+    return np.add(a, b)
+
+def np_minus(a, b):
+    return np.subtract(a, b)
+
+def np_mul(a, b):
+    return np.multiply(a, b)
+
+def np_div(a, b):
+    return np.divide(a, b)
