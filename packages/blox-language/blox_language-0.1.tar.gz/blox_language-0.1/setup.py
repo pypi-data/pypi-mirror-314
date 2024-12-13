11612c6a7b245c2850f2a0c56411eb99515823f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 05:05:15 +0000

    Add package blox-language/0.1/blox_language-0.1.tar.gz

---
+++ packages/blox-language/blox_language-0.1.tar.gz/blox_language-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='blox_language',
+    version='0.1',
+    packages=find_packages(),
+    description='A programming language in python!',
+    author='Oscar S.I.',
+    author_email='bloxplay2014@gmail.com',
+    url='https://youtube.com/c/bloxplay',  # Your repo URL if available
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='language utility, tools',  # Add relevant keywords
+    install_requires=[
+    ],
+)
