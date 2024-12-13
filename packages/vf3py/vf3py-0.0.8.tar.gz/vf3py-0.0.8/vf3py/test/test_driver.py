6bf702088a38e6d19814c613abe6d84ae17f221e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 03:21:21 +0000

    Add package vf3py/0.0.8/vf3py-0.0.8.tar.gz

---
+++ packages/vf3py/vf3py-0.0.8.tar.gz/vf3py-0.0.8/vf3py/test/test_driver.py
@@ -0,0 +1,24 @@
+def run_tests():
+    from .basic_graphs import main as f_basic_graphs
+    from .edge_test import main as f_edge_attrs
+    from .mol_isoms import main as f_molecular
+    from .mono import main as f_mono
+    from .mono_random import main as f_mono_random
+
+    TEST_FUNCTIONS = {
+        'basic_graphs': f_basic_graphs,
+        'edge_attrs': f_edge_attrs,
+        'molecular': f_molecular,
+        'monomorphism': f_mono,
+        'random monomorphism': f_mono_random,
+    }
+
+    for function_name, function in TEST_FUNCTIONS.items():
+        print(f"=== Running test '{function_name}' ===")
+        function()
+    
+    print("OK")
+
+
+if __name__ == "__main__":
+    run_tests()
