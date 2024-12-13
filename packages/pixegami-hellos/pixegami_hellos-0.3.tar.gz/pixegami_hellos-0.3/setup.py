75b4837db10a9516ad58bf4e05d1032d9cebb398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 03:21:20 +0000

    Add package pixegami-hellos/0.3/pixegami_hellos-0.3.tar.gz

---
+++ packages/pixegami-hellos/pixegami_hellos-0.3.tar.gz/pixegami_hellos-0.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pixegami_hellos',
+    version='0.3',
+    packages=find_packages(),
+    install_requires=[],  # If you have dependencies, add them here
+)
