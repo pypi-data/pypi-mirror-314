e36fad8c8eee22efbfaab9395d6c5a95699d091b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 03:21:33 +0000

    Add package odoo-addon-picking-very-high-priority-mrp/15.0.1.0.0.2/odoo_addon_picking_very_high_priority_mrp-15.0.1.0.0.2-py3-none-any.whl

---
+++ packages/odoo-addon-picking-very-high-priority-mrp/odoo_addon_picking_very_high_priority_mrp-15.0.1.0.0.2-py3-none-any.whl/odoo/addons/picking_very_high_priority_mrp/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2022 Manuel Regidor <manuel.regidor@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Picking Very High Priority MRP",
+    "summary": "Picking Very High Priority MRP",
+    "version": "15.0.1.0.0",
+    "category": "Custom",
+    "website": "https://github.com/sygel-technology/sy-manufacturing",
+    "author": "Sygel, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "auto_install": True,
+    "depends": ["picking_very_high_priority", "mrp"],
+}
