ee3b565a078ee0266321ae4f9fd3e82a9b1fe9dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 03:22:05 +0000

    Add package Solveit/0.1.2/Solveit-0.1.2-py3-none-any.whl

---
+++ packages/Solveit/Solveit-0.1.2-py3-none-any.whl/solveit/__init__.py
@@ -0,0 +1,16 @@
+"""
+SolveIt: A Universal Problem-Solving Toolkit for Everyday Challenges
+"""
+
+__version__ = "0.1.0"
+__author__ = "Your Name"
+__license__ = "MIT"
+
+from . import logic
+from . import math
+from . import data
+from . import units
+from . import algorithms
+from . import time
+from . import finance
+from . import viz 
