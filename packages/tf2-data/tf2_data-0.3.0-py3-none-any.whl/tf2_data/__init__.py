a0e8184443911171de7c1014a076418ac82bc4c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 03:21:51 +0000

    Add package tf2-data/0.3.0/tf2_data-0.3.0-py3-none-any.whl

---
+++ packages/tf2-data/tf2_data-0.3.0-py3-none-any.whl/tf2_data/__init__.py
@@ -0,0 +1,16 @@
+__title__ = "tf2-data"
+__author__ = "offish"
+__version__ = "0.3.0"
+__license__ = "MIT"
+
+from .files import (
+    DEFINDEX_FULL_NAMES,
+    DEFINDEX_NAMES,
+    EFFECTS,
+    ITEM_NAME_IDS,
+    SCHEMA_ITEMS,
+)
+from .schema import IEconItems, Schema, SchemaItems
+from .static import *
+
+# flake8: noqa
