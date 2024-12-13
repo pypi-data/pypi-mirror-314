ce5194a61723ec6bd07c886da1dd5ca562cbf73a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 04:07:17 +0000

    Add package json-duplicate-keys/2024.12.12/json_duplicate_keys-2024.12.12.tar.gz

---
+++ packages/json-duplicate-keys/json_duplicate_keys-2024.12.12.tar.gz/json_duplicate_keys-2024.12.12/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+	name="json-duplicate-keys",
+	version="2024.12.12",
+	author="TP Cyber Security",
+	license="MIT",
+	author_email="tpcybersec2023@gmail.com",
+	description="Flatten/ Unflatten and Load(s)/ Dump(s) JSON File/ Object with Duplicate Keys",
+	long_description=open("README.md").read(),
+	long_description_content_type="text/markdown",
+	install_requires=open("requirements.txt").read().split(),
+	url="https://github.com/truocphan/json-duplicate-keys",
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 2",
+		"Programming Language :: Python :: Implementation :: Jython"
+	],
+	keywords=["TPCyberSec", "json", "duplicate keys", "json duplicate keys", "flatten", "unflatten"],
+	packages=["json_duplicate_keys"],
+)
