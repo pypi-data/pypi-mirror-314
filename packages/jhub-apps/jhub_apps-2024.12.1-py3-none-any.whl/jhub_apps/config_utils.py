1391b252a63787d2db99bc9cff3bfae357165ab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 04:06:20 +0000

    Add package jhub-apps/2024.12.1/jhub_apps-2024.12.1-py3-none-any.whl

---
+++ packages/jhub-apps/jhub_apps-2024.12.1-py3-none-any.whl/jhub_apps/config_utils.py
@@ -0,0 +1,60 @@
+from traitlets import Unicode, Union, List, Callable, Integer, Bool
+from traitlets.config import SingletonConfigurable, Enum
+
+
+class JAppsConfig(SingletonConfigurable):
+    apps_auth_type = Enum(
+        values=["oauth", "none"],
+        default_value="oauth",
+        help="Authentication for deployed apps, either",
+    ).tag(config=True)
+
+    python_exec = Unicode(
+        "python", help="Python executable to use for running all the commands"
+    ).tag(config=True)
+
+    app_title = Unicode(
+        "JHub Apps Launcher",
+        help="Title to display on the Home Page of JHub Apps Launcher",
+    ).tag(config=True)
+
+    app_icon = Unicode(
+        "https://jupyter.org/assets/homepage/main-logo.svg",
+        help="Icon to display on the Home Page of JHub Apps Launcher",
+    ).tag(config=True)
+
+    conda_envs = Union(
+        [List(trait=Unicode, default_value=[], minlen=0), Callable()],
+        help="""
+        A list of conda environment names for the app creator to display as dropdown to select.
+        Default value is the empty list. Also accepts conda_envs to be a callable function
+        which is evaluated for each render of the create app page.
+        """,
+    ).tag(config=True)
+
+    # This is to make sure that JupyterHub config is accessible inside services
+    # There is no way to get JupyterHub config inside services otherwise.
+    jupyterhub_config_path = Unicode(
+        "jupyterhub_config.py",
+        help="Path to JupyterHub config file.",
+    ).tag(config=True)
+
+    hub_host = Unicode(
+        "127.0.0.1",
+        help="Hub Host name, in k8s environment it would be the container name, e.g. 'hub'",
+    ).tag(config=True)
+
+    service_workers = Integer(
+        2,
+        help="The number of workers to create for the JHub Apps FastAPI service",
+    ).tag(config=True)
+
+    allowed_frameworks = Bool(
+        None,
+        help="Allow only a specific set of frameworks to spun up apps.",
+    ).tag(config=True)
+
+    blocked_frameworks = Bool(
+        None,
+        help="Disallow a set of frameworks to avoid spinning up apps using those frameworks",
+    ).tag(config=True)
