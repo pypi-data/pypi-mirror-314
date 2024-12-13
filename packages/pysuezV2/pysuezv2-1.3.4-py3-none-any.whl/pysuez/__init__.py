355e613f7e816f25dbe0ce6f74e179abab9591f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 04:05:47 +0000

    Add package pysuezV2/1.3.4/pysuezv2-1.3.4-py3-none-any.whl

---
+++ packages/pysuezV2/pysuezv2-1.3.4-py3-none-any.whl/pysuez/__init__.py
@@ -0,0 +1,13 @@
+from pysuez.models import (
+    AggregatedData,
+    AlertResult,
+    ContractResult,
+    DayDataResult,
+    InterventionResult,
+    LimestoneResult,
+    PriceResult,
+    QualityResult,
+)
+
+from pysuez.exception import PySuezConnexionError, PySuezDataError, PySuezError
+from pysuez.suez_client import SuezClient
