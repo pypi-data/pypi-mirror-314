97d2a99472ea11e7e7004d9a7938777b3c0786e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 05:05:17 +0000

    Add package periodicencryption/1.0.3/periodicencryption-1.0.3-py3-none-any.whl

---
+++ packages/periodicencryption/periodicencryption-1.0.3-py3-none-any.whl/periodicencryption/__init__.py
@@ -0,0 +1,8 @@
+# __init__.py
+__version__ = "1.0.3"
+__author__ = "NilsMT"
+__email__ = "nilsmoreauthomas@gmail.com"
+
+from periodicencryption.encryption import *
+from periodicencryption.vigenerecipher import *
+from periodicencryption.element import *
