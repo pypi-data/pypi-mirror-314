f068f58296b31fe7f8fd913ed6dccf81e411bed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 03:21:46 +0000

    Add package dq-batch-client/0.6.0/dq_batch_client-0.6.0-py2.py3-none-any.whl

---
+++ packages/dq-batch-client/dq_batch_client-0.6.0-py2.py3-none-any.whl/dq/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from .client import DQClient
+from .job import JobConfig
+
+__author__ = """Algolytics Technologies"""
+__email__ = 'info@algolytics.pl'
+__version__ = '0.6.0'
+
+__all__ = ['DQClient', 'JobConfig']
