bbd5e80d9f2a78f5971231d75dc998032ecd83ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 03:20:30 +0000

    Add package seelab-examples/1.0.9/seelab_examples-1.0.9-py3-none-any.whl

---
+++ packages/seelab-examples/seelab_examples-1.0.9-py3-none-any.whl/seelab_examples/mycode.py
@@ -0,0 +1,14 @@
+
+# Connect WG to A1
+
+from matplotlib import pyplot as plt
+
+def f1(x):                         #
+	return sin(x) + sin(3*x)/3
+
+p.load_table(abs(arange(-256,256)))
+p.set_wave(400)
+
+x,y = p.capture1('A1', 500,10)
+plt.plot(x,y)
+plt.show()
